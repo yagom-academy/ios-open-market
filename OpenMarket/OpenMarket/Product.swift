894ece5f629023266286f0a934dc83888832d4ba
odong-Tree <ss12357@naver.com>
2021-01-25 23:00:27 +0900

    feat: 유닛테스트 추가 및 폴더 정리

---
+++ OpenMarket/OpenMarket/Product.swift
@@ -0,0 +1,8 @@
+//
+//  Product.swift
+//  OpenMarket
+//
+//  Created by 김동빈 on 2021/01/25.
+//
+
+import Foundation
---
+++ OpenMarket/OpenMarket/URLManager.swift
@@ -0,0 +1,8 @@
+//
+//  URLManager.swift
+//  OpenMarket
+//
+//  Created by 김동빈 on 2021/01/25.
+//
+
+import Foundation
