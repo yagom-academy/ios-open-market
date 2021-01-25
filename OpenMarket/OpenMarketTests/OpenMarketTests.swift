e0c8433af255e46deb46333ac6259ea3e2e094e1
SM <hcooch2ch3@gmail.com>
2021-01-26 07:00:35 +0900

    fix: ItemToPatch, ItemToPost 프로퍼티 타입 수정에 맞게 테스트 코드 수정

--- OpenMarket/OpenMarketTests/OpenMarketTests.swift
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -75,13 +75,17 @@ class OpenMarketTests: XCTestCase {
     }
     
     func testItemToPost() {
+        let image = UIImage(systemName: "pencil")
+        guard let imageData = image?.jpegData(compressionQuality: 1) else {
+            return
+        }
         let itemToPost = ItemToPost(title: "MacBook Air",
                                     discription: "가장 얇고 가벼운 MacBook이 Apple M1 칩으로 완전히 새롭게 탈바꿈했습니다.",
                                     price: 1290000,
                                     currency: "KRW",
                                     stock: 10000,
                                     discountedPrice: 10000,
-                                    images: ["https://camp-open-market.s3.ap-northeast-2.amazonaws.com/images/1-1.png"],
+                                    images: [imageData],
                                     password: "123")
         do {
             let encodedData = try encoder.encode(itemToPost)
@@ -94,13 +98,17 @@ class OpenMarketTests: XCTestCase {
     }
     
     func testItemToPatch() {
+        let image = UIImage(systemName: "pencil")
+        guard let imageData = image?.jpegData(compressionQuality: 1) else {
+            return
+        }
         let itemToPatch = ItemToPatch(title: "MacBook Air",
                                       discription: "가장 얇고 가벼운 MacBook이 Apple M1 칩으로 완전히 새롭게 탈바꿈했습니다.",
                                       price: 1290000,
                                       currency: "KRW",
                                       stock: 10000,
                                       discountedPrice: nil,
-                                      images: ["https://camp-open-market.s3.ap-northeast-2.amazonaws.com/images/1-1.png"],
+                                      images: [imageData],
                                       password: "123")
         do {
             let encodedData = try encoder.encode(itemToPatch)
