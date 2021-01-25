25cd601e0d811739992a06e153257a4cbabddbde
SM <hcooch2ch3@gmail.com>
2021-01-26 04:54:55 +0900

    feat: ServerError type정의

---
+++ OpenMarket/OpenMarket/JsonData/ServerError.swift
@@ -0,0 +1,13 @@
+//
+//  ServerError.swift
+//  OpenMarket
+//
+//  Created by 임성민 on 2021/01/26.
+//
+
+import Foundation
+
+struct ServerError: Decodable {
+    let error: Bool
+    let reason: String
+}
