3121b412fbfebb85dc3439df38b7fe95fa2b32fd
odong-Tree <ss12357@naver.com>
2021-01-26 22:49:05 +0900

    feat: URLManager 타입 구현

--- OpenMarket/OpenMarket/URLManager.swift
+++ OpenMarket/OpenMarket/URLManager.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct URLManager {
+    enum Path: String {
+        case itemsList = "items/"
+        case item = "item/"
+    }
+    
+    let baseURL = "https://camp-open-market.herokuapp.com/"
+    
+    func makeURL(path: Path, value: Int) -> URL? {
+        var url = URLComponents(string: baseURL)
+        url?.path = path.rawValue + String(value)
+        
+        return url?.url
+    }
+}
