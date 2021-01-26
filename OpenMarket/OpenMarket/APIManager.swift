85f0b33c881e3a19a2c3746c9a914c571759884c
odong-Tree <ss12357@naver.com>
2021-01-26 16:17:24 +0900

    feat: APIManager 타입 설계

--- OpenMarket/OpenMarket/APIManager.swift
+++ OpenMarket/OpenMarket/APIManager.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct APIManager {
+    static func requestGET(url: URL) {
+        
+    }
+    
+    static func requestPOST(url: URL) {
+        
+    }
+    
+    static func requestPATCH(url: URL) {
+        
+    }
+    
+    static func requestDELETE(url: URL) {
+        
+    }
+}
