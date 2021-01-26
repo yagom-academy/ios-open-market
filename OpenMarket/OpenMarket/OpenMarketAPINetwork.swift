26929eb03de3c30df2bc2b070dd5d1cae7c1a9cb
taetae <taetae.dev@gmail.com>
2021-01-26 11:03:05 +0900

    feat: API JSON 파일을 decode 하기위한 구조체 정의

--- OpenMarket/OpenMarket/OpenMarketAPINetwork.swift
+++ OpenMarket/OpenMarket/OpenMarketAPINetwork.swift
@@ -1,11 +1,32 @@
 
 import Foundation
 
+enum ProductError: Error {
+    case error
+}
+
 struct OpenMarketAPINetwork {
-    let jsonDecoder = JSONDecoder()
-    
-    init() {
-        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+    func decodeFromAPI(completionHandler: @escaping (Result<ProductList, ProductError>) -> ()) {
+        let session = URLSession(configuration: .default)
+        guard let url:URL = URL(string: "https://camp-open-market.herokuapp.com/items/1/") else {
+            return
+        }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
+        
+        let dataTask = session.dataTask(with: urlRequest) { data,_,error  in
+            guard let data = data else {
+                return
+            }
+            do {
+                let productlist: ProductList = try JSONDecoder().decode(ProductList.self, from: data)
+                completionHandler(.success(productlist))
+            } catch {
+                print("JSON 에러")
+                completionHandler(.failure(.error))
+            }
+        }
+        dataTask.resume()
     }
     
 }
