1733bd7e48a3bca069d440cfb5497d17faa84bcc
Hojoon Kim <elddy0948@hotmail.co.kr>
2021-01-26 23:43:10 +0900

    add : NetworkConfig.swift, OpenMarketAPITypes.swift 파일 생성

---
+++ OpenMarket/OpenMarket/Utility/OpenMarketAPITypes.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+enum OpenMarketAPITypes{
+    case searchGoodsList(page: Int)
+    case registerGoods
+    case searchGoods(id: Int)
+    case editGoods(id: Int)
+    case deleteGoods(id: Int)
+    
+    var urlQuery: String {
+        switch self {
+        case .searchGoodsList(page: let page):
+            return "/items/\(page)"
+        case .registerGoods:
+            return "/item"
+        case .searchGoods(id: let id):
+            return "/item/\(id)"
+        case .editGoods(id: let id):
+            return "/item/\(id)"
+        case .deleteGoods(id: let id):
+            return "/item/\(id)"
+        }
+    }
+}
