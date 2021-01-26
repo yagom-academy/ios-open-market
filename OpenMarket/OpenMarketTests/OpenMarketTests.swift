c6b0c73796e17060dc03a061c129708a335f5643
taetae <taetae.dev@gmail.com>
2021-01-26 11:07:10 +0900

    test: Unit Test를 위한 Test code 추가

---
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -0,0 +1,48 @@
+//
+//  OpenMarketTests.swift
+//  OpenMarketTests
+//
+//  Created by 김태형 on 2021/01/25.
+//
+
+import XCTest
+@testable import OpenMarket
+
+class OpenMarketTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        let productDecoder = OpenMarketAPINetwork()
+        let pause = XCTestExpectation(description: "wait")
+        var productlist: ProductList?
+        
+        productDecoder.decodeFromAPI() { result in
+            switch result {
+            case .success(let data):
+                productlist = data
+            case .failure(let error):
+                print(error)
+            }
+            pause.fulfill()
+        }
+        wait(for: [pause], timeout: 5)
+        dump(productlist)
+        
+        XCTAssertNotNil(productlist)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
