c2942995288123d897bdf2bdc47677ea3c90b598
SM <hcooch2ch3@gmail.com>
2021-01-27 01:46:34 +0900

    feat: postItem func 구현

--- OpenMarket/OpenMarket/NetWork/OpenMarketAPI.swift
+++ OpenMarket/OpenMarket/NetWork/OpenMarketAPI.swift
@@ -76,4 +76,43 @@ class OpenMarketAPI {
             completionHandler(item)
         }.resume()
     }
+    
+    static func postItem(itemToPost: ItemToPost, _ completionHandler: @escaping(ItemAfterPost) -> Void) {
+        guard let url = URLManager.makeURL(type: .postItem, value: nil) else {
+            print("URL Error")
+            return
+        }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        guard let dataToPost = Parser.encodeData(itemToPost) else {
+            print("Encoding Error")
+            return
+        }
+        session.uploadTask(with: urlRequest, from: dataToPost) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            let successRange =  200..<300
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode,
+                  successRange.contains(statusCode) else {
+                print("Status Code Error")
+                return
+            }
+            
+            guard let data = data else {
+                print("No Data Error")
+                return
+            }
+            
+            guard let item = Parser.decodeData(ItemAfterPost.self, data) else {
+                print("Data Decoding Error")
+                return
+            }
+            
+            completionHandler(item)
+        }.resume()
+    }
 }
