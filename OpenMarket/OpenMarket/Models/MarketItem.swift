ef708c27df5b3ae1b98a027117f5fcdf739d54d4
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-01-26 16:55:36 +0900

    Feat #2 - MarketItem 타입 구현

---
+++ OpenMarket/OpenMarket/Models/MarketItem.swift
@@ -0,0 +1,50 @@
+//
+//  MarketItem.swift
+//  OpenMarket
+//
+//  Created by Kyungmin Lee on 2021/01/26.
+//
+
+import Foundation
+
+struct MarketItem: Decodable {
+    let id: Int
+    let title: String
+    let descriptions: String
+    let price: Int
+    let currency: String
+    let stock: Int
+    let discountedPrice: Int
+    let thumbnails: [String]
+    let images: [String]
+    let registrationDate: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case descriptions
+        case price
+        case currency
+        case stock
+        case discountedPrice = "discounted_price"
+        case thumbnails
+        case images
+        case registrationDate = "registration_date"
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
