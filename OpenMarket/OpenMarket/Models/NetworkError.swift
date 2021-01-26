6e757d13e6a42c6163f56f35bb7af793bf30bfe3
Hojoon Kim <elddy0948@hotmail.co.kr>
2021-01-26 21:40:16 +0900

    add : Data Task 사용 시 발생할 수 있는 에러를 나타내어 줄 Error 타입 정의
    Data Task 사용 시 발생할 수 있는 3개의 에러를 나타내어 줄 Error 타입 정의

--- OpenMarket/OpenMarket/Models/NetworkError.swift
+++ OpenMarket/OpenMarket/Models/NetworkError.swift
@@ -1,8 +1,20 @@
-//
-//  NetworkError.swift
-//  OpenMarket
-//
-//  Created by 김호준 on 2021/01/26.
-//
-
 import Foundation
+
+enum NetworkError: Error {
+    case requestError
+    case responseError
+    case dataError
+}
+
+extension NetworkError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .requestError:
+            return "잘못된 요청입니다."
+        case .responseError:
+            return "에러 코드 응답이 왔습니다."
+        case .dataError:
+            return "데이터가 오지 않았습니다."
+        }
+    }
+}
