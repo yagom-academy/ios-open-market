0899712ce9da6defcd8fe000830c544996f076c0
innieminnie <rkddlsl1001@gmail.com>
2021-01-26 10:34:04 +0900

    feat: Item 구조체 생성

---
+++ OpenMarket/OpenMarket/Item.swift
@@ -0,0 +1,15 @@
+
+import Foundation
+
+struct Item: Codable {
+    let id: Int
+    let title: String
+    let descriptions: String?
+    let price: Int
+    let currency: String
+    let stock: Int
+    let discountedPrice: Int?
+    let thumbnails: [String]
+    let images: [String]?
+    let registrationDate: Double
+}
