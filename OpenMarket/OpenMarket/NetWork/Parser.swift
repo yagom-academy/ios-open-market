75b7cb95616d4fcec95be2ca0f5eefff491240b1
SM <hcooch2ch3@gmail.com>
2021-01-27 01:42:45 +0900

    feat: Parser의 decodeData 함수의 catch error 세분화

--- OpenMarket/OpenMarket/NetWork/Parser.swift
+++ OpenMarket/OpenMarket/NetWork/Parser.swift
@@ -14,10 +14,26 @@ struct Parser {
         do {
             let response = try decoder.decode(type, from: data)
             return response
-        } catch let error {
-            print("decoding error: \(error.localizedDescription)")
-            return nil
         }
+        catch let DecodingError.dataCorrupted(context) {
+            print(context)
+        }
+        catch let DecodingError.keyNotFound(key, context) {
+            print("Key '\(key)' not found:", context.debugDescription)
+            print("codingPath:", context.codingPath)
+        }
+        catch let DecodingError.valueNotFound(value, context) {
+            print("Value '\(value)' not found:", context.debugDescription)
+            print("codingPath:", context.codingPath)
+        }
+        catch let DecodingError.typeMismatch(type, context)  {
+            print("Type '\(type)' mismatch:", context.debugDescription)
+            print("codingPath:", context.codingPath)
+        }
+        catch {
+            print("error: ", error)
+        }
+        return nil
     }
     
     static func encodeData<T: Encodable>(_ value: T) -> Data? {
