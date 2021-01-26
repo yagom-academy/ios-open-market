242aa213d7f24add6924d7999790aaad9472a302
SM <hcooch2ch3@gmail.com>
2021-01-27 02:39:04 +0900

    style: decodeData catch문 띄어쓰기 수정

--- OpenMarket/OpenMarket/NetWork/Parser.swift
+++ OpenMarket/OpenMarket/NetWork/Parser.swift
@@ -14,23 +14,18 @@ struct Parser {
         do {
             let response = try decoder.decode(type, from: data)
             return response
-        }
-        catch let DecodingError.dataCorrupted(context) {
+        } catch let DecodingError.dataCorrupted(context) {
             print(context)
-        }
-        catch let DecodingError.keyNotFound(key, context) {
+        } catch let DecodingError.keyNotFound(key, context) {
             print("Key '\(key)' not found:", context.debugDescription)
             print("codingPath:", context.codingPath)
-        }
-        catch let DecodingError.valueNotFound(value, context) {
+        } catch let DecodingError.valueNotFound(value, context) {
             print("Value '\(value)' not found:", context.debugDescription)
             print("codingPath:", context.codingPath)
-        }
-        catch let DecodingError.typeMismatch(type, context)  {
+        } catch let DecodingError.typeMismatch(type, context)  {
             print("Type '\(type)' mismatch:", context.debugDescription)
             print("codingPath:", context.codingPath)
-        }
-        catch {
+        } catch {
             print("error: ", error)
         }
         return nil
