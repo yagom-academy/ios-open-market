d01d054fcaf6daa52875e0a1bb2936ed5f687b64
Hojoon Kim <elddy0948@hotmail.co.kr>
2021-01-26 18:39:10 +0900

    feat : Market 모델에 대한 URLSession Unit Test 코드 구현
    Market 모델에 대한 URLSession Unit Test 코드 구현

--- OpenMarket/OpenMarketTests/OpenMarketTests.swift
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -30,6 +30,42 @@ class OpenMarketTests: XCTestCase {
         }
     }
     
+    func test_fetch_market_data_from_server() {
+        let expectation = XCTestExpectation(description: "fetch data")
+        
+        guard let url = URL(string: "https://camp-open-market.herokuapp.com/items/1") else {
+            XCTFail("Error with URL")
+            return
+        }
+        URLSession.shared.dataTask(with: url) {(data, response, error) in
+            if error != nil {
+                XCTFail()
+                return
+            }
+            guard let response = response as? HTTPURLResponse,
+                  (200...299).contains(response.statusCode) else {
+                XCTFail()
+                return
+            }
+            
+            guard let data = data else {
+                XCTFail()
+                return
+            }
+
+            do {
+                let json = try JSONDecoder().decode(Market.self, from: data)
+                XCTAssertEqual(json.page, 1)
+                XCTAssertEqual(json.itemList[0].id, 26)
+            } catch {
+                XCTFail()
+                return
+            }
+            expectation.fulfill()
+        }.resume()
+        wait(for: [expectation], timeout: 10.0)
+    }
+    
     func test_market_model() {
         guard let mockURL = Bundle.main.url(forResource: "Mock", withExtension: "json") else {
             XCTFail("Can't get json file")
