a8c2a1694a1a3d1670a34aaef491385c3959f294
innieminnie <rkddlsl1001@gmail.com>
2021-01-26 15:47:12 +0900

    feat: postProductList 메소드 추가

--- OpenMarket/OpenMarket/OpenMarketAPINetwork.swift
+++ OpenMarket/OpenMarket/OpenMarketAPINetwork.swift
@@ -10,7 +10,19 @@ enum OpenMarketNetworkError: Error {
 struct OpenMarketAPINetwork {
     private let baseURL = "https://camp-open-market.herokuapp.com"
     
-    func fetchProductList(with urlRequest: URLRequest, completionHandler: @escaping (Result<ProductList, OpenMarketNetworkError>) -> ()) {
+    func postProductList(of page: Int) {
+        let urlRequest = makeProductListRequestURL(of: page)
+        fetchProductList(with: urlRequest) { (result) in
+            switch result {
+            case .success(let productList):
+                print(productList)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    private func fetchProductList(with urlRequest: URLRequest, completionHandler: @escaping (Result<ProductList, OpenMarketNetworkError>) -> ()) {
         let dataTask = URLSession.shared.dataTask(with: urlRequest) { (data, response, error)  in
             guard let receivedData = data else {
                 completionHandler(.failure(.invalidData))
