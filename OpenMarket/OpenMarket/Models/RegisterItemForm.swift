84c9290f4eaab24122aa1af382364ab72946748f
wonhee <wonhee010@gmail.com>
2021-01-27 00:12:25 +0900

    refactor: registerForm add Encodable

--- OpenMarket/OpenMarket/Models/RegisterItemForm.swift
+++ OpenMarket/OpenMarket/Models/RegisterItemForm.swift
@@ -1,40 +1,18 @@
 import Foundation
 
 struct RegisterItemForm: Encodable {
-    var title: String?
-    var descriptions: String?
-    var price: Int?
-    var currency: String?
-    var stock: Int?
+    var title: String
+    var descriptions: String
+    var price: Int
+    var currency: String
+    var stock: Int
     var discountedPrice: Int?
-    var images: [String]?
-    var password: String?
+    var images: [String]
+    var password: String
     
-    var convertParameter: [String : Any]? {
-        guard let title = title,
-              let descriptions = descriptions,
-              let price = price,
-              let currency = currency,
-              let stock = stock,
-              let images = images,
-              let password = password else {
-            return nil
-        }
-
-        var parameter: [String : Any] = [:]
-        parameter["title"] = title
-        parameter["descriptions"] = descriptions
-        parameter["price"] = price
-        parameter["currency"] = currency
-        parameter["stock"] = stock
-        parameter["images"] = images
-        parameter["password"] = password
-
-        if let discountedPrice = discountedPrice {
-            parameter["discounted_price"] = discountedPrice
-        }
-
-        return parameter
+    enum CodingKeys: String, CodingKey {
+        case title, price, currency, stock, descriptions, images, password
+        case discountedPrice = "discounted_price"
     }
 }
 
--- OpenMarket/OpenMarket/ViewController.swift
+++ OpenMarket/OpenMarket/ViewController.swift
@@ -7,10 +7,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let networking = Networking()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        guard let testImage = UIImage(named: "test1")?.jpegData(compressionQuality: 0.8)?.base64EncodedString() else {
+            return
+        }
+        
+        let form = RegisterItemForm(title: "testtest", descriptions: "descdesc", price: 1, currency: "KRW", stock: 1, discountedPrice: 1, images: [testImage], password: "1234")
+        Networking().registerGoods(form: form)
     }
 
 
