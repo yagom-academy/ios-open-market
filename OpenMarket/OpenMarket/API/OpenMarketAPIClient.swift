e75c64b7cc634110f11a467ac2ee7971a8881892
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-01-27 21:17:38 +0900

    Feat #4 - API URL 처리를 위한 OpenMarketAPI 타입, 에러 처리를 위한 OpenMarketAPIError 타입 추가

--- OpenMarket/OpenMarket/API/OpenMarketAPIClient.swift
+++ OpenMarket/OpenMarket/API/OpenMarketAPIClient.swift
@@ -7,6 +7,38 @@
 
 import Foundation
 
+enum OpenMarketAPI {
+    case checkMarketPage
+    case registerMarketItem
+    case checkMarketItem
+    case modifyMarketItem
+    case deleteMarketItem
+    
+    static let baseURL = "https://camp-open-market.herokuapp.com/"
+    var path: String {
+        switch self {
+        case .checkMarketPage:
+            return "items/"
+        case .registerMarketItem:
+            return "item"
+        case .checkMarketItem:
+            return "item/"
+        case .modifyMarketItem:
+            return "item/"
+        case .deleteMarketItem:
+            return "item/"
+        }
+    }
+    var url: URL? {
+        return URL(string: OpenMarketAPI.baseURL + path)
+    }
+}
+
+enum OpenMarketAPIError: LocalizedError {
+    case unknownError
+    var errorDescription: String? { "unknown error" }
+}
+
 class OpenMarketAPIClient {
     
 }
