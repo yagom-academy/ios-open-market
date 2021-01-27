71805d79fced507cba6bc949e58445a721410cf3
yeon <jryoun0404@gmail.com>
2021-01-27 16:38:24 +0900

    Feat: ItemManager에 uploadData() 메서드 구현 - POST, PATCH 수행

--- OpenMarket/OpenMarket/Network/ItemManager.swift
+++ OpenMarket/OpenMarket/Network/ItemManager.swift
@@ -45,4 +45,48 @@ struct ItemManager {
         }
         dataTask.resume()
     }
+    
+    static func uploadData(method: HttpMethod, path: UrlPath, item: ItemUploadRequest, param: UInt?, completion: @escaping ((Result<Data?, OpenMarketError>) -> Void)) {
+        var url: String?
+        switch path {
+        case .item:
+            if let param = param {
+                url = Config.setUpUrl(method: method, path: .item, param: param)
+            }
+            else {
+                url = Config.setUpUrl(method: method, path: .item, param: nil)
+            }
+        case .items:
+            return  completion(.failure(.unknown))
+        }
+        guard let stringUrl = url, let requestUrl = URL(string: stringUrl) else {
+            return
+        }
+        
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = method.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        guard let jsonData = try? JSONEncoder().encode(item) else {
+            return
+        }
+        
+        let session: URLSession = URLSession(configuration: .default)
+        let dataTask: URLSessionUploadTask = session.uploadTask(with: request, from: jsonData) { (data: Data?, response: URLResponse?,error: Error?) in
+            if let error = error {
+                return completion(.failure(.failTransportData))
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                return completion(.failure(.failUploadData))
+            }
+            
+            guard let data = data else {
+                return completion(.failure(.failGetData))
+            }
+            return completion(.success(data))
+        }
+        dataTask.resume()
+    }
 }
