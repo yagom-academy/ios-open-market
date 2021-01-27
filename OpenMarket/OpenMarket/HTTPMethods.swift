80a2e90ea7a72148fe6b62106241d264cedb9d8b
taetae <taetae.dev@gmail.com>
2021-01-27 17:57:42 +0900

    feat: HTTP 메소드 종류를 담고 있는 enum 타입 추가

---
+++ OpenMarket/OpenMarket/HTTPMethods.swift
@@ -0,0 +1,15 @@
+//
+//  HttpMethods.swift
+//  OpenMarket
+//
+//  Created by 김태형 on 2021/01/27.
+//
+
+import Foundation
+
+enum HTTPMethods: String {
+    case post = "POST"
+    case get = "GET"
+    case put = "PUT"
+    case delete = "DELETE"
+}
