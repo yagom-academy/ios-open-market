8f6ef20cd409f802e97a8844486d6278ff28e482
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-01-27 20:03:52 +0900

    Feat #2 - Mock 데이터로 MarketItem, MarketPage 디코딩 유닛 테스트 기능 구현

---
+++ OpenMarket/OpenMarketTests/MarketItemDecodeMockDataTests.swift
@@ -0,0 +1,44 @@
+//
+//  MarketItemDecodeMockTests.swift
+//  OpenMarketTests
+//
+//  Created by Kyungmin Lee on 2021/01/27.
+//
+
+import XCTest
+@testable import OpenMarket
+
+class MarketItemDecodeMockDataTests: XCTestCase {
+    var sut: MarketItem!
+    
+    override func setUpWithError() throws {
+        if let dataAsset = NSDataAsset(name: "item") {
+            sut = try JSONDecoder().decode(MarketItem.self, from: dataAsset.data)
+        }
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+    
+    func testDecodedMockData() {
+        XCTAssertNotNil(sut)
+        XCTAssertEqual(sut.id, 1)
+        XCTAssertEqual(sut.title, "MacBook Air")
+        let lastDescriptionWord = sut.descriptions?.components(separatedBy: " ").last
+        XCTAssertEqual(lastDescriptionWord, "있습니다.")
+        XCTAssertEqual(sut.priceWithCurrency, "KRW 1,290,000")
+        XCTAssertEqual(sut.stock, 1000000000000)
+        let lastThumbnail = sut.thumbnails?.last
+        XCTAssertEqual(lastThumbnail, "https://camp-open-market.s3.ap-northeast-2.amazonaws.com/thumbnails/1-3.png")
+        let lastImage = sut.images?.last
+        XCTAssertEqual(lastImage, "https://camp-open-market.s3.ap-northeast-2.amazonaws.com/images/1-5.png")
+        let date: Date = Date(timeIntervalSince1970: 1611523563.719116)
+        let dateFormatter: DateFormatter = DateFormatter()
+        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+        dateFormatter.locale = NSLocale.autoupdatingCurrent
+        dateFormatter.dateFormat = "yyyy.MM.dd HH:MM"
+        let dateString = dateFormatter.string(from: date)
+        XCTAssertEqual(sut.registrationGMTDate, dateString)
+    }
+}
