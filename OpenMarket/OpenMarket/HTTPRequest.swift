b680f5a3cd2d23a378cb4b795cd27e82b0ea1e81
zdodev <iver0945@gmail.com>
2021-01-28 00:04:51 +0900

    feat: URLRequest 타입 생성.

--- OpenMarket/OpenMarket/HTTPRequest.swift
+++ OpenMarket/OpenMarket/HTTPRequest.swift
@@ -8,37 +8,125 @@
 import Foundation
 
 struct HTTPRequest {
-    let url: String = "https://camp-open-market.herokuapp.com/images/1"
+    private let urlString = "https://camp-open-market.herokuapp.com"
     
-    func request() throws {
-        let session = URLSession.shared
+    func CreateURLRequest(requestAPI: RequestAPI) -> URLRequest? {
+        switch requestAPI {
+        case .lookupList:
+            return lookupList()
+        default:
+            return nil
+        }
+    }
+    
+    func CreateItemRegistrationURLRequest(requestAPI: RequestAPI, bodyData: ItemRegistrationRequest) -> URLRequest? {
+        switch requestAPI {
+        case .itemRegistration:
+            return itemRegistration(bodyData: bodyData)
+        default:
+            return nil
+        }
+    }
+    
+    private func lookupList() -> URLRequest? {
+        guard var url = URL(string: urlString) else {
+            return nil
+        }
+        let path = "/items/1"
+        url.appendPathComponent(path)
         
-        guard let url = URL(string: url) else {
-            throw NetworkError.urlCreationFailed
+        let urlRequest = URLRequest(url: url)
+        
+        return urlRequest
+    }
+    
+    private func itemRegistration(bodyData: ItemRegistrationRequest) -> URLRequest? {
+        guard var url = URL(string: urlString) else {
+            return nil
+        }
+        let path = "/item"
+        url.appendPathComponent(path)
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = HTTPMethod.post.rawValue
+        let boundary = UUID().uuidString
+        urlRequest.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        urlRequest.httpBody = addMultipartFormDataToBody(paramters: bodyData, boundary: boundary)
+        
+        return urlRequest
+    }
+    
+    private func addMultipartFormDataToBody(paramters: ItemRegistrationRequest, boundary: String) -> Data? {
+        var body = Data()
+        guard let title = makeMultipartFormParameter(parameter: "title", value: paramters.title, boundary: boundary) else {
+            return nil
+        }
+        guard let descriptions = makeMultipartFormParameter(parameter: "descriptions", value: paramters.descriptions, boundary: boundary) else {
+            return nil
+        }
+        guard let price = makeMultipartFormParameter(parameter: "price", value: String(paramters.price), boundary: boundary) else {
+            return nil
+        }
+        guard let currency = makeMultipartFormParameter(parameter: "currency", value: paramters.currency, boundary: boundary) else {
+            return nil
+        }
+        guard let stock = makeMultipartFormParameter(parameter: "stock", value: String(paramters.stock), boundary: boundary) else {
+            return nil
+        }
+        guard let password = makeMultipartFormParameter(parameter: "password", value: paramters.password, boundary: boundary) else {
+            return nil
+        }
+        guard let lastBoundaryLine = "--\(boundary)--\r\n".data(using: .utf8) else {
+            return nil
         }
         
-        session.dataTask(with: url) { data, response, error in
-            if let error = error {
-                print(error)
-                return
-            }
-            
-            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
-                return
-            }
-            
-            if let data = data {
-                do {
-                    let decodedData = try JSONDecoder().decode(ItemList.self, from: data)
-                    print(decodedData)
-                } catch {
-                    print(error)
-                }
-            }
+        body.append(title)
+        body.append(descriptions)
+        body.append(price)
+        body.append(currency)
+        body.append(stock)
+        body.append(password)
+        body.append(lastBoundaryLine)
+        
+        return body
+    }
+    
+    private func makeMultipartFormParameter(parameter: String, value: String, boundary: String) -> Data? {
+        var body = Data()
+        
+        guard let boundaryLine = "--\(boundary)\r\n".data(using: .utf8) else {
+            return nil
         }
+        guard let contentDispositionLine = "Content-Disposition: form-data; name=\"\(parameter)\"\r\n\r\n".data(using: .utf8) else {
+            return nil
+        }
+        guard let data = value.data(using: .utf8) else {
+            return nil
+        }
+        guard let lineBreak = "\r\n".data(using: .utf8) else {
+            return nil
+        }
+        
+        body.append(boundaryLine)
+        body.append(contentDispositionLine)
+        body.append(data)
+        body.append(lineBreak)
+        
+        return body
     }
 }
 
-enum NetworkError: Error {
-    case urlCreationFailed
+enum RequestAPI {
+    case lookupList
+    case itemRegistration
+    case itemSpecification
+    case itemModification
+    case itemDeletion
+}
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case patch = "PATCH"
+    case delete = "DELETE"
 }
