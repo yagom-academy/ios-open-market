c600bf211c9831599d608ee25f75b8ce800cfc98
yeon <jryoun0404@gmail.com>
2021-01-27 16:38:48 +0900

    Feat: XCTest에 POST와 PATCH가 수행되는지 테스트

--- OpenMarket/OpenMarketTests/OpenMarketTests.swift
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -80,4 +80,108 @@ class OpenMarketTests: XCTestCase {
         XCTAssertEqual(currency, "KRW")
         XCTAssertEqual(stock, 90)
     }
+    
+    func testPostItem() {
+        let expectation = XCTestExpectation(description: "APIPrivoderTaskExpectation")
+        guard let image1 = UIImage(named: "testImage1"), let image2 = UIImage(named: "testImage2") else {
+            return
+        }
+        var imageDataStringArray: [String] = []
+        guard let imageData1 = UIImageToData(image: image1), let imageData2 = UIImageToData(image: image2) else {
+            return
+        }
+        imageDataStringArray.append(imageData1)
+        imageDataStringArray.append(imageData2)
+        
+        let newItem = ItemUploadRequest(title: "AirPod Max",
+                                        descriptions: "귀를 호강시켜주는 에어팟 맥스! 사주실 분 구해요ㅠ",
+                                        price: 700000,
+                                        currency: "KRW",
+                                        stock: 10000,
+                                        discountedPrice: nil,
+                                        images: imageDataStringArray,
+                                        password: "asdfqwerzxcv")
+        
+        ItemManager.uploadData(method: .post, path: .item, item: newItem, param: nil) { [self] result in
+            switch result {
+            case .success(let data):
+                guard let data = data else {
+                    return
+                }
+                do {
+                    item = try JSONDecoder().decode(Item.self, from: data)
+                    expectation.fulfill()
+                } catch {
+                    print(error)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
+        wait(for: [expectation], timeout: 10.0)
+        let title = item?.title
+        let description = item?.descriptions
+        let price = item?.price
+        let currency = item?.currency
+        let stock = item?.stock
+        
+        XCTAssertEqual(title, "AirPod Max")
+        XCTAssertEqual(description, "귀를 호강시켜주는 에어팟 맥스! 사주실 분 구해요ㅠ")
+        XCTAssertEqual(price, 700000)
+        XCTAssertEqual(currency, "KRW")
+        XCTAssertEqual(stock, 10000)
+    }
+    
+    func testPatchItem() {
+        let expectation = XCTestExpectation(description: "APIPrivoderTaskExpectation")
+        let pathcItem = ItemUploadRequest(title: nil,
+                                          descriptions: nil,
+                                          price: 750000,
+                                          currency: nil,
+                                          stock: 500000,
+                                          discountedPrice: nil,
+                                          images: nil,
+                                          password: "asdfqwerzxcv")
+        
+        ItemManager.uploadData(method: .patch, path: .item, item: pathcItem, param: 187) { [self] result in
+            switch result {
+            case .success(let data):
+                guard let data = data else {
+                    return
+                }
+                do {
+                    item = try JSONDecoder().decode(Item.self, from: data)
+                    expectation.fulfill()
+                } catch {
+                    print(error)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
+        wait(for: [expectation], timeout: 10.0)
+        let id = item?.id
+        let title = item?.title
+        let description = item?.descriptions
+        let price = item?.price
+        let currency = item?.currency
+        let stock = item?.stock
+        
+        XCTAssertEqual(id, 187)
+        XCTAssertEqual(title, "AirPod Max")
+        XCTAssertEqual(description, "귀를 호강시켜주는 에어팟 맥스! 사주실 분 구해요ㅠ")
+        XCTAssertEqual(price, 750000)
+        XCTAssertEqual(currency, "KRW")
+        XCTAssertEqual(stock, 500000)
+    }
+    
+    private func UIImageToData(image: UIImage) -> String? {
+        guard let data = image.jpegData(compressionQuality: 0.8)?.base64EncodedData() else {
+            return nil
+        }
+        let dataString = String(decoding: data, as: UTF8.self)
+        return dataString
+    }
 }
