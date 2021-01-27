54d7ce1c21d9a2f7c163860a055effb7a3543295
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-01-28 01:00:06 +0900

    Feat #4 - API 상품 조회 기능 구현

--- OpenMarket/OpenMarket/API/OpenMarketAPIClient.swift
+++ OpenMarket/OpenMarket/API/OpenMarketAPIClient.swift
@@ -8,20 +8,20 @@
 import Foundation
 
 enum OpenMarketAPI {
-    case checkMarketPage
+    case requestMarketPage
     case registerMarketItem
-    case checkMarketItem
+    case requestMarketItem
     case modifyMarketItem
     case deleteMarketItem
     
     static let baseURL = "https://camp-open-market.herokuapp.com/"
     var path: String {
         switch self {
-        case .checkMarketPage:
+        case .requestMarketPage:
             return "items/"
         case .registerMarketItem:
             return "item"
-        case .checkMarketItem:
+        case .requestMarketItem:
             return "item/"
         case .modifyMarketItem:
             return "item/"
@@ -34,11 +34,38 @@ enum OpenMarketAPI {
     }
 }
 
-enum OpenMarketAPIError: LocalizedError {
+enum OpenMarketAPIError: Error {
+    case invalidURL
     case unknownError
-    var errorDescription: String? { "unknown error" }
 }
 
 class OpenMarketAPIClient {
+    let urlSession: URLSession
     
+    init(urlSession: URLSession = .shared) {
+        self.urlSession = urlSession
+    }
+    
+    func requestMarketItem(id: Int, completion: @escaping (Result<MarketItem, Error>) -> Void) {
+        guard let url = OpenMarketAPI.requestMarketItem.url?.appendingPathComponent("\(id)") else {
+            completion(.failure(OpenMarketAPIError.invalidURL))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        let task = urlSession.dataTask(with: request) { data, urlResponse, error in
+            guard let response = urlResponse as? HTTPURLResponse, (200...399).contains(response.statusCode) else {
+                completion(.failure(error ?? OpenMarketAPIError.unknownError))
+                return
+            }
+            if let data = data,
+               let marketItem = try? JSONDecoder().decode(MarketItem.self, from: data) {
+                completion(.success(marketItem))
+                return
+            }
+            completion(.failure(OpenMarketAPIError.unknownError))
+        }
+        task.resume()
+    }
 }
