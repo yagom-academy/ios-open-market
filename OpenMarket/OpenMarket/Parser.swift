7a66a1c430ca1a2b8952162750af770dc72b169a
odong-Tree <ss12357@naver.com>
2021-01-28 22:32:03 +0900

    feat: Parser post, delete  메서드 구현 (에러처리 필요)

--- OpenMarket/OpenMarket/Parser.swift
+++ OpenMarket/OpenMarket/Parser.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 struct Parser<T: Codable> {
-    static func decodeJSONAPI(of url: URL, result: @escaping (Result<T, NetworkingError>) -> ()) {
+    static func decodeJSONAPI(url: URL, result: @escaping (Result<T, Error>) -> ()) {
         APIManager.requestGET(url: url) { (data: Data?, response: URLResponse?, error: Error?) in
             guard error == nil else {
                 result(.failure(<#T##NetworkingError#>))
             }
 
-            guard let response = response as? HTTPURLResponse, (200 ..< 299) ~= response.statusCode else {
+            guard let response = response as? HTTPURLResponse,
+                  (200...299).contains(response.statusCode) else {
                 result(.failure(<#T##NetworkingError#>))
-                
             }
             
             guard let data = data else {
@@ -27,6 +27,40 @@ struct Parser<T: Codable> {
                 let decodingResult = try JSONDecoder().decode(T.self, from: data)
                 result(.success(decodingResult))
             } catch {
+                result(.failure(error))
+            }
+        }
+    }
+    
+    static func postToAPI(url: URL, data: T, result: @escaping (Error) -> ()) {
+        guard let uploadData = try? JSONEncoder().encode(data) else {
+            return
+        }
+        
+        APIManager.requestPOST(url: url, uploadData: uploadData) { (data: Data?, response: URLResponse?, error: Error?) in
+            guard error == nil else {
+                result(<#T##NetworkingError#>)
+            }
+
+            guard let response = response as? HTTPURLResponse,
+                  (200...299).contains(response.statusCode) else {
+                result(<#T##NetworkingError#>)
+            }
+        }
+    }
+    
+    static func deleteAPI(url: URL, result: @escaping (Result<T, Error>) -> ()) {
+        APIManager.requestGET(url: url) { (data: Data?, response: URLResponse?, error: Error?) in
+            guard error == nil else {
+                result(.failure(<#T##NetworkingError#>))
+            }
+
+            guard let response = response as? HTTPURLResponse,
+                  (200...299).contains(response.statusCode) else {
+                result(.failure(<#T##NetworkingError#>))
+            }
+            
+            guard let data = data else {
                 result(.failure(<#T##NetworkingError#>))
             }
             
