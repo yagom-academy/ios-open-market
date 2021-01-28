84acceb84183be28bd958785fbd1246dba507a6c
innieminnie <rkddlsl1001@gmail.com>
2021-01-28 10:14:42 +0900

    style: 코드 스타일 일부 개선

--- OpenMarket/OpenMarketTests/MockModel/MockAPI.swift
+++ OpenMarket/OpenMarketTests/MockModel/MockAPI.swift
@@ -8,13 +8,25 @@ enum MockAPI {
     
     static let baseURL = "https://camp-open-market.herokuapp.com"
     
-    var sampleItems: NSDataAsset { NSDataAsset.init(name: "items")! }
-    var sampleItem: NSDataAsset { NSDataAsset.init(name: "item")! }
-    var sampleID: NSDataAsset { NSDataAsset.init(name: "id")! }
-    
-    var listPageOnePath: String { "items/1" }
-    var listPageOneURL: URL { URL(string: MockAPI.baseURL + listPageOnePath)! }
-    
-    var postProductPath: String {"item/"}
-    var postProductURL: URL { URL(string: MockAPI.baseURL + postProductPath)! }
+    var sampleItems: NSDataAsset {
+        NSDataAsset.init(name: "items")!
+    }
+    var sampleItem: NSDataAsset {
+        NSDataAsset.init(name: "item")!
+    }
+    var sampleID: NSDataAsset {
+        NSDataAsset.init(name: "id")!
+    }
+    var listPageOnePath: String {
+        "items/1"
+    }
+    var listPageOneURL: URL {
+        URL(string: MockAPI.baseURL + listPageOnePath)!
+    }
+    var postProductPath: String {
+        "item/"
+    }
+    var postProductURL: URL {
+        URL(string: MockAPI.baseURL + postProductPath)!
+    }
 }
