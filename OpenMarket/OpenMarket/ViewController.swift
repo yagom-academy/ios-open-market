463c5d88d62a953b2590893f808677fc9bdedf1b
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-01-29 03:44:19 +0900

    Feat #4 - 뷰 컨트롤러에서 API 사용 방법 추가

--- OpenMarket/OpenMarket/ViewController.swift
+++ OpenMarket/OpenMarket/ViewController.swift
@@ -12,7 +12,33 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // API 사용방법
+        OpenMarketAPIClient().requestMarketItem(id: 63) { result in
+            switch result {
+            case .success(let marketItem):
+                print("--- result of requestMarketItem ---")
+                print("id: \(marketItem.id)")
+                print("title: \(marketItem.title!)")
+                print("price: \(marketItem.priceWithCurrency)")
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+        
+        OpenMarketAPIClient().requestMarketPage(pageNumber: 1) { result in
+            switch result {
+            case .success(let marketPage):
+                print("--- result of requestMarketPage ---")
+                for i in 0...2 {
+                    print("pageNumber: \(marketPage.pageNumber)")
+                    print("\(marketPage.marketItems[i].id): \(marketPage.marketItems[i].title!)")
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
 
