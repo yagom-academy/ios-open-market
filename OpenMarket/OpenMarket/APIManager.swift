02db5dcf0054d8011443fc00c24753dce8681224
odong-Tree <ss12357@naver.com>
2021-01-28 22:31:03 +0900

    feat: APIManager POST, DELETE 요청 메서드 구현

--- OpenMarket/OpenMarket/APIManager.swift
+++ OpenMarket/OpenMarket/APIManager.swift
@@ -15,16 +15,24 @@ struct APIManager {
         URLSession.shared.dataTask(with: request, completionHandler: completionHandler).resume()
     }
     
-    static func requestPOST(url: URL) {
-        
+    static func requestPOST(url: URL, uploadData: Data, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    
+        URLSession.shared.uploadTask(with: request, from: uploadData, completionHandler: completionHandler).resume()
     }
     
     static func requestPATCH(url: URL) {
         
     }
     
-    static func requestDELETE(url: URL) {
+    static func requestDELETE(url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        var request = URLRequest(url: url)
+        request.httpMethod = "DELETE"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         
+        URLSession.shared.dataTask(with: request, completionHandler: completionHandler).resume()
     }
 }
 
