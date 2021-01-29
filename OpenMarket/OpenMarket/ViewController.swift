80fc474c1d8388d3aca9546f54eba04b0154f890
Hojoon Kim <elddy0948@hotmail.co.kr>
2021-01-29 11:09:22 +0900

    refactor : 사용하지 않는 코드 삭제

--- OpenMarket/OpenMarket/ViewController.swift
+++ OpenMarket/OpenMarket/ViewController.swift
@@ -16,11 +16,6 @@ class ViewController: UIViewController {
         
         let form = GoodsForm(title: "test-lasagna", descriptions: "desc", price: 100000000, currency: "KRW", stock: 1, discountedPrice: nil, images: [testImage], password: "1234")
         Networking.registerGoods(form: form)
-        
-//        let deleteForm = GoodsForm(id: 256, password: "1234")
-//        Networking.removeGoods(form: deleteForm)
-//        Networking.fetchGoodsList(page: 1) { result in
-//        }
     }
 }
 
