f8dfe9d2e4bdf1766596e266f81fc6400cdee6c8
HakJu <gkrwnrh@gmail.com>
2021-01-29 17:06:42 +0900

    test: makeURL 메서드 유닛테스트

--- OpenMarket/OpenMarketTests/OpenMarketTests.swift
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -6,27 +6,51 @@
 //
 
 import XCTest
+@testable import OpenMarket
 
 class OpenMarketTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testMakeItemsListURL() throws {
+        // 1. given
+        let expectResult = URL(string: "https://camp-open-market.herokuapp.com/items/1")
+        
+        // 2. when
+        let url = try URLManager.makeURL(type: .itemsListPage(1))
+        
+        // 3. then
+        XCTAssertEqual(url, expectResult, "Making URL is Failed")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testMakeRegistItemURL() throws {
+        // 1. given
+        let expectResult = URL(string: "https://camp-open-market.herokuapp.com/item")
+        
+        // 2. when
+        let url = try URLManager.makeURL(type: .registItem)
+        
+        // 3. then
+        XCTAssertEqual(url, expectResult, "Making URL is Failed")
     }
-
+    
+    func testMakeItemIdURL() throws {
+        // 1. given
+        let expectResult = URL(string: "https://camp-open-market.herokuapp.com/item/66")
+        
+        // 2. when
+        let url = try URLManager.makeURL(type: .itemId(66))
+        
+        // 3. then
+        XCTAssertEqual(url, expectResult, "Making URL is Failed")
+    }
+    
 }
