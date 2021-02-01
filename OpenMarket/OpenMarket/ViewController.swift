144dc5046057f75a520a1d6452e3a92338b4f823
wonhee <wonhee010@gmail.com>
2021-02-01 12:20:39 +0900

    feat: add TODO code, segment case

--- OpenMarket/OpenMarket/ViewController.swift
+++ OpenMarket/OpenMarket/ViewController.swift
@@ -13,17 +13,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpUI()
+        setUpCollection()
     }
     
+    // MARK: - setUp UI
     private func setUpUI () {
         segment.addTarget(self, action: #selector(changedSegmentValue(_:)), for: .valueChanged)
     }
     
+    private func setUpCollection() {
+        // TODO: Lasagna - CollectionView List Type cell regist
+        // TODO: Joons - CollectionVIew Grid Type cell Regist
+    }
+    
     @IBAction func touchUpAddButton(_ sender: UIButton) {
+        // TODO: add logic in step3
         print("➕")
     }
     
     @objc func changedSegmentValue(_ sender: UISegmentedControl) {
-        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            print("click list")
+        case 1:
+            print("click grid")
+        default:
+            self.showErrorAlert(with: OpenMarketError.unknown, okHandler: nil)
+        }
     }
 }
