34f0e4f32d2580cb0eca36dcb2c1162995e299ae
yeon <jryoun0404@gmail.com>
2021-02-02 13:22:51 +0900

    Feat: 네트워크가 되지 않는 상황에서의 테스트 객체 생성 및 테스트 코드 작성

--- OpenMarket/OpenMarketTests/OpenMarketTests.swift
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -47,13 +47,13 @@ class OpenMarketTests: XCTestCase {
             return
         }
         let patchItem = ItemToUpload(title: nil,
-                                          descriptions: nil,
-                                          price: 750000,
-                                          currency: nil,
-                                          stock: 500000,
-                                          discountedPrice: nil,
-                                          images: nil,
-                                          password: "asdfqwerzxcv")
+                                     descriptions: nil,
+                                     price: 750000,
+                                     currency: nil,
+                                     stock: 500000,
+                                     discountedPrice: nil,
+                                     images: nil,
+                                     password: "asdfqwerzxcv")
         guard let jsonData = try? JSONEncoder().encode(patchItem as? ItemToUpload) else {
             return
         }
@@ -67,7 +67,7 @@ class OpenMarketTests: XCTestCase {
     
     func testGetItemListAsync() {
         let expectation = XCTestExpectation(description: "APIPrivoderTaskExpectation")
-
+        
         ItemManager.shared.loadData(method: .get, path: .items, param: 1) { [self] result in
             switch result {
             case .success(let data):
@@ -84,16 +84,16 @@ class OpenMarketTests: XCTestCase {
                 print(error)
             }
         }
-
+        
         wait(for: [expectation], timeout: 5.0)
         let page = itemList?.page
-
+        
         XCTAssertEqual(page, 1)
     }
     
     func testGetItemDetail() {
         let expectation = XCTestExpectation(description: "APIPrivoderTaskExpectation")
-        ItemManager.shared.loadData(method: .get, path: .item, param: 68) { [self] result in
+        ItemManager.shared.loadData(method: .get, path: .item, param: 1) { [self] result in
             switch result {
             case .success(let data):
                 guard let data = data else {
@@ -117,11 +117,11 @@ class OpenMarketTests: XCTestCase {
         let currency = item?.currency
         let stock = item?.stock
         
-        XCTAssertEqual(id, 68)
-        XCTAssertEqual(title, "Mac mini")
-        XCTAssertEqual(price, 890000)
+        XCTAssertEqual(id, 1)
+        XCTAssertEqual(title, "MacBook Air")
+        XCTAssertEqual(price, 1290000)
         XCTAssertEqual(currency, "KRW")
-        XCTAssertEqual(stock, 90)
+        XCTAssertEqual(stock, 1000000000)
     }
     
     func testPostItem() {
@@ -137,13 +137,13 @@ class OpenMarketTests: XCTestCase {
         imageDataStringArray.append(airPodMaxImageData2)
         
         let newItem = ItemToUpload(title: "AirPod Max",
-                                        descriptions: "귀를 호강시켜주는 에어팟 맥스! 사주실 분 구해요ㅠ",
-                                        price: 700000,
-                                        currency: "KRW",
-                                        stock: 10000,
-                                        discountedPrice: nil,
-                                        images: imageDataStringArray,
-                                        password: "asdfqwerzxcv")
+                                   descriptions: "귀를 호강시켜주는 에어팟 맥스! 사주실 분 구해요ㅠ",
+                                   price: 700000,
+                                   currency: "KRW",
+                                   stock: 10000,
+                                   discountedPrice: nil,
+                                   images: imageDataStringArray,
+                                   password: "asdfqwerzxcv")
         
         ItemManager.shared.uploadData(method: .post, path: .item, item: newItem, param: nil) { [self] result in
             switch result {
@@ -180,13 +180,13 @@ class OpenMarketTests: XCTestCase {
         let expectation = XCTestExpectation(description: "APIPrivoderTaskExpectation")
         let param: UInt = 392
         let patchItem = ItemToUpload(title: nil,
-                                          descriptions: nil,
-                                          price: 750000,
-                                          currency: nil,
-                                          stock: 500000,
-                                          discountedPrice: nil,
-                                          images: nil,
-                                          password: "asdfqwerzxcv")
+                                     descriptions: nil,
+                                     price: 750000,
+                                     currency: nil,
+                                     stock: 500000,
+                                     discountedPrice: nil,
+                                     images: nil,
+                                     password: "asdfqwerzxcv")
         
         ItemManager.shared.uploadData(method: .patch, path: .item, item: patchItem, param: param) { [self] result in
             switch result {
@@ -255,5 +255,144 @@ class OpenMarketTests: XCTestCase {
         let id = deleteItem?.id
         
         XCTAssertEqual(id, param)
+    }
+    
+    var itemManagerWithoutNetwork: ItemManagerWithoutNetwork!
+    let session = MockURLSession()
+    
+    override func setUp() {
+        super.setUp()
+        itemManagerWithoutNetwork = ItemManagerWithoutNetwork(session: session)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testLoadResumeCalledWithoutNetwork() {
+        let dataTask = MockURLSessionDataTask()
+        session.nextDataTask = dataTask
+        
+        itemManagerWithoutNetwork.loadData(method: .get, path: .items, param: 1) { result in
+            // Return data
+        }
+        XCTAssert(dataTask.resumeWasCalled)
+    }
+    
+    func testLoadReturnDataWithoutNetwork() {
+        guard let mockDataURL = Bundle.main.url(forResource: "Item", withExtension: "json") else {
+            return
+        }
+        let expectedData = try? Data(contentsOf: mockDataURL)
+        session.nextData = expectedData
+        
+        var actualData: Data?
+        itemManagerWithoutNetwork.loadData(method: .get, path: .item, param: 1) { result in
+            switch result {
+            case .success(let data):
+                actualData = data
+            case .failure(let error):
+                print(error)
+            }
+        }
+        XCTAssertNotNil(actualData)
+    }
+}
+
+//MARK: Protocol for MOCK/Real
+protocol URLSessionProtocol {
+    typealias DataTaskResult = (Data?, URLResponse?, Error?) -> Void
+    func dataTask(with request: URLRequest, completionHandler: @escaping DataTaskResult) -> URLSessionDataTaskProtocol
+}
+
+protocol URLSessionDataTaskProtocol {
+    func resume()
+}
+
+//MARK: Conform the protocol
+extension URLSession: URLSessionProtocol {
+    func dataTask(with request: URLRequest, completionHandler: @escaping URLSessionProtocol.DataTaskResult) -> URLSessionDataTaskProtocol {
+        return dataTask(with: request, completionHandler: completionHandler) as URLSessionDataTask
+    }
+}
+
+extension URLSessionDataTask: URLSessionDataTaskProtocol {}
+
+//MARK: MOCK
+class MockURLSession: URLSessionProtocol {
+    var nextDataTask = MockURLSessionDataTask()
+    var nextData: Data?
+    var nextError: Error?
+    private (set) var lastURL: URL?
+    
+    func successHttpURLResponse(request: URLRequest) -> URLResponse {
+        return HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: "HTTP/1.1", headerFields: nil)!
+    }
+    
+    func dataTask(with request: URLRequest, completionHandler: @escaping DataTaskResult) -> URLSessionDataTaskProtocol {
+        lastURL = request.url
+        
+        completionHandler(nextData, successHttpURLResponse(request: request), nextError)
+        return nextDataTask
+    }
+}
+
+class MockURLSessionDataTask: URLSessionDataTaskProtocol {
+    private (set) var resumeWasCalled = false
+    
+    func resume() {
+        resumeWasCalled = true
+    }
+}
+
+//MARK: ItemManagerWithoutNetwork Implemenation
+struct ItemManagerWithoutNetwork {
+    typealias resultHandler = (Result<Data?, OpenMarketError>) -> Void
+    private let session: URLSessionProtocol
+    init(session: URLSessionProtocol) {
+        self.session = session
+    }
+    
+    func loadData(method: HttpMethod, path: PathOfURL, param: UInt, completion: @escaping resultHandler) {
+        guard let requestUrl = makeURL(method: method, path: path, param: param) else {
+            return completion(.failure(.failSetUpURL))
+        }
+        
+        guard let request = makeURLRequestWithoutRequestBody(method: method, requestURL: requestUrl) else {
+            return completion(.failure(.failMakeURLRequest))
+        }
+        
+        communicateToServerWithDataTask(with: request, completion: completion)
+    }
+    
+    private func makeURL(method: HttpMethod, path: PathOfURL, param: UInt? = nil) -> URL? {
+        var url: URL?
+        url = NetworkConfig.setUpUrl(method: method, path: path, param: param)
+        return url
+    }
+    
+    private func makeURLRequestWithoutRequestBody(method: HttpMethod, requestURL: URL) -> URLRequest? {
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = method.rawValue
+        return request
+    }
+    
+    private func communicateToServerWithDataTask(with request: URLRequest, completion: @escaping resultHandler) {
+        let dataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+            if let error = error {
+                return completion(.failure(.failTransportData))
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                return completion(.failure(.failDeleteData))
+            }
+            
+            guard let data = data else {
+                return completion(.failure(.failGetData))
+            }
+            return completion(.success(data))
+        }
+        dataTask.resume()
     }
 }
