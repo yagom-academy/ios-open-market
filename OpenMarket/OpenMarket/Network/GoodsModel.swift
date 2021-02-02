3425a07d15daf8cc3e5c0e4888a04cf588152460
wonhee <wonhee010@gmail.com>
2021-02-02 16:29:39 +0900

    feat: integrated Goods Network code

--- OpenMarket/OpenMarket/Network/GoodsModel.swift
+++ OpenMarket/OpenMarket/Network/GoodsModel.swift
@@ -15,7 +15,7 @@ struct GoodsModel {
         var path: String = NetworkConfig.openMarketFixedURL
         var method: HTTPMethod
         var headers: [String : String]?
-        var bodyParams: Data?
+        var bodyParams: [String : Any]?
         
         init(fetchID: UInt) {
             self.id = fetchID
@@ -27,7 +27,23 @@ struct GoodsModel {
             self.path.append(NetworkConfig.makeURLPath(api: .registerGoods, with: nil))
             self.method = .post
             self.headers = NetworkConfig.multipartContentType
-            
+            self.bodyParams = registerParams.convertParameter
+        }
+        
+        init(editParams: GoodsForm,
+             editID: UInt) {
+            self.path.append(NetworkConfig.makeURLPath(api: .editGoods, with: editID))
+            self.method = .patch
+            self.headers = NetworkConfig.multipartContentType
+            self.bodyParams = editParams.convertParameter
+        }
+        
+        init(deleteParams: GoodsForm,
+             deleteID: UInt) {
+            self.path.append(NetworkConfig.makeURLPath(api: .deleteGoods, with: deleteID))
+            self.method = .delete
+            self.headers = NetworkConfig.jsonContentType
+            self.bodyParams = deleteParams.convertParameter
         }
     }
     
@@ -42,7 +58,36 @@ struct GoodsModel {
         }
     }
     
-    func registerGoods() {
-        
+    func registerGoods(params: GoodsForm, completion: @escaping(Result<Any, Error>) -> Void) {
+        task.perform(request: GoodsModelRequest(registerParams: params), dataType: Goods.self) { result in
+            switch result {
+            case .success(let decodedData):
+                completion(.success(decodedData))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func editGoods(id: UInt, params: GoodsForm, completion: @escaping(Result<Any, Error>) -> Void) {
+        task.perform(request: GoodsModelRequest(editParams: params, editID: id), dataType: Goods.self) { result in
+            switch result {
+            case .success(let decodedData):
+                completion(.success(decodedData))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func deleteGoods(id: UInt, params: GoodsForm, completion: @escaping(Result<Any, Error>) -> Void) {
+        task.perform(request: GoodsModelRequest(deleteParams: params, deleteID: id), dataType: [String : UInt].self) { result in
+            switch result {
+            case .success(let decodedData):
+                completion(.success(decodedData))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
     }
 }
