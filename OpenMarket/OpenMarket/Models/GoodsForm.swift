bccf406cf74f7c73fd37e05ae2511f5fbd3bf148
wonhee <wonhee010@gmail.com>
2021-02-02 16:19:35 +0900

    feat: add convertParams func

--- OpenMarket/OpenMarket/Models/GoodsForm.swift
+++ OpenMarket/OpenMarket/Models/GoodsForm.swift
@@ -67,5 +67,34 @@ struct GoodsForm {
         self.images = nil
     }
     
-    var 
+    var convertParameter: [String : Any]? {
+        var parameter: [String : Any] = [:]
+        parameter["password"] = password
+        if let id = id {
+            parameter["id"] = id
+        }
+        if let title = title {
+            parameter["title"] = title
+        }
+        if let descriptions = descriptions {
+            parameter["descriptions"] = descriptions
+        }
+        if let price = price {
+            parameter["price"] = price
+        }
+        if let currency = currency {
+            parameter["currency"] = currency
+        }
+        if let stock = stock {
+            parameter["stock"] = stock
+        }
+        if let discountedPrice = discountedPrice {
+            parameter["discountedPrice"] = discountedPrice
+        }
+        // TODO: edit multi
+        if let images = images {
+            parameter["images"] = images
+        }
+        return parameter
+    }
 }
