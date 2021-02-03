da5e78fd675a8244d1566008f8b3d8bebb778e91
Hojoon Kim <elddy0948@hotmail.co.kr>
2021-02-04 00:01:53 +0900

    feat : add GoodsGridCollectionViewCell file

---
+++ OpenMarket/OpenMarket/Cells/GoodsGridCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  GoodsGridCollectionViewCell.swift
+//  OpenMarket
+//
+//  Created by 김호준 on 2021/02/03.
+//
+
+import UIKit
+
+class GoodsGridCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
