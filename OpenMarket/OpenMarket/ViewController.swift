f44bd09bb1808d8f7248443bae54c91fa123b230
wonhee <wonhee010@gmail.com>
2021-02-03 12:40:00 +0900

    feat: trimming Project

--- OpenMarket/OpenMarket/ViewController.swift
+++ OpenMarket/OpenMarket/ViewController.swift
@@ -7,12 +7,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var table: UITableView!
-    private let testArray = [
-        "https://wallpaperaccess.com/download/europe-4k-1369012",
-        "https://wallpaperaccess.com/download/europe-4k-1318341",
-        "https://wallpaperaccess.com/download/europe-4k-1379801"
-    ]
     enum SegmentValueTypes: Int, CaseIterable {
         case list = 0
         case grid
@@ -35,49 +29,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        table.dataSource = self
-        table.delegate = self
-        
-        table.register(UINib(nibName: "TestTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
-    
-//        MarketGoodsListModel.fetchMarketGoodsList(page: 1) { result in
-//            switch result {
-//            case .success(let data):
-//                debugPrint("👋: \(data)")
-//            case .failure(let error):
-//                debugPrint("❌:\(error.localizedDescription)")
-//            }
-//        }
-//        GoodsModel.fetchGoods(id: 1) { result in
-//            switch result {
-//            case .success(let data):
-//                debugPrint("👋: \(data)")
-//            case .failure(let error):
-//                debugPrint("❌:\(error.localizedDescription)")
-//            }
-//        }
-        
-        let testImage = UIImage(systemName: "pencil")!
-        let form = try? GoodsForm(registerPassword: "1234", title: "test-joons", descriptions: "test-joons", price: 10000, currency: "KRW", stock: 1, discountedPrice: nil, images: [testImage, testImage]).makeRegisterForm()
-        
-        GoodsModel.registerGoods(params: form!) { result in
-            switch result {
-            case .success(let data):
-                debugPrint("👋: \(data)")
-            case .failure(let error):
-                debugPrint("❌:\(error.localizedDescription)")
-            }
-        }
-        
-//        let editForm = try? GoodsForm(editPassword: "1234", title: "test-test-joons", descriptions: nil, price: nil, currency: nil, stock: nil, discountedPrice: nil, images: nil).makeEditForm()
-//        GoodsModel.editGoods(id: 67, params: editForm!) { result in
-//            switch result {
-//            case .success(let data):
-//                debugPrint("👋: \(data)")
-//            case .failure(let error):
-//                debugPrint("❌:\(error.localizedDescription)")
-//            }
-//        }
         setUpCollectionViewLayouts()
         setUpCollection()
         setUpSegment()
@@ -141,44 +92,6 @@ class ViewController: UIViewController {
         print("➕")
     }
 }
-extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 42
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as? TestTableViewCell else {
-            return UITableViewCell()
-        }
-        let token = ImageLoader.shared.load(urlString: self.testArray[indexPath.row % 3]) { result in
-            switch result {
-            case .failure(let error):
-                debugPrint("❌:\(error.localizedDescription)")
-            case .success(let image):
-                DispatchQueue.main.async {
-                    if let index: IndexPath = tableView.indexPath(for: cell) {
-                        if index.row == indexPath.row {
-                            cell.testImage.image = image
-                        }
-                    }
-                }
-            }
-        }
-        cell.onReuse = {
-            if let token = token {
-                ImageLoader.shared.cancelLoad(token)
-            }
-        }
-        return cell
-    }
-    
-    
-}
-extension ViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60
-    }
-}
 
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -189,5 +102,29 @@ extension ViewController: UICollectionViewDataSource {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! TestCollectionViewCell
         return cell
     }
-    
 }
+
+/* 이미지 불러올때 예시
+ guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as? TestTableViewCell else {
+     return UITableViewCell()
+ }
+ let token = ImageLoader.shared.load(urlString: self.testArray[indexPath.row % 3]) { result in
+     switch result {
+     case .failure(let error):
+         debugPrint("❌:\(error.localizedDescription)")
+     case .success(let image):
+         DispatchQueue.main.async {
+             if let index: IndexPath = tableView.indexPath(for: cell) {
+                 if index.row == indexPath.row {
+                     cell.testImage.image = image
+                 }
+             }
+         }
+     }
+ }
+ cell.onReuse = {
+     if let token = token {
+         ImageLoader.shared.cancelLoad(token)
+     }
+ }
+ */
