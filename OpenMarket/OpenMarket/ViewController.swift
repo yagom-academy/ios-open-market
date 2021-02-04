0a88248769b3427a1206f0aea53fdf841f74b4ca
yeon <jryoun0404@gmail.com>
2021-02-04 20:02:56 +0900

    Feat: ViewController에 segementControl 구현

--- OpenMarket/OpenMarket/ViewController.swift
+++ OpenMarket/OpenMarket/ViewController.swift
@@ -7,12 +7,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var itemCollectionView: UICollectionView!
+    @IBOutlet weak var itemTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpNavigationBar()
     }
-
-
+    
+    private func setUpNavigationBar() {
+        let titles = ["List", "Grid"]
+        let segmentControl: UISegmentedControl = UISegmentedControl(items: titles)
+        segmentControl.selectedSegmentTintColor = UIColor.systemBlue
+        segmentControl.backgroundColor = UIColor.white
+        segmentControl.selectedSegmentIndex = 0
+        for index in 0...titles.count - 1 {
+            segmentControl.setWidth(80, forSegmentAt: index)
+        }
+        segmentControl.sizeToFit()
+        segmentControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.systemBlue], for: .normal)
+        segmentControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.white], for: .selected)
+        segmentControl.addTarget(self, action: #selector(segmentChanged), for: .valueChanged)
+        segmentControl.sendActions(for: .valueChanged)
+        
+        navigationItem.titleView = segmentControl
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "+", style: .plain, target: self, action: #selector(moveToPostViewController))
+        navigationItem.rightBarButtonItem?.tintColor = UIColor.systemBlue
+    }
+    
+    @objc func segmentChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            itemTableView.isHidden = false
+            itemCollectionView.isHidden = true
+            DispatchQueue.main.async {
+                self.itemTableView.reloadData()
+            }
+        case 1:
+            itemTableView.isHidden = true
+            itemCollectionView.isHidden = false
+            DispatchQueue.main.async {
+                self.itemCollectionView.reloadData()
+            }
+        default:
+            break
+        }
+    }
+    
+    @objc func moveToPostViewController() {
+        
+    }
 }
 
