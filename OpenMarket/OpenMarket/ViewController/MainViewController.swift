2aa1947ab18ee21530b434a6eb390288ff5238df
SM <hcooch2ch3@gmail.com>
2021-02-05 01:44:48 +0900

    feat: MainViewController에 상품목록 요청기능 구현

--- OpenMarket/OpenMarket/ViewController/MainViewController.swift
+++ OpenMarket/OpenMarket/ViewController/MainViewController.swift
@@ -11,18 +11,57 @@ class MainViewController: UIViewController {
 
     @IBOutlet weak var tableView: UIView!
     @IBOutlet weak var collectionView: UIView!
+    var items: [Int: [Item]] = [:]
+    var itemsCount: Int {
+        return items.reduce(0) { currentCount, next in
+            currentCount + next.value.count
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let list = tableView.subviews.first?.subviews.first as? UITableView {
+            requestItems(page: 1) {
+                DispatchQueue.main.async {
+                    list.reloadData() // TODO: 코드 Depth 줄이기.
+                }
+            }
+        }
     }
     
     @IBAction func switchView(_ sender: UISegmentedControl){
         if sender.selectedSegmentIndex == 0 {
-            tableView.alpha = 1
-            collectionView.alpha = 0
+            tableView.isHidden = false
+            collectionView.isHidden = true
         } else {
-            tableView.alpha = 0
-            collectionView.alpha = 1
+            tableView.isHidden = true
+            collectionView.isHidden = false
         }
+    }
+}
+
+extension MainViewController {
+    func requestItems(page: Int, _ completionHandler: @escaping () -> Void) {
+        guard self.items[page] == nil else {
+            return
+        }
+        OpenMarketAPI.request(.loadItemList(page: page)) { (result: Result<ItemsToGet, Error>) in
+            switch result {
+            case .success(let data):
+                self.items.updateValue(data.items, forKey: data.page)
+                completionHandler()
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func getItem(_ row: Int) -> Item? {
+        let page = row / 20 + 1
+        let n = row % 20
+        if let itemsOfPage = items[page] {
+            return itemsOfPage[n]
+        }
+        return nil
     }
 }
