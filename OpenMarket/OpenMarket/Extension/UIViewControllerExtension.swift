fa62dc2d032d537dc9602b0fe4d6e16de506d519
wonhee <wonhee010@gmail.com>
2021-02-05 01:04:44 +0900

    refactor: delete unuse code

--- OpenMarket/OpenMarket/Extension/UIViewControllerExtension.swift
+++ OpenMarket/OpenMarket/Extension/UIViewControllerExtension.swift
@@ -10,15 +10,7 @@ import UIKit
 extension UIViewController {
     func showErrorAlert(with error
                             : Error, okHandler: ((UIAlertAction) -> Void)?) {
-        var errorMessage: String
-        if let openMarketError = error as? OpenMarketError {
-            errorMessage = openMarketError.localizedDescription
-        } else if let networkError = error as? NetworkError {
-            errorMessage = networkError.localizedDescription
-        } else {
-            errorMessage = OpenMarketError.unknown.localizedDescription
-        }
-        let alertController = UIAlertController(title: "오류", message: errorMessage, preferredStyle: .alert)
+        let alertController = UIAlertController(title: "오류", message: error.localizedDescription, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "확인", style: .default, handler: okHandler)
         let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
         alertController.addAction(okAction)
