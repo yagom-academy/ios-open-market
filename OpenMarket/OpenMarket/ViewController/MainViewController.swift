dbf3edd8567bc238866fe45790a0412e7978914b
sole <subuchappyv2@gmail.com>
2021-02-04 10:45:52 +0900

    feat: segmented control UI

---
+++ OpenMarket/OpenMarket/ViewController/MainViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MainViewController.swift
+//  OpenMarket
+//
+//  Created by sole on 2021/02/04.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UIView!
+    @IBOutlet weak var collectionView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()  
+    }
+    
+    @IBAction func switchView(_ sender: UISegmentedControl){
+        if sender.selectedSegmentIndex == 0 {
+            tableView.alpha = 1
+            collectionView.alpha = 0
+        } else {
+            tableView.alpha = 0
+            collectionView.alpha = 1
+        }
+    }
+}
