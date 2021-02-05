7072b39e5a5d6fdeabaeb1c8ede4e87c99041f56
taetae <taetae.dev@gmail.com>
2021-02-05 17:35:39 +0900

    feat: Grid 목록에서 텍스트가 뷰를 벗어나지 않도록 제약 추가

--- OpenMarket/OpenMarket/Cell/ProductGridViewCell.swift
+++ OpenMarket/OpenMarket/Cell/ProductGridViewCell.swift
@@ -38,31 +38,37 @@ class ProductGridViewCell: UICollectionViewCell {
         
         productNameLabel.translatesAutoresizingMaskIntoConstraints = false
         productNameLabel.font = .preferredFont(forTextStyle: .headline)
+        productNameLabel.textAlignment = .center
         productNameLabel.adjustsFontForContentSizeCategory = true
         productNameLabel.textColor = .black
         productNameLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
         productNameLabel.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+        productNameLabel.adjustsFontSizeToFitWidth = true
         
         productStockLabel.translatesAutoresizingMaskIntoConstraints = false
         productStockLabel.font = .preferredFont(forTextStyle: .body)
+        productStockLabel.textAlignment = .center
         productStockLabel.adjustsFontSizeToFitWidth = true
         productStockLabel.adjustsFontForContentSizeCategory = true
         productStockLabel.textColor = .gray
-        productStockLabel.textAlignment = .right
         productStockLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
         productStockLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        productStockLabel.adjustsFontSizeToFitWidth = true
         
         productPriceLabel.translatesAutoresizingMaskIntoConstraints = false
         productPriceLabel.font = .preferredFont(forTextStyle: .body)
+        productPriceLabel.textAlignment = .center
         productPriceLabel.adjustsFontForContentSizeCategory = true
         productPriceLabel.textColor = .gray
-        productPriceLabel.minimumScaleFactor = .leastNormalMagnitude
+        productPriceLabel.adjustsFontSizeToFitWidth = true
         
         productDiscountedPriceLabel.translatesAutoresizingMaskIntoConstraints = false
         productDiscountedPriceLabel.font = .preferredFont(forTextStyle: .body)
+        productDiscountedPriceLabel.textAlignment = .center
         productDiscountedPriceLabel.adjustsFontForContentSizeCategory = true
         productDiscountedPriceLabel.textColor = .gray
-        productDiscountedPriceLabel.minimumScaleFactor = .leastNormalMagnitude
+        productDiscountedPriceLabel.adjustsFontSizeToFitWidth = true
+
     }
     
     private func setUpConstraints() {
@@ -73,6 +79,15 @@ class ProductGridViewCell: UICollectionViewCell {
             productThumbnailImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
             productThumbnailImageView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.7),
             productThumbnailImageView.heightAnchor.constraint(equalTo: productThumbnailImageView.widthAnchor, multiplier: 1),
+            
+            productPriceLabel.leadingAnchor.constraint(equalTo: productThumbnailImageView.leadingAnchor),
+            productPriceLabel.trailingAnchor.constraint(equalTo: productThumbnailImageView.trailingAnchor),
+            
+            productNameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 5),
+            productNameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -5),
+            
+            productStockLabel.leadingAnchor.constraint(equalTo: productNameLabel.leadingAnchor),
+            productStockLabel.trailingAnchor.constraint(equalTo: productNameLabel.trailingAnchor),
 
             stackView.topAnchor.constraint(equalTo: productThumbnailImageView.bottomAnchor),
             stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
