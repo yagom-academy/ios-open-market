42a0a9eac8e5c310223157c4782f721ff837e5d6
jae57 <ggapsong@naver.com>
2021-02-05 12:48:31 +0900

    refactor: 네트워크 레이어 return 객체 Result type 으로 수정

--- OpenMarket/OpenMarket/NetworkAPI/NetworkLayer.swift
+++ OpenMarket/OpenMarket/NetworkAPI/NetworkLayer.swift
@@ -12,42 +12,54 @@ struct NetworkLayer {
     private let httpRequest = OpenMarketHTTPRequest()
     
     private init() {}
-    func requestItemList(page: Int, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+    func requestItemList(page: Int, completionHandler: @escaping (Result<ItemList, Error>) -> Void) {
         guard let urlRequest = httpRequest.buildItemList(page) else {
             return
         }
         request(urlRequest: urlRequest, modelType: ItemList.self, completionHandler: completionHandler)
     }
     
-    func requestRegistration(bodyData: ItemRegistrationRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+    func requestRegistration(bodyData: ItemRegistrationRequest, completionHandler: @escaping (Result<ItemSpecificationResponse, Error>) -> Void) {
         guard let urlRequest = httpRequest.buildItemRegistration(bodyData) else {
             return
         }
         request(urlRequest: urlRequest, modelType: ItemSpecificationResponse.self, completionHandler: completionHandler)
     }
     
-    func requestSpecification(id: Int, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+    func requestSpecification(id: Int, completionHandler: @escaping (Result<ItemSpecificationResponse, Error>) -> Void) {
         guard let urlRequest = httpRequest.buildItemSpecification(id) else {
             return
         }
         request(urlRequest: urlRequest, modelType: ItemSpecificationResponse.self, completionHandler: completionHandler)
     }
     
-    func requestModification(id: Int, bodyData: ItemModificationRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+    func requestModification(id: Int, bodyData: ItemModificationRequest, completionHandler: @escaping (Result<ItemSpecificationResponse, Error>) -> Void) {
         guard let urlRequest = httpRequest.buildItemModification(id, bodyData) else {
             return
         }
         request(urlRequest: urlRequest, modelType: ItemSpecificationResponse.self, completionHandler: completionHandler)
     }
     
-    func requestDeletion(id: Int, bodyData: ItemDeletionRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+    func requestDeletion(id: Int, bodyData: ItemDeletionRequest, completionHandler: @escaping (Result<ItemDeletionResponse, Error>) -> Void) {
         guard let urlRequest = httpRequest.buildItemDeletion(id, bodyData) else {
             return
         }
         request(urlRequest: urlRequest, modelType: ItemDeletionResponse.self, completionHandler: completionHandler)
     }
     
-    private func request<T: Decodable>(urlRequest: URLRequest, modelType: T.Type, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+    func requestImage(urlString: String, index: Int, completion: @escaping (UIImage, Int?) -> Void) {
+        guard let url = URL(string: urlString) else { return }
+        
+        DispatchQueue.global().async {
+            guard let data = try? Data(contentsOf: url),
+                  let image = UIImage(data: data) else { return }
+            DispatchQueue.main.async {
+                completion(image, index)
+            }
+        }
+    }
+    
+    private func request<T: Decodable>(urlRequest: URLRequest, modelType: T.Type, completionHandler: @escaping (Result<T, Error>) -> Void) {
         let session = URLSession.shared
         session.dataTask(with: urlRequest) { data, response, error in
             if let error = error {
@@ -58,9 +70,9 @@ struct NetworkLayer {
             if let data = data {
                 do {
                     let decodedData = try JSONDecoder().decode(modelType, from: data)
-                    completionHandler(data, response, error)
-                    print(decodedData)
+                    completionHandler(.success(decodedData))
                 } catch {
+                    completionHandler(.failure(error))
                     print(error)
                 }
             }
