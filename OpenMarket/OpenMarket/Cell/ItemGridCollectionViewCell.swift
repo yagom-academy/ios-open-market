fd8f576a4d9010258df0fac6b6f1c982537ef23c
jae57 <ggapsong@naver.com>
2021-02-05 12:23:04 +0900

    feat: 화면구현

---
+++ OpenMarket/OpenMarket/Cell/ItemGridCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ItemGridCollectionViewCell.swift
+//  OpenMarket
+//
+//  Created by 김지혜 on 2021/02/05.
+//
+
+import UIKit
+
+class ItemGridCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ OpenMarket/OpenMarket/Cell/ItemListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemListTableViewCell.swift
+//  OpenMarket
+//
+//  Created by 김지혜 on 2021/02/05.
+//
+
+import UIKit
+
+class ItemListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
