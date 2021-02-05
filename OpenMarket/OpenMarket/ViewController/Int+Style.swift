b0d46e723d2e6f09b4bcd502619635788b236baf
SM <hcooch2ch3@gmail.com>
2021-02-05 18:16:57 +0900

    feat: Int+Style 구현

--- OpenMarket/OpenMarket/ViewController/Int+Style.swift
+++ OpenMarket/OpenMarket/ViewController/Int+Style.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+extension Int {
+    func convertedToStringWithComma() -> String? {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter.string(from: NSNumber(value:self))
+    }
+}
