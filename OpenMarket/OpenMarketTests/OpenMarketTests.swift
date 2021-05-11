f25e2ef22aac0686b26efcdb17ba0b4a5880e491
inwoodev <dlsdn1207@gmail.com>
2021-05-12 00:05:01 +0900

    Test : 초기 테스트 구현

--- OpenMarket/OpenMarketTests/OpenMarketTests.swift
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -2,27 +2,41 @@
 //  OpenMarketTests.swift
 //  OpenMarketTests
 //
-//  Created by 황인우 on 2021/05/11.
+//  Created by Sunny, James on 2021/05/11.
 //
 
 import XCTest
 @testable import OpenMarket
 class OpenMarketTests: XCTestCase {
     private var sut_marketItemList: MarketItemList?
-    private var sut_marketItems: MarketItems?
     
-    override func setUpWithError() throws {
-        try super.setUpWithError()
-        guard let marketListAsset = NSDataAsset.init(name: "Items"),
-              let marketItemAsset = NSDataAsset.init(name: "Item") else { return }
-        sut_marketItemList = try JSONDecoder().decode(MarketItemList.self, from: marketListAsset.data)
-        sut_marketItems = try JSONDecoder().decode(MarketItems.self, from: marketItemAsset.data)
+    override func setUp() {
+        super.setUp()
+        guard let marketListAsset = NSDataAsset.init(name: "Items") else { return }
+        do {
+            sut_marketItemList = try JSONDecoder().decode(MarketItemList.self, from: marketListAsset.data)
 
+        } catch {
+            print(error.localizedDescription)
+        }
+        
     }
+    
     override func tearDownWithError() throws {
         try super.tearDownWithError()
-        sut_marketItems = nil
         sut_marketItemList = nil
     }
+    
+    func test_marketItemList_page() {
+        guard let marketItemList = sut_marketItemList else { return }
+        XCTAssertEqual(marketItemList.page, 2)
+    }
+    
+    func test_marketItemList_items_thumbnails() {
+        guard let marketItemList = sut_marketItemList else { return }
+        XCTAssertNil(marketItemList.items[0].thumbnails)
+        XCTAssertEqual(marketItemList.items[0].thumbnails[1], "aa")
+    }
+
 
 }
