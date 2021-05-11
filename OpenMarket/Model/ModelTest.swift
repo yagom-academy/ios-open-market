ca3791d326f9352e5ca4d11021786327c1609fd6
cjm <stepsw112@gmail.com>
2021-05-11 10:23:40 +0900

    Chore: 폴더 삭제

---
+++ OpenMarket/Model/ModelTest.swift
@@ -0,0 +1,32 @@
+//
+//  ModelTest.swift
+//  OpenMarketTests
+//
+//  Created by 최정민 on 2021/05/11.
+//
+
+import XCTest
+
+class ModelTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
