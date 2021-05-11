cca47e780645a1262af43aefe793c80c9dcc0430
Sookim18 <scstnghks@naver.com>
2021-05-11 11:16:56 +0900

    feat : UpdateArticle, CreateArticle 파일 추가 - 인코딩하는 데 필요한 모델 타입 구현

---
+++ OpenMarket/OpenMarket/CreateArticle.swift
@@ -0,0 +1,24 @@
+//
+//  CreateArticle.swift
+//  OpenMarket
+//
+//  Created by sookim on 2021/05/11.
+//
+
+import Foundation
+
+struct CreateArticle: Encodable {
+    let title: String
+    let descriptions: String
+    let price: Int
+    let currency: String
+    let stock: Int
+    let discountedPrice: Int?
+    let images: [String]
+    let password: String
+    
+    enum Codingkeys: String, CodingKey {
+        case title, descriptions, price, currency, stock, password, images
+        case discountedPrice = "discounted_price"
+    }
+}
