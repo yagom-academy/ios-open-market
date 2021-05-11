aa34038d833d2a60a7ccdc6c92454eae1366093f
Sookim18 <scstnghks@naver.com>
2021-05-11 11:13:44 +0900

    feat : EntireArticle, EssentialArticle 파일 추가 - 목록조회에 필요한 타입 구현

---
+++ OpenMarket/OpenMarket/EssentialArticle.swift
@@ -0,0 +1,25 @@
+//
+//  EssentialArticle.swift
+//  OpenMarket
+//
+//  Created by sookim on 2021/05/11.
+//
+
+import Foundation
+
+struct EssentialArticle: Codable {
+    let id: Int
+    let title: String
+    let price: Int
+    let currency: String
+    let stock: Int
+    let discountedPrice: Int?
+    let thumbnails: [String]
+    let registrationDate: Double
+    
+    enum CodingKeys : String, CodingKey {
+        case id, title, price, currency, stock, thumbnails
+        case discountedPrice = "discounted_price"
+        case registrationDate = "registration_date"
+    }
+}
