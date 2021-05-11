4b74a97be51d285400ce7f2133ed225e4508db31
inwoodev <dlsdn1207@gmail.com>
2021-05-12 00:00:47 +0900

    Feat : JSON 초기 모델 구현 - MarketItemList 구조체 - Items 구조체

--- OpenMarket/OpenMarket/MarketData.swift
+++ OpenMarket/OpenMarket/MarketData.swift
@@ -9,29 +9,58 @@ import Foundation
 
 //let url = "https://camp-open-market-2.herokuapp.com/"
 
-struct MarketItemList: Codable {
+struct MarketItemList: Decodable {
     let page: Int
-    let items: [MarketItems]
+    let items: [Items]
 }
 
-struct MarketItems: Codable {
-    let stock: Int
-    let registrationDate: Double
-    let currency: String
-    let title: String
-    let thumbnails: [String]
+struct Items: Decodable {
     let id: Int
-    let discountedPrice: Int?
+    let title: String
     let price: Int
+    let currency: String
+    let stock: Int
+    let descriptions: String
+    let discountedPrice: Int?
+    let thumbnails: [String]
+    let registrationDate: Double
     
     private enum CodingKeys: String, CodingKey {
-        case stock
-        case registrationDate = "registration_date"
-        case currency
-        case title
-        case thumbnails
         case id
-        case discountedPrice = "discounted_price"
+        case title
         case price
+        case currency
+        case stock
+        case descriptions
+        case discountedPrice = "discounted_price"
+        case thumbnails
+        case registrationDate = "registration_date"
     }
 }
+
+//struct MarketItem: Codable {
+//    let id: Int
+//    let title: String
+//    let descriptions: String
+//    let price: Int
+//    let currency: String
+//    let stock: Int
+//    let discountedPrice: Int?
+//    let thumbnails: [String]
+//    let images: [String]
+//    let registrationData: Double
+//    
+//    private enum CodingKeys: String, CodingKey {
+//        case id
+//        case title
+//        case descriptions
+//        case price
+//        case currency
+//        case stock
+//        case discountedPrice = "discounted_price"
+//        case thumbnails
+//        case images
+//        case registrationData = "registration_date"
+//    }
+//}
+
