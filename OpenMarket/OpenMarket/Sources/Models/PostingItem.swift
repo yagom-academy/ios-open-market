66396cf797cafd94bff65a317092415bf36bc933
Neph <tony3322@naver.com>
2021-05-12 15:20:39 +0900

    [Feat] #4 PostintItem의 textFields, fileFields 연산 프로퍼티 구현 및 descriptions 오타 수정

--- OpenMarket/OpenMarket/Sources/Models/PostingItem.swift
+++ OpenMarket/OpenMarket/Sources/Models/PostingItem.swift
@@ -8,21 +8,37 @@ import Foundation
 
 struct PostingItem: Encodable {
     let title: String
-    let description: String
+    let descriptions: String
     let price: Int
     let currency: String
     let stock: Int
     let discountedPrice: Int?
     let images: [Data]
     let password: String
+
     enum CodingKeys: String, CodingKey {
-        case title, description, price, stock, images, password
+        case title, descriptions, price, stock, images, password
         case discountedPrice = "discounted_price"
     }
+
     var textFields: [String: String?] {
-        return ["":""]
+        let fields: [String: String?] = [
+            "title": title,
+            "descriptions": descriptions,
+            "price": price.description,
+            "currency": currency,
+            "stock": stock.description,
+            "discountedPrice": discountedPrice?.description,
+            "password": password
+        ]
+
+        return fields
     }
+
     var fileFields: [Data] {
-        return [Data()]
+        var fields: [Data] = []
+        fields.append(contentsOf: images)
+
+        return fields
     }
 }
