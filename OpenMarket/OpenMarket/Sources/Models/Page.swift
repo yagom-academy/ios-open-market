a34e2983354128a10a0177ed996e929aab385030
duckbok <duckbok22@gmail.com>
2021-05-12 14:58:23 +0900

    [Feat] #3 Page 타입 구현

--- OpenMarket/OpenMarket/Sources/Models/Page.swift
+++ OpenMarket/OpenMarket/Sources/Models/Page.swift
@@ -5,4 +5,20 @@
 //  Created by duckbok on 2021/05/12.
 //
 
-import Foundation
+import UIKit
+
+struct Page {
+    let page: Int
+    let items: [Page.Item]
+
+    struct Item {
+        let id: Int
+        let title: String
+        let price: Int
+        let currency: String
+        let stock: Int
+        let discountedPrice: Int?
+        let thumbnails: [UIImage]
+        let registrationDate: Date
+    }
+}
