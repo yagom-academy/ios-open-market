5d3ef557a1809390334796926d87a7065cbc708b
Ian <isnn0103@gmail.com>
2021-05-13 20:38:42 +0900

    [Feat] #3 item 구초제 생성

--- OpenMarket/OpenMarket/Model/Item.swift
+++ OpenMarket/OpenMarket/Model/Item.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct item: Codable {
+    var id: Int
+    var title: String
+    var descriptions: String
+    var price: Int
+    var currency: String
+    var stock: Int
+    var thumnails: [String]
+    var images: [String]
+    var registration_date: Double
+}
