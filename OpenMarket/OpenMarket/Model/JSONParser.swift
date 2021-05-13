e91b0d594e7cfbf19891724062135dd46dbf4f60
dacodaco <imochi@naver.com>
2021-05-13 21:16:41 +0900

    [Feat] #3 JSONParser 타입 구현

--- OpenMarket/OpenMarket/Model/JSONParser.swift
+++ OpenMarket/OpenMarket/Model/JSONParser.swift
@@ -5,4 +5,16 @@
 //  Created by 김찬우 on 2021/05/13.
 //
 
-import Foundation
+import UIKit
+
+struct JSONParser<T: Codable> {
+    var convertedData: T?
+    
+    mutating func parse(assetName: String) {
+        let decoder = JSONDecoder()
+        
+        guard let assetData = NSDataAsset(name: assetName) else { return }
+        guard let convertedData = try? decoder.decode(T.self, from: assetData.data) else { return }
+        self.convertedData = convertedData
+    }
+}
