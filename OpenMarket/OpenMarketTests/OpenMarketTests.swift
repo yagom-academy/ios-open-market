76cbbd61e2060292dbc1f9e6b03a9f658ab9d37c
babywalnut <anfuza@naver.com>
2021-05-13 22:39:38 +0900

    feat : 파일구조개선 및 테스트코드 수정 1. ViewController.swift : test코드 주석처리 2. OpenMarketTests.swift : 유닛테스트 기능단위로 분리 	- NSDataAsset데이터 추출 	- 추출된 데이터 Decoding 	- Decoded 데이터 확인 3. Models 폴더 : 파싱에 필요한 모델정의한 파일들 경로이동 	Mdoels 		- EntireArticle.swift 		- EssentialArticle.swift 		- DetailArticle.swift 		- CreateArticle.swift 		- UpdateArticle.swift 		- DeleteArticle.swift

--- OpenMarket/OpenMarketTests/OpenMarketTests.swift
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -9,45 +9,66 @@ import XCTest
 @testable import OpenMarket
 
 class OpenMarketTests: XCTestCase {
-
-    func testDecodeEntireArticleItems() {
+    func extractData(_ item: String) -> NSDataAsset? {
+        guard let itemData = NSDataAsset(name: item) else {
+            return nil
+        }
+        return itemData
+    }
+    
+    func decodeExtractedData<T: Decodable>(_ object: T.Type, of data: Data) -> T? {
         let decoder = JSONDecoder()
-        guard let itemData = NSDataAsset(name: "Items") else { XCTFail(); return }
         
         do {
-            let contents = try decoder.decode(EntireArticle.self, from: itemData.data)
-            XCTAssertEqual(contents.page, 1)
+            let contents = try decoder.decode(object, from: data)
+            return contents
         } catch {
-            XCTFail()
+            return nil
         }
     }
     
-    func testDecodeDetailArticleDescriptions() {
-        let decoder = JSONDecoder()
-        guard let itemData = NSDataAsset(name: "Item") else { XCTFail(); return }
+    func test_Mock_Item데이터추출() {
+        XCTAssertNil(extractData("Itemss"))
+    }
+    
+    func test_Mock_Items데이터추출() {
+        XCTAssertNil(extractData("Items"))
+    }
+    
+    func test_Mock_Items디코딩() {
+        let extractedData = extractData("Items")
+        XCTAssertNil(decodeExtractedData(EntireArticle.self, of: extractedData!.data))
+    }
+    
+    func test_Mock_Item디코딩_withEsstialArticle() {
+        let extractedData = extractData("Item")
+        XCTAssertNil(decodeExtractedData(EssentialArticle.self, of: extractedData!.data))
+    }
+    
+    func test_Mock_Item디코딩_withDetailArticle() {
+        let extractedData = extractData("Item")
+        XCTAssertNil(decodeExtractedData(DetailArticle.self, of: extractedData!.data))
+    }
+    
+    func test_추출된데이터확인() {
+        let itemData = extractData("Item")
         
-        do {
-            let contents = try decoder.decode(DetailArticle.self, from: itemData.data)
-            XCTAssertEqual(contents.id, 1)
-            XCTAssertEqual(contents.title, "abc")
-            XCTAssertEqual(contents.price, 123)
-            XCTAssertEqual(contents.descriptions, "abc")
-            XCTAssertEqual(contents.currency, "abc")
-            XCTAssertEqual(contents.stock, 123)
-            XCTAssertEqual(contents.images, [
-                "https://camp-open-market.s3.ap-northeast-2.amazonaws.com/images/1-1.png",
-                "https://camp-open-market.s3.ap-northeast-2.amazonaws.com/images/1-2.png"
-            ])
-            XCTAssertEqual(contents.discountedPrice, 123)
-            XCTAssertEqual(contents.thumbnails, [
-                "https://camp-open-market.s3.ap-northeast-2.amazonaws.com/thumbnails/1-1.png",
-                "https://camp-open-market.s3.ap-northeast-2.amazonaws.com/thumbnails/1-2.png"
-            ])
-            XCTAssertEqual(contents.registrationDate, 123.12)
-            
-        } catch {
-            XCTFail()
-        }
+        guard let contents = decodeExtractedData(DetailArticle.self, of: itemData!.data) else { XCTFail(); return }
+        XCTAssertEqual(contents.id, 1)
+        XCTAssertEqual(contents.title, "abc")
+        XCTAssertEqual(contents.price, 123)
+        XCTAssertEqual(contents.descriptions, "abc")
+        XCTAssertEqual(contents.currency, "KRW")
+        XCTAssertEqual(contents.stock, 123)
+        XCTAssertEqual(contents.images, [
+            "https://camp-open-market.s3.ap-northeast-2.amazonaws.com/images/1-.png",
+            "https://camp-open-market.s3.ap-northeast-2.amazonaws.com/images/1-2.png"
+        ])
+        XCTAssertEqual(contents.discountedPrice, 123)
+        XCTAssertEqual(contents.thumbnails, [
+            "https://camp-open-market.s3.ap-northeast-2.amazonaws.com/thumbnails/1-.png",
+            "https://camp-open-market.s3.ap-northeast-2.amazonaws.com/thumbnails/1-.png"
+        ])
+        XCTAssertEqual(contents.registrationDate, 123.12)
     }
 }
-
