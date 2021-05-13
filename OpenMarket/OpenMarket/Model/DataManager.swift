f74f0f9a988314d32e0b7447c8c47b0c6c456b08
eunseo5355 <eunseo5355@naver.com>
2021-05-13 15:04:47 +0900

    ✨ [Feat] 서버에 데이터 요청하는 메소드 구현

--- OpenMarket/OpenMarket/Model/DataManager.swift
+++ OpenMarket/OpenMarket/Model/DataManager.swift
@@ -7,3 +7,36 @@
 
 import Foundation
 
+struct DataManager {
+    
+    func requestItemList(url: String) {
+        let session = URLSession.shared
+        guard let requestURL = URL(string: url) else { return }
+        
+        session.dataTask(with: requestURL) { data, response, error in
+            guard error == nil else {
+                print(error!.localizedDescription)
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse,
+                  (200...299).contains(response.statusCode) else {
+                print("response")
+                return
+            }
+            
+            if let mimeType = response.mimeType, mimeType == "application/json",
+               let data = data {
+                do {
+                    let itemListResponse = try JSONDecoder().decode(ItemList.self, from: data)
+                    print(itemListResponse)
+                } catch(let error) {
+                    print(error.localizedDescription)
+                }
+            }
+        
+        }.resume()
+    }
+}
+
+
