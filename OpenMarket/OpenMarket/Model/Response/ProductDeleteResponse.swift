d2bfb343da83b11005fd9a06e8d357b4e5e2e354
kane-young <lyw2100@naver.com>
2021-05-13 23:59:04 +0900

    Refactor : 프로토콜 채택 안된 struct 재정의

--- OpenMarket/OpenMarket/Model/Response/ProductDeleteResponse.swift
+++ OpenMarket/OpenMarket/Model/Response/ProductDeleteResponse.swift
@@ -7,20 +7,20 @@
 
 import Foundation
 
-struct ProductDeleteResponse: Decodable {
+struct ProductDeleteResponse: InfoSearchable, Detailable {
   let id: Int
   let title: String
-  let descriptions: String
+  let description: String
   let price: Int
   let currency: String
   let stock: Int
   let discountedPrice: Int?
   let thumbnails: [String]
-  let images: [String]
-  let registrationDate: Int
+  let images: [Data]
+  let registrationDate: Double
   
   enum CodingKeys: String, CodingKey {
-    case id, title, descriptions, price, currency, stock, thumbnails, images
+    case id, title, description, price, currency, stock, thumbnails, images
     case discountedPrice = "discounted_price"
     case registrationDate = "registration_date"
   }
