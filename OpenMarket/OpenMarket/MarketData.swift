30ad6879cb48567c9adcffdbfd3c82307cc11c19
inwoodev <dlsdn1207@gmail.com>
2021-05-13 16:08:06 +0900

    Refactor : JSON모델 수정
    여러 JSON파일에 호환되도록 수정
    - descriptions, discounted Price 옵셔널 타입으로 설정
    - 불필요한 코드 제거

--- OpenMarket/OpenMarket/MarketData.swift
+++ OpenMarket/OpenMarket/MarketData.swift
@@ -7,60 +7,26 @@
 
 import Foundation
 
-//let url = "https://camp-open-market-2.herokuapp.com/"
-
-struct MarketItemList: Decodable {
+struct MarketItemList: Codable {
     let page: Int
-    let items: [Items]
+    let items: [Item]
 }
 
-struct Items: Decodable {
+struct Item: Codable {
     let id: Int
     let title: String
     let price: Int
     let currency: String
     let stock: Int
-//    let descriptions: String
-//    let discountedPrice: Int?
+    let descriptions: String?
+    let discountedPrice: Int?
     let thumbnails: [String]
+    let images: [String]?
     let registrationDate: Double
     
     private enum CodingKeys: String, CodingKey {
-        case id
-        case title
-        case price
-        case currency
-        case stock
-//        case descriptions
-//        case discountedPrice = "discounted_price"
-        case thumbnails
+        case id, title, price, currency, stock, descriptions, thumbnails, images
+        case discountedPrice = "discounted_price"
         case registrationDate = "registration_date"
     }
 }
-
-//struct MarketItem: Codable {
-//    let id: Int
-//    let title: String
-//    let descriptions: String
-//    let price: Int
-//    let currency: String
-//    let stock: Int
-//    let discountedPrice: Int?
-//    let thumbnails: [String]
-//    let images: [String]
-//    let registrationData: Double
-//    
-//    private enum CodingKeys: String, CodingKey {
-//        case id
-//        case title
-//        case descriptions
-//        case price
-//        case currency
-//        case stock
-//        case discountedPrice = "discounted_price"
-//        case thumbnails
-//        case images
-//        case registrationData = "registration_date"
-//    }
-//}
-
