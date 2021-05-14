c7d2f36f07c41b889e2df6dd4385f0e1de393ee6
helloworldjay <sj.seungjin.baek@gmail.com>
2021-05-14 15:51:37 +0900

    [#3]feat: implement ItemListFetcther

--- OpenMarket/OpenMarket/APINetwork/ItemListFetcher.swift
+++ OpenMarket/OpenMarket/APINetwork/ItemListFetcher.swift
@@ -7,6 +7,24 @@
 
 import Foundation
 
-class ItemGet {
+class ItemListFetcher {
     
+    private var page = 1
+    var itemList: ItemListVO?
+    
+    func fetchItemList() throws {
+        let url = OpenMarketAPI.baseURL + OpenMarketAPIPathByDescription.itemListSearch.description + "\(self.page)"
+        guard let apiURI = URL(string: url) else { throw APIError.NotFound404Error }
+        
+        let semaphore: DispatchSemaphore = DispatchSemaphore(value: 0)
+        let task = URLSession.shared.dataTask(with: apiURI, completionHandler: { data, response, error in
+            guard let data = data, error == nil else { return }
+            var result: ItemListVO?
+            result = try? JSONDecoder().decode(ItemListVO.self, from: data)
+            self.itemList = result
+            semaphore.signal()
+        })
+        task.resume()
+        semaphore.wait()
+    }
 }
