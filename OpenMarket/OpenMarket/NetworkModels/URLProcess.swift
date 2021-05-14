3108caed627e9ca4735f05a0496e8d39ec091502
Sookim18 <scstnghks@naver.com>
2021-05-14 15:59:05 +0900

    test : URLProcess (URL관련 처리 클래스 추가)임시

---
+++ OpenMarket/OpenMarket/NetworkModels/URLProcess.swift
@@ -0,0 +1,49 @@
+//
+//  URLProcess.swift
+//  OpenMarket
+//
+//  Created by sookim on 2021/05/14.
+//
+
+import Foundation
+
+class URLProcess {
+    
+    let baseURL:URL? = URL(string: "https://camp-open-market-2.herokuapp.com/")
+    
+    
+    func setURLPath(methodType: String) -> URL? {
+        
+        switch methodType {
+        case HttpMethodType.get.stringMethod :
+            guard let relativeURL = URL(string: "items/1", relativeTo: baseURL) else { return nil }
+            return relativeURL
+        case HttpMethodType.post.stringMethod :
+            guard let relativeURL = URL(string: "item", relativeTo: baseURL) else { return nil }
+            return relativeURL
+        case HttpMethodType.patch.stringMethod :
+            guard let relativeURL = URL(string: "item/112", relativeTo: baseURL) else { return nil }
+            return relativeURL
+        case HttpMethodType.delete.stringMethod :
+            guard let relativeURL = URL(string: "item/112", relativeTo: baseURL) else { return nil }
+            return relativeURL
+        default:
+            return nil
+        }
+    }
+    
+    func setURLRequest(requestMethodType: String, boundary: String) -> URLRequest? {
+        // URLRequest 객체를 정의
+        guard let url = setURLPath(methodType: requestMethodType) else {
+            return nil
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = requestMethodType
+        // HTTP 메시지 헤더
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        
+        return request
+    }
+    
+}
--- OpenMarket/OpenMarket/ViewController.swift
+++ OpenMarket/OpenMarket/ViewController.swift
@@ -13,24 +13,15 @@ class ViewController: UIViewController {
         
         let getArticle = GetEssentialArticle()
         let postArticle = PostCreateArticle()
+        let urlProcess = URLProcess()
         
         getArticle.getParsing()
         
-        let profileImage:UIImage = UIImage(named:"github")!
-        let imageData:Data = profileImage.pngData()!
-        
-        
-        let url = URL(string: "https://camp-open-market.s3.ap-northeast-2.amazonaws.com/thumbnails/C0392506-1D33-4755-8509-C09DC37822AA.png")
-        var image: UIImage?
-        
-        let data = try? Data(contentsOf: url!)
+        let pngImage = convertDataToAssetImage(imageName: "github")
         
         
         // UUID로 고유 식별자 추출
         let yagomBoundary = "Boundary-\(UUID().uuidString)"
-        
-        // URL 객체 정의
-        guard let yagomURL = URL(string: "https://camp-open-market-2.herokuapp.com/item") else { print("yagomURL에러"); return }
         
         // Text 데이터
         var httpBody = Data()
@@ -41,18 +32,29 @@ class ViewController: UIViewController {
         httpBody.append(postArticle.convertFormField(name: "currency", value: "USD", boundary: yagomBoundary).data(using: .utf8)!)
         httpBody.append(postArticle.convertFormField(name: "stock", value: "214", boundary: yagomBoundary).data(using: .utf8)!)
         httpBody.append(postArticle.convertFormField(name: "discounted_price", value: "2420", boundary: yagomBoundary).data(using: .utf8)!)
-        httpBody.append(postArticle.convertFileData(fieldName: "images[]", fileName: "github.png", mimeType: "image/png", fileData: imageData, boundary: yagomBoundary))
+        httpBody.append(postArticle.convertFileData(fieldName: "images[]", fileName: "github.png", mimeType: "image/png", fileData: pngImage, boundary: yagomBoundary))
         httpBody.append(postArticle.convertFormField(name: "password", value: "1234", boundary: yagomBoundary).data(using: .utf8)!)
         httpBody.append("--\(yagomBoundary)--".data(using: .utf8)!)
         
         
-        // URLRequest 객체를 정의
-        var request = URLRequest(url: yagomURL)
-        request.httpMethod = "POST"
-        
-        // HTTP 메시지 헤더
-        request.setValue("multipart/form-data; boundary=\(yagomBoundary)", forHTTPHeaderField: "Content-Type")
-        
+        guard let request = urlProcess.setURLRequest(requestMethodType: "POST", boundary: yagomBoundary) else { return }
+
         postArticle.postData(request: request, requestBody: httpBody)
+    }
+    
+    func convertDataToAssetImage(imageName: String) -> Data {
+        let profileImage:UIImage = UIImage(named: imageName)!
+        let imageData:Data = profileImage.pngData()!
+        
+        return imageData
+    }
+    
+    func convertDataToURLImage(imageURL: String) -> Data {
+        let url = URL(string: imageURL)
+        var image: UIImage?
+        
+        let data = try? Data(contentsOf: url!)
+        
+        return data!
     }
 }
