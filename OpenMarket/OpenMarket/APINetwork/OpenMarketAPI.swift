15496a938b050e43ce88ab9005267a267cd1692e
helloworldjay <sj.seungjin.baek@gmail.com>
2021-05-14 15:52:34 +0900

    [#3]feat: implement OpenMarketAPI

--- OpenMarket/OpenMarket/APINetwork/OpenMarketAPI.swift
+++ OpenMarket/OpenMarket/APINetwork/OpenMarketAPI.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+enum OpenMarketAPI {
+    static let baseURL = "https://camp-open-market-2.herokuapp.com/"
+    
+}
+
+enum HTTPMethod {
+    case get
+    case post
+    case patch
+    case delete
+    
+    var description: String {
+        switch self {
+        case .get : return "GET"
+        case .post : return "POST"
+        case .patch : return "PATCH"
+        case .delete : return "DELETE"
+        }
+    }
+}
+
+enum OpenMarketAPIPathByDescription: CustomStringConvertible {
+
+    case itemListSearch
+    case itemRegister
+    case itemSearch
+    case itemEdit
+    case itemDeletion
+
+    var description: String {
+        switch self {
+        case .itemListSearch:
+            return "items/"
+        default:
+            return "item/"
+        }
+    }
+}
