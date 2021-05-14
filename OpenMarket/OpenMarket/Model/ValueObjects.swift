359a99cb6b425fd5e777b34f2327c3cf18631645
helloworldjay <sj.seungjin.baek@gmail.com>
2021-05-14 15:50:48 +0900

    [#3]feat: implement ValueObject

--- OpenMarket/OpenMarket/Model/ValueObjects.swift
+++ OpenMarket/OpenMarket/Model/ValueObjects.swift
@@ -13,12 +13,23 @@ struct ItemVO {
     var thumnails, images: [String]?
     var registrationDate: Double?
 }
-    
+
 extension ItemVO: Codable {
     enum CodingKeys: String, CodingKey {
         case id, price, stock, title, currency, thumnails, images
         case description = "descriptions"
         case discountedPrice = "discounted_price"
         case registrationDate = "registration_date"
+    }
+}
+
+struct ItemListVO {
+    var page: UInt?
+    var itemList: [ItemVO]? // 타입 고민
+}
+
+extension ItemListVO: Codable {
+    enum CodingKeys: String, CodingKey {
+        case itemList = "items"
     }
 }
