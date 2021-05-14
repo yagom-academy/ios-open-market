239e3b6ba4158ebfc5bd62c33c9cd946622a3f07
eunseo5355 <eunseo5355@naver.com>
2021-05-14 18:34:35 +0900

    ✅ [Test] 테스트 코드 삭제

--- OpenMarket/OpenMarketTests/OpenMarketTests.swift
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -9,11 +9,4 @@ import XCTest
 @testable import OpenMarket
 
 class OpenMarketTests: XCTestCase {
-
-    func testRequestItemList_itemURL을주었을때_data를print한다() {
-        let dataManager = DataManager()
-        
-        dataManager.requestItemList(url: "https://camp-open-market-2.herokuapp.com/items/1")
-    }
-
 }
