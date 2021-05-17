d69c9ad875139777932b7d91fe6544e92439b64d
Ian <isnn0103@gmail.com>
2021-05-17 17:14:58 +0900

    [Test] #3 Item JSON parsing Test 구현

--- OpenMarket/OpenMarketTests/OpenMarketTests.swift
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -18,14 +18,20 @@ class OpenMarketTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func test_ItemJSON데이터가_JSONParser에_Decoding_돼서_id의_값이_1인지확인해보는_함수() {
+    func test_ItemJSON데이터가_JSONParser에_Decoding_돼서_thumbnails의_값을_확인하는함수() {
         var jsonParserItem = JSONParser<Item>()
         
         jsonParserItem.parse(assetName: "Item")
         
-        guard let data: Item = jsonParserItem.convertedData else { return }
+        guard let data: Item = jsonParserItem.convertedData else {
+            XCTFail()
+            return
+        }
         
-        XCTAssertEqual(data.id, 1)
+        XCTAssertEqual(data.thumbnails, [
+            "https://camp-open-market.s3.ap-northeast-2.amazonaws.com/thumbnails/1-1.png",
+            "https://camp-open-market.s3.ap-northeast-2.amazonaws.com/thumbnails/1-2.png"
+        ])
     }
     
     func test_ItemsJSON데이터가_JSONParser에_Decoding_돼서_page의_값이_1인지확인해보는_함수() {
