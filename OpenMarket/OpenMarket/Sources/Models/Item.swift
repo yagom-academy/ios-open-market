984b96bc87185b6b71ad9536da5197c7b210e8a2
duckbok <duckbok22@gmail.com>
2021-05-18 10:45:17 +0900

    [Refactor] #3 모델타입에 Decodable 채택

--- OpenMarket/OpenMarket/Sources/Models/Item.swift
+++ OpenMarket/OpenMarket/Sources/Models/Item.swift
@@ -5,9 +5,9 @@
 //  Created by duckbok on 2021/05/12.
 //
 
-import UIKit
+import Foundation
 
-struct Item {
+struct Item: Decodable {
     let id: Int
     let title: String
     let descriptions: String
@@ -15,7 +15,13 @@ struct Item {
     let currency: String
     let stock: Int
     let discountedPrice: Int?
-    let thumbnails: [UIImage]
-    let images: [UIImage]
-    let registrationData: Date
+    let thumbnails: [String]
+    let images: [String]
+    let registrationDate: TimeInterval
+
+    private enum CodingKeys: String, CodingKey {
+        case id, title, descriptions, price, currency, stock, thumbnails, images
+        case discountedPrice = "discounted_price"
+        case registrationDate = "registration_date"
+    }
 }
