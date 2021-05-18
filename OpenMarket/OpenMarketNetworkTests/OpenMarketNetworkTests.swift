ec79b0dd951a9282d95bfef9ba3580f395715343
inwoodev <dlsdn1207@gmail.com>
2021-05-18 21:41:47 +0900

    Test : URLProtocol을 활용한 테스트
    문제점: test가 실패하는데 이유를 파악하지 못함

---
+++ OpenMarket/OpenMarketNetworkTests/OpenMarketNetworkTests.swift
@@ -0,0 +1,81 @@
+//
+//  OpenMarketNetworkTests.swift
+//  OpenMarketNetworkTests
+//
+//  Created by 황인우 on 2021/05/18.
+//
+
+import XCTest
+@testable import OpenMarket
+// internal으로 선언되면 접근 원래는 불가능
+// testable을 통해 접근가능
+class OpenMarketNetworkTests: XCTestCase {
+    
+    var sut_openMarketAPIProvider: OpenMarketAPIProvider!
+    var sut_openMarketItemURL = URL(string: "https://camp-open-market-2.herokuapp.com/items/1")!
+    var sut_openMarketListItemURL = URL(string: "https://camp-open-market-2.herokuapp.com/item")!
+    var sut_marketItemList: MarketItemList!
+    var sut_detailedItemInformation: DetailedItemInformation!
+    
+    override func setUpWithError() throws {
+        setUpProtocolClassesConfiguration()
+        try super.setUpWithError()
+        
+    }
+    
+    override func tearDownWithError() throws {
+        sut_openMarketAPIProvider = nil
+        sut_marketItemList = nil
+        try super.tearDownWithError()
+    }
+    
+    func setUpProtocolClassesConfiguration() {
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [MockURLProtocol.self]
+        let urlSession = URLSession.init(configuration: configuration)
+        sut_openMarketAPIProvider = OpenMarketAPIProvider(urlSession: urlSession)
+    }
+    // MARK: Test Successful Response
+    func test_OpenMarketList_SuccessfulResponse() {
+        let expectation = XCTestExpectation()
+        // given
+        
+        let url = sut_openMarketListItemURL
+        guard let itemListRequest = NSDataAsset.init(name: "Items") else { return }
+        let requestData = try? JSONDecoder().decode(MarketItemList.self, from: itemListRequest.data)
+        MockURLProtocol.requestHandler = { request in
+            
+            guard let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil) else {
+                print("no response")
+                return (HTTPURLResponse(), Data())
+            }
+            
+            return (response, itemListRequest.data)
+        }
+        // when
+        sut_openMarketAPIProvider.fetchItemListData { result in
+            switch result {
+        // then
+            case .success(let responseData):
+                XCTAssertEqual(requestData, responseData)
+            case .failure(let error):
+                XCTFail("error was not expected: \(error)")
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1.0)
+    }
+}
+
+extension MarketItemList: Equatable {
+    public static func == (lhs: MarketItemList, rhs: MarketItemList) -> Bool {
+        return lhs.items[0].id == rhs.items[0].id
+    }
+}
+
+extension DetailedItemInformation: Equatable {
+    public static func == (lhs: DetailedItemInformation, rhs: DetailedItemInformation) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+}
