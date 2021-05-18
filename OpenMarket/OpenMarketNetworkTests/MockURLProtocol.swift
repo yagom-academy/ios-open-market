a144084903c5dc05be5aeb5bc4df962f4c2c3051
inwoodev <dlsdn1207@gmail.com>
2021-05-18 21:03:02 +0900

    Feat : MockURLProtocol 클래스 구현
    - 네트워크 동작 테스트용 추상 클래스 구현

--- OpenMarket/OpenMarketNetworkTests/MockURLProtocol.swift
+++ OpenMarket/OpenMarketNetworkTests/MockURLProtocol.swift
@@ -9,6 +9,8 @@ import Foundation
 
 class MockURLProtocol: URLProtocol {
     
+    static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data?))?
+    
     override class func canInit(with request: URLRequest) -> Bool {
         return true
     }
@@ -17,19 +19,23 @@ class MockURLProtocol: URLProtocol {
         return request
     }
     
-    static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data?))?
-    
     override func startLoading() {
         guard let handler = MockURLProtocol.requestHandler else {
             fatalError("Handler is unavailable.")
         }
+    
+        do {
+            let (response, data) = try handler(request)
+        client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
         
-//        do {
-//            let (response, data) = try handler(request)
-//        }
-//
-//        client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
-//        
+        guard let data = data else { return }
+        
+        client?.urlProtocol(self, didLoad: data)
+        client?.urlProtocolDidFinishLoading(self)
+        
+        } catch {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
         
         
     }
