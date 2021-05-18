3b4308b47262bb147f0c841447e4029e72c50a51
helloworldjay <sj.seungjin.baek@gmail.com>
2021-05-18 21:38:35 +0900

    [#3]feat: implement codes for test

--- OpenMarket/OpenMarketTests/OpenMarketTests.swift
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -1,32 +1,42 @@
-//
-//  OpenMarketTests.swift
-//  OpenMarketTests
-//
-//  Created by Seungjin Baek on 2021/05/18.
-//
-
 import XCTest
+@testable import OpenMarket
 
 class OpenMarketTests: XCTestCase {
-
+    
+    func readLocalFile(forName name: String) -> Data? {
+        do {
+            if let bundlePath = Bundle.main.path(forResource: name,
+                                                 ofType: "json"),
+               let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
+                return jsonData
+            }
+        } catch {
+            print(error)
+        }
+        
+        return nil
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func test_JSONParser_for_Item() throws {
+        guard let localJSONData = readLocalFile(forName: "Item") else { throw APIError.JSONParseError }
+        let jsonParser = JSONParser()
+        guard let parsedItemData: Item = try? jsonParser.parseJSONDataToValueObject(with: localJSONData) else { throw APIError.JSONParseError }
+        XCTAssertNotNil(parsedItemData)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_JSONParser_for_ItemList() throws {
+        guard let localJSONData = readLocalFile(forName: "Items") else { throw APIError.JSONParseError }
+        let jsonParser = JSONParser()
+        guard let parsedItemListData: ItemList = try? jsonParser.parseJSONDataToValueObject(with: localJSONData) else { throw APIError.JSONParseError }
+        XCTAssertNotNil(parsedItemListData)
     }
-
 }
