f5bc005e58f0457470a72832f61d65b58c2a2c74
Neph <tony3322@naver.com>
2021-05-18 21:23:22 +0900

    [Test] #2 APIModel unit test

--- OpenMarket/OpenMarketTests/Sources/APIModelTests.swift
+++ OpenMarket/OpenMarketTests/Sources/APIModelTests.swift
@@ -9,26 +9,90 @@ import XCTest
 @testable import OpenMarket
 
 class APIModelTests: XCTestCase {
-    let mockPageData: Data = NSDataAsset(name: "Page")!.data
-    let mockItemData: Data = NSDataAsset(name: "Item")!.data
-
+    var dummyPostingItem: PostingItem!
+    var dummyPatchingItem: PatchingItem!
+    
     override func setUpWithError() throws {
-        try super.setUpWithError()
-    }
-
-    func test_RespondedPage가_잘_Decode된다() {
-        XCTAssertNotNil(try? JSONDecoder().decode(Page.self, from: mockPageData))
-    }
-
-    func test_RespondedItem가_잘_Decode된다() {
-        XCTAssertNotNil(try? JSONDecoder().decode(Item.self, from: mockItemData))
-    }
-    
-    func test_formData의_textFields() {
+        dummyPostingItem = PostingItem(title: "끔찍한 디버깅",
+                                       descriptions: "끔찍한 디버깅입니다.",
+                                       price: 1000,
+                                       currency: "KRW",
+                                       stock: 10,
+                                       discountedPrice: nil,
+                                       images: [UIImage(systemName: "zzz")!.jpegData(compressionQuality: 1)!],
+                                       password: "1234")
         
+        dummyPatchingItem = PatchingItem(title: "귀여운 바나나",
+                                         descriptions: "귀여운 바나나입니다.",
+                                         price: 1000,
+                                         currency: nil,
+                                         stock: nil,
+                                         discountedPrice: nil,
+                                         images: [UIImage(systemName: "zzz")!.jpegData(compressionQuality: 1)!],
+                                         password: "1234")
     }
     
-    func test_formData의_fileFields() {
+    func test_PostingItem의_textFields() {
+        let sut = dummyPostingItem.textFields
+        let expectedResult: [(key:String, value: String)] = [
+            ("title", "끔찍한 디버깅"),
+        ("descriptions", "끔찍한 디버깅입니다."),
+            ("price", "1000"),
+        ("currency", "KRW"),
+            ("stock", "10"),
+            ("password", "1234")
+        ]
+        
+        for i in 0..<sut.count {
+            XCTAssertEqual(sut[i].key, expectedResult[i].key)
+            XCTAssertEqual(sut[i].value, expectedResult[i].value)
+        }
+    }
+    
+    func test_PostingItem의_fileFields() {
+        let sut = dummyPostingItem.fileFields
+        let jpegData = UIImage(systemName: "zzz")!.jpegData(compressionQuality: 1)!
+        let expectedResult: [(key:String, value: Data)] = [
+            ("images[]", jpegData)
+        ]
+        
+        for i in 0..<sut.count {
+            XCTAssertEqual(sut[i].key, expectedResult[i].key)
+            XCTAssertEqual(sut[i].value, expectedResult[i].value)
+        }
+    }
+    
+    func test_PacthingItem의_textFields() {
+        let sut = dummyPatchingItem.textFields
+        let expectedResult: [(key:String, value: String)] = [
+            ("title", "귀여운 바나나"),
+        ("descriptions", "귀여운 바나나입니다."),
+            ("price", "1000"),
+            ("password", "1234")
+        ]
+        
+        for i in 0..<sut.count {
+            XCTAssertEqual(sut[i].key, expectedResult[i].key)
+            XCTAssertEqual(sut[i].value, expectedResult[i].value)
+        }
+    }
+    
+    func test_PacthingItem의_fileFields() {
+        let sut = dummyPatchingItem.fileFields
+        let jpegData = UIImage(systemName: "zzz")!.jpegData(compressionQuality: 1)!
+        let expectedResult: [(key:String, value: Data)] = [
+            ("images[]", jpegData)
+        ]
+        
+        for i in 0..<sut.count {
+            XCTAssertEqual(sut[i].key, expectedResult[i].key)
+            XCTAssertEqual(sut[i].value, expectedResult[i].value)
+        }
+    }
+    
+    func test_UIKit() {
+        let data = UIImage(systemName: "zzz")!.jpegData(compressionQuality: 1)!
+        XCTAssertEqual(String(decoding: data, as: UTF8.self), String(decoding: data, as: UTF8.self))
         
     }
 }
