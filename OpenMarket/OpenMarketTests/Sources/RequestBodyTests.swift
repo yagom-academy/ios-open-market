84b387e61e67e90a5a1b5dd483820dc32571ecbc
Neph <tony3322@naver.com>
2021-05-18 20:27:25 +0900

    [Chore] #2 변경된 로직 테스트 위한 파일 생성

---
+++ OpenMarket/OpenMarketTests/Sources/RequestBodyTests.swift
@@ -0,0 +1,20 @@
+//
+//  RequestBodyTests.swift
+//  OpenMarketTests
+//
+//  Created by 천수현 on 2021/05/18.
+//
+
+import XCTest
+
+class RequestBodyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+}
