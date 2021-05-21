5a9d1167e3a2f4cf66e1806ccb7da322cac958fd
Ian <isnn0103@gmail.com>
2021-05-21 14:19:22 +0900

    [Feat] #3 getServersData 메소드 생성

--- OpenMarket/OpenMarket/ServerConnector.swift
+++ OpenMarket/OpenMarket/ServerConnector.swift
@@ -7,10 +7,14 @@
 
 import Foundation
 
+struct servedItems {
+    static var items: Items!
+}
+
 struct ServerConnector {
     let domain: String
     let url: URL
-    let urlRequest: URLRequest
+    var urlRequest: URLRequest
     
     init(domain: String) {
         self.domain = domain
@@ -18,8 +22,30 @@ struct ServerConnector {
         self.urlRequest = URLRequest(url: url)
     }
     
-    func getServersData() {}
-    
+    mutating func getServersData() {
+        self.urlRequest.httpMethod = "Get"
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            if let error = error {
+                print("에러")
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                print("200번대 상태가 아닙니다 !")
+                return
+            }
+            
+            if let mimeType = httpResponse.mimeType,
+               mimeType == "application/json",
+               let data = data,
+               let convertedData = try? JSONDecoder().decode(Items.self, from: data) {
+                servedItems.items = convertedData
+            }
+        }
+        task.resume()
+    }
+
     func postClientsData() {}
     
     func patchSeversData() {}
