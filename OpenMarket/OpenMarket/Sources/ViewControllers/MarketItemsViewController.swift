d0b3a0d8d9263a153ce261cde06470cad1a0602e
duckbok <duckbok22@gmail.com>
2021-05-21 13:41:18 +0900

    [Feat] #6 메인화면을 위한 CollectionView 기본 설정

--- OpenMarket/OpenMarket/Sources/ViewControllers/MarketItemsViewController.swift
+++ OpenMarket/OpenMarket/Sources/ViewControllers/MarketItemsViewController.swift
@@ -7,10 +7,34 @@
 import UIKit
 
 class MarketItemsViewController: UIViewController {
+    lazy var collectionView: UICollectionView = {
+        var collectionView = UICollectionView(frame: view.frame, collectionViewLayout: UICollectionViewFlowLayout())
+
+        collectionView.register(ItemCell.self, forCellWithReuseIdentifier: ItemCell.reuseIdentifier)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+}
+
+extension MarketItemsViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
+
+extension MarketItemsViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize()
+    }
 }
---
+++ OpenMarket/OpenMarket/Sources/Views/ItemCell.swift
@@ -0,0 +1,12 @@
+//
+//  ItemCell.swift
+//  OpenMarket
+//
+//  Created by duckbok on 2021/05/21.
+//
+
+import UIKit
+
+class ItemCell: UICollectionViewCell {
+    static let reuseIdentifier = "itemCell"
+}
