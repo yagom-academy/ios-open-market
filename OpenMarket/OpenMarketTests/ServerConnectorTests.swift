711c0e12a9186be08256fa2c9f9fe957d1afdacf
dacodaco <imochi@naver.com>
2021-05-21 14:22:37 +0900

    [Test] ServerConnectorTests 파일생성

---
+++ OpenMarket/OpenMarketTests/ServerConnectorTests.swift
@@ -0,0 +1,12 @@
+//
+//  ServerConnectorTests.swift
+//  OpenMarketTests
+//
+//  Created by 김찬우 on 2021/05/21.
+//
+
+import XCTest
+
+@testable import OpenMarket
+
+class ServerConnectorTests: XCTestCase { }
