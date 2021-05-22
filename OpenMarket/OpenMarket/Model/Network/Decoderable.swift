cf28c7877ef9a2568944d28b36791541e2309473
fezravien <yjaewoong@naver.com>
2021-05-22 18:39:10 +0900

    chore : MarketNetwork와 파일 분리

---
+++ OpenMarket/OpenMarket/Model/Network/Decoderable.swift
@@ -0,0 +1,14 @@
+//
+//  Decoderable.swift
+//  OpenMarket
+//
+//  Created by 윤재웅 on 2021/05/22.
+//
+
+import Foundation
+
+protocol Decoderable {
+    func decode<T>(_ type: T.Type, from: Data) throws -> T where T : Decodable
+}
+
+extension JSONDecoder: Decoderable { }
