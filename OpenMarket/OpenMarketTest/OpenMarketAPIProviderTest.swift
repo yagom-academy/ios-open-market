88b1e27a5bf658ec501b8284152f79680f61afc3
kane-young <lyw2100@naver.com>
2021-05-23 00:45:48 +0900

    Style : 테스트 코드내에 given, when, then 추가 및 개행변경

--- OpenMarket/OpenMarketTest/OpenMarketAPIProviderTest.swift
+++ OpenMarket/OpenMarketTest/OpenMarketAPIProviderTest.swift
@@ -28,9 +28,13 @@ class OpenMarketAPIProviderTest: XCTestCase {
   
   //MARK: Test Function
   func test_Get_Item_Success() {
+    //given
     setRequestHandler(shouldSuccess: true)
+    
+    //when
     openMarketProvider.getData(apiRequestType: .loadProduct(id: 1), completionHandler: { result in
       switch result {
+      //then
       case .success:
         XCTAssert(true)
       case .failure:
@@ -43,9 +47,13 @@ class OpenMarketAPIProviderTest: XCTestCase {
   }
   
   func test_Get_Item_Failure() {
+    //given
     setRequestHandler(shouldSuccess: false)
+    
+    //when
     openMarketProvider.getData(apiRequestType: .loadProduct(id: 1), completionHandler: { result in
       switch result {
+      //then
       case .success:
         XCTFail()
       case .failure:
@@ -58,15 +66,22 @@ class OpenMarketAPIProviderTest: XCTestCase {
   }
   
   func test_Post_Item_Success() {
+    //given
     setRequestHandler(shouldSuccess: true)
-    let dummyProduct =
-      OpenMarket.ProductRegisterRequest(title: "MacBook Pro", descriptions: "", price: 0,
-                                        currency: "", stock: 0, discountedPrice: 0, images: [],
-                                        password: "")
+    let dummyProduct = OpenMarket.ProductRegisterRequest(title: "MacBook Pro",
+                                                         descriptions: "",
+                                                         price: 0,
+                                                         currency: "",
+                                                         stock: 0,
+                                                         discountedPrice: 0,
+                                                         images: [],
+                                                         password: "")
     
+    //when
     openMarketProvider.postProduct(product: dummyProduct,
                                    apiRequestType: .postProduct,
                                    completionHandler: { result in
+                                    //then
                                     switch result {
                                     case .success:
                                       XCTAssert(true)
@@ -80,16 +95,23 @@ class OpenMarketAPIProviderTest: XCTestCase {
   }
   
   func test_Post_Item_Failure() {
+    //given
     setRequestHandler(shouldSuccess: false)
-    let dummyProduct =
-      OpenMarket.ProductRegisterRequest(title: "MacBook Pro", descriptions: "", price: 0,
-                                        currency: "", stock: 0, discountedPrice: 0, images: [],
-                                        password: "")
+    let dummyProduct = OpenMarket.ProductRegisterRequest(title: "MacBook Pro",
+                                                         descriptions: "",
+                                                         price: 0,
+                                                         currency: "",
+                                                         stock: 0,
+                                                         discountedPrice: 0,
+                                                         images: [],
+                                                         password: "")
     
+    //when
     openMarketProvider.postProduct(product: dummyProduct,
                                    apiRequestType: .postProduct,
                                    completionHandler: { result in
                                     switch result {
+                                    //then
                                     case .success:
                                       XCTFail()
                                     case .failure:
@@ -102,85 +124,103 @@ class OpenMarketAPIProviderTest: XCTestCase {
   }
   
   func test_Update_Item_Success() {
+    //given
     setRequestHandler(shouldSuccess: true)
-    let dummyProduct =
-      OpenMarket.ProductUpdateRequest(title: "MacBook Pro", descriptions: "", price: 0,
-                                      currency: "", stock: 0, discountedPrice: 0, images: [],
-                                      password: "")
+    let dummyProduct = OpenMarket.ProductUpdateRequest(title: "MacBook Pro",
+                                                         descriptions: "",
+                                                         price: 0,
+                                                         currency: "",
+                                                         stock: 0,
+                                                         discountedPrice: 0,
+                                                         images: [],
+                                                         password: "")
     
-    openMarketProvider
-      .updateProduct(product: dummyProduct,
-                     apiRequestType: .patchProduct(id: 1), completionHandler: { result in
-                      switch result {
-                      case .success:
-                        XCTAssert(true)
-                      case .failure:
-                        XCTFail()
-                      }
-                      self.expectation.fulfill()
-                     })
+    //when
+    openMarketProvider.updateProduct(product: dummyProduct,
+                                     apiRequestType: .patchProduct(id: 1),
+                                     completionHandler: { result in
+                                      //then
+                                      switch result {
+                                      case .success:
+                                        XCTAssert(true)
+                                      case .failure:
+                                        XCTFail()
+                                      }
+                                      self.expectation.fulfill()
+                                     })
     
     wait(for: [expectation], timeout: 5)
   }
   
   func test_Update_Item_Failure() {
+    //given
     setRequestHandler(shouldSuccess: false)
-    let dummyProduct =
-      OpenMarket.ProductUpdateRequest(title: "MacBook Pro", descriptions: "", price: 0,
-                                      currency: "", stock: 0, discountedPrice: 0, images: [],
-                                      password: "")
+    let dummyProduct = OpenMarket.ProductUpdateRequest(title: "MacBook Pro",
+                                                         descriptions: "",
+                                                         price: 0,
+                                                         currency: "",
+                                                         stock: 0,
+                                                         discountedPrice: 0,
+                                                         images: [],
+                                                         password: "")
     
-    openMarketProvider
-      .updateProduct(product: dummyProduct,
-                     apiRequestType: .patchProduct(id: 1), completionHandler: { result in
-                      switch result {
-                      case .success:
-                        XCTFail()
-                      case .failure:
-                        XCTAssert(true)
-                      }
-                      self.expectation.fulfill()
-                     })
+    //when
+    openMarketProvider.updateProduct(product: dummyProduct,
+                                     apiRequestType: .patchProduct(id: 1),
+                                     completionHandler: { result in
+                                      switch result {
+                                      //then
+                                      case .success:
+                                        XCTFail()
+                                      case .failure:
+                                        XCTAssert(true)
+                                      }
+                                      self.expectation.fulfill()
+                                     })
     
     wait(for: [expectation], timeout: 5)
   }
   
   func test_Delete_Item_Success() {
+    //given
     setRequestHandler(shouldSuccess: true)
-    let dummyUserInfomation =
-      OpenMarket.ProductDeleteRequest(password: "")
+    let dummyUserInfomation = OpenMarket.ProductDeleteRequest(password: "")
     
-    openMarketProvider
-      .deleteProduct(product: dummyUserInfomation,
-                     apiRequestType: .deleteProduct(id: 1), completionHandler: { result in
-                      switch result {
-                      case .success:
-                        XCTAssert(true)
-                      case .failure:
-                        XCTFail()
-                      }
-                      self.expectation.fulfill()
-                     })
+    //when
+    openMarketProvider.deleteProduct(product: dummyUserInfomation,
+                                     apiRequestType: .deleteProduct(id: 1),
+                                     completionHandler: { result in
+                                      switch result {
+                                      //then
+                                      case .success:
+                                        XCTAssert(true)
+                                      case .failure:
+                                        XCTFail()
+                                      }
+                                      self.expectation.fulfill()
+                                     })
     
     wait(for: [expectation], timeout: 5)
   }
   
   func test_Delete_Item_Failure() {
+    //given
     setRequestHandler(shouldSuccess: false)
-    let dummyUserInfomation =
-      OpenMarket.ProductDeleteRequest(password: "")
+    let dummyUserInfomation = OpenMarket.ProductDeleteRequest(password: "")
     
-    openMarketProvider
-      .deleteProduct(product: dummyUserInfomation,
-                     apiRequestType: .deleteProduct(id: 1), completionHandler: { result in
-                      switch result {
-                      case .success:
-                        XCTFail()
-                      case .failure:
-                        XCTAssert(true)
-                      }
-                      self.expectation.fulfill()
-                     })
+    //when
+    openMarketProvider.deleteProduct(product: dummyUserInfomation,
+                                     apiRequestType: .deleteProduct(id: 1),
+                                     completionHandler: { result in
+                                      switch result {
+                                      //then
+                                      case .success:
+                                        XCTFail()
+                                      case .failure:
+                                        XCTAssert(true)
+                                      }
+                                      self.expectation.fulfill()
+                                     })
     
     wait(for: [expectation], timeout: 5)
   }
