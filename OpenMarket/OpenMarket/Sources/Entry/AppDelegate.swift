9eb250f9e77612d46efa5ab18d01a431797e04d9
duckbok <duckbok22@gmail.com>
2021-05-24 23:45:02 +0900

    [Chore] Main.Storyboard 제거

--- OpenMarket/OpenMarket/Sources/Entry/AppDelegate.swift
+++ OpenMarket/OpenMarket/Sources/Entry/AppDelegate.swift
@@ -12,8 +12,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+
         let rootViewController = UINavigationController(rootViewController: MarketItemsViewController())
         window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
+
         return true
     }
 }
