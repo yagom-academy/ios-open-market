d6a014ca1e62a82da70d8c17294341cce76b7fe9
Neph <tony3322@naver.com>
2021-05-24 23:27:36 +0900

    [Fix] 화면 이동시 버벅이던 문제 수정

--- OpenMarket/OpenMarket/Sources/ViewControllers/ItemManagingViewController.swift
+++ OpenMarket/OpenMarket/Sources/ViewControllers/ItemManagingViewController.swift
@@ -8,5 +8,8 @@
 import UIKit
 
 class ItemManagingViewController: UIViewController {
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
 }
