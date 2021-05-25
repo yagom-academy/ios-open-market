9e24be6f5c4d7a1bcfdb9fafcf17defbee236f0c
cjm <stepsw112@gmail.com>
2021-05-25 19:39:52 +0900

    Refactor: UICollectionViewListCell 적용

--- OpenMarket/Model/Response/ItemsOfPageReponse.swift
+++ OpenMarket/Model/Response/ItemsOfPageReponse.swift
@@ -10,24 +10,22 @@ import Foundation
 struct ItemsOfPageReponse: Decodable, Equatable {
     var page: Int
     var items: [Item]
-    
-    struct Item: Decodable, Equatable {
-        var id: Int
-        var title: String
-        var price: Int
-        var currency: String
-        var stock: Int
-        var discountedPrice: Int?
-        var thumbnails: [String]
-        var registrationDate: Double
-        
-        private enum CodingKeys: String, CodingKey {
-            case id, title, price, currency, stock, thumbnails
-            case discountedPrice = "discounted_price"
-            case registrationDate = "registration_date"
-        }
-    }
 }
 
-
+struct Item: Decodable, Equatable {
+    var id: Int
+    var title: String
+    var price: Int
+    var currency: String
+    var stock: Int
+    var discountedPrice: Int?
+    var thumbnails: [String]
+    var registrationDate: Double
+    
+    private enum CodingKeys: String, CodingKey {
+        case id, title, price, currency, stock, thumbnails
+        case discountedPrice = "discounted_price"
+        case registrationDate = "registration_date"
+    }
+}
 
--- OpenMarket/ViewModel/CellViewModel.swift
+++ OpenMarket/ViewModel/CellViewModel.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+struct CellViewModel {
+    var item: Item
+}
