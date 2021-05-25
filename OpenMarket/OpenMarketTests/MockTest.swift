4666ee33e2dfb8b1fe35f47a6f8abaa6d68d7588
stevenkim18 <11882@naver.com>
2021-05-25 17:37:35 +0900

    test: 상품 목록 조회 테스트, 공통되는 부분 setUp함수로 분리

--- OpenMarket/OpenMarketTests/MockTest.swift
+++ OpenMarket/OpenMarketTests/MockTest.swift
@@ -10,13 +10,63 @@ import XCTest
 
 class MockTest: XCTestCase {
     
-    func test_상품_조회() {
-        // given
+    var networkHelper: NetworkHelper!
+    
+    override func setUpWithError() throws {
         let configuration = URLSessionConfiguration.default
         configuration.protocolClasses = [MockURLProtocol.self]
         let mockSession = URLSession(configuration: configuration)
-        let networkHelper = NetworkHelper(session: mockSession)
+        networkHelper = NetworkHelper(session: mockSession)
+    }
+    
+    override func tearDownWithError() throws {
+        networkHelper = nil
+    }
+    
+    func test_상품_목록_조회() {
         
+        // given
+        let pageNum = 1
+        
+        guard let data = NSDataAsset(name: "items")?.data else {
+            XCTFail()
+            return
+        }
+        
+        guard let mockJsonList = try? JSONDecoder().decode(ProductList.self, from: data) else {
+            XCTFail()
+            return
+        }
+        
+        MockURLProtocol.requsetHandler = { request in
+            let response = HTTPURLResponse(url: URL(string: RequestAddress.readList(page: pageNum).url)!,
+                                           statusCode: 200,
+                                           httpVersion: nil,
+                                           headerFields: nil)
+            return (data, response, nil)
+        }
+        
+        let promise = expectation(description: "Mock test")
+        
+        // when
+        networkHelper.readList(pageNum: pageNum) { result in
+            
+            // then
+            switch result {
+            case .success(let list):
+                XCTAssertEqual(list.page, mockJsonList.page)
+                XCTAssertEqual(list.items.count, mockJsonList.items.count)
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 1)
+    }
+    
+    func test_상품_조회() {
+        
+        // given
         guard let data = NSDataAsset(name: "item")?.data else {
             XCTFail()
             return
@@ -28,7 +78,10 @@ class MockTest: XCTestCase {
         }
         
         MockURLProtocol.requsetHandler = { request in
-            let response = HTTPURLResponse(url: URL(string: RequestAddress.readItem(id: 1).url)!, statusCode: 200, httpVersion: nil, headerFields: nil)
+            let response = HTTPURLResponse(url: URL(string: RequestAddress.readItem(id: 1).url)!,
+                                           statusCode: 200,
+                                           httpVersion: nil,
+                                           headerFields: nil)
             return (data, response, nil)
         }
         
@@ -51,12 +104,8 @@ class MockTest: XCTestCase {
     
     func test_상품_등록() {
         
+        // given
         let mockItemForm = ProductForm(title: "수지", descriptions: "수지좋아", price: 1000, currency: "USD", stock: 90, discountedPrice: 999, images: [(UIImage(named: "vanilla")?.pngData())!], password: "1234")
-        
-        let configuration = URLSessionConfiguration.default
-        configuration.protocolClasses = [MockURLProtocol.self]
-        let mockSession = URLSession(configuration: configuration)
-        let networkHelper = NetworkHelper(session: mockSession)
         
         let promise = expectation(description: "Mock test")
         
@@ -85,7 +134,10 @@ class MockTest: XCTestCase {
             return (mockJsonData, response, nil)
         }
         
+        // when
         networkHelper.createItem(itemForm: mockItemForm) { result in
+            
+            // then
             switch result {
             case .success(let item):
                 XCTAssertEqual(item.title, mockItemForm.title)
