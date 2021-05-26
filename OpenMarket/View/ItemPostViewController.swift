3e9748c45d976b8cc1c6d1af8c26ff317fed24a0
cjm <stepsw112@gmail.com>
2021-05-27 00:53:37 +0900

    Feat: UI 부분 완성

---
+++ OpenMarket/View/ItemPostViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ItemPostViewController.swift
+//  OpenMarket
+//
+//  Created by 최정민 on 2021/05/26.
+//
+
+import UIKit
+
+class ItemPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
