92eee3a433358cc5afc3745edffd53359e411303
ryan-son <ryan.son1002@gmail.com>
2021-05-27 23:52:33 +0900

    💄 [UI] GridCell 추가 및 ListCell UI 수정
    - OpenMarketCell 프로토콜 추가

--- OpenMarket/OpenMarket/View/Cell/GridCollectionViewCell.swift
+++ OpenMarket/OpenMarket/View/Cell/GridCollectionViewCell.swift
@@ -1,14 +1,14 @@
 //
-//  ListCollectionViewCell.swift
+//  GridCollectionViewCell.swift
 //  OpenMarket
 //
-//  Created by 배은서 on 2021/05/25.
+//  Created by Ryan-Son on 2021/05/27.
 //
 
 import UIKit
 
-class ListCollectionViewCell: UICollectionViewCell, OpenMarketCell {
-    
+class GridCollectionViewCell: UICollectionViewCell, OpenMarketCell {
+
     @IBOutlet weak var thumbnailImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var stockLabel: UILabel!
