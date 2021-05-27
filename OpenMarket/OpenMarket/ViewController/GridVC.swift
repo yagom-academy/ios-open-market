c013d8bb30f7c5f83a0d3c6fb3b97dbf623daaa1
summer0817 <step33@naver.com>
2021-05-28 00:59:28 +0900

    fix: Storyboard CollectionView Cell 사이즈 수정

--- OpenMarket/OpenMarket/ViewController/GridVC.swift
+++ OpenMarket/OpenMarket/ViewController/GridVC.swift
@@ -13,7 +13,6 @@ class GridVC: UIViewController {
     
     var items: [Item] = []
    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,19 +20,6 @@ class GridVC: UIViewController {
         collectionView.dataSource = self
         
         NotificationCenter.default.addObserver(self, selector: #selector(Self.setupItems(_:)), name: NotificationNames.items.notificaion, object: nil)
-        
-        let flowLayout: UICollectionViewFlowLayout
-        flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
-        flowLayout.minimumLineSpacing = 10
-        flowLayout.minimumInteritemSpacing = 10
-        
-        let halfWidth: CGFloat = UIScreen.main.bounds.width / 2.0
-        flowLayout.itemSize = CGSize(width: halfWidth - 30, height: 350)
-        
-        self.collectionView.collectionViewLayout = flowLayout
-        super.viewDidLoad()
-        print("viewDidLoad")
     }
     
     @objc func setupItems(_ notification: Notification) {
@@ -48,8 +34,6 @@ class GridVC: UIViewController {
             }
         }
     }
-
-    
 }
 
 
@@ -64,17 +48,6 @@ extension GridVC: UICollectionViewDataSource {
         return self.items.count
     }
     
-    
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//
-//            let itemSpacing: CGFloat = 10 // 가로에서 cell과 cell 사이의 거리
-//            let textAreaHeight: CGFloat = 65 // textLabel이 차지하는 높이
-//            let width: CGFloat = (collectionView.bounds.width - itemSpacing)/2 // 셀 하나의 너비
-//            let height: CGFloat = width * 10/7 + textAreaHeight //셀 하나의 높이
-//
-//            return CGSize(width: width, height: height)
-//        }
-    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridVCCell", for: indexPath) as! GridVCCell
         
@@ -84,7 +57,6 @@ extension GridVC: UICollectionViewDataSource {
             cell.item = items[indexPath.row]
             cell.setupItem()
         }
-        
         return cell
     }
     
