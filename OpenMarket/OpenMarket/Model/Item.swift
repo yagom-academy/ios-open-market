8ed9c75b98ffc521cde8703db9d21572118b71b3
dacodaco <imochi@naver.com>
2021-05-28 18:32:28 +0900

    [Feat]#1 Item 타입 구현

--- OpenMarket/OpenMarket/Model/Item.swift
+++ OpenMarket/OpenMarket/Model/Item.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct Item: Decodable {
+    let id: Int
+    let title: String
+    let price: Int
+    let currency: String
+    let stock: Int
+    let discountedPrice: Int?
+    let thumbnails: [String]
+    let registrationDate: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case id, title, price, currency, stock, thumbnails
+        case discountedPrice = "discounted_price"
+        case registrationDate = "registration_date"
+    }
+}
