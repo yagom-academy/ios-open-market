431660d2b64e924ec2f57e9d8d627175565fc379
fezravien <yjaewoong@naver.com>
2021-05-30 01:19:27 +0900

    feat : CollectionView Grid ContentView 구현
    Grid 모드 ContentView 구현

---
+++ OpenMarket/OpenMarket/View/ItemGrid/ItemGridContentView.swift
@@ -0,0 +1,115 @@
+//
+//  ItemGridContentView.swift
+//  OpenMarket
+//
+//  Created by 윤재웅 on 2021/05/28.
+//
+
+import Foundation
+import UIKit
+
+@available(iOS 14.0, *)
+class ItemGridContentView: UIView, UIContentView {
+    @IBOutlet var containerView: UIView!
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var discountPrice: UILabel!
+    @IBOutlet weak var stock: UILabel!
+    private var currentConfiguration: ItemConfiguration!
+    var configuration: UIContentConfiguration {
+        get {
+            return currentConfiguration
+        }
+        set {
+            guard let newConfiguration = newValue as? ItemConfiguration else { return }
+            apply(newConfiguration)
+        }
+    }
+    
+    init(configuration: ItemConfiguration) {
+        super.init(frame: .zero)
+        loadNib()
+        apply(configuration)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+@available(iOS 14.0, *)
+extension ItemGridContentView {
+    private func loadNib() {
+        Bundle.main.loadNibNamed("\(ItemGridContentView.self)", owner: self, options: nil)
+        addSubview(containerView)
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: self.topAnchor, constant: 0),
+            containerView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0),
+            containerView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0),
+            containerView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0),
+        ])
+        containerView.layer.cornerRadius = 10
+        containerView.layer.borderWidth = 2
+        containerView.layer.borderColor = UIColor.lightGray.cgColor
+        
+    }
+    
+    private func apply(_ configuration: ItemConfiguration) {
+        guard currentConfiguration != configuration else { return }
+        currentConfiguration = configuration
+        fetchImage(configuration)
+        cancelTextLine(configuration)
+        setSoldOutText(configuration)
+        self.title.text = configuration.title
+    }
+
+    private func cancelTextLine(_ configuration: ItemConfiguration) {
+        resetPrice()
+        if configuration.discountPrice == nil {
+            self.price.text = String("\(configuration.currency!) \(configuration.price ?? 0)")
+            self.discountPrice.isHidden = true
+        } else {
+            let attributeString: NSMutableAttributedString = NSMutableAttributedString(string: String("\(configuration.currency!) \(configuration.price ?? 0)"))
+            attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 2, range: NSMakeRange(0, attributeString.length))
+            self.price.text = String("\(configuration.currency!) \(configuration.price ?? 0)")
+            self.price.textColor = .red
+            self.price.attributedText = attributeString
+            self.discountPrice.isHidden = false
+            self.discountPrice.text = "\(configuration.currency!) " + String(configuration.discountPrice!)
+        }
+    }
+    
+    private func setSoldOutText(_ configuration: ItemConfiguration) {
+        guard let stock = configuration.stock else { return }
+        resetStock()
+        if stock == 0 {
+            self.stock.textColor = .systemOrange
+            self.stock.text = String("품절")
+        } else {
+            self.stock.text = String("재고 : \(stock)")
+        }
+    }
+    
+    private func resetPrice() {
+        self.price.textColor = .systemGray
+        self.price.attributedText = nil
+    }
+    
+    private func resetStock() {
+        self.stock.textColor = .systemGray
+    }
+    
+    private func fetchImage(_ configuration: ItemConfiguration)   {
+        guard let image = configuration.image, let firstImage = image.first else { return }
+        guard let url = URL(string: firstImage) else { return }
+        
+        DispatchQueue.global().async {
+            guard let data = try? Data(contentsOf: url) else { return }
+            DispatchQueue.main.async {
+                self.itemImageView.image = UIImage(data: data)!
+            }
+        }
+    }
+}
