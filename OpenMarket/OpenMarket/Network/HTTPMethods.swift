f8a53fbaa50c34982e0fbd33c5c141c324b6e375
Inwoo Hwang <dlsdn1207@yonsei.ac.kr>
2021-05-31 16:38:32 +0900

    Feat : HTTP 메서드 구현 네트워크 통신을 하기위해 GET, POST, PATCH, DELETE 메서드를 열거형으로 구현

---
+++ OpenMarket/OpenMarket/Network/HTTPMethods.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethods.swift
+//  OpenMarket
+//
+//  Created by 황인우 on 2021/05/31.
+//
+
+import Foundation
+
+public enum HTTPMethods: String {
+    case get = "GET"
+    case post = "POST"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
