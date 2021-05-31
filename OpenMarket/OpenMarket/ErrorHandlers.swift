1c8b7039bde27e0d99c2c224e7212addd749383d
dacodaco <imochi@naver.com>
2021-05-31 19:20:14 +0900

    [Feat]#1 Request 구현을 위한 ClientRequest타입 구현

---
+++ OpenMarket/OpenMarket/ErrorHandlers.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorHandlers.swift
+//  OpenMarket
+//
+//  Created by 김찬우 on 2021/05/31.
+//
+
+import Foundation
+
+enum ModelTypeError: Error {
+    case bindingError
+}
--- OpenMarket/OpenMarket/Model/APIInformations.swift
+++ OpenMarket/OpenMarket/Model/APIInformations.swift
@@ -8,8 +8,58 @@
 import Foundation
 
 enum DescriptionAboutMenu: String {
-    case 목록조회 = "GET"
+    case 목록조회, 상품조회 = "GET"
     case 상품등록 = "POST"
     case 상품수정 = "PATCH"
     case 상품삭제 = "DELETE"
+}
+
+struct Header {
+    let url: URL
+    let mehtod: String
+    let contentsType: String
+}
+
+struct Body {
+    let item: Item
+}
+
+struct ClientRequest {
+    let baseURLWithString: String = "https://camp-open-market-2.herokuapp.com/"
+    var page: Int = 0
+    var path: String = ""
+    let contentType: String = ""
+    let descriptionAboutMenu: DescriptionAboutMenu
+    var header: Header?
+    var body: Body?
+    
+    init(page: Int, descriptionAboutMenu: DescriptionAboutMenu){
+        self.page = page
+        self.descriptionAboutMenu = descriptionAboutMenu
+    }
+    
+    mutating func setProperty(by description: DescriptionAboutMenu){
+        switch description {
+        case .목록조회:
+            path = "items/:\(page)"
+            header = Header(url: URL(string: baseURLWithString + path)!, mehtod: description.rawValue, contentsType: "application/json")
+            body = nil
+        case .상품등록:
+            path = "item"
+            header = Header(url: URL(string: baseURLWithString + path)!, mehtod: description.rawValue, contentsType: "multipart/form-data")
+            body = nil
+        case .상품조회:
+            if let body = body { path = "item/:\(body.item.id)" }
+            header = Header(url: URL(string: baseURLWithString + path)!, mehtod: description.rawValue, contentsType: "multipart/form-data")
+            body = nil
+        case .상품수정:
+            if let body = body { path = "item/:\(body.item.id)"}
+            header = Header(url: URL(string: baseURLWithString + path)!, mehtod: description.rawValue, contentsType: "multipart/form-data")
+            body = nil
+        case .상품삭제:
+            if let body = body { path = "item/:\(body.item.id)"}
+            header = Header(url: URL(string: baseURLWithString + path)!, mehtod: description.rawValue, contentsType: "application/json")
+            body = nil
+        }
+    }
 }
