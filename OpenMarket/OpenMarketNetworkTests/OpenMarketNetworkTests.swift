65b732d4a50c7b4fd7edb569abc32ee73350c91e
Inwoo Hwang <dlsdn1207@yonsei.ac.kr>
2021-06-01 18:24:10 +0900

    Test : 네트워크 비의존적인 네트워크 테스트 구현
    - MockURLSession을 주입 받은 네트워크 매니저를 활용한 테스트 코드 작성
    	- 네트워크 테스트 로직이 성공적일시 응답코드가 200인지 확인 테스트 진행
    	- MockURLSession이 잘못되었을 때 실패하는 테스트 실행

--- OpenMarket/OpenMarketNetworkTests/OpenMarketNetworkTests.swift
+++ OpenMarket/OpenMarketNetworkTests/OpenMarketNetworkTests.swift
@@ -8,35 +8,58 @@
 import XCTest
 @testable import OpenMarket
 final class OpenMarketNetworkTests: XCTestCase {
-    var sut_networkManager: NetworkManager!
-    var sut_apiNode: APINode!
+    var sut_mockNetworkManager: MockNetworkManager!
     
     override func setUpWithError() throws {
         try super.setUpWithError()
-        sut_networkManager = NetworkManager()
-        sut_apiNode = APINode()
+        sut_mockNetworkManager = MockNetworkManager(urlSession: MockURLSession())
         
     }
 
     override func tearDownWithError() throws {
         try super.tearDownWithError()
-        sut_networkManager = nil
-        sut_apiNode = nil
+        sut_mockNetworkManager = nil
+
     }
     
-    func test_getItemList_title() {
+    func test_getItemList_successfulResponse() {
+        // given
         let expectation = XCTestExpectation()
-
-        sut_networkManager.getItemList(node: sut_apiNode, page: 1) { result in
-            expectation.fulfill()
+        
+        // when
+        sut_mockNetworkManager.getItemList(page: 1) { result in
+            
+            // then
             switch result {
-            case .success(let itemList):
-                XCTAssertEqual(itemList.items[0].title, "MacBook Pro" )
+            case .success(let response):
+                XCTAssertEqual(response.statusCode, 200)
+                // then
             case .failure(let error):
                 XCTFail("\(error)")
             }
+            expectation.fulfill()
         }
-        wait(for: [expectation], timeout: 2)
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func test_getItemList_failureResponse() {
+        // given
+        sut_mockNetworkManager = MockNetworkManager(urlSession: MockURLSession.init(buildRequestFail: true))
+        let expectation = XCTestExpectation()
+        
+        // when
+        sut_mockNetworkManager.getItemList(page: 1) { result in
+            
+            // then
+            switch result {
+            case .success:
+                XCTFail()
+            case .failure(let error):
+                XCTAssertEqual(error.description, "Network is not responding")
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
     }
 
 }
