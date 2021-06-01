8a8a80d71b3ae9c38dded9555d8cfd0b36d67a66
Inwoo Hwang <dlsdn1207@yonsei.ac.kr>
2021-06-01 19:02:42 +0900

    Feat : NetworkManageable 프로토콜 구현
    - 프로토콜 기본구현으로 기존의 MockNetworkManager의 역할 대체
    - examineNetworkResponse 메서드 구현
    	해당 프로토콜을 NetworkManager가 채택하여 네트워크 응답을 테스트할 수있도록 구현

---
+++ OpenMarket/OpenMarket/Protocol/NetworkManageable.swift
@@ -0,0 +1,37 @@
+//
+//  NetworkManageable.swift
+//  OpenMarket
+//
+//  Created by James on 2021/06/01.
+//
+
+import Foundation
+
+protocol NetworkManageable {
+    var urlSession: URLSessionProtocol { get set }
+}
+extension NetworkManageable {
+    func examineNetworkResponse(page: Int, completionHandler: @escaping (_ result: Result <HTTPURLResponse, APIError>) -> Void) {
+        guard let url = URL(string: "\(OpenMarketAPI.connection.pathForItemList)\(page)") else {
+            return completionHandler(.failure(APIError.network))
+            
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        
+        urlRequest.httpMethod = HTTPMethods.get.rawValue
+        
+        urlSession.dataTask(with: urlRequest) { data, response, error in
+            if let dataError = error {
+                completionHandler(.failure(APIError.network))
+                print(dataError.localizedDescription)
+            }
+            guard let urlResponse = response as? HTTPURLResponse,
+                  urlResponse.statusCode == 200 else {
+                return completionHandler(.failure(APIError.network))
+            }
+            completionHandler(.success(urlResponse))
+            
+        }.resume()
+    }
+}
