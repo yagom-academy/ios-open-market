59493ad55596a89fecad53fd61529738fbf98211
Tak <oit5538@gmail.com>
2021-06-02 18:52:05 +0900

    feat : MarketAPI 구조체 생성

---
+++ OpenMarket/OpenMarket/Network/MarketAPI.swift
@@ -0,0 +1,39 @@
+//
+//  MarketAPI.swift
+//  OpenMarket
+//
+//  Created by Tak on 2021/06/01.
+//
+
+import Foundation
+
+enum MarketAPI {
+    static let baseURL = "https://camp-open-market-2.herokuapp.com/"
+    case items(page: Int)
+    case registrate
+    case item(id: Int)
+    case edit(id: Int)
+    case delete(id: Int)
+    
+    var url: URL? {
+        switch self {
+        case .items(let page):
+            return URL(string: MarketAPI.baseURL + "/items/" + "\(page)")
+        case .registrate:
+            return URL(string: MarketAPI.baseURL + "/item")
+        case .item(let id):
+            return URL(string: MarketAPI.baseURL + "/item/" + "\(id)")
+        case .edit(let id):
+            return URL(string: MarketAPI.baseURL + "/item/" + "\(id)")
+        case .delete(let id):
+            return URL(string: MarketAPI.baseURL + "/item/" + "\(id)")
+        }
+    }
+}
+
+enum MarketAPIMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case pahtch = "PATCH"
+    case delete = "DELETE"
+}
