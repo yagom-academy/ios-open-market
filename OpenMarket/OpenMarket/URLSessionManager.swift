435333506ff831d7064e9daa2be6e8398cd8871f
dacodaco <imochi@naver.com>
2021-06-03 14:00:46 +0900

    [Refactor]#1 getServerData 메서드 코드 정리

--- OpenMarket/OpenMarket/URLSessionManager.swift
+++ OpenMarket/OpenMarket/URLSessionManager.swift
@@ -7,33 +7,24 @@
 
 import Foundation
 class URLSessionManager<T: Decodable> {
-    let clientRequest: ClientRequest
+    let urlSession = URLSession()
+    let clientRequest: GETRequest
     
-    init(clientRequest: ClientRequest){
+    init(clientRequest: GETRequest){
         self.clientRequest = clientRequest
     }
     
-    func getServerData<T: Decodable>(completionHandler: @escaping (T) -> Void) {
-        URLSession.shared.dataTask(with: clientRequest.urlRequest.url!){ data, response, error in
-            if error != nil {
-                print("1")
-                return }
+    func getServerData<T: Decodable>(url: URL, completionHandler: @escaping (T) -> Void) {
+        urlSession.dataTask(with: url){ data, response, error in
+            if error != nil { return }
             
-            guard let httpResponse = response as? HTTPURLResponse else { return }
-            print(httpResponse.statusCode)
-            guard (200...299).contains(httpResponse.statusCode) else {
-                print("2")
-                return
-            }
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else { return }
             
             if let mimeType = httpResponse.mimeType,
                mimeType == "application/json",
                let data = data {
-                let decoder = JSONDecoder()
-                guard let convertedData = try? decoder.decode(T.self, from: data) else {
-                    print("3")
-                    return
-                }
+                guard let convertedData = try? JSONDecoder().decode(T.self, from: data) else { return }
                 completionHandler(convertedData)
             }
         }.resume()
