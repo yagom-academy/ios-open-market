90b281556e52096d1584a0795becad845a8b98a2
dacodaco <imochi@naver.com>
2021-06-03 13:40:25 +0900

    [Feat]#3 Get이외의 httpMethod를 위한 protocol-extension 구현

--- OpenMarket/OpenMarket/Model/Requestable.swift
+++ OpenMarket/OpenMarket/Model/Requestable.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+//MARK: - ClientRequest를 메서드에 따라 구분해서 생성해주어야 하므로 Protocol을 통해 구현했음.
 protocol Requestable {
     var urlRequest: URLRequest { get set }
     var baseURLWithString: String { get }
@@ -21,6 +22,7 @@ extension Requestable {
     }
 }
 
+// MARK:- Get메서드를 사용하는 목록조회, 상품조회의 URLRequest를 생성하기 위한 protocol-extension
 protocol GETRequestable: Requestable {
     mutating func makeURLRequest(page: Int, by description: HTTPMethod)
 }
@@ -32,6 +34,27 @@ extension GETRequestable {
             urlRequest.url = makeURL(path: "items/\(page)")
             urlRequest.httpMethod = httpMethod.rawValue
         case .상품조회:
+            urlRequest.httpMethod = httpMethod.rawValue
+        default:
+            return
+        }
+    }
+}
+
+// MARK:- Get 이외의 메서드를 사용하는 목록조회, 상품조회의 URLRequest를 생성하기 위한 protocol-extension
+// FIXME:- 기능요구서 상 Step3에서 구현 예정, frame만 만들어둔 상태.
+protocol NonGetRequestable: Requestable {
+    mutating func makeURLRequest(page: Int, by description: HTTPMethod)
+}
+
+extension NonGetRequestable {
+    mutating func makeURLRequest(page: Int, by description: HTTPMethod) {
+        switch description {
+        case .상품등록:
+            urlRequest.httpMethod = httpMethod.rawValue
+        case .상품삭제:
+            urlRequest.httpMethod = httpMethod.rawValue
+        case .상품수정:
             urlRequest.httpMethod = httpMethod.rawValue
         default:
             return
