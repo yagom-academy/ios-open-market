a1659d06746a2ae425e9e2e836082e2c40586b74
dacodaco <imochi@naver.com>
2021-06-07 20:16:33 +0900

    [Refactor]#1 Networkable 삭제를 통한 의존성 주입

--- OpenMarket/OpenMarket/Controller/NetworkManager.swift
+++ OpenMarket/OpenMarket/Controller/NetworkManager.swift
@@ -12,4 +12,20 @@ class NetworkManager<T: Decodable>: Networkable {
     init(clientRequest: GETRequest){
         self.clientRequest = clientRequest
     }
+    
+    func getServerData<T: Decodable>(url: URL, completionHandler: @escaping (T) -> Void) {
+        URLSession.shared.dataTask(with: url){ data, response, error in
+            if error != nil { return }
+
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else { return }
+
+            if let mimeType = httpResponse.mimeType,
+               mimeType == "application/json",
+               let data = data {
+                guard let convertedData = try? JSONDecoder().decode(T.self, from: data) else { return }
+                completionHandler(convertedData)
+            }
+        }.resume()
+    }
 }
