30c40fb3c9fe518a4db8403ed31430b124c7dcf5
dacodaco <imochi@naver.com>
2021-06-08 09:03:18 +0900

    [Feat]#1 NetworkError 구현

---
+++ OpenMarket/OpenMarket/Model/NetworkError.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkError.swift
+//  OpenMarket
+//
+//  Created by 김찬우 on 2021/06/08.
+//
+
+import Foundation
+
+enum NetworkError {
+    case receiveNothing
+    case receiveError
+    case receiveUnwantedResponse
+}
