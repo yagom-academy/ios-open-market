46a566afc3a94cddbd0797741675775f35b81ea4
Inwoo Hwang <dlsdn1207@yonsei.ac.kr>
2021-06-08 23:30:26 +0900

    Refactor : OpenMarketViewController 로직 수정
    - 네트워크 작업을 cellForItemAt 메서드로부터 분리
    - reloadData를 활용하여 받아온 데이터를 cell에 적용시키도록 수정

--- OpenMarket/OpenMarket/ViewController/OpenMarketViewController.swift
+++ OpenMarket/OpenMarket/ViewController/OpenMarketViewController.swift
@@ -9,7 +9,10 @@ import UIKit
 class OpenMarketViewController: UIViewController {
     private var layoutType = LayoutType.list
     private let networkManager: NetworkManageable = NetworkManager()
-
+    private var isPageRefreshing: Bool = false
+    private var nextPageToLoad: Int = 1
+    private var openMarketItemList: OpenMarketItemList?
+    
     lazy private var openMarketCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
@@ -34,8 +37,23 @@ class OpenMarketViewController: UIViewController {
         super.viewDidLoad()
         setUpCollectionView()
         setUpCollectionViewConstraint()
-        segmentedController.addTarget(self, action: #selector(didTapSegmentedControl(_:)), for: .valueChanged)
+        networkManager.getItemList(page: nextPageToLoad, isCurrentlyLoading: false) { [weak self] result in
+            switch result {
+            case .success(let itemList):
+                self?.openMarketItemList = itemList
+                DispatchQueue.main.async {
+                    self?.openMarketCollectionView.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        segmentedController.addTarget(self, action: #selector(didTapSegmentedControl(_:)), for: .valueChanged)
+        }
     
     private func setUpCollectionView() {
         self.view.addSubview(openMarketCollectionView)
@@ -64,6 +82,7 @@ class OpenMarketViewController: UIViewController {
         layout.itemSize = CGSize(width: width, height: height)
         return layout
     }
+    
 }
 extension OpenMarketViewController {
     
@@ -82,11 +101,15 @@ extension OpenMarketViewController {
     }
 }
 extension OpenMarketViewController: UICollectionViewDelegate {
-    
+
 }
 extension OpenMarketViewController: UICollectionViewDataSource {
+    
+    // MARK: - Cell Data
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 8
+        guard let validItemList = openMarketItemList else { return 0}
+        return validItemList.items.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -95,41 +118,24 @@ extension OpenMarketViewController: UICollectionViewDataSource {
             guard let cell: OpenMarketListCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: OpenMarketListCollectionViewCell.identifier, for: indexPath) as? OpenMarketListCollectionViewCell else {
                 return UICollectionViewCell()
             }
-            networkManager.getItemList(page: 1) { result in
-                switch result {
-                case .success(let itemList):
-                    DispatchQueue.main.async {
-                        guard let cellIndex = collectionView.indexPath(for: cell),
-                              cellIndex == indexPath else { return }
-                        cell.configure(itemList, indexPath: indexPath.row)
-                    }
-                case .failure(let error):
-                    print(error)
-                }
-            }
+            cell.configure(openMarketItemList, indexPath: indexPath.row)
+            nextPageToLoad += 1
             return cell
             
         case .grid:
-        guard let cell: OpenMarketGridCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "OpenMarketGridCollectionViewCell", for: indexPath) as? OpenMarketGridCollectionViewCell else {
-            return UICollectionViewCell()
-        }
-            networkManager.getItemList(page: 1) { result in
-                switch result {
-                case .success(let itemList):
-                    DispatchQueue.main.async {
-                        guard let cellIndex = collectionView.indexPath(for: cell),
-                              cellIndex == indexPath else { return }
-                        cell.configure(itemList, indexPath: indexPath.row)
-                    }
-                case .failure(let error):
-                    print(error)
-                }
+            guard let cell: OpenMarketGridCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: OpenMarketGridCollectionViewCell.identifier, for: indexPath) as? OpenMarketGridCollectionViewCell else {
+                return UICollectionViewCell()
             }
+            cell.configure(openMarketItemList, indexPath: indexPath.row)
+            nextPageToLoad += 1
             return cell
         }
     }
 }
 extension OpenMarketViewController: UICollectionViewDelegateFlowLayout {
+    
+    // MARK: - Cell Size
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         switch layoutType {
         case .list:
