a9e142ed867cef167af1f3cbe302d06c5c70207c
Inwoo Hwang <dlsdn1207@yonsei.ac.kr>
2021-06-14 22:32:25 +0900

    Feat : UIPickerViewDelegate, UITextViewDelegate 구현
    - 사용자가 화폐 단위를 pickerView를 통해 설정할 수 있도록 구현
    - TextView의 placeholder 구현을 위한 UITextViewDelegate 구현

--- OpenMarket/OpenMarket/ViewController/OpenMarketItemViewController.swift
+++ OpenMarket/OpenMarket/ViewController/OpenMarketItemViewController.swift
@@ -8,9 +8,127 @@
 import UIKit
 
 class OpenMarketItemViewController: UIViewController {
-
+    let currencyList = ["KRW", "USD", "BTC", "JPY", "EUR", "GBP", "CNY"]
+    
+    // MARK: - Views
+    
+    private lazy var itemTitleTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "상품명:"
+        textField.textColor = .black
+        return textField
+    }()
+    
+    private lazy var currencyTextField: UITextField = {
+        let textField = UITextField()
+        textField.inputView = currencyPickerView
+        textField.inputAccessoryView = pickerViewToolbar
+        return textField
+    }()
+    
+    private lazy var priceTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "가격:"
+        textField.keyboardType = .numberPad
+        return textField
+    }()
+    
+    private lazy var discountedPriceTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "(optional) 할인 가격:"
+        textField.keyboardType = .numberPad
+        return textField
+    }()
+    
+    private lazy var stockTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "수량:"
+        textField.keyboardType = .numberPad
+        return textField
+    }()
+    
+    private lazy var detailedInformationTextView: UITextView = {
+        let textView = UITextView()
+        textView.textColor = .darkGray
+        return textView
+    }()
+    
+    private lazy var currencyPickerView: UIPickerView = {
+        let pickerView = UIPickerView(frame: CGRect(x: 0, y: 200, width: view.frame.width, height: 300))
+        pickerView.backgroundColor = UIColor.white
+        return pickerView
+    }()
+    
+    private lazy var pickerViewToolbar: UIToolbar = {
+        let toolbar = UIToolbar()
+        toolbar.barStyle = .default
+        toolbar.isTranslucent = true
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "완료", style: .done, target: self, action: #selector(self.donePicker))
+        let cancelButton = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(self.donePicker))
+        
+        toolbar.setItems([doneButton, cancelButton], animated: true)
+        toolbar.isUserInteractionEnabled = true
+        return toolbar
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
+        currencyPickerView.dataSource = self
+        currencyPickerView.delegate = self
+        detailedInformationTextView.delegate = self
+    }
+}
+extension OpenMarketItemViewController {
+    
+    // MARK: - setUp UI Constraints
+    
+    private func setUpUIConstraints() {
+        
+    }
+}
 
+// MARK: - UIPickerViewDelegate, UIPickerViewDataSource
+
+extension OpenMarketItemViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyList[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        currencyTextField.text = currencyList[row]
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        currencyList.count
+    }
+    
+    @objc func donePicker() {
+        currencyTextField.resignFirstResponder()
+    }
+}
+
+// MARK: - UITextViewDelegate
+
+extension OpenMarketItemViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == .lightGray {
+            textView.text = nil
+            textView.textColor = .black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "상품 정보를 입력 해 주세요."
+            textView.textColor = .lightGray
+        }
     }
 }
