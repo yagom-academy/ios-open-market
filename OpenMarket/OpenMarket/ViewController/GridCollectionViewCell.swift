a25c6b1fa0498fcfd458e3a469e5386d8ee8fa52
summer0817 <step33@naver.com>
2021-06-15 23:35:41 +0900

    feat: collectionView

---
+++ OpenMarket/OpenMarket/ViewController/GridCollectionViewCell.swift
@@ -0,0 +1,68 @@
+//
+//  GridCollectionViewCell.swift
+//  OpenMarket
+//
+//  Created by ysp on 2021/06/13.
+//
+
+import UIKit
+
+class GridCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var discountedPriceLabel: UILabel!
+    @IBOutlet weak var stockLabel: UILabel!
+    
+    var item: Item?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.contentView.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        self.contentView.layer.cornerRadius = 8
+        contentView.layer.borderWidth = 1
+        contentView.layer.borderColor = #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1)
+    }
+    
+    func setup() {
+        titleLabel.text = "loading..."
+        priceLabel.text = ""
+        discountedPriceLabel.attributedText = NSAttributedString(string: "")
+        stockLabel.text = ""
+    }
+    
+    func setupItem() {
+        let numberFormatter = NumberFormatter()
+                numberFormatter.numberStyle = .decimal
+        if let data: Item = self.item {
+            thumbnailImageView.downloadImage(from: data.thumbnails[0])
+            titleLabel.text = data.title
+       
+            guard let dataPrice = numberFormatter.string(from: NSNumber(value: data.price)) else { return }
+            priceLabel.text = data.currency + " \(dataPrice)"
+            priceLabel.textColor = UIColor.lightGray
+            
+ 
+            discountedPriceLabel.textColor = UIColor.lightGray
+            if let discountedPrice = data.discountedPrice {
+                discountedPriceLabel.text = data.currency + " \(discountedPrice)"
+                priceLabel.attributedText = priceLabel.text?.attributedStrikeThrough()
+                priceLabel.textColor = UIColor.red
+            }
+            
+            
+            if data.stock == 0 {
+                stockLabel.text = "품절"
+                stockLabel.textColor = UIColor.orange
+            } else {
+                guard let datastock = numberFormatter.string(from: NSNumber(value: data.stock)) else { return }
+                stockLabel.text = "잔여수량 : " + "\(datastock)"
+                stockLabel.textColor = UIColor.lightGray
+            }
+        } else {
+            print("no item")
+        }
+    }
+}
+
--- OpenMarket/OpenMarket/ViewController/GridVC.swift
+++ OpenMarket/OpenMarket/ViewController/GridVC.swift
@@ -18,6 +18,15 @@ class GridVC: UIViewController {
         
         collectionView.dataSource = self
         notificationAddObserver()
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        let myCustomCollectionViewCellNib = UINib(nibName: String(describing: GridCollectionViewCell.self), bundle: nil)
+        
+       
+        self.collectionView.register(myCustomCollectionViewCellNib, forCellWithReuseIdentifier: String(describing: GridCollectionViewCell.self))
+        
+        // 콜렉션뷰의 콜렉션뷰 레이아웃을 설정한다.
+        self.collectionView.collectionViewLayout = createCompositionalLayoutForGrid()
     }
     
     func notificationAddObserver() {
@@ -35,6 +44,47 @@ class GridVC: UIViewController {
     }
 }
 
+extension GridVC {
+    fileprivate func createCompositionalLayoutForGrid() -> UICollectionViewLayout {
+        print("createCompositionalLayoutForSecond() called")
+        // 콤포지셔널 레이아웃 생성
+        let layout = UICollectionViewCompositionalLayout{
+            // 만들게 되면 튜플 (키: 값, 키: 값) 의 묶음으로 들어옴 반환 하는 것은 NSCollectionLayoutSection 콜렉션 레이아웃 섹션을 반환해야함
+            (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            // 아이템에 대한 사이즈 - absolute 는 고정값, estimated 는 추측, fraction 퍼센트
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+            
+            // 위에서 만든 아이템 사이즈로 아이템 만들기
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            // 아이템 간의 간격 설정
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+            
+            // 변경할 부분
+            let groupHeight =  NSCollectionLayoutDimension.fractionalWidth(1/2)
+            
+            // 그룹사이즈
+            let grouSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: groupHeight)
+            
+            // 그룹사이즈로 그룹 만들기
+//            let group = NSCollectionLayoutGroup.horizontal(layoutSize: grouSize, subitems: [item, item, item])
+            
+            // 변경할 부분
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: grouSize, subitem: item, count: 2)
+            
+            // 그룹으로 섹션 만들기
+            let section = NSCollectionLayoutSection(group: group)
+//            section.orthogonalScrollingBehavior = .groupPaging
+            
+            // 섹션에 대한 간격 설정
+            section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+            return section
+        }
+        return layout
+    }
+}
+
 extension GridVC: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -42,11 +92,8 @@ extension GridVC: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: GridVCCell.self), for: indexPath) as! GridVCCell
         
-        cell.layer.borderColor = UIColor.lightGray.cgColor
-        cell.layer.borderWidth = 1.0
-        cell.layer.cornerRadius = 10.0
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: GridCollectionViewCell.self), for: indexPath) as! GridCollectionViewCell
         
         cell.setup()
 
