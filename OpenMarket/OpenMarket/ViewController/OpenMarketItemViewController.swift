73cb44b775b7f76255f8f4de7b7915f1ba9c8d38
Inwoo Hwang <dlsdn1207@yonsei.ac.kr>
2021-06-19 00:33:17 +0900

    Refactor : 비밀번호 textField 알림 추가
    -  유저가 textfield를 빈값으로 입력을 끝내거나 숫자만 입력시
       경고 메세지 나오도록 설정

--- OpenMarket/OpenMarket/ViewController/OpenMarketItemViewController.swift
+++ OpenMarket/OpenMarket/ViewController/OpenMarketItemViewController.swift
@@ -267,6 +267,31 @@ extension OpenMarketItemViewController: UITextFieldDelegate {
 }
 
 extension OpenMarketItemViewController: TextFieldConvertible {
+    
+    func alertInvalidTextField(_ alertController: UIAlertController) {
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func convertPasswordTextFieldToDictionary(_ itemToPost: OpenMarketItemToPost, _ textField: UITextField) {
+        itemInformation.updateValue(textField.text, forKey: itemToPost.key)
+        let alertController = UIAlertController(title: "비밀번호 설정", message: "비밀번호를 영문, 숫자를 사용해서 입력 해 주세요", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(action)
+        
+        guard let text = textField.text,
+              Int(text) == nil,
+              !text.isEmpty else {
+            alertInvalidTextField(alertController)
+            return
+        }
+
+        itemInformation.updateValue(text, forKey: itemToPost.key)
+    }
+    
+    func convertOptionalTextFieldToDictionary(_ itemToPost: OpenMarketItemToPost, _ textField: UITextField) {
+        itemInformation.updateValue(textField.text, forKey: itemToPost.key)
+    }
+    
     func convertTextFieldToDictionary(_ itemToPost: OpenMarketItemToPost, _ textField: UITextField) {
         guard let text = textField.text else { return }
         if let number = Int(text) {
