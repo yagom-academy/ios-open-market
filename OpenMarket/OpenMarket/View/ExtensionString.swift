c59372d71936f7c570f258bf8d6f7bf5cf669212
kio <un19451024@gmail.com>
2021-06-21 15:33:28 +0900

    Chore: 불필요한 코드 삭제

--- OpenMarket/OpenMarket/View/ExtensionString.swift
+++ OpenMarket/OpenMarket/View/ExtensionString.swift
@@ -13,10 +13,4 @@ extension String {
         attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: NSMakeRange(0,attributeString.length))
         return attributeString
     }
-    
-    func removeStrikeThrough() -> NSAttributedString {
-        let attributeString = NSMutableAttributedString(string: self)
-        attributeString.removeAttribute(NSAttributedString.Key.strikethroughStyle, range: NSMakeRange(0, attributeString.length))
-        return attributeString
-    }
 }
