3602bf0a070aba17a2175469522a551c9ccfebab
jaemuyeo <jemking94@gmail.com>
2021-08-10 14:30:50 +0900

    feat: MarketItem 타입 구현
    * Codable 채택
    * CodinKeys 추가: registrationDate

---
+++ OpenMarket/OpenMarket/Models/MarketItem.swift
@@ -0,0 +1,24 @@
+//
+//  MarketItem.swift
+//  OpenMarket
+//
+//  Created by 잼킹 on 2021/08/10.
+//
+
+import Foundation
+
+struct MarketItem: Codable {
+    var id: Int
+    var title: String
+    var descriptions: String
+    var price: Int
+    var currency: String
+    var stock: Int
+    var thumbnails: [String]
+    var images: [String]
+    var registrationDate: Double
+    
+    enum CodinKeys: String, CodingKey {
+        case registrationDate = "registration_date"
+    }
+}
