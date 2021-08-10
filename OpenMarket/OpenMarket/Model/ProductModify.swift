1a8ce2fe5125146ba4c82e645462b7aa117b7d32
pyeondaeho <pdhou23@kakao.com>
2021-08-10 16:29:52 +0900

    feat: ProductModify, ProductDelete 모델 타입 구현

---
+++ OpenMarket/OpenMarket/Model/ProductModify.swift
@@ -0,0 +1,27 @@
+//
+//  ProductModify.swift
+//  OpenMarket
+//
+//  Created by Charlotte, Hosinging on 2021/08/10.
+//
+
+import Foundation
+
+struct ProductModify: Codable, ContainDescriptionProductProtocol {
+    var description: String
+    var images: [String]
+    var id: Int
+    var title: String
+    var price: Int
+    var currency: String
+    var stock: String
+    var discountedPrice: Int
+    var thumnails: [String]
+    var registrationDate: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case description, images, id, title, price, currency, stock, thumnails
+        case discountedPrice = "discounted_price"
+        case registrationDate = "registration_date"
+    }
+}
