4b52f1b4e1aebbb7637142802f3939e0430b4dd0
YongHoonJJo <starctak@gmail.com>
2021-08-10 18:25:13 +0900

    feat: JSONDecodable protocol + extension 구현
    - readLocalFile(for:) : json 파일을 읽어오는 메서드
    - decodeJSON<T>(from:) : Data 를 디코딩하는 메서드
    - MarketItems 타입 내 프로퍼티 옵셔널 추가.

--- OpenMarket/OpenMarket/Models/MarketItem.swift
+++ OpenMarket/OpenMarket/Models/MarketItem.swift
@@ -10,15 +10,18 @@ import Foundation
 struct MarketItem: Codable {
     var id: Int
     var title: String
-    var descriptions: String
+    var descriptions: String?
     var price: Int
+    var discountedPrice: Int?
     var currency: String
     var stock: Int
     var thumbnails: [String]
-    var images: [String]
+    var images: [String]?
     var registrationDate: Double
     
-    enum CodinKeys: String, CodingKey {
+    enum CodingKeys: String, CodingKey {
         case registrationDate = "registration_date"
+        case discountedPrice = "discounted_price"
+        case id, title, descriptions, price, currency, stock, thumbnails, images
     }
 }
