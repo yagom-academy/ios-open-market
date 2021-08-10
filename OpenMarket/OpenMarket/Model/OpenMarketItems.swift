82dbab346cf6cb7d456559ed641093bba9abf5de
Ldoy <leedoy7989@gmail.com>
2021-08-10 15:54:13 +0900

    feat: OpenMarketItems와 ParsingManager의 parse  메서드 이용한 유닛테스트 진행

--- OpenMarket/OpenMarket/Model/OpenMarketItems.swift
+++ OpenMarket/OpenMarket/Model/OpenMarketItems.swift
@@ -9,28 +9,30 @@ import Foundation
 
 struct OpenMarketItems: Codable {
     let page: Int
-    let items: [Item]?
-}
-
-struct Item: Codable {
-    let id: Int
-    let title: String
-    let price: Int
-    let currency: String
-    let stock: Int
-    let discountedPrice: String?
-    let thumbnails: [String]
-    let registrationDate: Double
+    let items: [Item]
     
-    enum CodingKeys: String, CodingKey {
-        case id
-        case title
-        case price
-        case currency
-        case stock
-        case discountedPrice = "discounted_price"
-        case thumbnails
-        case registrationDate = "registration_date"
+    struct Item: Codable {
+        let id: Int
+        let title: String
+        let price: Int
+        let currency: String
+        let stock: Int
+        let discountedPrice: Int?
+        let thumbnails: [String]
+        let registrationDate: Double
+        
+        enum CodingKeys: String, CodingKey {
+            case id
+            case title
+            case price
+            case currency
+            case stock
+            case discountedPrice = "discounted_price"
+            case thumbnails
+            case registrationDate = "registration_date"
+        }
     }
 }
+
+
 
--- OpenMarket/OpenMarketTests/OpenMarketTests.swift
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -9,14 +9,50 @@ import XCTest
 @testable import OpenMarket
 
 class OpenMarketTests: XCTestCase {
-    //데이터
-    //테스트 item, items 각각 파싱 되는지
     func test_OpenMarketItems의_페이지가_1이다() {
         //given
         let assetData = NSDataAsset(name: "Items")!
+        
         //when
-        let decodedData = try! JSONDecoder().decode(OpenMarketItems.self, from: assetData.data)
+        let decodedData = try! ParsingManager.parse(data: assetData.data, type: OpenMarketItems.self)
+        
         //then
         XCTAssertEqual(decodedData.page, 1)
+    }
+    
+    func test_OpenMarketItems의_items배열의첫번째인덱스의id가_1이다() {
+        //given
+        let assetData = NSDataAsset(name: "Items")!
+        
+        //when
+        let decodedData = try! ParsingManager.parse(data: assetData.data, type: OpenMarketItems.self)
+        
+        //then
+        XCTAssertEqual(decodedData.items.first?.id, 1)
+    }
+    
+    func test_Item에셋파일을_OpenMarketItems타입으로파싱했을때_id가_1이다() {
+        //given
+        let assetData = NSDataAsset(name: "Item")!
+        
+        //when
+        let decodedData = try! ParsingManager.parse(data: assetData.data, type: OpenMarketItems.Item.self)
+        
+        //then
+        XCTAssertEqual(decodedData.id, 1)
+    }
+    
+    func test_Item에셋파일을_OpenMarketItems타입으로파싱했을때_실패한다() {
+        //given
+        let assetData = NSDataAsset(name: "Items")!
+        
+        //when
+        do {
+            _ = try ParsingManager.parse(data: assetData.data, type: OpenMarketItems.self)
+        } catch let error as ParsingError {
+            //then
+            XCTAssertEqual(error, .decodingError)
+        } catch {
+        }
     }
 }
