1d46ce8f56dd0185df8a159a895060e703f515c1
pyeondaeho <pdhou23@kakao.com>
2021-08-10 15:36:12 +0900

    feat: ProductListSearch.swift파일 생성 - ProductListSearch타입 구현 - Product타입 구현

--- OpenMarket/OpenMarket/Model/ProductListSearch.swift
+++ OpenMarket/OpenMarket/Model/ProductListSearch.swift
@@ -7,4 +7,24 @@
 
 import Foundation
 
+struct ProductListSearch: Codable {
+    let page: Int
+    let items: [Product]
+}
 
+struct Product: Codable, ProductProtocol {
+    var id: Int
+    var title: String
+    var price: Int
+    var currency: String
+    var stock: String
+    var discountedPrice: Int
+    var thumnails: [String]
+    var registrationDate: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case id, title, price, currency, stock, thumnails
+        case discountedPrice = "discounted_price"
+        case registrationDate = "registration_date"
+    }
+}
