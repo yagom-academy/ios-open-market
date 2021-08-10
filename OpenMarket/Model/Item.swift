1b4f39b3d9518e9698fa9ea04a587f5e008478f5
brian2931 <sem789456@gmail.com>
2021-08-10 14:46:38 +0900

    feat: added Item type

---
+++ OpenMarket/Model/Item.swift
@@ -0,0 +1,29 @@
+//
+//  Item.swift
+//  OpenMarket
+//
+//  Created by 수박, ehd on 2021/08/10.
+//
+
+import Foundation
+
+struct Item {
+    
+    let id: Int
+    let title: String
+    let descriptions: String?
+    let price: Int
+    let currency: String
+    let stock: Int
+    let thumbnailURLs: [String]
+    let imageURLs: [String]?
+    let registrationDate: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case id, title, descriptions, price, currency, stock
+        case thumbnailURLs = "thumbnails"
+        case imageURLs = "images"
+        case registrationDate = "registration_date"
+    }
+}
+
