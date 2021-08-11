321b02523fdbd22df967517817ae729b22fec02c
joey-ful <isjoeyful@gmail.com>
2021-08-11 14:54:19 +0900

    feat: Item 모델 타입 구현
    * Item과 Page 타입 모두 Decodable 채택

---
+++ OpenMarket/OpenMarket/Item.swift
@@ -0,0 +1,27 @@
+//
+//  Item.swift
+//  OpenMarket
+//
+//  Created by 홍정아 on 2021/08/11.
+//
+
+import Foundation
+
+struct Item: Decodable {
+    let id: Int
+    let title: String
+    let descriptions: String
+    let price: Int
+    let currency: String
+    let stock: Int
+    let discountedPrice: Int?
+    let thumbnails: [String]
+    let images: [String]
+    let registrationDate: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case id, title, descriptions, price, currency, stock, thumbnails, images
+        case discountedPrice = "discounted_price"
+        case registrationDate = "registration_date"
+    }
+}
--- OpenMarket/OpenMarket/Page.swift
+++ OpenMarket/OpenMarket/Page.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Page {
+struct Page: Decodable {
     let page: Int
     let items: [Page.Item]
     
