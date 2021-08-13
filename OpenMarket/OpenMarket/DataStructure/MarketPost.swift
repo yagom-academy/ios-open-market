df25d698fe215b56b93af62a124161595b934351
jgkim1008 <rlawnsrjs100@naver.com>
2021-08-13 21:05:14 +0900

    feat: 상품 등록 및 수정을 위한 MarketPost 타입 구현

---
+++ OpenMarket/OpenMarket/DataStructure/MarketPost.swift
@@ -0,0 +1,19 @@
+//
+//  MarketPost.swift
+//  OpenMarket
+//
+//  Created by 김준건 on 2021/08/13.
+//
+
+import UIKit
+
+struct MarketPost {
+    let title: String
+    let descriptions: String
+    let price: Int
+    let currency: String
+    let stock: Int
+    let discountedPrice: Int?
+    let images: [UIImage]
+    let password: String
+}
