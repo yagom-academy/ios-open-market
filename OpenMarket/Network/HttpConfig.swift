e46206b0e2101d29157655280a3dfe431fe652b6
soo941226 <83933153+soo941226@users.noreply.github.com>
2021-08-18 00:18:08 +0900

    feat: Add UUID at HttpConfig because of form-data

--- OpenMarket/Network/HttpConfig.swift
+++ OpenMarket/Network/HttpConfig.swift
@@ -10,4 +10,8 @@ import Foundation
 enum HttpConfig {
     static let baseURL = "https://camp-open-market-2.herokuapp.com/"
     static let invailedPath = "AppError: current path is invailed"
+    
+    static var bounday: String {
+        return UUID().uuidString
+    }
 }
