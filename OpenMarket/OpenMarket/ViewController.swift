9820a6cf44c6b9794dc88a7c69e75b3d33c61812
KimWanki <seoulkwk95@gmail.com>
2021-08-17 17:29:15 +0900

    feat: HttpInfo 열거형 타입 구현

--- OpenMarket/OpenMarket/ViewController.swift
+++ OpenMarket/OpenMarket/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var url = HttpInfo.deleteItem(id: 7)
+        print(url.urlInfo.url,url.urlInfo.type)
     }
 
 
