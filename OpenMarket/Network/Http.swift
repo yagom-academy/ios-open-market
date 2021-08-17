098980836151062d7544f7adbe128efedbde9b30
soo941226 <83933153+soo941226@users.noreply.github.com>
2021-08-18 00:07:45 +0900

    refactor: Change arg of Http.getItem & extend Http
    * Extend with POST, PATCH, DELETE method

--- OpenMarket/Network/Http.swift
+++ OpenMarket/Network/Http.swift
@@ -15,13 +15,31 @@ protocol Http {
     
     func getItem(
         id: UInt,
-        completionHandler: @escaping (Result<Item, HttpError>) -> Void
+        completionHandler: @escaping (Result<ItemDetail, HttpError>) -> Void
     )
 }
 
 
 extension Http {
-    func postItem(){}
-    func patchItem(){}
-    func deleteItem(){}
+    func postItem(_ item: Item){
+        let baseURL = HttpConfig.baseURL + HttpMethod.post.type
+        
+        
+        guard let url = URL(string: baseURL) else {
+            return
+        }
+        
+        let boundary = HttpConfig.bounday
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = HttpMethod.post.type
+    }
+    
+    func patchItem(_ item: Item){
+        
+    }
+    
+    func deleteItem(password: String){
+        
+    }
 }
