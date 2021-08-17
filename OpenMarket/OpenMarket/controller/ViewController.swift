7d84f42f3037fce5c0ab03838da5f47a699e9f48
soo941226 <83933153+soo941226@users.noreply.github.com>
2021-08-18 00:14:00 +0900

    chore: Renew Directories

---
+++ OpenMarket/OpenMarket/controller/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  OpenMarket - ViewController.swift
+//  Created by yagom.
+//  Copyright © yagom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        undesirable Test()
+    }
+}
+
+
+extension ViewController {
+    func undesirableTest(){
+        
+        let session = Session()
+        
+        let pageIndex: UInt = 1
+        let itemIndexWhichIsExist: UInt = 43
+        let itemIndexWhichIsNotExist: UInt = 9999
+        
+        session.getItems(pageIndex: pageIndex) { result in
+            print("")
+            switch result {
+            case .success(let itemList):
+                print("lists: \(itemList)")
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
+        session.getItem(id: itemIndexWhichIsExist) { result in
+            print("")
+            switch result {
+            case .success(let item):
+                print("item be existed: \(item)")
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
+        session.getItem(id: itemIndexWhichIsNotExist) { result in
+            print("")
+            switch result {
+            case .success(let item):
+                print("item be not existed: \(item)")
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
+
