a8c9593759b6ad43cd758ce0c98021863edb6bbc
soo941226 <83933153+soo941226@users.noreply.github.com>
2021-08-18 23:38:37 +0900

    feat: Add three methods to Session about postItem

--- OpenMarket/Network/Session.swift
+++ OpenMarket/Network/Session.swift
@@ -5,7 +5,7 @@
 //  Created by kjs on 2021/08/17.
 //
 
-import Foundation
+import UIKit
 
 struct Session: Http, Decoder {
     
@@ -39,6 +39,57 @@ struct Session: Http, Decoder {
         }
     }
     
+    func postItem(
+        item: ItemRequestable,
+        images: [UIImage],
+        completionHandler: @escaping (Result<ItemDetail, HttpError>) -> Void
+    ) {
+        let path = HttpConfig.baseURL + HttpMethod.post.path
+        
+        guard let url = URL(string: path) else {
+            return
+        }
+        
+        let boundary = HttpConfig.boundary
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = HttpConfig.POST
+        request.setValue(
+            HttpConfig.requestHeader + boundary,
+            forHTTPHeaderField: HttpConfig.contentType
+        )
+        
+        let boundaryWithPrefix = HttpConfig.boundaryPrefix + boundary
+        var imageDatas = [Data]()
+        
+        for image in images {
+            guard let jpegData = image.jpegData(compressionQuality: 1) else {
+                continue
+            }
+            
+            imageDatas.append(jpegData)
+        }
+        
+        request.httpBody = buildedFormData(from: imageDatas, boundary: boundaryWithPrefix)
+        
+        let stringDatas = buildedFormData(from: item, boundary: boundaryWithPrefix)
+        
+        request.httpBody?.append(stringDatas)
+
+        URLSession.shared
+            .dataTask(with: request) { data, response, error in
+                guard let data = guardedDataAbout(data: data, response: response, error: error) else {
+                    let error = HttpError(message: HttpConfig.unknownError)
+                    completionHandler(.failure(error))
+                    return
+                }
+
+                completionHandler(parse(from: data, to: ItemDetail.self))
+
+            }
+            .resume()
+    }
+    
 
     func buildedFormData<Model>(
         from model: Model,
@@ -51,14 +102,32 @@ struct Session: Http, Decoder {
             guard let value = value else { continue }
             
             form += boundary + .newLine
-            form += "Content-Disposition: form-data;"
-            form += "name=\"\(key)" + .newLine + .newLine
+            form += "Content-Disposition: form-data; "
+            form += "name=\"\(key)\"" + .newLine + .newLine
             form += "\(String(describing: value))" + .newLine
         }
         
         form += boundary +  "--"
         
-        return form.data(using: .utf8)!
+        return form.data(using: .utf8) ?? Data()
+    }
+    
+    func buildedFormData(
+        from datas: [Data],
+        boundary: String
+    ) -> Data {
+        var form = Data()
+        
+        for data in datas {
+            form += (boundary + String.newLine).utf8
+            form += "Content-Disposition: form-data; name=\"images[]\"".utf8
+            form += (String.newLine + "Content-Type: image/jpeg").utf8
+            form += (String.newLine + .newLine).utf8
+            form += data
+            form += String.newLine.utf8
+        }
+        
+        return form
     }
     
     private func guardedDataAbout(
