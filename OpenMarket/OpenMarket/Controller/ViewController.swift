06baeaed44d07fe5f0c441545086ef093c1e1cbe
soo941226 <83933153+soo941226@users.noreply.github.com>
2021-08-18 23:42:12 +0900

    test: Test post method in ViewController...

--- OpenMarket/OpenMarket/Controller/ViewController.swift
+++ OpenMarket/OpenMarket/Controller/ViewController.swift
@@ -10,7 +10,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        undesirableTest()
+        let dummy = ItemRequestable(
+            title:"치킨",
+            descriptions: "지금 먹어야 가장 맛있는 치킨",
+            price: 20000,
+            currency: "KRW",
+            stock: 1,
+            discountedPrice: 22000,
+            password: "password~~"
+        )
+        
+        Session().postItem(item: dummy, images: [#imageLiteral(resourceName: "compressed")]) { result in
+            switch result {
+            case .success(let itemDetail):
+                print(itemDetail)
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
+//        undesirableTest()
     }
 }
 
@@ -33,7 +52,7 @@ extension ViewController {
                 print(error)
             }
         }
-        
+
         session.getItem(id: itemIndexWhichIsExist) { result in
             print("")
             switch result {
@@ -43,7 +62,7 @@ extension ViewController {
                 print(error)
             }
         }
-        
+
         session.getItem(id: itemIndexWhichIsNotExist) { result in
             print("")
             switch result {
