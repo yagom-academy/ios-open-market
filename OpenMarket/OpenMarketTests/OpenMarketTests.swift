b6466eecd87d178710ab14f185b220d352d240e3
YesCoach <sbsii1020@gmail.com>
2021-08-19 00:55:35 +0900

    test: 네트워크 무관 테스트 케이스 생성

--- OpenMarket/OpenMarketTests/OpenMarketTests.swift
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -8,7 +8,6 @@
 import XCTest
 @testable import OpenMarket
 class OpenMarketTests: XCTestCase {
- 
     func test_ItemsData_타입으로_디코딩을_성공한다() {
         //given
         let expectInputValue = "MockItems"
@@ -73,5 +72,43 @@ class OpenMarketTests: XCTestCase {
             //then
             XCTAssertEqual(error, JsonError.decodingFailed)
         } catch { }
+    }
+    
+    func test_getItems를_네트워크_무관_테스트에_성공한다() {
+        //given
+        let page = "1"
+        let sut = NetworkManager(session: MockURLSession(isRequestSucess: true))
+        let expectInputValue = "MockItems"
+        guard let jsonData = try? MockJsonDecoder.receiveDataAsset(assetName: expectInputValue) else {
+            return
+        }
+        //when
+        let decodedData = try! MockJsonDecoder.decodeJsonFromData(type: ItemsData.self, data: jsonData.data)
+        sut.getItems(page: page) { result in
+            switch result {
+        //then
+            case .success(let item):
+                XCTAssertEqual(item.items[0].title, decodedData.items[0].title)
+            case .failure:
+                XCTFail()
+            }
+        }
+    }
+    
+    func test_getItems를_네트워크_무관_테스트에_실패한다() {
+        //given
+        let page = "1"
+        let sut = NetworkManager(session: MockURLSession(isRequestSucess: true))
+        
+        //when
+        sut.getItems(page: page) { result in
+            switch result {
+            //then
+            case .success:
+                XCTFail()
+            case .failure(let error):
+                XCTAssertEqual(error.localizedDescription, "unknownError")
+            }
+        }
     }
 }
