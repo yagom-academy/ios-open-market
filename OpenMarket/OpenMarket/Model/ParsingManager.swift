15c79f0a3894ad57ab01977e83c05597cc419a7d
YongwooKim <keepingitflow@gmail.com>
2021-08-19 20:23:27 +0900

    feat: JSON 파일 파싱작업할 ParsingManager 타입 구현
    convertFromSnakeCase 속성 추가한 초기화 추가
    주어진 Data 와 원하는 T.Type으로 Decoding 하는 parse 메서드 구현

---
+++ OpenMarket/OpenMarket/Model/ParsingManager.swift
@@ -0,0 +1,29 @@
+//
+//  ParsingManager.swift
+//  OpenMarket
+//
+//  Created by Yongwoo Marco on 2021/08/19.
+//
+
+import Foundation
+
+enum ParsingError: Error {
+    case failDecoding
+}
+
+struct ParsingManager {
+    let decoder = JSONDecoder()
+    
+    init() {
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+    }
+
+    func parse<T: Decodable>(_ data: Data, to type: T.Type) -> Result<T, Error> {
+        do {
+            let result = try decoder.decode(type, from: data)
+            return .success(result)
+        } catch {
+            return .failure(ParsingError.failDecoding)
+        }
+    }
+}
