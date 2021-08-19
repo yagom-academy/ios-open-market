d3eac12efe9f88c94328338cccdc4415ac69246c
ellenJ <jcrescent61@gmail.com>
2021-08-19 22:44:44 +0900

    refactor: 불필요한 코드 삭제

--- OpenMarket/OpenMarket/ViewController.swift
+++ OpenMarket/OpenMarket/ViewController.swift
@@ -11,8 +11,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        var url = HttpInfo.deleteItem(id: 7)
-        print(url.urlInfo.url,url.urlInfo.type)
     }
 
 
