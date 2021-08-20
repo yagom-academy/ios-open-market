e9a8bc596f3f5d1807d0e14b4f5282975d4c1f26
ellenJ <jcrescent61@gmail.com>
2021-08-20 15:29:56 +0900

    feat: NetworkHandler request 메소드 구현

--- OpenMarket/OpenMarket/SupportingFiles/NetworkHandler.swift
+++ OpenMarket/OpenMarket/SupportingFiles/NetworkHandler.swift
@@ -9,4 +9,28 @@ import Foundation
 
 struct NetworkHandler {
     
+    func request(api: OpenMarketAPI, form: DataForm? = nil) {
+        guard let url = URL(string: api.request.url) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = api.request.method.description
+        request.httpBody = try? form?.createBody()
+        request.setValue(form?.contentType, forHTTPHeaderField: "Content-Type")
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { data, response, error in
+            
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+        }.resume()
+    }
 }
