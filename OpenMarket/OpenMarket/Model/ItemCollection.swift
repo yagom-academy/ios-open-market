1790a157af2aef0a4c29b66efb650d2a906087ac
ellenJ <jcrescent61@gmail.com>
2021-08-23 17:56:27 +0900

    feat: 모델 클래스로 변경 및 ItemInfo 타입 구현

--- OpenMarket/OpenMarket/Model/ItemCollection.swift
+++ OpenMarket/OpenMarket/Model/ItemCollection.swift
@@ -7,7 +7,18 @@
 
 import Foundation
 
-struct ItemCollection: Codable {
+class ItemCollection: Codable {
     let page: Int
     let items: [Item]
+}
+
+class Item: Codable {
+    let id: Int
+    let title: String
+    let price: Int
+    let currency: String
+    let stock: Int
+    let discountedPrice: Int?
+    let thumbnails: [String]
+    let registrationDate: Double
 }
