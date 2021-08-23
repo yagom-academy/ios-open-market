0204b243fe0bf5dd619b1b21894ee6310c4a08b1
jaemuyeo <jemking94@gmail.com>
2021-08-23 17:59:18 +0900

    feat: ImageDownloadManager 타입 선언 및 프로퍼티, 메서드 구현
    - Data(contentsOf:) 메서드를 대체하기 위한 urlSession을 이용한 이미지 다운로드 로직 구현

---
+++ OpenMarket/OpenMarket/Managers/ImageDownloadManager.swift
@@ -0,0 +1,44 @@
+//
+//  ImageDownloadManager.swift
+//  OpenMarket
+//
+//  Created by Jost, 잼킹 on 2021/08/23.
+//
+
+import UIKit
+
+class ImageDownloadManager {
+    private var imageDownloadTasks: [URLSessionTask] = []
+    
+    func downloadImage(at index: Int, with url: String, completion: @escaping (IndexPath) -> Void) {
+        guard ImageCacheManager.shared.loadCachedData(for: url) == nil,
+              let imageUrl = URL(string: url),
+              !imageDownloadTasks.contains(where: { task in
+                return task.originalRequest?.url == imageUrl
+              }) else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: imageUrl) { (data, resopnce, error) in
+            guard error == nil else {
+                return
+            }
+            
+            if let imageData = data,
+               let image = UIImage(data: imageData) {
+                ImageCacheManager.shared.setData(of: image, for: url)
+                let reloadItemIndexPath = IndexPath(row: index, section: 0)
+                completion(reloadItemIndexPath)
+                self.completeTask()
+            }
+        }
+        task.resume()
+        imageDownloadTasks.append(task)
+    }
+    
+    private func completeTask() {
+        imageDownloadTasks = imageDownloadTasks.filter {
+            $0.state != .completed
+        }
+    }
+}
