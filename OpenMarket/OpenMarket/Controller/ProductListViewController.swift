fdf559e5408a057bccf5cc600b8944a5239faf97
jgkim1008 <rlawnsrjs100@naver.com>
2021-08-23 18:46:15 +0900

    feat: ProductListVC에서 제품 리스트에 대한 데이터를 요청하는 메서드 작성
    * collectionView의 오토 레이아웃 수정
    * Indicator 추가

--- OpenMarket/OpenMarket/Controller/ProductListViewController.swift
+++ OpenMarket/OpenMarket/Controller/ProductListViewController.swift
@@ -7,17 +7,42 @@
 import UIKit
 
 class ProductListViewController: UIViewController {
-    @IBOutlet weak var openMarketCollectionView: UICollectionView!
-
+    @IBOutlet private weak var openMarketCollectionView: UICollectionView!
+    @IBOutlet private weak var loadingIndicatorView: UIActivityIndicatorView!
+    private var networkManger = NetworkManager()
+    private var nextPageNumToBring = 1
+    private var productList: [Product] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.openMarketCollectionView.dataSource = self
         openMarketCollectionView.register(UINib(nibName: "OpenMarketItemCell", bundle: nil), forCellWithReuseIdentifier: "OpenMarketItemCell")
-        // Do any additional setup after loading the view.
+        loadingIndicatorView.startAnimating()
+        loadNextProductList(on: nextPageNumToBring)
     }
 
+}
+
+//MARK:- Fetch Product List
+extension ProductListViewController {
+    func loadNextProductList(on pageNum: Int) {
+        networkManger.lookUpProductList(on: pageNum) { result in
+            switch result {
+            case .success(let fetchedData):
+                let list = self.handleFetchedList(data: fetchedData).items
+                self.productList.append(contentsOf: list)
+                self.nextPageNumToBring += 1
+            case .failure(let error):
+                break
+            }
+        }
+    }
+    
+    func handleFetchedList(data: Data) -> Products {
+    }
 
 }
+
 extension ProductListViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 20
