bffca07d5c8cc6360d391eab7a7b7c217bffa78f
jgkim1008 <rlawnsrjs100@naver.com>
2021-08-24 21:52:03 +0900

    feat: custom cell에 컨텐츠들을 적용하는 메서드 구현
    * 컬렉션뷰 에서 cell은 각각의 item을 나타내는것이 명확하기에 불필요한 접두어 제거
    * cell이 재사용되기 전 완료되지 않은 dataTask가 있다면 취소되도록 prepareForReuse()에 코드 작성

--- OpenMarket/OpenMarket/View/CustomView/OpenMarketItemCell.swift
+++ OpenMarket/OpenMarket/View/CustomView/OpenMarketItemCell.swift
@@ -8,16 +8,41 @@
 import UIKit
 
 class OpenMarketItemCell: UICollectionViewCell {
-    
-    @IBOutlet weak var itemImageView: UIImageView!
-    @IBOutlet weak var itemTitleLabel: UILabel!
-    @IBOutlet weak var itemDiscountedPriceLabel: UILabel!
-    @IBOutlet weak var itemPriceLabel: UILabel!
-    @IBOutlet weak var itemStockLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var discountedPriceLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var stockLabel: UILabel!
+    private var imageDataTask: URLSessionDataTask?
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageDataTask?.cancel()
+        imageDataTask = nil
+    }
+    
+    func configure(from product: Product, with dataTask: URLSessionDataTask?) {
+        resetContents()
+        
+        imageDataTask = dataTask
+        titleLabel.text = product.title
+        priceLabel.text = product.price.description
+        stockLabel.text = product.stock.description
+        if let discountedPrice = product.discountedPrice {
+            discountedPriceLabel.text = discountedPrice.description
+        }
+    }
+    
+    private func resetContents() {
+        titleLabel.text = nil
+        priceLabel.text = nil
+        stockLabel.text = nil
+        discountedPriceLabel.text = nil
     }
 
 }
