00d784a1586098aee5371c459ccb284600b54e04
YesCoach <sbsii1020@gmail.com>
2021-08-24 17:39:20 +0900

    test: UnitTest 수정 Decoder 예외 처리(XCTFail()) Network 무관 테스트 수정

--- OpenMarket/OpenMarketTests/OpenMarketTests.swift
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -13,11 +13,13 @@ class OpenMarketTests: XCTestCase {
         //given
         let expectInputValue = "MockItems"
         guard let jsonData = try? MockJsonDecoder.receiveDataAsset(assetName: expectInputValue) else {
-            return
+            return XCTFail()
         }
         
         //when
-        let decodedData = try! MockJsonDecoder.decodeJsonFromData(type: ItemsData.self, data: jsonData.data)
+        guard let decodedData = try? MockJsonDecoder.decodeJsonFromData(type: ItemsData.self, data: jsonData.data) else {
+            return XCTFail()
+        }
         let result = decodedData.items[0].title
         let expectResult = "MacBook Pro"
         
@@ -29,7 +31,7 @@ class OpenMarketTests: XCTestCase {
         //given
         let expectInputValue = "MockItems"
         guard let jsonData = try? MockJsonDecoder.receiveDataAsset(assetName: expectInputValue) else {
-            return
+            return XCTFail()
         }
         
         //when
@@ -46,11 +48,13 @@ class OpenMarketTests: XCTestCase {
         //given
         let expectInputValue = "MockItem"
         guard let jsonData = try? MockJsonDecoder.receiveDataAsset(assetName: expectInputValue) else {
-            return
+            return XCTFail()
         }
         
         //when
-        let decodedData = try! MockJsonDecoder.decodeJsonFromData(type: ItemData.self, data: jsonData.data)
+        guard let decodedData = try? MockJsonDecoder.decodeJsonFromData(type: ItemData.self, data: jsonData.data) else {
+            return XCTFail()
+        }
         let result = decodedData.title
         let expectResult = "MacBook Pro"
         
@@ -63,7 +67,7 @@ class OpenMarketTests: XCTestCase {
         //given
         let expectInputValue = "MockItem"
         guard let jsonData = try? MockJsonDecoder.receiveDataAsset(assetName: expectInputValue) else {
-            return
+            return XCTFail()
         }
         
         //when
@@ -75,17 +79,19 @@ class OpenMarketTests: XCTestCase {
         } catch { }
     }
     
-    func test_get을_호출시_네트워크_무관_테스트에_성공한다() {
+    func test_get을_호출시_디코드가_성공하고_네트워크_리스폰스를_200번대로_받으면_테스트에_성공한다() {
         //given
-        let id = 13
-        let sut = NetworkManager(session: MockURLSession(isRequestSucess: true), valuableMethod: [.get])
+        let expectBool = true
         let expectInputValue = "MockItem"
+        let sut = NetworkManager(session: MockURLSession(isRequestSucess: expectBool), valuableMethod: [.get])
         guard let jsonData = try? MockJsonDecoder.receiveDataAsset(assetName: expectInputValue) else {
-            return
+            return XCTFail()
         }
         //when
-        let decodedData = try! MockJsonDecoder.decodeJsonFromData(type: ItemData.self, data: jsonData.data)
-        sut.commuteWithAPI(API: GetItemAPI(id: id)) { result in
+        guard let decodedData = try? MockJsonDecoder.decodeJsonFromData(type: ItemData.self, data: jsonData.data) else {
+            return XCTFail()
+        }
+        sut.commuteWithAPI(API: GetItemAPI(id: 13)) { result in
             switch result {
             //then
             case .success(let item):
@@ -99,31 +105,27 @@ class OpenMarketTests: XCTestCase {
         }
     }
     
-    func test_get의_Request가_제대로_구성되지않으면_실패한다(){
+    func test_get의_HTTP메서드가_제대로_구성되지_않으면_테스트에_실패한다() {
         //given
-        let id = 13
-        let sut = NetworkManager(session: MockURLSession(isRequestSucess: true), valuableMethod: [.post])
+        let sut = NetworkManager(session: MockURLSession(isRequestSucess: false), valuableMethod: [.post])
         
         //when
-        sut.commuteWithAPI(API: GetItemAPI(id: id)){ result in
-            guard case .failure(let error) = result else {
-                return XCTFail()
+        sut.commuteWithAPI(API: GetItemAPI(id: 13)){ result in
+            if case .failure(let error) = result {
+                //then
+                XCTAssertEqual(error as? NetworkError, NetworkError.invalidResult)
             }
-            //then
-            XCTAssertEqual(error as? NetworkError, NetworkError.invalidResult)
-            
         }
         
     }
     
     
-    func test_get을_호출시_네트워크_무관_테스트에_실패한다() {
+    func test_get을_호출시_리스폰스의_상태코드가_402이면_테스트에_실패한다() {
         //given
-        let id = 13
         let sut = NetworkManager(session: MockURLSession(isRequestSucess: false), valuableMethod: [.get])
         
         //when
-        sut.commuteWithAPI(API: GetItemAPI(id: id)){ result in
+        sut.commuteWithAPI(API: GetItemAPI(id: 13)){ result in
             guard case .failure(let error) = result else {
                 return XCTFail()
             }
