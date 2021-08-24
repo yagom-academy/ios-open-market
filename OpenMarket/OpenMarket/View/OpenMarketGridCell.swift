6a1e1e8be528d733240b1b7eadfead16eb522246
KimWanki <seoulkwk95@gmail.com>
2021-08-25 01:38:21 +0900

    refactor: OpenMarketGridCell 리팩토링. String+Extension strikethrough 구현

--- OpenMarket/OpenMarket/View/OpenMarketGridCell.swift
+++ OpenMarket/OpenMarket/View/OpenMarketGridCell.swift
@@ -8,53 +8,51 @@
 import UIKit
 
 class OpenMarketGridCell: UICollectionViewCell {
+    static let cellIdentifier = "\(OpenMarketGridCell.self)"
+    
     @IBOutlet weak var itemImage: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var discountedPriceLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var stockLabel: UILabel!
     
-    static let cellIdentifier = "\(OpenMarketGridCell.self)"
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.layer.cornerRadius = 10.0
+        self.layer.borderWidth = 1.5
+        self.layer.borderColor = UIColor.systemGray5.cgColor
+        self.layer.backgroundColor = UIColor.white.cgColor
+    }
     
     override func prepareForReuse() {
         itemImage.image = nil
+        
         titleLabel.text = nil
-        discountedPriceLabel.text = nil
         priceLabel.attributedText = nil
-        priceLabel.textColor = .gray
+        discountedPriceLabel.text = nil
         stockLabel.text = nil
-        stockLabel.textColor = .gray
-    }
-    
-    func setUpLabels(item: [Item], indexPath: IndexPath) {
+        
         titleLabel.textColor = .black
         priceLabel.textColor = .gray
         discountedPriceLabel.textColor = .gray
         stockLabel.textColor = .gray
-        
+    }
+    
+    func setUpLabels(item: [Item], indexPath: IndexPath) {
         let item = item[indexPath.item]
         titleLabel.text = item.title
-        priceLabel.text = PriceFormatter(iso: item.currency, price: item.price).formatNumber()
         stockLabel.text = "\(item.stock)"
-        
         if let discountedPrice = item.discountedPrice {
-            let formatedDiscountedPrice = PriceFormatter(iso: item.currency, price: discountedPrice).formatNumber()
-            let formattedPrice = PriceFormatter(iso: item.currency, price: item.price).formatNumber()
-            let attributedString = NSMutableAttributedString(string: formattedPrice)
-            attributedString.addAttribute(.baselineOffset, value: 0, range: NSMakeRange(0, attributedString.length))
-            attributedString.addAttribute(.strikethroughStyle, value: 1, range: NSMakeRange(0, attributedString.length))
-            discountedPriceLabel.text = formatedDiscountedPrice
-            priceLabel.attributedText = attributedString
+            discountedPriceLabel.text = PriceFormatter(iso: item.currency, price: discountedPrice).formatNumber()
+            priceLabel.attributedText = PriceFormatter(iso: item.currency, price: item.price).formatNumber().strikethroughStyle()
             priceLabel.textColor = .red
         } else {
+            priceLabel.text = PriceFormatter(iso: item.currency, price: item.price).formatNumber()
             discountedPriceLabel.text = ""
         }
-        
-        if item.stock == 0 {
-            stockLabel.text = "품절"
+        stockLabel.text = item.stock > 10000 ? "잔여수량 : 9999개" : item.stock > 0 ? "잔여수량 : \(item.stock)개" : "품절"
+        if item.stock <= 0 {
             stockLabel.textColor = .orange
-        } else {
-            stockLabel.text = "잔여수량 : \(item.stock)"
         }
     }
     
@@ -65,10 +63,14 @@ class OpenMarketGridCell: UICollectionViewCell {
             }
         }
     }
-    
-    func configureCellStyle() {
-        layer.cornerRadius = 10.0
-        layer.borderWidth = 1.5
-        layer.borderColor = UIColor.systemGray5.cgColor
+}
+
+extension String {
+    func strikethroughStyle() -> NSAttributedString {
+        let attributedString = NSMutableAttributedString(string: self)
+        attributedString.addAttribute(.baselineOffset, value: 0, range: NSMakeRange(0, attributedString.length))
+        attributedString.addAttribute(.strikethroughStyle, value: 1, range: NSMakeRange(0, attributedString.length))
+        attributedString.addAttribute(.foregroundColor, value: UIColor.red, range: NSMakeRange(0, attributedString.length))
+        return attributedString
     }
 }
