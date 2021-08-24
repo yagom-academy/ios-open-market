5fa3a5f7ac62a054013140f93353406f2296bbc3
soll4u <dasollpark@gmail.com>
2021-08-24 20:07:38 +0900

    feat: 상품 등록 화면으로 이동하는 로직 구현

---
+++ OpenMarket/OpenMarket/Controller/PostViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PostViewController.swift
+//  OpenMarket
+//
+//  Created by Dasoll Park on 2021/08/24.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
