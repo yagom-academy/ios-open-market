f961f4550d1978b65811c59c172d8e9a7455ac93
ohsg0226 <ohsg0226@naver.com>
2021-08-24 14:58:37 +0900

    feat: ProductListCollectionView 구현
    -network 통신부분 구현
    -collectionview datasource구현
    -collectionview delegate 구현

--- OpenMarket/OpenMarket/Contorller/ProductListViewController.swift
+++ OpenMarket/OpenMarket/Contorller/ProductListViewController.swift
@@ -11,8 +11,97 @@ class ProductListViewController: UIViewController {
     @IBOutlet weak var productListCollectionView: UICollectionView!
     @IBOutlet weak var loadListIndicator: UIActivityIndicatorView!
     
+    var productList = [Product]()
+    let networkManager = NetworkManager()
+    let parsingManager = ParsingManager()
+    var currentPage = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureNavigationItem()
+        configureCollectionView()
+        loadListIndicator.hidesWhenStopped = true
+        loadProductList(page: currentPage)
+    }
+    
+    private func configureNavigationItem() {
+        self.navigationItem.title = "야아 마켓"
+        let addBarButton = UIBarButtonItem(barButtonSystemItem: .add, target: nil, action: nil)
+        self.navigationItem.rightBarButtonItem = addBarButton
+    }
+    
+    private func configureCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        guard let collectionView = productListCollectionView else { return }
+        collectionView.register(ProductListCustomCollectionViewCell.nib(), forCellWithReuseIdentifier: ProductListCustomCollectionViewCell.identifier)
+        
+        collectionView.collectionViewLayout = layout
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        view.addSubview(collectionView)
+    }
+    
+    private func loadProductList(page: Int) {
+        loadListIndicator.startAnimating()
+        self.currentPage = page + 1
+        let apiModel = GetAPI.lookUpProductList(page: page, contentType: .noBody)
+        networkManager.request(apiModel: apiModel) { [self] result in
+            switch result {
+            case .success(let data):
+                guard let parsingData = parsingManager.decodingData(data: data, model: Page.self),
+                      !parsingData.products.isEmpty else { return }
+                for product in parsingData.products {
+                    productList.append(product)
+                    DispatchQueue.main.async {
+                        loadListIndicator.stopAnimating()
+                        self.productListCollectionView?.reloadData()
+                    }
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
 }
+
+extension ProductListViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView,
+                        didEndDisplaying cell: UICollectionViewCell,
+                        forItemAt indexPath: IndexPath) {
+        if indexPath.item == self.productList.count - 20 {
+            loadProductList(page: currentPage)
+        }
+    }
+}
+
+extension ProductListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProductListCustomCollectionViewCell.identifier, for: indexPath)
+        
+        guard let customCell = cell as? ProductListCustomCollectionViewCell else {
+            return cell
+        }
+        return cell
+    }
+}
+extension ProductListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width/2-10, height: collectionView.frame.height/3)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        let inset = UIEdgeInsets(top: 2, left: 5, bottom: 2, right: 5)
+        return inset
+    }
+}
+
 
