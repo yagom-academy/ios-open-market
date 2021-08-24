3893d06162293983ff57b40317270d01fbdabd96
jgkim1008 <rlawnsrjs100@naver.com>
2021-08-24 18:37:11 +0900

    refactor: 이미지 로드시 캐시를 쓰도록 코드 수정
    * 적정한 용량값을 알수 없어 기본 메모리 캐시용량을 512MB 로 설정
    * 내부캐시를 먼저 확인한후, 없을때만 서버에 요청하기 위해 해당 returnCacheDataElseLoad 정책을 사용

--- OpenMarket/OpenMarket/Manager/ImageModule.swift
+++ OpenMarket/OpenMarket/Manager/ImageModule.swift
@@ -10,9 +10,19 @@ import UIKit.UIImage
 
 struct ImageModule: ImageRequestable {
     private let rangeOfSuccessState = 200...299
+    private let customURLSession: URLSession = {
+        let oneKilobyte = 1024
+        let oneMegabyte = oneKilobyte * oneKilobyte
+        URLCache.shared.memoryCapacity = 512 * oneMegabyte
+        
+        let config = URLSessionConfiguration.default
+        config.requestCachePolicy = .returnCacheDataElseLoad
+        
+        return URLSession(configuration: config)
+    }()
     
     func loadImage(with url: URL, completionHandler: @escaping (Result<UIImage, Error>) -> Void) -> URLSessionDataTask {
-        let dataTask = URLSession.shared.dataTask(with: url) { data, response, error in
+        let dataTask = customURLSession.dataTask(with: url) { data, response, error in
             if let response = response as? HTTPURLResponse,
                rangeOfSuccessState.contains(response.statusCode),
                let data = data, let image = UIImage(data: data) {
