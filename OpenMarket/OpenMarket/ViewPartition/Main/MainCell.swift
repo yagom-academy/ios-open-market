7dc32d52705265a942959b418add989232fc8fa2
soo941226 <83933153+soo941226@users.noreply.github.com>
2021-08-27 14:51:21 +0900

    feat: During adding accessibility options in cell

--- OpenMarket/OpenMarket/ViewPartition/Main/MainCell.swift
+++ OpenMarket/OpenMarket/ViewPartition/Main/MainCell.swift
@@ -16,25 +16,65 @@ class MainCell: UICollectionViewCell {
     @IBOutlet weak var remainedStockLabel: UILabel!
     
     private let stockPrefix = "잔여수량 : "
+    private let space = " "
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         layerStyling()
+        initializeDefaultAccessibility()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         layerStyling()
+        initializeDefaultAccessibility()
     }
     
     func configure(with item: Goods) {
+        configureData(with: item)
+        configureAccessibility(with: item)
+    }
+    
+    private func configureData(with item: Goods) {
+        let pricePrefix = item.currency + space
         drawImage(with: item.thumbnailURLs.first)
         titleLabel.text = item.title
-        originalPriceLabel.text = PrefixFormatter.toDecimal(from: item.price)
-        discountedPriceLabel.text =  (item.discountedPrice?.description ?? "")
+        originalPriceLabel.text = NumberFormatter.toDecimal(
+            from: item.price,
+            withPrefix: pricePrefix
+        )
+        discountedPriceLabel.text = NumberFormatter.toDecimal(
+            from: item.discountedPrice,
+            withPrefix: pricePrefix
+        )
         remainedStockLabel.text = stockPrefix + item.stock.description
     }
     
+    private func initializeDefaultAccessibility() {
+        discountedPriceLabel.accessibilityLabel = "할인가 : "
+    }
+    
+    private func configureAccessibility(with item: Goods) {
+        if discountedPriceLabel.text != nil,
+           let text = originalPriceLabel.text{
+            originalPriceLabel.isAccessibilityElement = false
+            discountedPriceLabel.isAccessibilityElement = true
+            
+            let string = NSMutableAttributedString(string: text)
+            let range = NSRange(location: 0, length: string.length)
+            string.addAttribute(.strikethroughStyle, value: 1, range: range)
+            string.addAttribute(.foregroundColor, value: UIColor.gray, range: range)
+            
+            
+            originalPriceLabel.attributedText = string
+        } else {
+            originalPriceLabel.isAccessibilityElement = true
+            discountedPriceLabel.isAccessibilityElement = false
+            
+            discountedPriceLabel.accessibilityLabel = nil
+            originalPriceLabel.accessibilityValue = originalPriceLabel.text
+        }
+    }
     
     private func drawImage(with path: String?) {
         guard let path = path else {
