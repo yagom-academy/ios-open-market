009ce4997687aeda5ec56ca90de4de440008243a
yescoach <sbsii1020@gmail.com>
2021-08-31 18:13:11 +0900

    feat: NetworkManager 통신 담당 메소드 구현

--- OpenMarket/OpenMarket/NetworkManager/NetworkManager.swift
+++ OpenMarket/OpenMarket/NetworkManager/NetworkManager.swift
@@ -7,18 +7,53 @@
 
 import Foundation
 
+typealias Parameters = [String: Any]
+
 struct NetworkManager {
     private let session: URLSession
+    let boundary = "Boundary-\(UUID().uuidString)"
     
     init(session: URLSession) {
         self.session = session
     }
     
-    func commuteWithAPI(with url: String) {
-        guard let url = URL(string: url) else {
+    func commuteWithAPI(with API: RequestAPI) {
+        guard let url = URL(string: API.url.description) else {
+            print("Invalid URL")
             return
         }
-        let request = URLRequest(url: url)
+
+        var request = URLRequest(url: url)
+        request.httpMethod = API.method.description
+        
+        if API.contentType == .multipart {
+            request.setValue(API.contentType.description + boundary, forHTTPHeaderField: "Content-Type")
+        } else {
+            request.setValue(API.contentType.description, forHTTPHeaderField: "Content-Type")
+        }
+
+        if let api = API as? RequestableWithBody {
+            var body = Data()
+            let lineBreakPoint = "\r\n"
+
+            if let parameters = api.parameters {
+                for (key, value) in parameters {
+                    body.append("--\(boundary)\(lineBreakPoint)")
+                    body.append("Content-Disposition: form-data; name=\"\(key)\"\(lineBreakPoint + lineBreakPoint)")
+                    body.append("\(value)\(lineBreakPoint)")
+                }
+            }
+            
+            if let medias = api.images {
+                for media in medias {
+                    body.append("--\(boundary)\(lineBreakPoint)")
+                    body.append("Content-Disposition: form-data; name=\"\(media.fieldName)\"; filename=\"\(media.fileName)\"\(lineBreakPoint)")
+                    body.append("Content-Type: \(media.mimeType)\(lineBreakPoint + lineBreakPoint)")
+                    body.append(media.fileData)
+                    body.append(lineBreakPoint)
+                }
+            }
+        }
         
         session.dataTask(with: request) { data, response, error in
             if let error = error {
@@ -29,10 +64,17 @@ struct NetworkManager {
                 print("invalid response")
                 return
             }
-            
             if let data = data, let response = try? JSONDecoder().decode(Items.self, from: data) {
                 return
             }
         }.resume()
+    }
+}
+
+extension Data {
+    mutating func append(_ string: String) {
+        if let data = string.data(using: .utf8){
+            append(data)
+        }
     }
 }
---
+++ OpenMarket/OpenMarket/NetworkManager/RequestAPI.swift
@@ -0,0 +1,84 @@
+//
+//  RequestAPI.swift
+//  OpenMarket
+//
+//  Created by 박태현 on 2021/08/31.
+//
+
+import Foundation
+
+//MARK: HTTP Requeset API 프로토콜 구현부
+protocol RequestAPI {
+    var url: APIURL { get }
+    var method: APIMethod { get }
+    var contentType: ContentType { get }
+}
+
+protocol RequestableWithoutBody: RequestAPI {
+}
+
+protocol RequestableWithBody: RequestAPI {
+    var parameters: Parameters? { get }
+    var images: [Media]? { get }
+}
+
+//MARK: HTTP Request API 타입 구현부
+struct GetItemsAPI: RequestableWithoutBody {
+    var url: APIURL
+    var method: APIMethod = .get
+    var contentType: ContentType = .json
+    
+    init(page: Int) {
+        self.url = .getItems(page: page)
+    }
+}
+
+struct GetItemAPI: RequestableWithoutBody {
+    var url: APIURL
+    var method: APIMethod = .get
+    var contentType: ContentType = .json
+    
+    init(id: Int) {
+        self.url = .getItem(id: id)
+    }
+}
+
+struct PostItemAPI: RequestableWithBody {
+    var url: APIURL = .post
+    var method: APIMethod = .post
+    var contentType: ContentType = .multipart
+    var parameters: Parameters?
+    var images: [Media]?
+    
+    init(parameters: Parameters?, images: [Media]?) {
+        self.parameters = parameters
+        self.images = images
+    }
+}
+
+struct PatchItemAPI: RequestableWithBody {
+    var url: APIURL
+    var method: APIMethod = .patch
+    var contentType: ContentType = .multipart
+    var parameters: Parameters?
+    var images: [Media]?
+    
+    init(id: Int, parameters: Parameters?, images: [Media]?) {
+        self.url = APIURL.patch(id: id)
+        self.parameters = parameters
+        self.images = images
+    }
+}
+
+struct DeleteItemAPI: RequestableWithBody {
+    var url: APIURL
+    var method: APIMethod = .delete
+    var contentType: ContentType = .json
+    var parameters: Parameters?
+    var images: [Media]? = nil
+    
+    init(id: Int, parameters: Parameters?) {
+        self.url = APIURL.delete(id: id)
+        self.parameters = parameters
+    }
+}
