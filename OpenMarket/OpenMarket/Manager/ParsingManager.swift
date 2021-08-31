62472c4cd232ee3b94230708f6a5d4b655ff61fd
산체스 <xognsl87@gmail.com>
2021-08-31 10:45:32 +0900

    feat: 디코딩에 사용할 ParsingManager 구현 ParsingManager 내 receivedDataAsset 구현 ParsingManager 내 decodedJsondata 구현

---
+++ OpenMarket/OpenMarket/Manager/ParsingManager.swift
@@ -0,0 +1,38 @@
+//
+//  JsonManager.swift
+//  OpenMarket
+//
+//  Created by tae hoon park on 2021/08/31.
+//
+
+import UIKit
+
+enum ParsingError: Error, LocalizedError {
+    case assetFailed
+    case decodingFailed
+    
+    var errorDescription: String {
+        switch self {
+        case .assetFailed:
+            return "에셋 데이터를 불러오는데 실패했습니다."
+        case .decodingFailed:
+            return "디코딩에 실패했습니다."
+        }
+    }
+}
+
+struct ParsingManager {
+    static func receivedDataAsset(assetName: String) throws -> NSDataAsset {
+        guard let dataAsset = NSDataAsset(name: assetName) else {
+            throw ParsingError.assetFailed
+        }
+        return dataAsset
+    }
+    
+    static func decodedJsonData<T: Decodable>(type: T.Type, data: Data) throws -> T {
+        guard let decodedData = try? JSONDecoder().decode(type, from: data) else {
+            throw ParsingError.decodingFailed
+        }
+        return decodedData
+    }
+}
