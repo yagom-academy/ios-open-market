828617986d57d0b57e97bbc241b29e11dcdd9960
산체스 <xognsl87@gmail.com>
2021-08-31 16:06:04 +0900

    test: ItemData 타입을 사용한 디코딩 테스트 구현

--- OpenMarket/OpenMarketTests/OpenMarketTests.swift
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -16,17 +16,32 @@ class OpenMarketTests: XCTestCase {
         parsingManager = ParsingManager()
     }
     
-    func test_ItemsData_타입으로_디코딩에_성공한다() {
+    func test_Items_에셋파일을_ItemsData_타입으로_디코딩에_성공하면_title은_MacBookPro다() {
         //given
         let expectInputValue = "Items"
         //when
         guard let jsonData = try? parsingManager?.receivedDataAsset(assetName: expectInputValue),
               let decodedData = try? parsingManager?.decodedJsonData(type: ItemsData.self, data: jsonData.data)
-              else {
+        else {
             return XCTFail()
         }
         let expectResult = "MacBook Pro"
         let result = decodedData.items.first?.title
+        //then
+        XCTAssertEqual(expectResult, result)
+    }
+    
+    func test_Item_에셋파일을_ItemData_타입으로_디코딩에_성공하면_price는_1690000이다() {
+        //given
+        let expectInputValue = "Item"
+        //when
+        guard let jsonData = try? parsingManager?.receivedDataAsset(assetName: expectInputValue),
+              let decodedData = try? parsingManager?.decodedJsonData(type: ItemData.self, data: jsonData.data)
+        else {
+            return XCTFail()
+        }
+        let expectResult = 1690000
+        let result = decodedData.price
         //then
         XCTAssertEqual(expectResult, result)
     }
