43f54e59ee936a166b890f64c648648f73fc2b42
산체스 <xognsl87@gmail.com>
2021-09-01 16:55:34 +0900

    test: get, post, patch, delete 를 확인하는 코드추가 viewDidLoad 에 코드 추가

--- OpenMarket/OpenMarket/Controller/ViewController.swift
+++ OpenMarket/OpenMarket/Controller/ViewController.swift
@@ -7,12 +7,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let networkManager = NetworkManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    // getItems
+        networkManager.commuteWithAPI(API: GetItemsAPI(page: 1)) { _ in
+        }
+        
+        
+    // getItem
+//        networkManager.commuteWithAPI(API: GetItemAPI(id: 643)) { _ in
+//        }
+        
+    
+    // post
+//        guard let testImage = imageData(image: #imageLiteral(resourceName: "test1"), mimeType: .png) else { return }
+//        let postMultipart = MultipartFormData(title: "test", descriptions: "test1", price: 100, currency: "KRW", stock: 5, discountedPrice: 50, password: "12345")
+//        networkManager.commuteWithAPI(API: PostAPI(parameter: postMultipart.parameter, image: [testImage])) { _ in
+//        }
+        
+        
+    //Patch
+//        let patchMultipart = MultipartFormData(title: "modify", descriptions: "test2", password: "12345")
+//        networkManager.commuteWithAPI(API: PatchAPI(id: 643, parameter: patchMultipart.parameter, image: nil)) { _ in
+//        }
+        
+    
+    //Delete
+//        networkManager.commuteWithAPI(API: DeleteAPI(id: 643, password: "12345")) { _ in
+//
+//        }
     }
-
-
+    
 }
 
