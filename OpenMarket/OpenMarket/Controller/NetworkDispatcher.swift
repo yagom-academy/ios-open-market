ad519ec4fdf9b5994a443893283162704143e428
yoonju lee <rsyjlee@gmail.com>
2021-09-03 01:26:21 +0900

    feat: NetworkError enum 구현
    * Network 통신시 발생하는 에러들을 NetworkError로 구현
    * 에러 구현 및 fatalError() 대체

--- OpenMarket/OpenMarket/Controller/NetworkDispatcher.swift
+++ OpenMarket/OpenMarket/Controller/NetworkDispatcher.swift
@@ -7,6 +7,26 @@
 
 import Foundation
 
+enum NetworkError: Error, LocalizedError {
+    case serverError
+    case clientError
+    case dataNotFound
+    case invalidURL
+    
+    var errorDescription: String {
+        switch self {
+        case .serverError:
+            return "서버에서 에러가 발생했습니다."
+        case .clientError:
+            return "클라이언트 에러가 발생했습니다."
+        case .dataNotFound:
+            return "데이터를 찾지 못했습니다."
+        case .invalidURL:
+            return "유효하지 않은 URL입니다."
+        }
+    }
+}
+
 struct NetworkDispatcher {
     private let session: URLSessionProtocol
     init(session: URLSessionProtocol) {
@@ -15,7 +35,7 @@ struct NetworkDispatcher {
     
     func prepareForRequest(of request: Request, _ suffix: String) -> Result<URLRequest, Error> {
         guard let url = URL(string: Request.baseURL + request.path + suffix) else {
-            fatalError()
+            return .failure(NetworkError.invalidURL)
         }
         let userRequest = URLRequest(url: url)
         return .success(userRequest)
