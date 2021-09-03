a069ddec1b81f183c018f8715118e1fc3afe1196
yoonju lee <rsyjlee@gmail.com>
2021-09-03 17:09:13 +0900

    feat: ParsingManager 타입 구현
    * JSON의 key값과 Model 타입의 프로퍼티 Case Styles를 keyDecodingStrategy를 사용해 일치시킴

---
+++ OpenMarket/OpenMarket/Controller/ParsingManager.swift
@@ -0,0 +1,27 @@
+//
+//  ParsingManager.swift
+//  OpenMarket
+//
+//  Created by 이윤주 on 2021/09/03.
+//
+
+import Foundation
+
+struct ParsingManager {
+    enum ParsingError: Error {
+        case decodingFailed
+    }
+    
+    private let decoder = JSONDecoder()
+    init(decoder: JSONDecoder) {
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+    }
+    
+    func parse<T: Decodable>(_ data: Data, to model: T.Type) -> Result<T, ParsingError> {
+        let parsedData = try? decoder.decode(model, from: data)
+        guard let jsonData = parsedData else {
+            return .failure(.decodingFailed)
+        }
+        return .success(jsonData)
+    }
+}
