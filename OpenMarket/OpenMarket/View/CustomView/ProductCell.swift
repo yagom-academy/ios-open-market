cbf9d199794192bcf443f1d96cc8d6632fc4b69f
산체스 <xognsl87@gmail.com>
2021-09-04 21:32:13 +0900

    feat: ProductCell 타입 구현 ProductCell 타입 구현하여 productListCell.xib 와 연결

---
+++ OpenMarket/OpenMarket/View/CustomView/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  OpenMarket
+//
+//  Created by tae hoon park on 2021/09/04.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var discontedPriceLabel: UILabel!
+    @IBOutlet weak var stcokLabel: UILabel!
+}
