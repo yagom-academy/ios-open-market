3a1b321fecdc82f16191642c0b1a8fe36fb8b797
yaewonLee <yw990324@gmail.com>
2021-09-06 20:04:57 +0900

    feat: UnitTest 관련 메소드들 구현

--- OpenMarket/OpenMarket/Controllers/ViewController.swift
+++ OpenMarket/OpenMarket/Controllers/ViewController.swift
@@ -16,25 +16,25 @@ class ViewController: UIViewController {
 //            switch result {
 //            case .success(let data):
 //                self.productList.append(data)
-//                print(self.productList)
+//                print(self.productList.first?.items.first)
 //            case .failure(let error):
 //                print(error.localizedDescription)
 //            }
 //        }
-        
-        guard let mediaImage = Media(image: #imageLiteral(resourceName: "iPad8")), let mediaImage2 = Media(image: #imageLiteral(resourceName: "iPad_Air4")) else { return }
-        var imageList: [Media] = [mediaImage, mediaImage2]
-
-        let registProduct = RegistProductModel(title: "iPad", descriptions: "좋아요", price: 100_000, currency: "KRW", stock: 5, discountedPrice: 80_000, password: "1234")
-        let bodyParameter = registProduct.createProduct()
-
-        APIManager.shared.registProduct(parameters: bodyParameter, media: imageList) { result in
-            switch result {
-            case .success(let data):
-                print("성공")
-            case .failure(let error):
-                print(error)
-            }
-        }
+//
+//        guard let mediaImage = Media(image: #imageLiteral(resourceName: "iPad8")), let mediaImage2 = Media(image: #imageLiteral(resourceName: "iPad_Air4")) else { return }
+//        var imageList: [Media] = [mediaImage, mediaImage2]
+//
+//        let registProduct = RegistProductModel(title: "iPad", descriptions: "좋아요", price: 100_000, currency: "KRW", stock: 5, discountedPrice: 80_000, password: "1234")
+//        let bodyParameter = registProduct.createProduct()
+//
+//        APIManager.shared.registProduct(parameters: bodyParameter, media: imageList) { result in
+//            switch result {
+//            case .success(let data):
+//                print("성공")
+//            case .failure(let error):
+//                print(error)
+//            }
+//        }
     }
 }
--- OpenMarket/OpenMarketTests/OpenMarketTests.swift
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -6,27 +6,68 @@
 //
 
 import XCTest
+@testable import OpenMarket
 
 class OpenMarketTests: XCTestCase {
-
+    
+    var decoder: JSONDecoder!
+    var data: Data!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        decoder = JSONDecoder()
     }
-
+    
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        decoder = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func test_MockData와ProductListSearchModel디코딩이성공하면_error를throw하지않는다() throws {
+        let url = Bundle.main.url(forResource: "Items", withExtension: "json")!
+        data = try Data(contentsOf: url)
+        XCTAssertNoThrow(try decoder.decode(ProductListSearchModel.self, from: data))
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_네트워킹하지않고fetchProductList함수를호출에성공하면_같은title값이나온다() throws {
+        let session = MockURLSession(isRequestSuccess: false)
+        let successApiManager = APIManager(session: session)
+        successApiManager.fetchProductList(page: 1) { result in
+            switch result {
+            case .success(let data):
+                XCTAssertEqual(data.items.first?.title, "MacBook Pro")
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
         }
     }
-
+    func test_네트워킹하지않고fetchProductList함수호출에실패하면_error를throw한다() throws{
+        let session = MockURLSession(isRequestSuccess: false)
+        let failedApiManager = APIManager(session: session)
+        
+        failedApiManager.fetchProductList(page: 1) { result in
+            switch result {
+            case .success(let data):
+                XCTAssertEqual(data.items.first?.title, "MacBook Pro")
+            case .failure(let error):
+                let stringError:String = "\(error)"
+                XCTAssertEqual(stringError, "requestFailed")
+            }
+        }
+    }
+    func test_네트워킹하여상품목록을가져오는데성공하면_상품목록페이지가1이나온다() throws {
+        var productList = [ProductListSearchModel]()
+        APIManager.shared.fetchProductList(page: 1) { result in
+            switch result {
+            case .success(let data):
+                productList.append(data)
+                XCTAssertEqual(productList.first?.page, 1)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
+
+
+
+
+
