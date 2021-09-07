07f797282076bed9c3b4aafdb5d980b4dbde0a23
yescoach <sbsii1020@gmail.com>
2021-09-08 01:19:28 +0900

    feat: ViewController, CollectionViewControllerCell 생성 코드로 UI 구현

---
+++ OpenMarket/OpenMarket/Controller/CollectionViewCell.swift
@@ -0,0 +1,98 @@
+//
+//  CollectionViewCell.swift
+//  OpenMarket
+//
+//  Created by 박태현 on 2021/09/03.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    static let cellID = "cellID"
+    var imageView: UIImageView!
+    var stackView: UIStackView!
+    var titleLabel: UILabel!
+    var priceLabel: UILabel!
+    var discountedPriceLabel: UILabel!
+    var stockLabel: UILabel!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpCell()
+        setConstraints()
+        setConfigure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setUpCell() {
+        imageView = UIImageView()
+        stackView = UIStackView()
+        titleLabel = UILabel()
+        priceLabel = UILabel()
+        discountedPriceLabel = UILabel()
+        stockLabel = UILabel()
+
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        stackView.distribution = .fillProportionally
+
+        stackView.addArrangedSubview(titleLabel)
+        stackView.addArrangedSubview(priceLabel)
+        stackView.addArrangedSubview(discountedPriceLabel)
+        stackView.addArrangedSubview(stockLabel)
+
+        self.imageView.image = #imageLiteral(resourceName: "1f363")
+        self.contentView.addSubview(imageView)
+        self.contentView.addSubview(stackView)
+    }
+
+    private func setConstraints() {
+        self.imageView.translatesAutoresizingMaskIntoConstraints = false
+        self.stackView.translatesAutoresizingMaskIntoConstraints = false
+        self.titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.priceLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.discountedPriceLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.stockLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        imageView.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 10).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 10).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -10).isActive = true
+        imageView.heightAnchor.constraint(equalTo: self.contentView.heightAnchor, multiplier: 0.55).isActive = true
+
+        stackView.topAnchor.constraint(equalTo: self.imageView.bottomAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 10).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -10).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -6).isActive = true
+    }
+
+    private func setConfigure() {
+        self.layer.borderColor = UIColor.gray.cgColor
+        self.layer.borderWidth = 2.0
+        self.layer.cornerRadius = 10.0
+
+        titleLabel.adjustsFontSizeToFitWidth = true
+        priceLabel.adjustsFontSizeToFitWidth = true
+        discountedPriceLabel.adjustsFontSizeToFitWidth = true
+        stockLabel.adjustsFontSizeToFitWidth = true
+    }
+
+    func configureCell(item: Item) {
+        titleLabel.text = item.title
+        priceLabel.text = item.price.description
+        if let discountedPrice = item.discountedPrice {
+            discountedPriceLabel.text = discountedPrice.description
+        }
+        stockLabel.text = item.stock.description
+
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.imageView.image = nil
+        self.priceLabel.text = nil
+        self.stockLabel.text = nil
+    }
+}
--- OpenMarket/OpenMarket/Controller/ViewController.swift
+++ OpenMarket/OpenMarket/Controller/ViewController.swift
@@ -8,8 +8,118 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let collectionView: UICollectionView = {
+        let flowlayout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowlayout)
+
+        return collectionView
+    }()
+
+    let networkManager = NetworkManager()
+    var items: [Item] = []
+    var page: Int = 1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        configureView()
+
+        networkManager.commuteWithAPI(API.GetItems(page: 1)) { result in
+            if case .success(let data) = result {
+                guard let data = try? JSONDecoder().decode(Items.self, from: data) else {
+                    return
+                }
+                self.items.append(contentsOf: data.items)
+                self.page += 1
+
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                }
+            }
+        }
+    }
+
+    func configureView() {
+        let safeArea = view.safeAreaLayoutGuide
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+
+        collectionView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor).isActive = true
+
+        collectionView.backgroundColor = .white
+        collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: CollectionViewCell.cellID)
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.cellID, for: indexPath) as? CollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.configureCell(item: items[indexPath.item])
+        cell.backgroundColor = .green
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    var insetForSection: CGFloat {
+        return 10
+    }
+    var insetForCellSpacing: CGFloat {
+        return 10
+    }
+    var cellForEachRow: Int {
+        return 2
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (view.frame.width - (insetForSection * 2 + insetForCellSpacing)) / cellForEachRow
+        return CGSize(width: width, height: width)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return insetForCellSpacing
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: insetForSection, left: insetForSection, bottom: insetForSection, right: insetForSection)
+    }
+}
+
+extension CGFloat {
+    static func / (lhs: CGFloat, rhs: Int) -> CGFloat {
+        return lhs / CGFloat(rhs)
+    }
+}
+
+// MARK: Extension for using Canvas
+import SwiftUI
+struct ViewControllerRepresentable: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> ViewController {
+        return ViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+    }
+
+    typealias UIViewControllerType = ViewController
+}
+@available(iOS 13.0.0, *)
+struct ViewPreview: PreviewProvider {
+    static var previews: some View {
+        ViewControllerRepresentable()
     }
 }
