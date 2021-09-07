0daa08be68b140d9a2a21b3980a0b8930b844129
산체스 <xognsl87@gmail.com>
2021-09-07 09:30:20 +0900

    feat: strikeThrough 구현 로직 추가 extension String 에 strikeThrough 메서드 구현

---
+++ OpenMarket/OpenMarket/Extension/String+extension.swift
@@ -0,0 +1,16 @@
+//
+//  String+extension.swift
+//  OpenMarket
+//
+//  Created by tae hoon park on 2021/09/07.
+//
+
+import UIKit
+
+extension String {
+    func strikeThrough() -> NSAttributedString {
+        let attributeString: NSMutableAttributedString = NSMutableAttributedString(string: self)
+        attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: attributeString.length))
+        return attributeString
+    }
+}
