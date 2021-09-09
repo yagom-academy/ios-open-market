7efd30d7211f3c019a477b44a2159ead36747278
산체스 <xognsl87@gmail.com>
2021-09-09 17:36:30 +0900

    refactor: horizontalNumber 연산 프로퍼티로 수정 조건을 프로퍼티안에서 지정할 수 있도록 horizontalNumber 변수 수정

--- OpenMarket/OpenMarket/CollectionView/CompositionalLayout.swift
+++ OpenMarket/OpenMarket/CollectionView/CompositionalLayout.swift
@@ -9,19 +9,14 @@ import UIKit
 
 struct CompositionalLayout {
     func create(portraitHorizontalNumber: Int, landscapeHorizontalNumber: Int, verticalSize: CGFloat, scrollDirection: ScrollDirection) -> UICollectionViewLayout {
-        var horizontalNumber = portraitHorizontalNumber
+        var horizontalNumber: Int {
+            UIDevice.current.orientation.isPortrait ? portraitHorizontalNumber : landscapeHorizontalNumber
+        }
         let layout = UICollectionViewCompositionalLayout { (_, _) -> NSCollectionLayoutSection? in
             let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
             let item = NSCollectionLayoutItem(layoutSize: itemSize)
             item.contentInsets = NSDirectionalEdgeInsets(top: 4, leading: 6, bottom: 4, trailing: 6)
             let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(verticalSize))
-            
-            if UIDevice.current.orientation.isPortrait {
-                horizontalNumber = portraitHorizontalNumber
-            } else {
-                horizontalNumber = landscapeHorizontalNumber
-            }
-            
             let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: horizontalNumber)
             let section = NSCollectionLayoutSection(group: group)
             if scrollDirection == .horizontal {
