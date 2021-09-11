86949ff58c151b22b35b22bf85c7fa195e642ff5
yoonju lee <rsyjlee@gmail.com>
2021-09-11 21:14:20 +0900

    feat: ItemGridCell 생성

---
+++ OpenMarket/OpenMarket/View/ItemGridCell.swift
@@ -0,0 +1,48 @@
+//
+//  ItemGridCell.swift
+//  OpenMarket
+//
+//  Created by 이윤주 on 2021/09/11.
+//
+
+import UIKit
+
+class ItemGridCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private let itemImageView: UIImageView = {
+        let imageView = UIImageView(frame: .zero)
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+
+    let titleLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.textAlignment = .center
+        return label
+    }()
+
+    let discountedPriceLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.textAlignment = .center
+        return label
+    }()
+
+    let priceLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.textAlignment = .center
+        return label
+    }()
+
+    let stockLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.textAlignment = .center
+        return label
+    }()
+}
