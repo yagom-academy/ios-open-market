1857f6953393f8680613bf639b1cbd81dbf67a22
yaewonLee <yw990324@gmail.com>
2021-09-12 21:01:59 +0900

    no message

--- OpenMarket/OpenMarket/Controllers/ViewController.swift
+++ OpenMarket/OpenMarket/Controllers/ViewController.swift
@@ -9,5 +9,30 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        //        APIManager.shared.fetchProductList(page: 1) { result in
+        //            switch result {
+        //            case .success(let data):
+        //                self.productList.append(data)
+        //                print(self.productList.first?.items.first)
+        //            case .failure(let error):
+        //                print(error.localizedDescription)
+        //            }
+        //        }
+        //
+        guard let mediaImage = Media(image: #imageLiteral(resourceName: "iPad8")), let mediaImage2 = Media(image: #imageLiteral(resourceName: "iPad_Air4")) else { return }
+        let imageList: [Media] = [mediaImage, mediaImage2]
+        
+        let registProduct = Item(title: "iPad", descriptions: "좋아요", price: 100_000, currency: "KRW", stock: 5, discountedPrice: 80_000, password: "1234")
+        let bodyParameter = registProduct.createRegistProduct()
+        
+        APIManager.shared.registProduct(parameters: bodyParameter, media: imageList) { result in
+            switch result {
+            case .success(let data):
+                print("성공")
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
     }
 }
--- OpenMarket/OpenMarket/Model/MultipartForm/HTTPMethods.swift
+++ OpenMarket/OpenMarket/Model/MultipartForm/HTTPMethods.swift
@@ -5,29 +5,27 @@
 //  Created by 이예원 on 2021/09/06.
 //
 
-import Foundation
-
-typealias HTTPBodyParameter = [String: Any]
-
-enum HTTPMethods: CustomStringConvertible {
-    case boundary
-    case httpHeader
-    case httpHeaderField
-    case contentDisposition
-    case lastBoundary
-    
-    var description: String {
-        switch self {
-        case .boundary:
-            return "Boundary-\(UUID().uuidString)"
-        case .httpHeader:
-            return "multipart/form-data; boundary=\(Self.boundary.description)"
-        case .httpHeaderField:
-            return "Content-Type:"
-        case .contentDisposition:
-            return "Content-Disposition: form-data; name="
-        case .lastBoundary:
-            return "--\(Self.boundary.description)--\r\n"
-        }
-    }
-}
+//import Foundation
+//
+//enum HTTPMethods: CustomStringConvertible {
+//    case boundary
+//    case httpHeader
+//    case httpHeaderField
+//    case contentDisposition
+//    case lastBoundary
+//    
+//    var description: String {
+//        switch self {
+//        case .boundary:
+//            return "Boundary-\(UUID().uuidString)"
+//        case .httpHeader:
+//            return "multipart/form-data; boundary=\(Self.boundary.description)"
+//        case .httpHeaderField:
+//            return "Content-Type:"
+//        case .contentDisposition:
+//            return "Content-Disposition: form-data; name="
+//        case .lastBoundary:
+//            return "--\(Self.boundary.description)--\r\n"
+//        }
+//    }
+//}
