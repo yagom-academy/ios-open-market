5d1d9b9f2d2ecca73b971c83db6545a87a8bed38
yoonju lee <rsyjlee@gmail.com>
2021-09-15 08:30:58 +0900

    feat: Cell을 구성하는 메소드 구현
    * Cell의 contentView를 채우고 오토레이아웃을 설정하는 로직 구현

--- OpenMarket/OpenMarket/View/ItemGridCell.swift
+++ OpenMarket/OpenMarket/View/ItemGridCell.swift
@@ -12,6 +12,8 @@ class ItemGridCell: UICollectionViewCell {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        addSubViews()
+        setUpStackView()
     }
 
     required init?(coder: NSCoder) {
@@ -47,4 +49,45 @@ class ItemGridCell: UICollectionViewCell {
         label.textAlignment = .center
         return label
     }()
+
+    let stackView: UIStackView = {
+        let stackView = UIStackView()
+
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .center
+        stackView.backgroundColor = .white
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+
+        return stackView
+    }()
+}
+
+extension ItemGridCell {
+    func addSubViews() {
+        contentView.addSubview(itemImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(discountedPriceLabel)
+        contentView.addSubview(priceLabel)
+        contentView.addSubview(stockLabel)
+        contentView.addSubview(stackView)
+    }
+
+    func setUpStackView() {
+        stackView.addArrangedSubview(itemImageView)
+        stackView.addArrangedSubview(titleLabel)
+        stackView.addArrangedSubview(discountedPriceLabel)
+        stackView.addArrangedSubview(priceLabel)
+        stackView.addArrangedSubview(stockLabel)
+
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 2),
+            stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -2),
+            stackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 2),
+            stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -2),
+
+            itemImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.7),
+            itemImageView.heightAnchor.constraint(equalTo: itemImageView.widthAnchor)
+        ])
+    }
 }
