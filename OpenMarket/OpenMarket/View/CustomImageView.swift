a8d0a3e262cab8c8c9ada8312e32e718f1bd8754
yoonju lee <rsyjlee@gmail.com>
2021-09-15 15:29:04 +0900

    feat: 이미지 캐시 기능 구현

--- OpenMarket/OpenMarket/View/CustomImageView.swift
+++ OpenMarket/OpenMarket/View/CustomImageView.swift
@@ -7,22 +7,31 @@
 
 import UIKit
 
+let imageCache = NSCache<AnyObject, AnyObject>()
+
 class CustomImageView: UIImageView {
     var task: URLSessionDataTask!
-
+    
     func loadImage(from url: URL) {
         image = nil
-
+        
+        if let imageFromCache = imageCache.object(forKey: url.absoluteString as AnyObject) as? UIImage {
+            self.image = imageFromCache
+            return
+        }
+        
         if let task = task {
             task.cancel()
         }
-
+        
         task = URLSession.shared.dataTask(with: url) { data, response, error in
             guard let data = data,
                   let newImage = UIImage(data: data) else {
                 return print(NetworkError.invalidURL)
             }
-
+            
+            imageCache.setObject(newImage, forKey: url.absoluteString as AnyObject)
+            
             DispatchQueue.main.async {
                 self.image = newImage
             }
