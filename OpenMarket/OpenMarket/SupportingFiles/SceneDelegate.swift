2751f3b6134aec7233f39d85c7bfa21af8dc54ce
yoonju lee <rsyjlee@gmail.com>
2021-09-15 11:00:26 +0900

    feat: navigation controller 추가

--- OpenMarket/OpenMarket/SupportingFiles/SceneDelegate.swift
+++ OpenMarket/OpenMarket/SupportingFiles/SceneDelegate.swift
@@ -14,8 +14,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         let mainViewController = ItemListViewController()
+        let navigationViewController = UINavigationController(rootViewController: mainViewController)
 
-        window?.rootViewController = mainViewController
+        window?.rootViewController = navigationViewController
         window?.makeKeyAndVisible()
     }
 
