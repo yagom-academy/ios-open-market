7a140642c1d9a25d1db9111ee72ede11b006de15
GOSARI <goeunrhdms@gmail.com>
2022-01-03 19:08:30 +0900

    feat: Product 타입 구현

--- OpenMarket/OpenMarket/Product.swift
+++ OpenMarket/OpenMarket/Product.swift
@@ -8,6 +8,29 @@
 import Foundation
 
 struct Product: Codable {
-
+    let ID: Int
+    let vendorID: Int
+    let name: String
+    let thumbnail: String
+    let currency: String
+    let price: Int
+    let bargainPrice: Int
+    let discountedPrice: Int
+    let stock: Int
+    let createdAt: String
+    let issuedAt: String
     
+    private enum CodingKeys: String, CodingKey {
+        case ID = "id"
+        case vendorID = "vendor_id"
+        case name
+        case thumbnail
+        case currency
+        case price
+        case bargainPrice = "bargain_price"
+        case discountedPrice = "discounted_price"
+        case stock
+        case createdAt = "created_at"
+        case issuedAt = "issued_at"
+    }
 }
