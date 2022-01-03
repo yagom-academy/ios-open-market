526a25723d4a4638e85c893b7838256bbe7f6a1e
yim2627 <yim2627@gmail.com>
2022-01-03 17:30:43 +0900

    feat: ProductList 타입 구현 #2
    - SwiftLint disabled_rules에 trailing_whitespace 추가

---
+++ OpenMarket/OpenMarket/Models/ProductDetail.swift
@@ -0,0 +1,1 @@
+struct ProductDetail: Codable {}
---
+++ OpenMarket/OpenMarket/Models/ProductList.swift
@@ -0,0 +1,21 @@
+struct ProductList: Codable {
+    let pageNo: Int
+    let itemsPerPage: Int
+    let totalCount: Int
+    let offset: Int
+    let limit: Int
+    let pages: [ProductDetail]
+    let lastPage: Int
+    let hasNext: Bool
+    let hasPrev: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case pageNo = "page_no"
+        case itemsPerPage = "items_per_page"
+        case totalCount = "total_count"
+        case lastPage = "last_page"
+        case hasNext = "has_next"
+        case hasPrev = "has_prev"
+        case offset, limit, pages
+    }
+}
