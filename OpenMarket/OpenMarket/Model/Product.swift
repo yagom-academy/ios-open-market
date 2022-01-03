86bb94a6a534e1311a4ff09ed74bcacd002c413f
yanghojoon <yanghojoon8487@gmail.com>
2022-01-03 15:54:58 +0900

    feat: JSON 파싱을 위한 Product, Currency 타입 구현

---
+++ OpenMarket/OpenMarket/Model/Product.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+struct Product: Codable {
+    let id: Int
+    let vendorId: Int
+    let name: String
+    let thumbnail: String
+    let currency: Currency
+    let price: Int
+    let bargainPrice: Int
+    let discountedPrice: Int
+    let stock: Int
+    let createdAt: Date
+    let issuedAt: Date
+}
