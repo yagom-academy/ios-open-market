14e655194f663bec0efca0b6367e5b82e1513d0c
namu <simjh27@gmail.com>
2022-01-03 16:40:18 +0900

    feat: Page 타입 CodingKey 구현

--- OpenMarket/OpenMarket/Model/Page.swift
+++ OpenMarket/OpenMarket/Model/Page.swift
@@ -17,4 +17,15 @@ struct Page: Codable {
     let hasNext: Bool
     let hasPrev: Bool
     let pages: [Product]
+    enum CodingKeys: String, CodingKey {
+        case pageNumber = "page_no"
+        case itemsPerPage = "items_per_page"
+        case totalCount = "total_count"
+        case offset
+        case limit
+        case lastPage = "last_page"
+        case hasNext = "has_next"
+        case hasPrev = "has_prev"
+        case pages
+    }
 }
