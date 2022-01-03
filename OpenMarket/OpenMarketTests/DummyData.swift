b7ce22799fb4eac6ab43934f58894c12dda33543
smart8612 <smart8612@gmail.com>
2022-01-03 17:18:08 +0900

    test: DummyData 타입 구현 #2

---
+++ OpenMarket/OpenMarketTests/DummyData.swift
@@ -0,0 +1,21 @@
+//
+//  DummyData.swift
+//  OpenMarketTests
+//
+//  Created by JeongTaek Han on 2022/01/03.
+//
+
+import Foundation
+
+typealias DataTaskCompletionHandler = (Data?, URLResponse?, Error?) -> Void
+
+struct DummyData {
+    let data: Data?
+    let response: URLResponse?
+    let error: Error?
+    var completionHandler: DataTaskCompletionHandler?
+    
+    func completion() {
+        completionHandler?(data, response, error)
+    }
+}
---
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -0,0 +1,24 @@
+//
+//  OpenMarketTests.swift
+//  OpenMarketTests
+//
+//  Created by JeongTaek Han on 2022/01/03.
+//
+
+import XCTest
+
+class OpenMarketTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func testExample() throws {
+        
+    }
+
+}
