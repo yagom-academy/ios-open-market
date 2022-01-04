c33e2aa6b77625928956d376fd386f86ff8d3345
zoe <kny0651@naver.com>
2022-01-04 16:18:07 +0900

    feat: APIServicable extension의  parse 메서드 구현 #1

--- OpenMarket/OpenMarket/Model/APIServicable.swift
+++ OpenMarket/OpenMarket/Model/APIServicable.swift
@@ -14,5 +14,16 @@ protocol APIServicable {
     func delete(productID: Int, productSecret: String)
     func get(productID: Int)
     func get(pageNumber: Int, itemsPerPage: Int)
-    func parse<T: Decodable>(with data: Data) -> T
+    func parse<T: Decodable>(with data: Data) -> T?
+}
+
+extension APIServicable {
+    func parse<T: Decodable>(with data: Data) -> T? {
+        do {
+            let data = try JSONDecoder().decode(T.self, from: data)
+            return data
+        } catch {
+            return nil
+        }
+    }
 }
