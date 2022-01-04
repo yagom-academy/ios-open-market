179a2a84594a2f0f03c20757ec677bf18a9e945c
YohanBlessYou <duddn5906@naver.com>
2022-01-04 11:59:57 +0900

    feat: URLSession Extension 구현 #1

---
+++ OpenMarket/OpenMarket/Model/URLSession+Extension.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+extension URLSession {
+    func request<T: Codable>(urlRequest: URLRequest, expecting: T.Type, completion: @escaping (Result<T, Error>) -> Void) {
+        let task = dataTask(with: urlRequest) { (data, response, error) in
+            guard let data = data else {
+                if let error = error {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                if let error = error {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard (200...299) ~= response.statusCode else {
+                if let error = error {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let result = try? JSONDecoder().decode(expecting, from: data) else {
+                if let error = error {
+                    completion(.failure(error))
+                }
+                return
+            }
+            completion(.success(result))
+        }
+        task.resume()
+    }
+}
