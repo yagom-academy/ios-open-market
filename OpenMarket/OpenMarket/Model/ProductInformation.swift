f556e6d2b8f6a4116ec5c613d51f657a9d311e2c
wooyani77 <wooyani77@gmail.com>
2022-01-04 11:28:20 +0900

    feat: ProductInformation 모델 타입 구현 및 파일분리 #1 #2

---
+++ OpenMarket/OpenMarket/Model/ProductInformation.swift
@@ -0,0 +1,22 @@
+struct ProductInformation: Decodable {
+    let id: Int
+    let vendorId: Int
+    let name: String
+    let thumbnail: String
+    let currency: String
+    let price: Int
+    let bargainPrice: Int
+    let discountedPrice: Int
+    let stock: Int
+    let createdAt: String
+    let issuedAt: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case id, name, thumbnail, currency, price, stock
+        case vendorId = "vendor_id"
+        case bargainPrice = "bargain_price"
+        case discountedPrice = "discounted_price"
+        case createdAt = "created_at"
+        case issuedAt = "issued_at"
+    }
+}
