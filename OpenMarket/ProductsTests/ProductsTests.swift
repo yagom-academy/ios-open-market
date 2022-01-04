17afbb57639524548ad570b0a2009d2379d99fae
oksunwoo <oksunwo@naver.com>
2022-01-04 22:05:25 +0900

    test: Parser 타입메서드의 변경으로 인한 테스트수정 #3

--- OpenMarket/ProductsTests/ProductsTests.swift
+++ OpenMarket/ProductsTests/ProductsTests.swift
@@ -1,24 +1,23 @@
 import XCTest
 
 class ProductsTests: XCTestCase {
-    func test_없는_Json파일명을Parser에넣었을_때nil을반환하는지() {
-        let notExistJsonName = "OpenMarket"
-        let result = try! Parser.decode(from: notExistJsonName)
+    var sut: Data?
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
         
-        XCTAssertNil(result)
+        sut = NSDataAsset(name: "products")?.data
     }
     
-    func test_제대로된_JSON파일명을_Parser에_넣었을때_pageNumber가_1이_나오는지() {
-        let jsonName = "products"
-        let decoder = try! Parser.decode(from: jsonName)
+    func test_decoding된_JSON데이터의_productList의_pageNumber가_1인지() {
+        let decoder = try! Parser<ProductList>.decode(from: sut!)
         let result = decoder!.pageNumber
         
         XCTAssertEqual(result, 1)
     }
     
-    func test_decoding된_JSON데이터의_pages의_첫번째_데이터의_이름이_pizza인지() {
-        let jsonName = "products"
-        let decoder = try! Parser.decode(from: jsonName)
+    func test_decoding된_productList_pages의_첫번째데이터이름이_pizza인지() {
+        let decoder = try! Parser<ProductList>.decode(from: sut!)
         let result = decoder!.pages[0].name
         
         XCTAssertEqual(result, "pizza")
