2327b3f120496976c453a0146f4fe5c83a984e9e
smart8612 <smart8612@gmail.com>
2022-01-04 16:26:07 +0900

    test: URLSessionProvider 테스트 코드 추가 #3

--- OpenMarket/OpenMarket/Model/URLSessionProvider.swift
+++ OpenMarket/OpenMarket/Model/URLSessionProvider.swift
@@ -9,28 +9,36 @@ import Foundation
 
 enum URLSessionProviderError: Error {
     case statusError
+    case unknownError
+}
+
+protocol URLSessionProtocol {
+    func dataTask(with request: URLRequest,
+                  completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
 }
 
 class URLSessionProvider {
-    let session: URLSession
+    let session: URLSessionProtocol
     var baseURL: String = "https://market-training.yagom-academy.kr/"
     
-    init(session: URLSession, baseURL: String) {
+    init(session: URLSessionProtocol, baseURL: String) {
         self.session = session
         self.baseURL = baseURL
     }
     
     func request(_ request: URLRequest, completionHandler: @escaping (Result<Data, URLSessionProviderError>) -> Void) {
-        let task = session.dataTask(with: request) { data, response, error in
+        let task = session.dataTask(with: request) { data, response, _ in
             guard let httpRespose = response as? HTTPURLResponse,
                   (200...299).contains(httpRespose.statusCode) else {
                 return completionHandler(.failure(.statusError))
             }
             guard let data = data else {
-                return completionHandler(.failure(.statusError))
+                return completionHandler(.failure(.unknownError))
             }
             return completionHandler(.success(data))
         }
         task.resume()
     }
 }
+
+extension URLSession: URLSessionProtocol { }
--- OpenMarket/OpenMarketTests/OpenMarketTests.swift
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -8,45 +8,58 @@
 import XCTest
 
 class OpenMarketTests: XCTestCase {
-    var sutURLSession: StubURLSession!
+    
+    var sutURLSessionProvider: URLSessionProvider!
+    
     override func setUpWithError() throws {
         let data = NSDataAsset(name: "products")?.data
         guard let url = URL(string: "https://market-training.yagom-academy.kr/") else {
             return
         }
+        
         let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
         let dummy = DummyData(data: data, response: response, error: nil)
-        sutURLSession = StubURLSession(dummy: dummy)
+        let urlSession = StubURLSession(dummy: dummy)
+        
+        sutURLSessionProvider = URLSessionProvider(
+            session: urlSession,
+            baseURL: "https://market-training.yagom-academy.kr/"
+        )
+        
     }
 
     override func tearDownWithError() throws {
-        sutURLSession = nil
+        sutURLSessionProvider = nil
     }
 
     func testExample() throws {
         guard let url = URL(string: "https://market-training.yagom-academy.kr/") else {
             return
         }
-        let task = sutURLSession.dataTask(with: url) { (data, response, error) in
-            let decoder = JSONDecoder()
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
-            dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
-            decoder.dateDecodingStrategy = .formatted(dateFormatter)
-            guard let data = data else {
-                XCTFail()
-                return
-            }
-            do {
-                let result = try decoder.decode(Page.self, from: data)
-                XCTAssertNotNil(result)
-            }
-            catch {
-                XCTFail()
+        
+        sutURLSessionProvider.request(URLRequest(url: url)) { result in
+            
+            switch result {
+            case .success(let data):
+                let decoder = JSONDecoder()
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+                dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+                decoder.dateDecodingStrategy = .formatted(dateFormatter)
+                
+                do {
+                    let result = try decoder.decode(Page.self, from: data)
+                    print(result)
+                    XCTAssertNotNil(result)
+                } catch {
+                    XCTFail("error")
+                }
+                
+            case .failure(let error):
+                XCTFail("error")
             }
             
         }
-        task.resume()
     }
 
 }
