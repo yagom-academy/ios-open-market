b9773ec1c0b1a5f1e8e47a28a58a0591d8e29f83
ICS-Asan <carpediem1015@gmail.com>
2022-01-04 14:46:46 +0900

    feat: checkProductDetail 메서드 구현 #3
    - get메서드로 API를 가져오는 checkProductDetail 메서드 구현

--- OpenMarket/OpenMarket/Model/APIManager.swift
+++ OpenMarket/OpenMarket/Model/APIManager.swift
@@ -3,10 +3,11 @@ import Foundation
 class APIManager {
     
     let apiHost: String = "https://market-training.yagom-academy.kr/"
-    var apiStatus: String = "ㅁㅁㅁ"
+    var apiStatus: String?
+    var product: OpenMarketPage?
     let semaphore = DispatchSemaphore(value: 0)
     
-    func healthChecker() {
+    func checkAPIHealth() {
         let url: String = apiHost + "healthChecker"
         var request = URLRequest(url: URL(string: url)!, timeoutInterval: Double.infinity)
         request.httpMethod = "GET"
@@ -18,6 +19,29 @@ class APIManager {
                 return
             }
             self.apiStatus = String(data: data, encoding: .utf8)!
+            self.semaphore.signal()
+        }
+        task.resume()
+        semaphore.wait()
+    }
+    
+    func checkProductDetail(from id: Int) {
+        let url: String = apiHost + "api/products/" + "\(id)"
+        var request = URLRequest(url: URL(string: url)!, timeoutInterval: Double.infinity)
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                print(String(describing: error))
+                self.semaphore.signal()
+                return
+            }
+            do {
+                let convertedData = try JSONParser.decodeData(of: data, how: OpenMarketPage.self)
+                self.product = convertedData
+            } catch {
+                print(error.localizedDescription)
+            }
             self.semaphore.signal()
         }
         task.resume()
