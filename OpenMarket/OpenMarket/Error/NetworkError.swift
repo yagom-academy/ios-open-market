c4406dd82b0685f78b82cb932e3f338db7077d83
llghdud921 <llghdud921@naver.com>
2022-01-04 11:46:07 +0900

    feat: network class 타입 및 NetworkError 추가 #3

---
+++ OpenMarket/OpenMarket/Error/NetworkError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkError.swift
+//  OpenMarket
+//
+//  Created by 이호영 on 2022/01/04.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case responseCasting
+    case statusCode
+}
---
+++ OpenMarket/OpenMarket/Model/Network.swift
@@ -0,0 +1,31 @@
+//
+//  Network.swift
+//  OpenMarket
+//
+//  Created by 이호영 on 2022/01/04.
+//
+
+import Foundation
+
+final class Network {
+    let session = URLSession.shared
+    
+    func start(request: URLRequest, completion: @escaping (Result<Data?, Error>) -> Void) {
+        session.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            guard let response = response as? HTTPURLResponse else {
+                completion(.failure(NetworkError.responseCasting))
+                return
+            }
+            guard (200...299).contains(response.statusCode) else {
+                completion(.failure(NetworkError.statusCode))
+                return
+            }
+            completion(.success(data))
+        }.resume()
+    }
+    
+}
