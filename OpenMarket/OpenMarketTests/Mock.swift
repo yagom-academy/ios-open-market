3e1e45f5f54db47e84a37939aeba192ebd895c3f
July911 <min95911@nate.com>
2022-01-04 20:18:13 +0900

    test: Mock URLSession 타입 구현 #2

--- OpenMarket/OpenMarketTests/Mock.swift
+++ OpenMarket/OpenMarketTests/Mock.swift
@@ -1,24 +1,42 @@
 import Foundation
 
-//struct DummyData {
-//    let data: Data?
-//    let response: URLResponse?
-//    let error: Error?
-//}
-//
-//class StubURLSession: URLSessionProtocol {
-//    func dataTask(with urlRequest: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> ()) -> StubURLSessionDataTask{
-//
-//    }
-//
-//    var dummyData: DummyData?
-//
-//}
-//
-//class StubURLSessionDataTask {
-//
-//}
-//
-//protocol URLSessionProtocol {
-//    func dataTask(with urlRequest: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> ()) -> StubURLSessionDataTask
-//}
+class StubURLSession {
+    let alwaysSuccess: Bool
+    var dummyData: Data?
+    
+    init(alwaysSuccess: Bool) {
+        self.alwaysSuccess = alwaysSuccess
+    }
+    
+    func dataTask(with urlRequest: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> ()) -> URLSessionDataTask{
+        
+        guard let url = urlRequest.url else {
+            return StubURLSessionDataTask()
+        }
+        
+        let sucessResponse = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
+        let failureResponse = HTTPURLResponse(url: url, statusCode: 404, httpVersion: nil, headerFields: nil)
+        let sessionDataTask = StubURLSessionDataTask()
+        
+        if alwaysSuccess {
+            sessionDataTask.resumeDidCall = {
+                completion(self.dummyData, sucessResponse, nil)
+            }
+        } else {
+            sessionDataTask.resumeDidCall = {
+                completion(nil, failureResponse, nil)
+            }
+        }
+        
+        return sessionDataTask
+    }
+}
+
+class StubURLSessionDataTask: URLSessionDataTask {
+    var resumeDidCall: () -> Void = {}
+
+    override func resume() {
+        resumeDidCall()
+    }
+}
+
--- OpenMarket/OpenMarketTests/OpenMarketTests.swift
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -4,19 +4,19 @@ import XCTest
 
 class OpenMarketTests: XCTestCase {
 
-    //테스트명 : 디코딩 테스트
-    // - 디코딩 : JSON -> ProductHTTPResponse
-    // - 인풋 : JSON
-    // - 아웃풋 : ProductHTTPResponse
-    
-    //테스트명 : 인코딩 테스트
-    // - 인코딩 : 메서드파라미터 -> JSON
-    // - 인풋 : 메서드파라미터
-    // - 아웃풋 : JSON
-
-
     func testExample() throws {
-
+        let productId = 16
+        let url = URL(string: "https://market-training.yagom-academy.kr/api/products/\(productId)")!
+        let session = StubURLSession(alwaysSuccess: true)
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
         
+        session.dataTask(with: request) { data, response, error in
+            guard let response = response as? HTTPURLResponse else {
+                return
+            }
+            XCTAssertTrue((200..<300) ~= response.statusCode)
+        }.resume()
     }
+    
 }
