f546da7623d1b3976c2a2cae9bf485c2c1cea0a3
aCafela-coffee <acafelacoffee@gmail.com>
2022-01-05 21:31:27 +0900

    style: 줄바꿈 규칙 변경

--- OpenMarket/OpenMarket/Util/NetworkTask.swift
+++ OpenMarket/OpenMarket/Util/NetworkTask.swift
@@ -10,136 +10,144 @@ enum NetworkTask {
         task.resume()
     }
     
-    static func requestProductRegistration(identifier: String,
-                                           salesInformation: SalesInformation,
-                                           images: [String: Data],
-                                           completionHandler: @escaping (Result<Data, Error>)
-                                           -> Void) {
-        guard let url = NetworkAddress.productRegistration.url else { return }
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue(identifier, forHTTPHeaderField: "identifier")
-        request.addValue("multipart/form-data; boundary=\(boundary)",
-                         forHTTPHeaderField: "Content-Type")
-        let body = buildBody(with: salesInformation, images: images)
-        request.httpBody = body
-        let task = dataTask(with: request, completionHandler: completionHandler)
-        task.resume()
-    }
-    
-    static func requestProductModification(identifier: String,
-                                           productId: Int,
-                                           information: ModificationInformation,
-                                           completionHandler: @escaping (Result<Data, Error>)
-                                           -> Void) {
-        guard let url = NetworkAddress.productModification(productId: productId).url else {
-            return
+    static func requestProductRegistration(
+        identifier: String,
+        salesInformation: SalesInformation,
+        images: [String: Data],
+        completionHandler: @escaping (Result<Data, Error>) -> Void) {
+            guard let url = NetworkAddress.productRegistration.url else { return }
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.addValue(identifier, forHTTPHeaderField: "identifier")
+            request.addValue("multipart/form-data; boundary=\(boundary)",
+                             forHTTPHeaderField: "Content-Type")
+            let body = buildBody(with: salesInformation, images: images)
+            request.httpBody = body
+            let task = dataTask(with: request, completionHandler: completionHandler)
+            task.resume()
         }
-        var request = URLRequest(url: url)
-        request.httpMethod = "PATCH"
-        request.addValue(identifier, forHTTPHeaderField: "identifier")
-        request.httpBody = try? JSONParser.encode(from: information)
-        let task = dataTask(with: request, completionHandler: completionHandler)
-        task.resume()
-    }
     
-    static func requestProductSecret(productId: Int,
-                                     identifier: String,
-                                     secret: String,
-                                     completionHandler: @escaping (Result<Data, Error>) -> Void) {
-        guard let url = NetworkAddress.secret(productId: productId).url else { return }
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue(identifier, forHTTPHeaderField: "identifier")
-        request.httpBody = try? JSONParser.encode(from: secret)
-        let task = dataTask(with: request, completionHandler: completionHandler)
-        task.resume()
-    }
-    
-    static func requestRemoveProduct(identifier: String,
-                                     productId: Int,
-                                     productSecret: String,
-                                     completionHandler: @escaping (Result<Data, Error>) -> Void) {
-        guard let url = NetworkAddress.removeProduct(productId: productId,
-                                                     productSecret: productSecret).url else { return }
-        var request = URLRequest(url: url)
-        request.httpMethod = "DELETE"
-        request.addValue(identifier, forHTTPHeaderField: "identifier")
-        request.httpBody = try? JSONParser.encode(from: productSecret)
-        let task = dataTask(with: request, completionHandler: completionHandler)
-        task.resume()
-    }
-    
-    static func requestProductDetail(productId: Int,
-                                     completionHandler: @escaping (Result<Data, Error>) -> Void) {
-        guard let url = NetworkAddress.productDetail(productId: productId).url else { return }
-        let request = URLRequest(url: url)
-        let task = dataTask(with: request, completionHandler: completionHandler)
-        task.resume()
-    }
-    
-    static func requestProductList(pageNumber: Int,
-                                   itemsPerPage: Int,
-                                   completionHandler: @escaping (Result<Data, Error>) -> Void) {
-        guard let url = NetworkAddress.productList(pageNumber: pageNumber,
-                                                   itemsPerPage: itemsPerPage).url else { return }
-        let request = URLRequest(url: url)
-        let task = dataTask(with: request, completionHandler: completionHandler)
-        task.resume()
-    }
-    
-    private static func dataTask(with request: URLRequest,
-                                 completionHandler: @escaping (Result<(Data), Error>) -> Void) -> URLSessionDataTask {
-        let dataTask = URLSession.shared.dataTask(with: request) { data, response, error in
-            if let error = error {
-                completionHandler(.failure(error))
+    static func requestProductModification(
+        identifier: String,
+        productId: Int,
+        information: ModificationInformation,
+        completionHandler: @escaping (Result<Data, Error>) -> Void) {
+            guard let url = NetworkAddress.productModification(productId: productId).url else {
                 return
             }
-            guard let httpResponse = response as? HTTPURLResponse,
-                  (200...299).contains(httpResponse.statusCode) else {
-                      completionHandler(.failure(NetworkError.httpError))
-                      return
-                  }
-            guard let data = data else { return }
-            completionHandler(.success(data))
+            var request = URLRequest(url: url)
+            request.httpMethod = "PATCH"
+            request.addValue(identifier, forHTTPHeaderField: "identifier")
+            request.httpBody = try? JSONParser.encode(from: information)
+            let task = dataTask(with: request, completionHandler: completionHandler)
+            task.resume()
         }
-        return dataTask
-    }
     
-    private static func buildBody(with salesInformation: SalesInformation,
-                                  images: [String: Data]) -> Data? {
-        guard let endBoundary = "\r\n--\(boundary)--".data(using: .utf8) else {
-            return nil
+    static func requestProductSecret(
+        productId: Int,
+        identifier: String,
+        secret: String,
+        completionHandler: @escaping (Result<Data, Error>) -> Void) {
+            guard let url = NetworkAddress.secret(productId: productId).url else { return }
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.addValue(identifier, forHTTPHeaderField: "identifier")
+            request.httpBody = try? JSONParser.encode(from: secret)
+            let task = dataTask(with: request, completionHandler: completionHandler)
+            task.resume()
         }
-        guard let newLine = "\r\n".data(using: .utf8) else {
-            return nil
+    
+    static func requestRemoveProduct(
+        identifier: String,
+        productId: Int,
+        productSecret: String,
+        completionHandler: @escaping (Result<Data, Error>) -> Void) {
+            guard let url = NetworkAddress.removeProduct(
+                productId: productId,
+                productSecret: productSecret).url else { return }
+            var request = URLRequest(url: url)
+            request.httpMethod = "DELETE"
+            request.addValue(identifier, forHTTPHeaderField: "identifier")
+            request.httpBody = try? JSONParser.encode(from: productSecret)
+            let task = dataTask(with: request, completionHandler: completionHandler)
+            task.resume()
         }
-        guard let salesInformation = try? JSONParser.encode(from: salesInformation) else {
-            return nil
+    
+    static func requestProductDetail(
+        productId: Int,
+        completionHandler: @escaping (Result<Data, Error>) -> Void) {
+            guard let url = NetworkAddress.productDetail(productId: productId).url else { return }
+            let request = URLRequest(url: url)
+            let task = dataTask(with: request, completionHandler: completionHandler)
+            task.resume()
         }
-        var data = Data()
-        var paramsBody = ""
-        paramsBody.append("--\(boundary)\r\n")
-        paramsBody.append("Content-Disposition: form-data; name=\"params\"\r\n\r\n")
-        guard let paramsBody = paramsBody.data(using: .utf8) else {
-            return nil
+    
+    static func requestProductList(
+        pageNumber: Int,
+        itemsPerPage: Int,
+        completionHandler: @escaping (Result<Data, Error>) -> Void) {
+            guard let url = NetworkAddress.productList(
+                pageNumber: pageNumber,
+                itemsPerPage: itemsPerPage).url else { return }
+            let request = URLRequest(url: url)
+            let task = dataTask(with: request, completionHandler: completionHandler)
+            task.resume()
         }
-        data.append(paramsBody)
-        data.append(salesInformation)
-        for (fileName, image) in images {
-            var imagesBody = ""
-            imagesBody.append("\r\n--\(boundary)\r\n")
-            imagesBody.append("Content-Disposition: form-data; name=\"images\"; filename=\(fileName)\r\n")
-            guard let imagesBody = imagesBody.data(using: .utf8) else {
+    
+    private static func dataTask(
+        with request: URLRequest,
+        completionHandler: @escaping (Result<(Data), Error>) -> Void) -> URLSessionDataTask {
+            let dataTask = URLSession.shared.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    completionHandler(.failure(error))
+                    return
+                }
+                guard let httpResponse = response as? HTTPURLResponse,
+                      (200...299).contains(httpResponse.statusCode) else {
+                          completionHandler(.failure(NetworkError.httpError))
+                          return
+                      }
+                guard let data = data else { return }
+                completionHandler(.success(data))
+            }
+            return dataTask
+        }
+    
+    private static func buildBody(
+        with salesInformation: SalesInformation,
+        images: [String: Data]) -> Data? {
+            guard let endBoundary = "\r\n--\(boundary)--".data(using: .utf8) else {
                 return nil
             }
-            data.append(imagesBody)
-            data.append(newLine)
-            data.append(image)
+            guard let newLine = "\r\n".data(using: .utf8) else {
+                return nil
+            }
+            guard let salesInformation = try? JSONParser.encode(from: salesInformation) else {
+                return nil
+            }
+            var data = Data()
+            var paramsBody = ""
+            paramsBody.append("--\(boundary)\r\n")
+            paramsBody.append("Content-Disposition: form-data; name=\"params\"\r\n\r\n")
+            guard let paramsBody = paramsBody.data(using: .utf8) else {
+                return nil
+            }
+            data.append(paramsBody)
+            data.append(salesInformation)
+            for (fileName, image) in images {
+                var imagesBody = ""
+                imagesBody.append("\r\n--\(boundary)\r\n")
+                imagesBody.append("Content-Disposition: form-data; name=\"images\"; filename=\(fileName)\r\n")
+                guard let imagesBody = imagesBody.data(using: .utf8) else {
+                    return nil
+                }
+                data.append(imagesBody)
+                data.append(newLine)
+                data.append(image)
+            }
+            data.append(endBoundary)
+            return data
         }
-        data.append(endBoundary)
-        return data
-    }
 }
 
 extension NetworkTask {
@@ -168,7 +176,9 @@ extension NetworkTask {
             case let .productList(pageNumber, itemsPerPage):
                 var urlComponents = URLComponents(string: Self.apiHost + "/api/products?")
                 let pageNumber = URLQueryItem(name: "page_no", value: String(pageNumber))
-                let itemsPerPage = URLQueryItem(name: "items_per_page", value: String(itemsPerPage))
+                let itemsPerPage = URLQueryItem(
+                    name: "items_per_page", value: String(itemsPerPage)
+                )
                 urlComponents?.queryItems?.append(pageNumber)
                 urlComponents?.queryItems?.append(itemsPerPage)
                 return urlComponents?.url
