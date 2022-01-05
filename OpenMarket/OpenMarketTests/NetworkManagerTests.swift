fb393662146ee65e108c0b39b6cfa2fc398834a7
Ari <lee_ari95@icloud.com>
2022-01-05 17:11:48 +0900

    test: 상품리스트 조회에 맞는 Request를 반환하는 테스트코드 추가 #5
    - setUpWithError, tearDownWithError 메소드를 재정의하여 테스트 기본 세팅 리팩토링

--- OpenMarket/OpenMarketTests/NetworkManagerTests.swift
+++ OpenMarket/OpenMarketTests/NetworkManagerTests.swift
@@ -9,8 +9,20 @@ import XCTest
 @testable import OpenMarket
 
 class NetworkManagerTests: XCTestCase {
+    var sutNetworkManager: NetworkManager?
     var stubProducts = NSDataAsset(name: "products")!.data
     var stubProduct = NSDataAsset(name: "product")!.data
+    
+    override func setUpWithError() throws {
+        let session = MockSession.session
+        let netWork = Network(session: session)
+        let parser = StubParser()
+        sutNetworkManager = NetworkManager(network: netWork, parser: parser)
+    }
+    
+    override func tearDownWithError() throws {
+        sutNetworkManager = nil
+    }
     
     func test_Fetch_Success() {
         // given
@@ -18,17 +30,12 @@ class NetworkManagerTests: XCTestCase {
         let data = self.stubProducts
         let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
         MockURLProtocol.mockURLs = [url: (nil, data, response)]
-        let session = MockSession.session
-        
-        let netWork = Network(session: session)
-        let parser = StubParser()
-        let networkManager = NetworkManager(network: netWork, parser: parser)
         
         let request = URLRequest(url: url)
         let decodingtype = Products.self
         let expectation = XCTestExpectation(description: "네트워크 실행")
         
-        networkManager.fetch(request: request, decodingType: decodingtype) { result in
+        sutNetworkManager?.fetch(request: request, decodingType: decodingtype) { result in
         
             // then
             switch result {
@@ -48,18 +55,13 @@ class NetworkManagerTests: XCTestCase {
         let data = self.stubProducts
         let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
         MockURLProtocol.mockURLs = [url: (nil, data, response)]
-        let session = MockSession.session
-        
-        let netWork = Network(session: session)
-        let parser = StubParser()
-        let networkManager = NetworkManager(network: netWork, parser: parser)
         
         let request = URLRequest(url: url)
         let decodingtype = StubProduct.self
         let expectation = XCTestExpectation(description: "네트워크 실행")
         
         //when
-        networkManager.fetch(request: request, decodingType: decodingtype) { result in
+        sutNetworkManager?.fetch(request: request, decodingType: decodingtype) { result in
         
             // then
             switch result {
@@ -79,18 +81,13 @@ class NetworkManagerTests: XCTestCase {
         let data = self.stubProducts
         let response = HTTPURLResponse(url: url, statusCode: 404, httpVersion: nil, headerFields: nil)
         MockURLProtocol.mockURLs = [url: (nil, data, response)]
-        let session = MockSession.session
-        
-        let netWork = Network(session: session)
-        let parser = StubParser()
-        let networkManager = NetworkManager(network: netWork, parser: parser)
         
         let request = URLRequest(url: url)
         let decodingtype = Products.self
         let expectation = XCTestExpectation(description: "네트워크 실행")
         
         //when
-        networkManager.fetch(request: request, decodingType: decodingtype) { result in
+        sutNetworkManager?.fetch(request: request, decodingType: decodingtype) { result in
         
             // then
             switch result {
@@ -102,5 +99,18 @@ class NetworkManagerTests: XCTestCase {
             expectation.fulfill()
         }
         wait(for: [expectation], timeout: 5)
+    }
+    
+    func test_request_상품리스트조회() {
+        //given
+        let url = NetworkConstant.products(page: 1, itemsPerPage: 10).url
+        
+        //when
+        let result = sutNetworkManager?.request(page: 1, itemsPerPage: 10)
+        
+        //then
+        XCTAssertNotNil(result)
+        XCTAssertEqual(result?.url, url)
+        XCTAssertEqual(result?.httpMethod, NetworkConstant.HTTPMethod.get.rawValue)
     }
 }
