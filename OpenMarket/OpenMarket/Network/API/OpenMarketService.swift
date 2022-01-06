23e166e55992d4bde165bd0fa88d4330f9d2d601
smart8612 <smart8612@gmail.com>
2022-01-06 16:51:54 +0900

    feat: OpenMarketService createProduct 구현 #3

--- OpenMarket/OpenMarket/Network/API/OpenMarketService.swift
+++ OpenMarket/OpenMarket/Network/API/OpenMarketService.swift
@@ -9,7 +9,7 @@ import Foundation
 
 enum OpenMarketService {
     case checkHealth
-    case createProduct(id: String, params: String, images: [Data])
+    case createProduct(sellerID: String, params: Data, images: [Data])
     case updateProduct
     case showProductSecret(sellerID: String, sellerPW: String, productID: Int)
     case deleteProduct(sellerID: String, productID: Int, productSecret: String)
@@ -29,15 +29,17 @@ extension OpenMarketService {
             var request = URLRequest(url: url)
             request.httpMethod = self.method
             return request
-        case .createProduct(let id, let params, let images):
-            guard let url = URL(string: self.baseURL + self.path) else {
-                return nil
-            }
+        case .createProduct(let sellerID, let params, let images):
+            guard let url = URL(string: baseURL + self.path) else { return nil }
             var urlRequest = URLRequest(url: url)
             urlRequest.httpMethod = self.method
-            urlRequest.addValue(id, forHTTPHeaderField: "identifier")
-            urlRequest.addValue("multipart/form-data", forHTTPHeaderField: "Content-Type")
-            
+            urlRequest.addValue(sellerID, forHTTPHeaderField: "identifier")
+            urlRequest.addValue("multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW", forHTTPHeaderField: "Content-Type")
+            let body = NSMutableData()
+            makeBody(target: body, name: "params", data: params)
+            makeBodyImage(target: body, name: "images", images: images)
+            body.append("------WebKitFormBoundary7MA4YWxkTrZu0gW--\r\n")
+            urlRequest.httpBody = body as Data
             return urlRequest
         case .updateProduct:
             return URLRequest(url: URL(string: "")!)
@@ -81,14 +83,46 @@ extension OpenMarketService {
         switch self {
         case .checkHealth, .showProductDetail, .showPage:
             return "GET"
-        case .createProduct:
+        case .createProduct, .showProductSecret:
             return "POST"
         case .updateProduct:
             return "PATCH"
-        case .showProductSecret:
-            return "POST"
         case .deleteProduct:
             return "DELETE"
         }
     }
+}
+
+extension OpenMarketService {
+    
+    func makeBody(target: NSMutableData, name:String, data: Data) {
+        target.append("------WebKitFormBoundary7MA4YWxkTrZu0gW\r\n")
+        target.append("Content-Disposition: form-data; name=\"\(name)\"\r\n")
+        target.append("Content-Type: application/json\r\n")
+        target.append("\r\n")
+        target.append(data)
+        target.append("\r\n")
+    }
+
+    func makeBodyImage(target: NSMutableData, name:String, images: [Data]) {
+        for image in images {
+            target.append("------WebKitFormBoundary7MA4YWxkTrZu0gW\r\n")
+            target.append("Content-Disposition: form-data; name=\"\(name)\"; filename=\"\(UUID().uuidString).png\"\r\n")
+            target.append("Content-Type: image/png\r\n")
+            target.append("\r\n")
+            target.append(image)
+            target.append("\r\n")
+        }
+    }
+    
+}
+
+extension NSMutableData {
+    
+    func append(_ string: String) {
+        if let data = string.data(using: .utf8) {
+            self.append(data)
+        }
+    }
+    
 }
