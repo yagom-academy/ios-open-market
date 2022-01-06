e44134cd6ad207a714119968ade5de8476ef119c
Ari <lee_ari95@icloud.com>
2022-01-06 23:48:21 +0900

    refactor: Network 프로퍼티에 접근제어 부여하여 개선 #6

--- OpenMarket/OpenMarket/Model/Network/Network.swift
+++ OpenMarket/OpenMarket/Model/Network/Network.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 final class Network: Networkable {
-    let session: URLSession
+    private let session: URLSession
     
     init(session: URLSession = .shared) {
         self.session = session
