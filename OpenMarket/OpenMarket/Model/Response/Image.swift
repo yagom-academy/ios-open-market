7bd46d8d242767272fda5ad3a4ffb8befaf7f73f
Ari <lee_ari95@icloud.com>
2022-01-06 23:05:01 +0900

    refactor: Image의 프로퍼티 네이밍을 명확하게 수정 #6

--- OpenMarket/OpenMarket/Model/Response/Image.swift
+++ OpenMarket/OpenMarket/Model/Response/Image.swift
@@ -11,11 +11,12 @@ struct Image: Codable {
     let id: Int
     let url: String
     let thumbnailURL: String
-    let succeed: Bool
+    let uploadSucceed: Bool
     let issuedAt: String
 
     enum CodingKeys: String, CodingKey {
-        case id, url, succeed
+        case id, url
+        case uploadSucceed = "succeed"
         case thumbnailURL = "thumbnail_url"
         case issuedAt = "issued_at"
     }
