608aa1ef7389b1cf8e0ca769178c1717c1ec0a5d
oksunwoo <oksunwo@naver.com>
2022-01-06 21:43:44 +0900

    feat: Vendors 타입 구현 #1

---
+++ OpenMarket/OpenMarket/Model/Vendors.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+struct Vendors: Codable {
+    let name: String
+    let id: Int
+    let createdAt: String
+    let issuedAt: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name, id
+        case createdAt = "created_at"
+        case issuedAt = "issued_at"
+    }
+}
