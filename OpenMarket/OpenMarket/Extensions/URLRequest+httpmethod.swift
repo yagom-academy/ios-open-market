a9e671e12985fe0bfd28a21564451844f0a405a6
yim2627 <yim2627@gmail.com>
2022-01-06 16:32:48 +0900

    feat: API 타입 구현 및 URLRequest extension 수정 #4

--- OpenMarket/OpenMarket/Extensions/URLRequest+httpmethod.swift
+++ OpenMarket/OpenMarket/Extensions/URLRequest+httpmethod.swift
@@ -1,27 +1,19 @@
 import Foundation
 
-enum HttpMethod<Body: Encodable> {
-    case get
-    case post(Body)
-    case patch(Body)
-    case delete
-}
-
 extension URLRequest {
-    init<Body: Encodable>(url: URL, method: HttpMethod<Body>) {
+    init(url: URL, api: API) {
         self.init(url: url)
         
-        switch method {
-        case .get:
-            self.httpMethod = "GET"
-        case .post(let body):
-            self.httpMethod = "POST"
-            self.httpBody = try? JSONEncoder().encode(body)
-        case .patch(let body):
-            self.httpMethod = "PATCH"
-            self.httpBody = try? JSONEncoder().encode(body)
-        case .delete:
-            self.httpMethod = "DELETE"
+        self.httpMethod = api.httpMethod
+        
+        switch api {
+        case .productDetail, .productList:
+            return
+        case .productRegister(let body, let id), .productUpdate(let body, let id):
+            self.addValue(id, forHTTPHeaderField: "identifier")
+            self.httpBody = body
+        case .deleteProduct(let id), .productSecret(let id):
+            self.addValue(id, forHTTPHeaderField: "identifier")
         }
     }
 }
---
+++ OpenMarket/OpenMarket/Utils/API.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+enum API {
+    case productDetail
+    case productList
+    case productRegister(body: Data, id: String)
+    case productUpdate(body: Data, id: String)
+    case deleteProduct(id: String)
+    case productSecret(id: String)
+    
+    var httpMethod: String {
+        switch self {
+        case .productDetail, .productList:
+            return "GET"
+        case .productRegister, .productSecret:
+            return "POST"
+        case .productUpdate:
+            return "PATCH"
+        case .deleteProduct:
+            return "DELETE"
+        }
+    }
+}
