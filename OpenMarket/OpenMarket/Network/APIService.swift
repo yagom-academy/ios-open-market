af2013efa8983e923bb579507ba2fd460c89c0cc
yim2627 <yim2627@gmail.com>
2022-01-07 16:06:30 +0900

    refactor: Data extension 접근제어자 및 위치 변경 #5

--- OpenMarket/OpenMarket/Network/APIService.swift
+++ OpenMarket/OpenMarket/Network/APIService.swift
@@ -217,3 +217,12 @@ private extension APIService {
         return data
     }
 }
+
+private extension Data {
+    mutating func appendString(_ string: String) {
+        guard let data = string.data(using: .utf8) else {
+            return
+        }
+        self.append(data)
+    }
+}
