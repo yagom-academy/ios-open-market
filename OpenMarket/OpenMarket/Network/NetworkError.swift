3020c7ba212c006d241528090f86c9ea0614a4a4
GOSARI <goeunrhdms@gmail.com>
2022-01-07 15:43:23 +0900

    feat: URLSession과 UnitTest를 위한 MockURLSession 추가

--- OpenMarket/OpenMarket/Network/NetworkError.swift
+++ OpenMarket/OpenMarket/Network/NetworkError.swift
@@ -1,10 +1,3 @@
-//
-//  Error.swift
-//  OpenMarket
-//
-//  Created by 서녕 on 2022/01/07.
-//
-
 import Foundation
 
 enum NetworkError: Error {
---
+++ OpenMarket/OpenMarket/Network/URLSession.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+class URLSessionProvider {
+    let session: URLSessionProtocol
+    let baseURL = "https://market-training.yagom-academy.kr/"
+    
+    init(session: URLSessionProtocol = URLSession.shared) {
+        self.session = session
+    }
+    
+    func dataTask(request: URLRequest, completionHandler: @escaping (Result<Data, NetworkError>) -> Void) {
+        let task = session.dataTask(with: request) { data, urlResponse, error in
+            
+            guard let httpResponse = urlResponse as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                      return completionHandler(.failure(.statusCodeError))
+                  }
+            
+            if let data = data {
+                return completionHandler(.success(data))
+            }
+            
+            completionHandler(.failure(.unknownFailed))
+        }
+        
+        task.resume()
+    }
+    
+    func getUser(id: Int, completionHandler: @escaping (Result<Data, NetworkError>) -> Void) {
+        guard let url = URL(string: baseURL) else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        dataTask(request: request, completionHandler: completionHandler)
+    }
+}
