0d566aa8338d59def414236a6720570addabd86e
ChaminLee <trueman9512@naver.com>
2022-01-07 16:25:30 +0900

    refactor: case가 아닌 default로 잔여 케이스 관리 #5

--- OpenMarket/OpenMarket/Extensions/URLRequest+httpmethod.swift
+++ OpenMarket/OpenMarket/Extensions/URLRequest+httpmethod.swift
@@ -8,13 +8,13 @@ extension URLRequest {
         self.httpMethod = api.httpMethod
         
         switch api {
-        case .productDetail, .productList:
-            return
         case .productRegister(let body, let id), .productUpdate(let body, let id), .productSecret(let body, let id):
             self.addValue(id, forHTTPHeaderField: "identifier")
             self.httpBody = body
         case .deleteProduct(let id):
             self.addValue(id, forHTTPHeaderField: "identifier")
+        default:
+            return
         }
     }
 }
