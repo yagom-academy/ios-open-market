2a8e1c886147cc1972016aa3df2b069854c3d032
forestjae <forestsj30@gmail.com>
2022-01-07 10:24:20 +0900

    test: 상품 상세조회 테스트 구현

--- OpenMarket/OpenMarketTests/OpenMarketTests.swift
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -21,17 +21,30 @@ class OpenMarketTests: XCTestCase {
         guard let data = NSDataAsset(name: "products")?.data else {
             return
         }
-
+        print(data)
         guard let result = try? JSONDecoder().decode(ProductList.self, from: data) else {
             return
         }
 
         sut.retrieveProductList(session: MockURLSession()) { productList in
+            print(productList)
             XCTAssertEqual(productList.itemsPerPage, result.itemsPerPage)
             XCTAssertEqual(productList.pages.first?.identification,
                            result.pages.first?.identification)
             XCTAssertEqual(productList.pages.first?.currency, result.pages.first?.currency)
             XCTAssertEqual(productList.pages.first?.createdAt, result.pages.first?.createdAt)
+
+            expectaion.fulfill()
+        }
+        wait(for: [expectaion], timeout: 2.0)
+    }
+
+    func test_retrieveProduct() {
+        let expectaion = XCTestExpectation(description: "파싱안됨")
+
+        sut.retrieveProduct(productIdentification: 87, session: HTTPUtility.defaultSession) { product in
+            print(product)
+            XCTAssertEqual(product.name, "aladdin")
 
             expectaion.fulfill()
         }
