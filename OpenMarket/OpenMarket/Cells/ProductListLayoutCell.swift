eea469a1e1a62238817fb380a627417739c309a4
ChaminLee <trueman9512@naver.com>
2022-01-10 22:47:45 +0900

    refactor: ProductListLayoutCell layout 수정 및 ImageLoader 구현 #6
    - stockLabel layout 변경
    - createPriceText내 attributedText 생성 로직 변경
    - defaultContent내 layout 조정 및 label 속성값 변경
    - NSMutableAttributedString의 strikeThroughStyle 로직 수정

--- OpenMarket/OpenMarket/Cells/ProductListLayoutCell.swift
+++ OpenMarket/OpenMarket/Cells/ProductListLayoutCell.swift
@@ -45,21 +45,22 @@ class ProductListLayoutCell: UICollectionViewListCell {
             return
         }
         
-        let stockConstraints = [
-            stockLabel.leadingAnchor.constraint(greaterThanOrEqualTo: listContentView.trailingAnchor),
-            stockLabel.topAnchor.constraint(equalTo: listContentView.topAnchor),
-            stockLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
-        ]
-        
         [listContentView, stockLabel].forEach {
             contentView.addSubview($0)
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
+        
         listContentView.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
         
+        let stockConstraints = [
+            stockLabel.leadingAnchor.constraint(greaterThanOrEqualTo: listContentView.trailingAnchor),
+            stockLabel.centerYAnchor.constraint(equalTo: listContentView.centerYAnchor),
+            stockLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8)
+        ]
+        
         NSLayoutConstraint.activate([
-            listContentView.topAnchor.constraint(equalTo: contentView.topAnchor),
-            listContentView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            listContentView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            listContentView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
             listContentView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor)
         ])
         
@@ -73,6 +74,7 @@ class ProductListLayoutCell: UICollectionViewListCell {
             return nil
         }
         
+        let priceAttributedText = NSMutableAttributedString()
         let spacing = " "
         
         if product.discountedPrice == 0 {
@@ -80,10 +82,13 @@ class ProductListLayoutCell: UICollectionViewListCell {
                 .normalStyle(string: "\(product.currency) \(product.price)")
         }
         
-        return NSMutableAttributedString()
-            .strikeThroughStyle(string: "\(product.currency) \(product.discountedPrice)")
-            .normalStyle(string: spacing)
-            .normalStyle(string: "\(product.currency) \(product.price)")
+        priceAttributedText.append(NSMutableAttributedString()
+                            .strikeThroughStyle(string: "\(product.currency) \(product.discountedPrice)"))
+        priceAttributedText.append(NSMutableAttributedString().normalStyle(string: spacing))
+        priceAttributedText.append(NSMutableAttributedString()
+                            .normalStyle(string: "\(product.currency) \(product.price)"))
+        
+        return priceAttributedText
     }
     
     private func createStockText(product: ProductDetail?) -> NSMutableAttributedString? {
@@ -109,24 +114,39 @@ class ProductListLayoutCell: UICollectionViewListCell {
         
         var content = defaultConfiguration().updated(for: state)
         
-//        content.image = state.prevItem?.thumbnail // imageLoader 만들어야함
+        content.textToSecondaryTextVerticalPadding = 10
+        
+        content.image = ImageLoader.loadImage(from: state.prevItem?.thumbnail)
+        content.imageProperties.reservedLayoutSize = CGSize(width: 50, height: 50)
+        
         content.text = state.prevItem?.name
+        content.textProperties.font = .preferredFont(forTextStyle: .headline)
+        
         content.secondaryAttributedText = createPriceText(product: state.prevItem) ?? nil
+        content.secondaryTextProperties.font = .preferredFont(forTextStyle: .body)
+        
         listContentView.configuration = content
         
         stockLabel.attributedText = createStockText(product: state.prevItem) ?? nil
+        stockLabel.textColor = .gray
     }
 }
 
 private extension NSMutableAttributedString {
     func strikeThroughStyle(string: String) -> NSMutableAttributedString {
         let attributedString = NSMutableAttributedString(string: string)
-        attributedString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: self.length))
-        attributedString.addAttribute(NSAttributedString.Key.strikethroughColor, value: UIColor.red, range: NSRange(location: 0, length: self.length))
+        attributedString.addAttributes([
+            NSAttributedString.Key.strikethroughStyle: NSUnderlineStyle.single.rawValue,
+            NSAttributedString.Key.foregroundColor: UIColor.systemRed
+        ], range: NSRange(location: 0, length: attributedString.length))
+
         return attributedString
     }
     
     func normalStyle(string: String) -> NSMutableAttributedString {
-        return NSMutableAttributedString(string: string)
+        let attributedString = NSMutableAttributedString(string: string)
+        attributedString.addAttributes([NSAttributedString.Key.foregroundColor: UIColor.gray], range: NSRange(location: 0, length: attributedString.length))
+    
+        return attributedString
     }
 }
---
+++ OpenMarket/OpenMarket/Utils/ImageLoader.swift
@@ -0,0 +1,16 @@
+import Foundation
+import UIKit
+
+struct ImageLoader {
+    static func loadImage(from urlString: String?) -> UIImage? {
+        guard let urlString = urlString,
+              let imgURL = URL(string: urlString),
+              let imgData = try? Data(contentsOf: imgURL) else {
+            return nil
+        }
+        
+        let image = UIImage(data: imgData)
+        
+        return image
+    }
+}
