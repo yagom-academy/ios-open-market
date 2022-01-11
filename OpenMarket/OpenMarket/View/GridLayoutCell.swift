1d6b0ff39ecb1700cedf4ddce7ba82f99a21325a
namu <simjh27@gmail.com>
2022-01-11 17:10:20 +0900

    feat: gridLayoutCell 구현 #5

---
+++ OpenMarket/OpenMarket/View/GridLayoutCell.swift
@@ -0,0 +1,43 @@
+//
+//  GridLayoutCell.swift
+//  OpenMarket
+//
+//  Created by Jae-hoon Sim on 2022/01/11.
+//
+
+import UIKit
+
+class GridLayoutCell: UICollectionViewCell {
+    static var reuseIdentifier: String { "gridCell" }
+    static var nib: UINib {
+        UINib(nibName: "GridLayoutCell", bundle: nil)
+    }
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var priceStackView: UIStackView!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var stockLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func configureContents(image: UIImage, productName: String, price: String, discountedPrice: String?, stock: String) {
+        imageView.image = image
+        productNameLabel.text = productName
+        priceLabel.text = price
+        stockLabel.text = stock
+        
+        if let discounted = discountedPrice {
+            priceLabel.textColor = .systemRed
+            let attributeString =  NSMutableAttributedString(string: price)
+            attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 2, range: NSMakeRange(0, attributeString.length))
+            priceLabel.attributedText = attributeString
+            let discountedLabel = UILabel()
+            discountedLabel.text = discounted
+            priceStackView.addArrangedSubview(discountedLabel)
+        }
+    }
+}
