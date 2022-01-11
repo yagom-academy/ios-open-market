fd52e4dcf5821a7f257e360dbba6420a0d8aa525
aCafela-coffee <acafelacoffee@gmail.com>
2022-01-11 15:07:14 +0900

    feat: 테이블 뷰 추가

--- OpenMarket/OpenMarket/Controller/ProductsTableViewController.swift
+++ OpenMarket/OpenMarket/Controller/ProductsTableViewController.swift
@@ -1,77 +1,76 @@
 import UIKit
 
 class ProductsTableViewController: UITableViewController {
-
+    private let reuseIdentifier = "productsListCell"
+    private var productsList: ProductsList?
+    private let jsonParser: JSONParser = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SS"
+        let jsonParser = JSONParser(
+            dateDecodingStrategy: .formatted(formatter),
+            keyDecodingStrategy: .convertFromSnakeCase,
+            keyEncodingStrategy: .convertToSnakeCase
+        )
+        return jsonParser
+    }()
+    private lazy var networkTask = NetworkTask(jsonParser: jsonParser)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let nibName = UINib(nibName: "ProductsTableViewCell", bundle: nil)
+        tableView.register(nibName, forCellReuseIdentifier: reuseIdentifier)
+        loadProductsList()
     }
-
+    
+    private func loadProductsList() {
+        networkTask.requestProductList(pageNumber: 1, itemsPerPage: 20) { result in
+            switch result {
+            case .success(let data):
+                self.productsList = try? self.jsonParser.decode(from: data)
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            case .failure(let error):
+                let alert = UIAlertController(
+                    title: "Network error",
+                    message: "데이터를 불러오지 못했습니다.\n\(error.localizedDescription)",
+                    preferredStyle: .alert
+                )
+                let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(okAction)
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return productsList?.pages.count ?? 0
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+    
+    override func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: reuseIdentifier,
+            for: indexPath
+        ) as? ProductsTableViewCell,
+              let product = productsList?.pages[indexPath.row],
+              let url = URL(string: product.thumbnail),
+              let imageData = try? Data(contentsOf: url) else {
+                  return ProductsTableViewCell()
+              }
+        let image = UIImage(data: imageData)
+        cell.productImageView.image = image
+        cell.titleLabel.attributedText = product.attributedTitle
+        cell.priceLabel.attributedText = product.attributedPrice
+        cell.stockLabel.attributedText = product.attributedStock
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- OpenMarket/OpenMarket/View/ProductsTableViewCell.swift
+++ OpenMarket/OpenMarket/View/ProductsTableViewCell.swift
@@ -1,10 +1,3 @@
-//
-//  ProductsTableViewCell.swift
-//  OpenMarket
-//
-//  Created by 양호준 on 2022/01/11.
-//
-
 import UIKit
 
 class ProductsTableViewCell: UITableViewCell {
@@ -12,9 +5,4 @@ class ProductsTableViewCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var stockLabel: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
 }
