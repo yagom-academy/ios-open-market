4535e7b14e4b469cdaf3749b2c328a523bccea20
oksunwoo <oksunwo@naver.com>
2022-01-13 00:24:46 +0900

    feat: SegmentedControl 구현 #5
    - NavigationController 추가
    - SegmentedControl 클래스로 UI요소 업데이트

---
+++ OpenMarket/OpenMarket/View/SegmentedControl.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class SegmentedControl: UISegmentedControl {
+    func setUpUI() {
+        let items = ["LIST","GRID"]
+        
+        items.enumerated().forEach { (index, title) in
+            setTitle(title, forSegmentAt: index)
+        }
+
+        layer.borderColor = UIColor.systemBlue.cgColor
+        layer.borderWidth = 2
+        selectedSegmentTintColor = UIColor.systemBlue
+        selectedSegmentIndex = 0
+        setTitleTextAttributes([.foregroundColor: UIColor.systemBlue], for: .normal)
+        setTitleTextAttributes([.foregroundColor: UIColor.white], for: .selected)
+    }
+}
--- OpenMarket/OpenMarket/ViewController.swift
+++ OpenMarket/OpenMarket/ViewController.swift
@@ -1,11 +1,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var segment: SegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setSegmentedControl()
     }
 
-
+    func setSegmentedControl() {
+        self.navigationItem.titleView = segment
+        segment.setUpUI()
+    }
 }
-
