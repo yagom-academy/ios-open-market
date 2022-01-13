cf4d2eb6d75ff281d8c5d7af7f499faa5f06252e
yeha <dev.yeha@gmail.com>
2022-01-13 16:26:44 +0900

    feat: Grid를 구성할 custom cell 기본 설정 구현 #6
    - 내부 UI 요소 프로퍼티 생성
    - 프로퍼티를 구성하는 configure 메서드 구현

--- OpenMarket/OpenMarket/View/GridCollectionViewCell.swift
+++ OpenMarket/OpenMarket/View/GridCollectionViewCell.swift
@@ -1,12 +1,49 @@
-//
-//  GridCollectionViewCell.swift
-//  OpenMarket
-//
-//  Created by yeha on 2022/01/13.
-//
-
 import UIKit
 
 class GridCollectionViewCell: UICollectionViewCell {
-    
+    let itemStackView = UIStackView()
+    let thumbnailImageView = UIImageView()
+    let nameLabel = UILabel()
+    let priceStackView = UIStackView()
+    let priceLabel = UILabel()
+    let bargainPriceLabel = UILabel()
+    let stockLabel = UILabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
+
+extension GridCollectionViewCell {
+    func configure() {
+        itemStackView.translatesAutoresizingMaskIntoConstraints = false
+        priceStackView.addArrangedSubview(priceLabel)
+        priceStackView.addArrangedSubview(bargainPriceLabel)
+        itemStackView.addArrangedSubview(thumbnailImageView)
+        itemStackView.addArrangedSubview(nameLabel)
+        itemStackView.addArrangedSubview(priceStackView)
+        itemStackView.addArrangedSubview(stockLabel)
+
+        self.layer.borderColor = UIColor.systemGray.cgColor
+        self.layer.borderWidth = 2
+        self.layer.cornerRadius = 8
+        self.layer.backgroundColor = UIColor.white.cgColor
+        self.contentView.addSubview(itemStackView)
+
+        itemStackView.axis = .vertical
+        itemStackView.alignment = .center
+        itemStackView.distribution = .fill
+
+        NSLayoutConstraint.activate([
+            itemStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            itemStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            itemStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            itemStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
 }
