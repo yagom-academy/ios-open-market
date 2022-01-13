fbf95215e17669f840fd586ddf9d0d1276ad75e4
horeng2 <huski.a.a@gmail.com>
2022-01-13 20:23:39 +0900

    feat: TableViewControlloer 내부에 Cell 구현

--- OpenMarket/OpenMarket/View Controller/TableViewController.swift
+++ OpenMarket/OpenMarket/View Controller/TableViewController.swift
@@ -1,37 +1,38 @@
-////
-////  TableViewController.swift
-////  OpenMarket
-////
-////  Created by 고은 on 2022/01/13.
 //
-//import UIKit
+//  TableViewController.swift
+//  OpenMarket
 //
-//class TableViewController: UITableViewController {
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "reuseIdentifier")
-//        // Uncomment the following line to preserve selection between presentations
-//        // self.clearsSelectionOnViewWillAppear = false
-//
-//        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-//        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-//    }
-//
-//    // MARK: - Table view data source
-//
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        return 1
-//    }
-//
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 1
-//    }
-//
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-//        cell.backgroundColor = .systemIndigo
-//        
-//        return cell
-//    }
-//}
+//  Created by 고은 on 2022/01/13.
+
+import UIKit
+
+@available(iOS 14.0, *)
+class TableViewController: UITableViewController {
+    @IBOutlet var productListTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "reuseIdentifier")
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var contentConfig = cell.defaultContentConfiguration()
+        contentConfig.image = UIImage(systemName: "")
+        contentConfig.text = ""
+
+        cell.contentConfiguration = contentConfig
+        
+        return cell
+    }
+}
