78b9d55bf8f712383e2a74eb3adadabf2a1569a1
wooyani77 <wooyani77@gmail.com>
2022-01-13 17:27:41 +0900

    feat: collectionView 생성 #5
    - segmentedControl을 이용한 화면전환 구현 및 테스트

--- OpenMarket/OpenMarket/ViewController.swift
+++ OpenMarket/OpenMarket/ViewController.swift
@@ -2,15 +2,39 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var segment: SegmentedControl!
+    @IBOutlet weak var listCollectionView: UICollectionView!
+    @IBOutlet weak var gridCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setSegmentedControl()
+        setUpListCollectionView()
     }
 
     func setSegmentedControl() {
         self.navigationItem.titleView = segment
         segment.setUpUI()
+    }
+    
+    @IBAction func changeView(_ sender: SegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            listCollectionView.isHidden = false
+            gridCollectionView.isHidden = true
+        case 1:
+            listCollectionView.isHidden = true
+            gridCollectionView.isHidden = false
+        default:
+            break
+        }
+    }
+}
+
+extension ViewController {
+    func setUpListCollectionView() {
+        // 뷰 전환 테스트
+        listCollectionView.backgroundColor = .red
+        gridCollectionView.backgroundColor = .blue
     }
 }
