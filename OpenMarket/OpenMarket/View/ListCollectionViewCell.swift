563d1e60c91151977bf4c87891154c2d43cb4c98
oksunwoo <oksunwo@naver.com>
2022-01-17 21:55:33 +0900

    feat: listCollectionView의 Cell타입 구현 #5

---
+++ OpenMarket/OpenMarket/View/ListCollectionViewCell.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class ListCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var originalPriceLabel: UILabel!
+    @IBOutlet weak var discountedPriceLabel: UILabel!
+    @IBOutlet weak var stockLabel: UILabel!
+}
