10e77fb356ca1e5c3f180fc9535aa97e941d9370
forestjae <forestsj30@gmail.com>
2022-01-18 12:31:59 +0900

    refactor: addAttributes메서드를 사용하여 코드 중복 제거 #7

--- OpenMarket/OpenMarket/Extension/String+Extension.swift
+++ OpenMarket/OpenMarket/Extension/String+Extension.swift
@@ -4,12 +4,13 @@ import UIKit
 extension String {
     func eraseOriginalPrice() -> NSMutableAttributedString {
         let attributeString = NSMutableAttributedString(string: self)
-        attributeString.addAttribute(.strikethroughStyle,
-                                     value: NSUnderlineStyle.single.rawValue,
-                                     range: NSRange(location: 0, length: attributeString.length))
-        attributeString.addAttribute(.foregroundColor,
-                                     value: UIColor.systemRed,
-                                     range: NSRange(location: 0, length: attributeString.length))
+        attributeString.addAttributes(
+            [
+            .strikethroughStyle: NSUnderlineStyle.single.rawValue,
+            .foregroundColor: UIColor.systemRed
+            ],
+            range: NSRange(location: 0, length: attributeString.length)
+        )
         return attributeString
     }
 }
