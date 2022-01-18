4297ee8c1efb58ef3a303c1c1169b5582a3a6e9b
yanghojoon <yanghojoon8487@gmail.com>
2022-01-18 18:52:59 +0900

    feat: collectionView에 이미지 추가 기능 구현

--- OpenMarket/OpenMarket/Controller/ProductRegistrationViewController.swift
+++ OpenMarket/OpenMarket/Controller/ProductRegistrationViewController.swift
@@ -2,21 +2,23 @@ import UIKit
 
 class ProductRegistrationViewController: UIViewController, UINavigationControllerDelegate {
     private let imagePickerController = UIImagePickerController()
+    private var images = [UIImage]()
     
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imagesCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagesCollectionView.dataSource = self
         setUpImagePicker()
+    }
+    
+    @objc func presentImagePicker() {
+        present(imagePickerController, animated: true, completion: nil)
     }
     
     private func setUpImagePicker() {
         imagePickerController.sourceType = .photoLibrary
         imagePickerController.delegate = self
-    }
-    
-    @IBAction func touchUpInsideAddButton(_ sender: UIButton) {
-        present(imagePickerController, animated: true, completion: nil)
     }
 }
 
@@ -25,11 +27,54 @@ extension ProductRegistrationViewController: UIImagePickerControllerDelegate {
         _ picker: UIImagePickerController,
         didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]
     ) {
-        var image: UIImage? = nil
         if let newImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            image = newImage
+            images.append(newImage)
+            imagesCollectionView.reloadData()
         }
-        imageView.image = image
         picker.dismiss(animated: true, completion: nil)
     }
+}
+
+extension ProductRegistrationViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if section == 0 {
+            return images.count
+        } else {
+            return 1
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: UICollectionViewCell.reuseIdentifier,
+            for: indexPath
+        )
+        if indexPath.section == 0 {
+            let image = images[indexPath.item]
+            let imageView = UIImageView(frame: cell.contentView.frame)
+            imageView.image = image
+            imageView.contentMode = .scaleAspectFit
+            cell.contentView.addSubview(imageView)
+        } else {
+            let button = UIButton(type: .system)
+            let image = UIImage(systemName: "plus")
+            button.setTitle(nil, for: .normal)
+            button.setImage(image, for: .normal)
+            button.addTarget(self, action: #selector(presentImagePicker), for: .touchUpInside)
+            button.backgroundColor = .opaqueSeparator
+            cell.contentView.addSubview(button)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.topAnchor.constraint(equalTo: cell.contentView.topAnchor).isActive = true
+            button.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor).isActive = true
+            button.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor).isActive = true
+            button.trailingAnchor.constraint(equalTo: cell.contentView.trailingAnchor).isActive = true
+        }
+        return cell
+    }
+    
+    
 }
