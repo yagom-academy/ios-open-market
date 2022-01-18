805c86d509ebd48c701ce071c02af33fe185980d
smart8612 <smart8612@gmail.com>
2022-01-18 13:52:08 +0900

    feat: 상품 등록 페이지 레이아웃 구성 #6

---
+++ OpenMarket/OpenMarket/Controller/ProductCreateViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCreateViewController.swift
+//  OpenMarket
+//
+//  Created by JeongTaek Han on 2022/01/18.
+//
+
+import UIKit
+
+class ProductCreateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
