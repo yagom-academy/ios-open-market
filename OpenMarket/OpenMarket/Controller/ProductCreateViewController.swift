006720ad01d29a492b2b7f90a44fb16528783362
smart8612 <smart8612@gmail.com>
2022-01-18 14:51:47 +0900

    feat: 이미지 추가 클릭시 action sheet 동작 기능 구현 #7

--- OpenMarket/OpenMarket/Controller/ProductCreateViewController.swift
+++ OpenMarket/OpenMarket/Controller/ProductCreateViewController.swift
@@ -7,12 +7,35 @@
 
 import UIKit
 
-class ProductCreateViewController: UIViewController {
-
+final class ProductCreateViewController: UIViewController {
+    
+    private var images: [UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func imageAddbuttonClicked(_ sender: UIButton) {
+        let alert = UIAlertController(
+            title: "사진을 불러옵니다",
+            message: "어디서 불러올까요?",
+            preferredStyle: .actionSheet
+        )
+        alert.addAction(title: "카메라", style: .default)
+        alert.addAction(title: "앨범", style: .default)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+}
+
+// MARK: - AlertController Utilities
+fileprivate extension UIAlertController {
+    
+    func addAction(title: String, style: UIAlertAction.Style, handler: ((UIAlertAction) -> Void)? = nil) {
+        let action = UIAlertAction(title: title, style: style, handler: handler)
+        self.addAction(action)
+    }
+    
 }
