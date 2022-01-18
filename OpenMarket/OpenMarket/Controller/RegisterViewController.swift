aef59e20993c60bcb14d8fecce8da6722cf1dd6c
Ari <lee_ari95@icloud.com>
2022-01-18 11:21:21 +0900

    feat: 컬렉션뷰에 추가할 커스텀 셀을 생성 #13
    - ImageCell 타입과 XIB파일 생성
    - 셀의 레이아웃을 설정하기 위해 FlowLayout 설정
    - 컬렉션뷰 스크롤바를 제거

--- OpenMarket/OpenMarket/Controller/RegisterViewController.swift
+++ OpenMarket/OpenMarket/Controller/RegisterViewController.swift
@@ -9,11 +9,57 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpCollectionView()
+    }
+    
+    func setUpCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        let cellNib = UINib(nibName: ImageCell.nibName, bundle: .main)
+        collectionView.register(cellNib, forCellWithReuseIdentifier: ImageCell.identifier)
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        collectionView.collectionViewLayout = flowLayout
+        collectionView.showsHorizontalScrollIndicator = false
     }
     
     @IBAction func cancelButtonTapped(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension RegisterViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: ImageCell.identifier,
+            for: indexPath
+        ) as? ImageCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+}
+
+extension RegisterViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        let width = collectionView.safeAreaLayoutGuide.layoutFrame.width / 3
+        return CGSize(width: width, height: width)
     }
 }
---
+++ OpenMarket/OpenMarket/View/ImageCell.swift
@@ -0,0 +1,20 @@
+//
+//  ImageCell.swift
+//  OpenMarket
+//
+//  Created by Ari on 2022/01/18.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+    static let identifier = "imageCell"
+    static let nibName = "ImageCell"
+    
+    @IBOutlet var imageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        imageView.layer.cornerRadius = 10
+    }
+}
