ab489b7a58f06234e72fe5e7c8bcabbb12613e79
forestjae <forestsj30@gmail.com>
2022-01-19 18:14:11 +0900

    feat: ProductImageCell 기본구현

---
+++ OpenMarket/OpenMarket/View/ProductImageCell.swift
@@ -0,0 +1,19 @@
+//
+//  ProductImageCell.swift
+//  OpenMarket
+//
+//  Created by 이승재 on 2022/01/19.
+//
+
+import UIKit
+
+class ProductImageCell: UICollectionViewCell {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
