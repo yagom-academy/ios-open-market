2b8ac38becc331a2d411f50a7ca4a673abfed5b0
zoe <kny0651@naver.com>
2022-01-19 18:10:06 +0900

    test: 상품등록, 상품수정 메서드에 대한 테스트 추가 #7
    - Stub request에 register/update 케이스 추가

--- OpenMarket/OpenMarketTests/Test Doubles/StubRequest.swift
+++ OpenMarket/OpenMarketTests/Test Doubles/StubRequest.swift
@@ -10,6 +10,8 @@ import Foundation
 enum StubRequest {
     case getPage
     case getProduct
+    case registerProduct
+    case updateProduct
     
     var data: Data {
         switch self {
@@ -17,6 +19,10 @@ enum StubRequest {
             return Data(StubRequest.getPageData)
         case .getProduct:
             return Data(StubRequest.getProductData)
+        case .registerProduct:
+            return Data(StubRequest.registerProductData)
+        case .updateProduct:
+            return Data(StubRequest.updateProductData)
         }
     }
 }
@@ -70,6 +76,71 @@ extension StubRequest {
             "thumbnail_url": "https://s3.ap-northeast-2.amazonaws.com/media.yagom-academy.kr/training-resources/3/thumb/b07b4b206eec11ecabfa73cd0451c065.jpeg",
             "succeed": true,
             "issued_at": "2022-01-06T00:00:00.00"
+        }
+    ],
+    "vendors": {
+        "name": "Vendor2",
+        "id": 3,
+        "created_at": "2021-12-27T00:00:00.00",
+        "issued_at": "2021-12-27T00:00:00.00"
+        }
+    }
+    """.utf8
+    
+    static let registerProductData = """
+    {
+    "id": 536,
+    "vendor_id": 3,
+    "name": "zoe",
+    "thumbnail": "https://s3.ap-northeast-2.amazonaws.com/media.yagom-academy.kr/training-resources/3/thumb/5b5a5f22790111ec9173b76aa80a2e69.jpeg",
+    "currency": "KRW",
+    "price": 0.0,
+    "description": "desc",
+    "bargain_price": 0.0,
+    "discounted_price": 0.0,
+    "stock": 0,
+    "created_at": "2022-01-19T08:25:27.27",
+    "issued_at": "2022-01-19T08:25:27.27",
+    "images": [
+        {
+            "id": 365,
+            "url": "https://s3.ap-northeast-2.amazonaws.com/media.yagom-academy.kr/training-resources/3/origin/5b5a5f22790111ec9173b76aa80a2e69.jpeg",
+            "thumbnail_url": "https://s3.ap-northeast-2.amazonaws.com/media.yagom-academy.kr/training-resources/3/thumb/5b5a5f22790111ec9173b76aa80a2e69.jpeg",
+            "succeed": true,
+            "issued_at": "2022-01-19T08:25:27.27"
+        }
+    ],
+    "vendors": {
+        "name": "Vendor2",
+        "id": 3,
+        "created_at": "2021-12-27T00:00:00.00",
+        "issued_at": "2021-12-27T00:00:00.00"
+        }
+    }
+    """.utf8
+
+
+    static let updateProductData = """
+    {
+    "id": 533,
+    "vendor_id": 3,
+    "name": "aladdin",
+    "thumbnail": "https://s3.ap-northeast-2.amazonaws.com/media.yagom-academy.kr/training-resources/3/thumb/808a439c78f811ecabfafb03c5e0d3f1.jpeg",
+    "currency": "KRW",
+    "price": 100000.0,
+    "description": "ㅇㅇㅇ",
+    "bargain_price": 99900.0,
+    "discounted_price": 100.0,
+    "stock": 0,
+    "created_at": "2022-01-19T00:00:00.00",
+    "issued_at": "2022-01-19T00:00:00.00",
+    "images": [
+        {
+            "id": 362,
+            "url": "https://s3.ap-northeast-2.amazonaws.com/media.yagom-academy.kr/training-resources/3/origin/808a439c78f811ecabfafb03c5e0d3f1.jpeg",
+            "thumbnail_url": "https://s3.ap-northeast-2.amazonaws.com/media.yagom-academy.kr/training-resources/3/thumb/808a439c78f811ecabfafb03c5e0d3f1.jpeg",
+            "succeed": true,
+            "issued_at": "2022-01-19T00:00:00.00"
         }
     ],
     "vendors": {
--- OpenMarket/OpenMarketTests/Tests/APIServiceTests.swift
+++ OpenMarket/OpenMarketTests/Tests/APIServiceTests.swift
@@ -10,14 +10,14 @@ import XCTest
 
 final class APIServiceTests: XCTestCase {
     func testGetPage_givenSuccessfulRequest_expectCorrectData() {
-        let mockURLSession = StubURLSession(isSuccessfulRequest: true, mockRequest: .getPage)
-        let sut = MarketAPIService(session: mockURLSession)
-        let mockData = try? JSONDecoder().decode(Page.self, from: mockURLSession.request.data)
+        let stubURLSession = StubURLSession(isSuccessfulRequest: true, mockRequest: .getPage)
+        let sut = MarketAPIService(session: stubURLSession)
+        let expectedData = try? JSONDecoder().decode(Page.self, from: stubURLSession.request.data)
         
         sut.fetchPage(pageNumber: 1, itemsPerPage: 1) { result in
             switch result {
             case .success(let data):
-                XCTAssertEqual(data, mockData)
+                XCTAssertEqual(data, expectedData)
             case .failure:
                 XCTFail()
             }
@@ -25,8 +25,8 @@ final class APIServiceTests: XCTestCase {
     }
     
     func testGetPage_givenFailureRequest_expectInvalidRequestError() {
-        let mockURLSession = StubURLSession(isSuccessfulRequest: false, mockRequest: .getPage)
-        let sut = MarketAPIService(session: mockURLSession)
+        let stubURLSession = StubURLSession(isSuccessfulRequest: false, mockRequest: .getPage)
+        let sut = MarketAPIService(session: stubURLSession)
         
         sut.fetchPage(pageNumber: 1, itemsPerPage: 1) { result in
             switch result {
@@ -39,14 +39,14 @@ final class APIServiceTests: XCTestCase {
     }
     
     func testGetProduct_givenSuccessfulRequest_expectCorrectData() {
-        let mockURLSession = StubURLSession(isSuccessfulRequest: true, mockRequest: .getProduct)
-        let sut = MarketAPIService(session: mockURLSession)
-        let mockData = try? JSONDecoder().decode(Product.self, from: mockURLSession.request.data)
+        let stubURLSession = StubURLSession(isSuccessfulRequest: true, mockRequest: .getProduct)
+        let sut = MarketAPIService(session: stubURLSession)
+        let expectedData = try? JSONDecoder().decode(Product.self, from: stubURLSession.request.data)
         
         sut.fetchProduct(productID: 87) { result in
             switch result {
             case .success(let data):
-                XCTAssertEqual(data, mockData)
+                XCTAssertEqual(data, expectedData)
             case .failure:
                 XCTFail()
             }
@@ -54,10 +54,122 @@ final class APIServiceTests: XCTestCase {
     }
     
     func testGetProduct_givenFailureRequest_expectInvalidRequestError() {
-        let mockURLSession = StubURLSession(isSuccessfulRequest: false, mockRequest: .getProduct)
-        let sut = MarketAPIService(session: mockURLSession)
+        let stubURLSession = StubURLSession(isSuccessfulRequest: false, mockRequest: .getProduct)
+        let sut = MarketAPIService(session: stubURLSession)
         
         sut.fetchProduct(productID: 87) { result in
+            switch result {
+            case .success(_):
+                XCTFail()
+            case .failure(let error):
+                XCTAssertEqual(error, .unsuccessfulStatusCode(statusCode: 410))
+            }
+        }
+    }
+    
+    func testRegisterProduct_givenSuccessfulRequest_expectCorrectData() {
+        let stubURLSession = StubURLSession(isSuccessfulRequest: true, mockRequest: .getPage)
+        let sut = MarketAPIService(session: stubURLSession)
+        let expectedData = try? JSONDecoder().decode(Product.self, from: stubURLSession.request.data)
+        guard let image = UIImage(named: "olaf") else {
+            return
+        }
+        let dummyProduct = PostProduct(
+            name: "zoe",
+            descriptions: "desc",
+            price: 0,
+            currency: "KRW",
+            discountedPrice: 0,
+            stock: 0,
+            secret: "password"
+        )
+        let dummyImage = ProductImage(
+            name: "https://s3.ap-northeast-2.amazonaws.com/media.yagom-academy.kr/training-resources/3/origin/5b5a5f22790111ec9173b76aa80a2e69.jpeg",
+            type: .jpeg,
+            image: image
+        )
+        
+        sut.registerProduct(product: dummyProduct, images: [dummyImage]) { result in
+            switch result {
+            case .success(let data):
+                XCTAssertEqual(data, expectedData)
+            case .failure:
+                XCTFail()
+            }
+        }
+    }
+    
+    func testRegisterProduct_givenFailureRequest_expectInvalidRequestError() {
+        let stubURLSession = StubURLSession(isSuccessfulRequest: false, mockRequest: .registerProduct)
+        let sut = MarketAPIService(session: stubURLSession)
+        guard let image = UIImage(named: "olaf") else {
+            return
+        }
+        let dummyProduct = PostProduct(
+            name: "zoe",
+            descriptions: "desc",
+            price: 0,
+            currency: "KRW",
+            discountedPrice: 0,
+            stock: 0,
+            secret: "password"
+        )
+        let dummyImage = ProductImage(
+            name: "https://s3.ap-northeast-2.amazonaws.com/media.yagom-academy.kr/training-resources/3/origin/5b5a5f22790111ec9173b76aa80a2e69.jpeg",
+            type: .jpeg,
+            image: image
+        )
+        
+        sut.registerProduct(product: dummyProduct, images: [dummyImage]) { result in
+            switch result {
+            case .success(_):
+                XCTFail()
+            case .failure(let error):
+                XCTAssertEqual(error, .unsuccessfulStatusCode(statusCode: 410))
+            }
+        }
+    }
+    
+    func testUpdateProduct_givenSuccessfulRequest_expectCorrectData() {
+        let stubURLSession = StubURLSession(isSuccessfulRequest: true, mockRequest: .updateProduct)
+        let sut = MarketAPIService(session: stubURLSession)
+        let expectedData = try? JSONDecoder().decode(Product.self, from: stubURLSession.request.data)
+        let dummyProduct = PatchProduct(
+            name: "aladdin",
+            descriptions: nil,
+            thumbnailID: nil,
+            price: nil,
+            currency: nil,
+            discountedPrice: nil,
+            stock: nil,
+            secret: "password"
+        )
+        
+        sut.updateProduct(productID: 533, product: dummyProduct) { result in
+            switch result {
+            case .success(let data):
+                XCTAssertEqual(data, expectedData)
+            case .failure:
+                XCTFail()
+            }
+        }
+    }
+    
+    func testUpdateProduct_givenFailureRequest_expectInvalidRequestError() {
+        let stubURLSession = StubURLSession(isSuccessfulRequest: false, mockRequest: .updateProduct)
+        let sut = MarketAPIService(session: stubURLSession)
+        let dummyProduct = PatchProduct(
+            name: "aladdin",
+            descriptions: nil,
+            thumbnailID: nil,
+            price: nil,
+            currency: nil,
+            discountedPrice: nil,
+            stock: nil,
+            secret: "password"
+        )
+        
+        sut.updateProduct(productID: 533, product: dummyProduct) { result in
             switch result {
             case .success(_):
                 XCTFail()
