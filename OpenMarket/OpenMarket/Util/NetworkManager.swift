9a84bd71ccee8dc912ef2b2e512fe7ff6e44fca0
Nicholas <83689084+Kim-EunsooSilver@users.noreply.github.com>
2022-01-19 21:44:36 +0900

    refactor: dataTask 개선

--- OpenMarket/OpenMarket/Util/NetworkManager.swift
+++ OpenMarket/OpenMarket/Util/NetworkManager.swift
@@ -14,79 +14,42 @@ struct NetworkManager {
                    itmesPerPage: Int,
                    completion: @escaping (Result<ItemList, Error>) -> Void)
   {
-    
     let url = URLMaker.itemListURL(pageNo: pageNo, itmesPerPage: itmesPerPage)
-
-    let request = URLRequest(url: url)
+    let request = URLRequest(url: url, httpMethod: .get)
     
-    let dataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
-      guard error == nil else {
-        guard let error = error else {
+    let dataTask = session.dataTask(request: request) { result in
+      switch result {
+      case .success(let data):
+        guard let result = try? JSONDecoder().decode(ItemList.self, from: data) else {
           return
         }
+        completion(.success(result))
+      case .failure(let error):
         completion(.failure(error))
-        return
       }
-      guard let httpResponse = response as? HTTPURLResponse,
-                (200...299).contains(httpResponse.statusCode)
-      else {
-        guard let error = error else {
-          return
-        }
-        completion(.failure(error))
-        return
-      }
-      guard let recievedData = data else {
-        return
-      }
-      
-      guard let result = try? JSONDecoder().decode(ItemList.self, from: recievedData) else {
-        return
-      }
-      completion(.success(result))
-      
     }
-    dataTask.resume()
     
+    dataTask.resume()
   }
   
   func getItemInfo(itemId: Int,
                    completion: @escaping (Result<Item, Error>) -> Void)
   {
-    
     let url = URLMaker.itemInfoURL(itemId: itemId)
-    print(url)
-    let request = URLRequest(url: url)
+    let request = URLRequest(url: url, httpMethod: .get)
     
-    let dataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
-      guard error == nil else {
-        guard let error = error else {
+    let dataTask = session.dataTask(request: request) { result in
+      switch result {
+      case .success(let data):
+        guard let result = try? JSONDecoder().decode(Item.self, from: data) else {
           return
         }
+        completion(.success(result))
+      case .failure(let error):
         completion(.failure(error))
-        return
       }
-      
-      guard let httpResponse = response as? HTTPURLResponse,
-                (200...299).contains(httpResponse.statusCode)
-      else {
-        guard let error = error else {
-          return
-        }
-        completion(.failure(error))
-        return
-      }
-      
-      guard let recievedData = data else {
-        return
-      }
-      
-      guard let result = try? JSONDecoder().decode(Item.self, from: recievedData) else {
-        return
-      }
-      completion(.success(result))
-      
     }
+    
     dataTask.resume()
   }
   
