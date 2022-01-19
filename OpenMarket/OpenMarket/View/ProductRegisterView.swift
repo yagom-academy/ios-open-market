09c45229818717916f05b1d125f2cf4ab13880f8
forestjae <forestsj30@gmail.com>
2022-01-19 15:57:01 +0900

    feat: ProductRegisterView 생성

---
+++ OpenMarket/OpenMarket/View/ProductRegisterView.swift
@@ -0,0 +1,13 @@
+import Foundation
+import UIKit
+
+class ProductRegisterView: UIStackView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init(coder: NSCoder) {
+        fatalError()
+    }
+}
