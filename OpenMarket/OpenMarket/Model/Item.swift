05e1c2913c63135597065f1d54145381710771b8
Nicholas <83689084+Kim-EunsooSilver@users.noreply.github.com>
2022-01-19 19:04:08 +0900

    refactor: Item 타입에 프로퍼티 추가

--- OpenMarket/OpenMarket/Model/Item.swift
+++ OpenMarket/OpenMarket/Model/Item.swift
@@ -17,14 +17,17 @@ struct Item: Codable {
   let bargainPrice: Double
   let discountedPrice: Double
   let stock: Int
+  let images: [ItemImage]?
+  let vendorInfo: VendorInfo?
   let createdAt: String
   let issuedAt: String
   
-  enum CodingKeys : String, CodingKey{
-    case id, name, thumbnail, currency, price, stock
+  enum CodingKeys : String, CodingKey {
+    case id, name, thumbnail, currency, price, stock, images
     case vendorId = "vendor_id"
     case bargainPrice = "bargain_price"
     case discountedPrice = "discounted_price"
+    case vendorInfo = "vendors"
     case createdAt = "created_at"
     case issuedAt = "issued_at"
   }
