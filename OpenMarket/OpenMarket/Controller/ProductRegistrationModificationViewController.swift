cdb95ba54c25a7bb1947e04dda4f1c3a99a5fc53
hoBahk <qudgh849@gmail.com>
2022-01-20 04:16:45 +0900

    feat: 키보드가 컨텐츠를 가리지 않도록 수정

--- OpenMarket/OpenMarket/Controller/ProductRegistrationModificationViewController.swift
+++ OpenMarket/OpenMarket/Controller/ProductRegistrationModificationViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ProductRegistrationModificationViewController: UIViewController, ImagePickerable {
-  
+  @IBOutlet weak var mainScrollView: UIScrollView!
   @IBOutlet weak var stackView: UIStackView!
   @IBOutlet weak var nameTextField: UITextField!
   @IBOutlet weak var fixedPriceTextField: UITextField!
@@ -22,20 +22,68 @@ class ProductRegistrationModificationViewController: UIViewController, ImagePick
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    keyboardNotification()
   }
   
-  @IBAction func button(_ sender: Any) {
+  @IBAction private func button(_ sender: Any) {
     if images.count < 5 {
       actionSheetAlertForImage()
     } else {
       showAlert(message: "이미지는 5개까지 등록 가능합니다.")
     }
   }
-
-  func appendImageView(image: UIImage) {
+  
+  private func appendImageView(image: UIImage) {
     let imageView = UIImageView(image: image)
-    imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 1).isActive = true
     stackView.addArrangedSubview(imageView)
+    imageView.heightAnchor.constraint(
+      equalTo: imageView.widthAnchor,
+      multiplier: 1
+    ).isActive = true
+  }
+}
+
+extension ProductRegistrationModificationViewController {
+  private func keyboardNotification() {
+    self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(endEditing)))
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(keyboardWillShow(_:)),
+      name: UIResponder.keyboardWillShowNotification,
+      object: nil
+    )
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(keyboardWillHide(_:)),
+      name: UIResponder.keyboardWillHideNotification,
+      object: nil
+    )
+  }
+  
+  @objc private func endEditing(){
+    view.endEditing(true)
+  }
+  
+  @objc private func keyboardWillShow(_ sender: Notification) {
+    guard let userInfo = sender.userInfo,
+          let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
+            return
+          }
+    let keyboardHeight = keyboardFrame.size.height
+    let contentInset = UIEdgeInsets(
+      top: 0.0,
+      left: 0.0,
+      bottom: keyboardHeight,
+      right: 0.0
+    )
+    mainScrollView.contentInset = contentInset
+    mainScrollView.scrollIndicatorInsets = contentInset
+  }
+  
+  @objc private func keyboardWillHide(_ sender: Notification) {
+    let contentInset = UIEdgeInsets.zero
+    mainScrollView.contentInset = contentInset
+    mainScrollView.scrollIndicatorInsets = contentInset
   }
 }
 
