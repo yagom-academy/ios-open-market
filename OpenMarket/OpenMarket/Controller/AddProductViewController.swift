ccadddb3353bfe2f9a6a5fdc2700188a8d25d77a
namu <simjh27@gmail.com>
2022-01-20 16:23:04 +0900

    feat: 상품상세뷰 타이틀, 취소 버튼, Done 버튼 생성 #4

--- OpenMarket/OpenMarket/Controller/AddProductViewController.swift
+++ OpenMarket/OpenMarket/Controller/AddProductViewController.swift
@@ -11,9 +11,31 @@ class AddProductViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .blue
+        configure()
     }
-    
+
+    private func configure() {
+        view.backgroundColor = .white
+
+        // navi bar
+        title = "상품등록"
+
+        let closeButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(closeButtonDidTap))
+        navigationItem.leftBarButtonItem = closeButton
+
+        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(doneButtonDidTap))
+        navigationItem.rightBarButtonItem = doneButton
+    }
+
+    @objc
+    func closeButtonDidTap() {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    @objc
+    func doneButtonDidTap() {
+        (print("POST")) //POST actions..+ reload collectionView(?)
+        self.dismiss(animated: true, completion: nil)
+    }
 
 }
