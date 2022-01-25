79b94c246adde670981030ba0e83529b0eedbb86
forestjae <forestsj30@gmail.com>
2022-01-25 20:46:08 +0900

    refactor: 상품수정,등록화면 분리에 따른 리팩토링

--- OpenMarket/OpenMarket/Domain/Catalog/ProductsCatalogViewController.swift
+++ OpenMarket/OpenMarket/Domain/Catalog/ProductsCatalogViewController.swift
@@ -59,7 +59,7 @@ extension ProductsCatalogViewController {
     }
 
     @objc func touchUpRegisterButton() {
-        let rootViewController = RegisterProductViewController.init(productIdentification: nil)
+        let rootViewController = RegisterProductViewController.init()
         let navigationController = UINavigationController(rootViewController: rootViewController)
         navigationController.modalPresentationStyle = .fullScreen
         self.present(navigationController, animated: true)
@@ -271,7 +271,7 @@ extension ProductsCatalogViewController: UICollectionViewDelegate {
 
         collectionView.deselectItem(at: indexPath, animated: true)
 
-        let rootViewController = RegisterProductViewController.init(productIdentification: item.identification)
+        let rootViewController = ModifyProductViewController.init(productIdentification: item.identification)
         let navigationController = UINavigationController(rootViewController: rootViewController)
         navigationController.modalPresentationStyle = .fullScreen
         self.present(navigationController, animated: true)
