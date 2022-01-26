19716dd01386a63b938bd920df2034a2dde71046
oksunwoo <oksunwo@naver.com>
2022-01-26 16:27:06 +0900

    refactor: available Declaration attribute 부여 #6

--- OpenMarket/OpenMarket/View/Cell/GridCollectionViewCell.swift
+++ OpenMarket/OpenMarket/View/Cell/GridCollectionViewCell.swift
@@ -15,6 +15,7 @@ class GridCollectionViewCell: UICollectionViewCell {
         configureUI()
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
--- OpenMarket/OpenMarket/View/Cell/ListCollectionViewCell.swift
+++ OpenMarket/OpenMarket/View/Cell/ListCollectionViewCell.swift
@@ -17,6 +17,7 @@ class ListCollectionViewCell: UICollectionViewCell {
         configureUI()
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
