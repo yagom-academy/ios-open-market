21be83564d75a34ec2b158a2a69940dd28e9fc21
yeha <dev.yeha@gmail.com>
2022-01-27 16:15:29 +0900

    feat: 상품 등록 화면의 Done, Cancel 버튼에 기능 연결 #2

---
+++ OpenMarket/OpenMarket/Views/ItemRegisterAndModifyManager.swift
@@ -0,0 +1,14 @@
+//
+//  ItemRegisterAndModifyManager.swift
+//  OpenMarket
+//
+//  Created by yeha on 2022/01/27.
+//
+
+import Foundation
+
+class ItemRegisterAndModifyManager {
+
+    func fillWithInformation() {}
+    func upload() {}
+}
--- OpenMarket/OpenMarket/Views/MarketItemRegistrationViewController.swift
+++ OpenMarket/OpenMarket/Views/MarketItemRegistrationViewController.swift
@@ -15,7 +15,45 @@ class MarketItemRegistrationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupDelegate()
+        setupNavigationBar()
         registrationView.formViewDidLoad()
         view = registrationView
     }
+
+    private func setupDelegate() {
+        registrationView.photoCollectionView.delegate = self
+        registrationView.photoCollectionView.dataSource = self
+        imagePicker.delegate = self
+    }
+
+    private func setupNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithDefaultBackground()
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done,
+                                         target: self,
+                                         action: #selector(doneButtonDidTap))
+        let cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel,
+                                           target: self,
+                                           action: #selector(cancelButtonDidTap))
+        self.navigationItem.rightBarButtonItem = doneButton
+        self.navigationItem.leftBarButtonItem = cancelButton
+    }
+
+    @objc private func doneButtonDidTap() {
+        let currency = registrationView.currencySegmentedControl.titleForSegment(at: registrationView.currencySegmentedControl.selectedSegmentIndex)!
+        itemRegisterManager.fillWithInformation(registrationView.nameInputTextField.text,
+                                                registrationView.descriptionInputTextView.text,
+                                                registrationView.priceInputTextField.text,
+                                                currency,
+                                                registrationView.discountedPriceInputTextField.text,
+                                                registrationView.stockInputTextField.text)
+        itemRegisterManager.upload()
+    }
+
+    @objc private func cancelButtonDidTap() {
+        self.dismiss(animated: true, completion: nil)
+    }
+
 }
