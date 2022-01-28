b8579dc2bd6d298f32a5627ac82b62c1948888fa
yeha <dev.yeha@gmail.com>
2022-01-28 21:09:03 +0900

    style: 주석 제거

--- OpenMarket/OpenMarket/Utils/Extension/UIImage+Extension.swift
+++ OpenMarket/OpenMarket/Utils/Extension/UIImage+Extension.swift
@@ -1,10 +1,3 @@
-//
-//  UIImage+Extension.swift
-//  OpenMarket
-//
-//  Created by yeha on 2022/01/27.
-//
-
 import UIKit
 
 extension UIImage {
--- OpenMarket/OpenMarket/Utils/Protocol/Reusable.swift
+++ OpenMarket/OpenMarket/Utils/Protocol/Reusable.swift
@@ -1,10 +1,3 @@
-//
-//  Reusable.swift
-//  OpenMarket
-//
-//  Created by yeha on 2022/01/27.
-//
-
 protocol Reusable: AnyObject {
     static var identifier: String { get }
 }
