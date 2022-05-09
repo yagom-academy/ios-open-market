f87a6bddc4902c3f28b9a468c6349b4fbccfe77c
dudu <whqtkf12@naver.com>
2022-05-09 23:33:12 +0900

    add: OpenMarketTests 파일 생성

---
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -0,0 +1,17 @@
+//
+//  OpenMarketTests.swift
+//  OpenMarketTests
+//
+//  Created by dudu, safari on 2022/05/09.
+//
+
+import XCTest
+
+class OpenMarketTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+}
