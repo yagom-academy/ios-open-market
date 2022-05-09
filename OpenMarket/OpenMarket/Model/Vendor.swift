397850c017fe6ccda82ea72138079065ae86095c
onetool <xogns8696@gmail.com>
2022-05-09 22:58:40 +0900

    ✨Feat : Vendor Model 구현

---
+++ OpenMarket/OpenMarket/Model/Vendor.swift
@@ -0,0 +1,21 @@
+//
+//  Vendor.swift
+//  OpenMarket
+//
+//  Created by 김태훈 on 2022/05/09.
+//
+
+import Foundation
+
+struct Vendor: Codable {
+    let name: String
+    let id: Int
+    let createdAt: String
+    let issuedAt: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case createdAt = "created_at"
+        case issuedAt = "issued_at"
+        case name, id
+    }
+}
