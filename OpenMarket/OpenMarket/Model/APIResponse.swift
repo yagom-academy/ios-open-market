0bc0dc4e8f0208081d699394f457c16cf1137257
Quokkaa <rktrkdml@gmail.com>
2022-05-10 16:48:48 +0900

    feat: APIResponse 타입 구현 #3

--- OpenMarket/OpenMarket/Model/APIResponse.swift
+++ OpenMarket/OpenMarket/Model/APIResponse.swift
@@ -6,4 +6,19 @@
 
 import Foundation
 
-struct APIResponse: Decodable {}
+struct APIResponse: Decodable {
+  let pageNo: Int
+  let itemsPerPage: Int
+  let totalCount: Int
+  let offset: Int
+  let limit: Int
+  let pages: [Product]
+  
+  private enum CodingKeys: String, CodingKey {
+    case pageNo = "page_no"
+    case itemsPerPage = "items_per_page"
+    case totalCount = "total_count"
+    case offset, limit, pages
+  }
+}
+
