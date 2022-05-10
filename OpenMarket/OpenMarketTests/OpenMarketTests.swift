05f04b032b959f025bba471e7ed337a5cdc84a2e
Tiana <97kth01@naver.com>
2022-05-10 16:17:19 +0900

    test: OpenMarketTests 구현

---
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -0,0 +1,37 @@
+//
+//  OpenMarketTests.swift
+//  OpenMarketTests
+//
+//  Created by 김태현 on 2022/05/10.
+//
+
+import XCTest
+@testable import OpenMarket
+
+class OpenMarketTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+    
+    func test_parse할때_파일이름이올바르면_OpenMarketService반환() {
+        // given
+        let fileName = "products"
+        // when, then
+        XCTAssertNoThrow(try OpenMarketService.parse(fileName: fileName))
+    }
+    
+    func test_parse할때_파일이름이올바르면_Product배열반환() throws {
+        // given
+        let fileName = "products"
+        // when
+        let firstProduct = try OpenMarketService.parse(fileName: fileName).products.first
+        // then
+        XCTAssertEqual(firstProduct?.name, "Test Product")
+    }
+
+}
