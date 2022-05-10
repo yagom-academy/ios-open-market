d277db5f705e5e2d230162255094d412927ca37f
dnwhd0112 <dnwhd0112@naver.com>
2022-05-10 13:19:26 +0900

    ✨[feat]: JsonParser 파일 추가

---
+++ OpenMarket/OpenMarket/Model/JsonParser.swift
@@ -0,0 +1,15 @@
+//
+//  JsonDecoder.swift
+//  OpenMarket
+//
+//  Created by 우롱차,  on 2022/05/10.
+//
+
+import Foundation
+
+struct JsonParser {
+    let jsonDecoder = JSONDecoder()
+    let jsonEncoder = JSONEncoder()
+}
+
+
