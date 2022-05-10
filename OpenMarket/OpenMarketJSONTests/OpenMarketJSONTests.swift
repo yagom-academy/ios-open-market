9d94c30f7201fe14c8ff09be4cbd08db5c1ffe26
dnwhd0112 <dnwhd0112@naver.com>
2022-05-10 20:36:50 +0900

    ✨[feat]: 테스트 코드 수정

--- OpenMarket/OpenMarketJSONTests/OpenMarketJSONTests.swift
+++ OpenMarket/OpenMarketJSONTests/OpenMarketJSONTests.swift
@@ -10,31 +10,44 @@ import XCTest
 
 class OpenMarketJSONTests: XCTestCase {
     private var jsonParser: JsonParser!
-    private var jsonDecoder: JSONDecoder!
     
     override func setUpWithError() throws {
         try super.setUpWithError()
         jsonParser = JsonParser()
-        jsonDecoder = JSONDecoder()
     }
-
+    
     override func tearDownWithError() throws {
         try super.tearDownWithError()
         jsonParser = nil
-        jsonDecoder = nil
     }
-
+    
     func test_products_JSON_테스트파일을_decoding해서_결과는_NotNil() {
         // given
-        guard let asset = NSDataAsset(name: "products") else {
-            XCTFail("File Not Found.")
+        let testFileName = "PageInformationTest"
+        let extensionType = "json"
+        
+        guard let data = load(fileName: testFileName, extensionType: extensionType) else {
+            XCTFail()
             return
         }
         
         // when
-        let pageInformation: PageInformation? = jsonParser.decodingJson(json: asset.data)
+        let pageInformation: PageInformation? = jsonParser.decodingJson(json: data)
         
         // then
         XCTAssertNotNil(pageInformation)
+        XCTAssertNotNil(pageInformation?.pages)
+    }
+    
+    private func load(fileName: String, extensionType: String) -> Data? {
+        let testBundle = Bundle(for: type(of: self))
+        guard let fileLocation = testBundle.url(forResource: fileName, withExtension: extensionType) else { return nil }
+        do {
+            let data = try Data(contentsOf: fileLocation)
+            return data
+        } catch {
+            return nil
+        }
     }
 }
+
