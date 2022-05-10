5fa133f9b17ff33e703d119d13b66cc35bd77ed1
Grumpy <sewoong2890@gmail.com>
2022-05-10 16:04:27 +0900

    ✨Feat : HTTP 요청을 처리하는 RequestAssistant 구현 및 유닛 테스트 코드 작성

---
+++ OpenMarket/URLSessionTests/URLSessionTests.swift
@@ -0,0 +1,54 @@
+//
+//  URLSessionTests.swift
+//  URLSessionTests
+//
+//  Created by Grumpy, OneTool on 2022/05/10.
+//
+
+import XCTest
+
+class URLSessionTests: XCTestCase {
+
+    var sut: RequestAssistant!
+    
+    override func setUpWithError() throws {
+        sut = RequestAssistant()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+
+    func test_서버와_통신하여_HealthCheckerAPI를_호출하는지() {
+        sut.requestHealthCheckerApi() { result in
+            switch result {
+            case .success(let data):
+                XCTAssertEqual(data, "OK")
+            case .failure(_):
+                return
+            }
+        }
+    }
+    
+    func test_서버와_통신하여_상품_리스트_조회API를_호출하는지() {
+        sut.requestListApi(pageNum: 1, items_per_page: 10) { result in
+            switch result {
+            case .success(let data):
+                XCTAssertEqual(data.pageNumber, 1)
+            case .failure(_):
+                return
+            }
+        }
+    }
+    
+    func test_서버와_통신하여_상품_상세_조회API를_호출하는지() {
+        sut.requestDetailApi(product_id: 522) { result in
+            switch result {
+            case .success(let data):
+                XCTAssertEqual(data.name, "아이폰13")
+            case .failure(_):
+                return
+            }
+        }
+    }
+}
