ac0ff5a879fddae984b126ec08442453498af911
saafaaari <111dltldnjs@naver.com>
2022-05-10 11:03:25 +0900

    feat : NetworkError 케이스 추가

---
+++ OpenMarket/OpenMarket/Network/Network.swift
@@ -0,0 +1,15 @@
+//
+//  Network.swift
+//  OpenMarket
+//
+//  Created by dudu, safari on 2022/05/10.
+//
+
+import Foundation
+
+enum NetworkErorr: Error {
+    case unknown
+    case jsonError
+    case severError
+    case urlError
+}
