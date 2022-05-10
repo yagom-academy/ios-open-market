8342ee20174deffa8d80a70c6cac1cd2a3b197c7
Lingo <llingo@kakao.com>
2022-05-10 17:28:42 +0900

    feat: APIResponse의 lastPage, hasNext, hasPrev 프로퍼티 추가 #3

--- OpenMarket/OpenMarket/Model/APIResponse.swift
+++ OpenMarket/OpenMarket/Model/APIResponse.swift
@@ -13,11 +13,17 @@ struct APIResponse: Decodable {
   let offset: Int
   let limit: Int
   let pages: [Product]
+  let lastPage: Int
+  let hasNext: Bool
+  let hasPrev: Bool
   
   private enum CodingKeys: String, CodingKey {
     case pageNo = "page_no"
     case itemsPerPage = "items_per_page"
     case totalCount = "total_count"
+    case lastPage = "last_page"
+    case hasNext = "has_next"
+    case hasPrev = "has_prev"
     case offset, limit, pages
   }
 }
