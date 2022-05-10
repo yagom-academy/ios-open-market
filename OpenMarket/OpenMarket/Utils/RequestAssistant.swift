6eee9a0e70f9f022f41425aef0bbedf5e7c31e3a
onetool <xogns8696@gmail.com>
2022-05-10 17:52:09 +0900

    ♻️Refactor : RequestAssistant Refactoring

---
+++ OpenMarket/OpenMarket/Utils/RequestAssistant.swift
@@ -0,0 +1,75 @@
+//
+//  RequestAssistant.swift
+//  OpenMarket
+//
+//  Created by Grumpy, OneTool on 2022/05/10.
+//
+
+import Foundation
+
+
+final class RequestAssistant {
+    let sessionManager = URLSessionProvider(session: URLSession.shared)
+    
+    func requestListAPI(pageNum: Int, items_per_page: Int, completionHandler: @escaping ((Result<ProductList, OpenMarketError>) -> Void)) {
+        let path = "api/products"
+        let queryString = "?page_no=\(pageNum)&items_per_page=\(items_per_page)"
+        
+        sessionManager.sendGet(path: path + queryString, completionHandler: { data, response, error in
+            guard let data = data else {
+                return
+            }
+            guard let result = try? JSONDecoder().decode(ProductList.self, from: data) else {
+                completionHandler(.failure(.failDecode))
+                return
+            }
+            self.manageError(response: response, result: result, completionHandler: completionHandler)
+        })
+    }
+    
+    func requestDetailAPI(product_id: Int, completionHandler: @escaping ((Result<Product, OpenMarketError>) -> Void)) {
+        let path = "api/products/\(product_id)"
+        
+        sessionManager.sendGet(path: path, completionHandler: { data, response, error in
+            guard let data = data else {
+                return
+            }
+            guard let result = try? JSONDecoder().decode(Product.self, from: data) else {
+                completionHandler(.failure(.failDecode))
+                return
+            }
+            self.manageError(response: response, result: result, completionHandler: completionHandler)
+        })
+    }
+    
+    func requestHealthCheckerAPI(completionHandler: @escaping ((Result<String, OpenMarketError>) -> Void)) {
+        let path = "healthChecker"
+        sessionManager.sendGet(path: path, completionHandler: { data, response, error in
+            guard let data = data else {
+                return
+            }
+            if let result = String(data: data, encoding: .utf8) {
+                self.manageError(response: response, result: result, completionHandler: completionHandler)
+            }
+        })
+    }
+    
+    func manageError<T>(response: URLResponse?, result: T ,completionHandler: @escaping ((Result<T, OpenMarketError>) -> Void)) {
+        guard let response = response as? HTTPURLResponse else {
+            return
+        }
+        let statusCode = response.statusCode
+        switch statusCode {
+        case 200...299:
+            completionHandler(.success(result))
+        case 400:
+            completionHandler(.failure(.invalidData))
+        case 404:
+            completionHandler(.failure(.missingDestination))
+        case 500...599:
+            completionHandler(.failure(.invalidResponse))
+        default:
+            completionHandler(.failure(.unknownError))
+        }
+    }
+}
