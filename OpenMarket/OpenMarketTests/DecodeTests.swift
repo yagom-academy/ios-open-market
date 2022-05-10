a4075182d618414ffb2e7469ac15908490c2e05d
Lingo <llingo@kakao.com>
2022-05-10 16:28:32 +0900

    test: 단위테스트 모듈 생성 및 코드 컨벤션 적용 #3

---
+++ OpenMarket/OpenMarketTests/DecodeTests.swift
@@ -0,0 +1,19 @@
+//
+//  DecodeTests.swift
+//  DecodeTests
+//
+//  Created by Lingo, Quokka on 2022/05/10.
+//
+
+import XCTest
+@testable import OpenMarket
+
+final class DecodeTests: XCTestCase {
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+  }
+  
+  override func tearDownWithError() throws {
+    try super.tearDownWithError()
+  }
+}
