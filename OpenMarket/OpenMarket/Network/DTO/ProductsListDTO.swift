432c2105589242d3c8a0d7eeb402ac3385725f6e
Red <seryprk@gmail.com>
2022-05-10 23:29:55 +0900

    feat: ProductsListDTO 생성

---
+++ OpenMarket/OpenMarket/Network/DTO/ProductsListDTO.swift
@@ -0,0 +1,13 @@
+//
+//  ProductsListDTO.swift
+//  OpenMarket
+//
+//  Created by Red, Mino on 2022/05/10.
+//
+
+import Foundation
+
+struct ProductsListDTO: Encodable {
+    let pageNumber: Int
+    let perPages: Int
+}
