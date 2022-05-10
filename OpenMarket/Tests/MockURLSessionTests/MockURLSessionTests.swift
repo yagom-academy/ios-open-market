19e9eed52e19e456a559e057feb866c10b55401a
Grumpy <sewoong2890@gmail.com>
2022-05-10 21:17:14 +0900

    ♻️Refactor : 비동기 테스트를 위해 expectation 사용

--- OpenMarket/Tests/MockURLSessionTests/MockURLSessionTests.swift
+++ OpenMarket/Tests/MockURLSessionTests/MockURLSessionTests.swift
@@ -20,13 +20,21 @@ class MockURLSessionTests: XCTestCase {
     }
 
     func test_서버와_통신하는_대신에_MockData_데이터를_가져오는지() {
+        let expt = expectation(description: "waiting for test")
+        let successResult: Int = 1
+        var pageNumber: Int = 0
+        
         sut.sendGet(path: "api/products?page_no=1&items_per_page=10") { data, response, error in
             guard let data = data else {
                 return
             }
             let result = try? JSONDecoder().decode(ProductList.self, from: data)
-            XCTAssertEqual(result?.pageNumber, 1)
+            pageNumber = result!.pageNumber
+            expt.fulfill()
         }
+        
+        waitForExpectations(timeout: 1, handler: nil)
+        XCTAssertEqual(pageNumber, successResult)
     }
 
 }
--- OpenMarket/Tests/URLSessionTests/URLSessionTests.swift
+++ OpenMarket/Tests/URLSessionTests/URLSessionTests.swift
@@ -20,35 +20,57 @@ class URLSessionTests: XCTestCase {
     }
 
     func test_서버와_통신하여_HealthCheckerAPI를_호출하는지() {
+        let expt = expectation(description: "waiting for test")
+        let successResult: String = "OK"
+        var message: String = ""
+        
         sut.requestHealthCheckerAPI() { result in
             switch result {
             case .success(let data):
-                XCTAssertEqual(data, "OK")
+                print(data)
+                message = data
+                expt.fulfill()
             case .failure(_):
                 XCTFail()
             }
         }
+        waitForExpectations(timeout: 1, handler: nil)
+        XCTAssertEqual(message, successResult)
     }
     
     func test_서버와_통신하여_상품_리스트_조회API를_호출하는지() {
+        let expt = expectation(description: "waiting for test")
+        let successResult: Int = 1
+        var pageNumber: Int = 0
+        
         sut.requestListAPI(pageNum: 1, items_per_page: 10) { result in
             switch result {
             case .success(let data):
-                XCTAssertEqual(data.pageNumber, 1)
+                pageNumber = data.pageNumber
+                expt.fulfill()
             case .failure(_):
                 XCTFail()
             }
         }
+        waitForExpectations(timeout: 1, handler: nil)
+        XCTAssertEqual(pageNumber, successResult)
     }
     
     func test_서버와_통신하여_상품_상세_조회API를_호출하는지() {
+        let expt = expectation(description: "waiting for test")
+        let successResult: String = "아이폰13"
+        var name: String = ""
+        
         sut.requestDetailAPI(product_id: 522) { result in
             switch result {
             case .success(let data):
-                XCTAssertEqual(data.name, "아이폰13")
+                name = data.name
+                expt.fulfill()
             case .failure(_):
                 XCTFail()
             }
         }
+        waitForExpectations(timeout: 1, handler: nil)
+        XCTAssertEqual(name, successResult)
     }
 }
