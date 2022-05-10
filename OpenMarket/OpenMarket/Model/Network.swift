5a415a4d8f5a8c86951e5fc708f892953387dbe1
JoSH0318 <jsh9870@naver.com>
2022-05-10 17:19:53 +0900

    ✨[feat]: Network, 구조체 타입 구현
    * session, path, paramaters 프로퍼티 구현
    * get() 메서드 구현

--- OpenMarket/OpenMarket/Model/Network.swift
+++ OpenMarket/OpenMarket/Model/Network.swift
@@ -13,3 +13,50 @@ enum API {
         Network(path: "/healthChecker")
     }
 }
+
+struct Network {
+  private let session: URLSession
+  private let path: String
+  private let paramaters: [String: String]
+  
+  init(session: URLSession = URLSession.shared, path: String = "", paramaters: [String: String] = [:]) {
+      self.session = session
+      self.path = path
+      self.paramaters = paramaters
+  }
+  
+  func get(completionHandler: @escaping (Result<String, NetworkError>) -> Void) {
+    var urlComponents = URLComponents(string: API.hostApi + path)
+    urlComponents?.queryItems = paramaters.map {
+      URLQueryItem(name: $0.key, value: $0.value)
+    }
+    
+    guard let url = urlComponents?.url else {
+      completionHandler(.failure(.invalid))
+      return
+    }
+    
+    session.dataTask(with: url) { data, response, error in
+      guard error == nil else {
+        completionHandler(.failure(.invalid))
+        return
+      }
+      
+      guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+        return completionHandler(.failure(.statusCodeError))
+      }
+      
+      guard let data = data else {
+        completionHandler(.failure(.invalid))
+        return
+      }
+      
+      guard let text = String(data: data, encoding: .utf8) else {
+        completionHandler(.failure(.invalid))
+        return
+      }
+      
+      completionHandler(.success(text))
+    }.resume()
+  }
+}
