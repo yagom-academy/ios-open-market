53d012e2851deb5dea3b36f449f2ea73d6adea9a
onetool <xogns8696@gmail.com>
2022-05-10 12:44:42 +0900

    ✨Feat : MockURLSessionTests 생성 및 파일 위치 재배치

---
+++ OpenMarket/MockURLSessionTests/MockURLSessionTests.swift
@@ -0,0 +1,32 @@
+//
+//  MockURLSessionTests.swift
+//  MockURLSessionTests
+//
+//  Created by Grumpy, OneTool on 2022/05/10.
+//
+
+import XCTest
+
+class MockURLSessionTests: XCTestCase {
+
+    var sut: URLSessionProvider!
+    
+    override func setUpWithError() throws {
+       sut = URLSessionProvider(session: MockURLSession())
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+
+    func test_서버와_통신하는_대신에_MockData_데이터를_가져오는지() {
+        sut.sendGet(path: "api/products?page_no=1&items_per_page=10") { data, response, error in
+            guard let data = data else {
+                return
+            }
+            let result = try? JSONDecoder().decode(ProductList.self, from: data)
+            XCTAssertEqual(result?.pageNumber, 1)
+        }
+    }
+
+}
