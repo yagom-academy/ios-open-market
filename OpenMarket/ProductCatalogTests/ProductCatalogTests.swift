2e0714fec102c119019769bc78dbd2580721ea91
malrang <ruehd7924@gmail.com>
2022-05-10 17:20:00 +0900

    ♻️[refactor]: ProductCatalogTests 테스트 코드 수정

--- OpenMarket/ProductCatalogTests/ProductCatalogTests.swift
+++ OpenMarket/ProductCatalogTests/ProductCatalogTests.swift
@@ -6,33 +6,35 @@
 //
 
 import XCTest
+@testable import OpenMarket
 
 class ProductCatalogTests: XCTestCase {
     
-    func test_asset이_products이고_result를_Decode했을때_결과가_nil이_아닌지() {
-        //given
-        let asset = "products"
-        //when
-        let result = ParseManager<ProductCatalog>.parse(name: asset)
-        //then
-        XCTAssertNotNil(result)
+    var sut: ProductCatalog?
+    
+    override func setUp() {
+        sut = ProductCatalog.parse(name: "products")
     }
     
+    override func tearDown() {
+        sut = nil
+    }
+
     func test_data가_팥빙수이고_result를_Decode했을때_결과가_pages의_마지막_name과_같은지() {
         //given
         let data = "팥빙수"
         //when
-        let result = ParseManager<ProductCatalog>.parse(name: "products")
+        let result = sut?.pages?.last?.name
         //then
-        XCTAssertEqual(result?.pages?.last?.name, data)
+        XCTAssertEqual(result, data)
     }
     
     func test_data가_10이고_result를_Decode했을때_결과가_pages의_갯수가_data와_같은지() {
         //given
         let data = 10
         //when
-        let result = ParseManager<ProductCatalog>.parse(name: "products")
+        let result = sut?.pages?.count
         //then
-        XCTAssertEqual(result?.pages?.count, data)
+        XCTAssertEqual(result, data)
     }
 }
