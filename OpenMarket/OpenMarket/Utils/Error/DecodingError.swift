8eff34f801c03b705d93b2b36418714c91913640
Tiana <97kth01@naver.com>
2022-05-10 12:01:23 +0900

    feat: DecodingError 타입 구현

---
+++ OpenMarket/OpenMarket/Utils/Error/DecodingError.swift
@@ -0,0 +1,13 @@
+//
+//  DecodingError.swift
+//  OpenMarket
+//
+//  Created by 김태현 on 2022/05/10.
+//
+
+import Foundation
+
+enum DecodingError: Error {
+    case dataAssetFail
+    case decodeFail
+}
