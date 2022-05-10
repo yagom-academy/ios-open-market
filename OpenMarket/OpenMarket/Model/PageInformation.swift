0b3ccbdf074c767ff7244bb983c4ef429894b155
dnwhd0112 <dnwhd0112@naver.com>
2022-05-10 13:38:29 +0900

    ✨[feat]: PageInformation struct 데이터 모델 구현

---
+++ OpenMarket/OpenMarket/Model/PageInformation.swift
@@ -0,0 +1,36 @@
+//
+//  PageInformation.swift
+//  OpenMarket
+//
+//  Created by 우롱차, Donnie on 2022/05/10.
+//
+
+import Foundation
+
+struct PageInformation {
+    let pageNo: Int
+    let itemsPerPage: Int
+    let totalCount: Int
+    let offset: Int
+    let limit: Int
+    let pages: [productInformation]
+    let lastPage: Int
+    let hasNext: Bool
+    let hasPrev: Bool
+    
+    private enum Codingkeys: String, CodingKey {
+        case pageNo = "page_no"
+        case itemsPerPage = "items_per_page"
+        case totalCount = "total_count"
+        case offset
+        case limit
+        case pages
+        case lastPage = "last_page"
+        case hasNext = "has_next"
+        case hasPrev = "has_prev"
+    }
+}
+
+struct productInformation {
+    
+}
