98b932089c0ae538213bee62f90817c87a150c61
JoSH0318 <jsh9870@naver.com>
2022-05-10 17:20:17 +0900

    ✅ [test]: URLSessionTests, 필요없는 코드 제거

---
+++ OpenMarket/OpenMarketTests/URLSessionTests.swift
@@ -0,0 +1,20 @@
+//
+//  URLSessionTests.swift
+//  OpenMarketTests
+//
+//  Created by cathy, mmim.
+//
+
+import XCTest
+@testable import OpenMarket
+
+class URLSessionTests: XCTestCase {
+  
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+  }
+  
+  override func tearDownWithError() throws {
+    try super.tearDownWithError()
+  }
+}
