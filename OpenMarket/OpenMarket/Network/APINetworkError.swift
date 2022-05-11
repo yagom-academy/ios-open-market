2a3e12ad7b0076abc296dc2094ff1de8932bac02
Lingo <llingo@kakao.com>
2022-05-11 17:10:33 +0900

    feat: APINetworkError 에러 타입 구현

--- OpenMarket/OpenMarket/Network/APINetworkError.swift
+++ OpenMarket/OpenMarket/Network/APINetworkError.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+enum APINetworkError: LocalizedError {
+  case badRequest
+  
+  var errorDescription: String? {
+    switch self {
+    case .badRequest:
+      return "BAD_REQUEST"
+    }
+  }
+}
