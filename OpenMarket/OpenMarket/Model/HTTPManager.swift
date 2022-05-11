55dfb3d0f59a73f4aeb2901308b519c3d0839f70
papriOS <bluecrab30@naver.com>
2022-05-11 14:02:59 +0900

    feat: listenHealthChecker 구현

--- OpenMarket/OpenMarket/Model/HTTPManager.swift
+++ OpenMarket/OpenMarket/Model/HTTPManager.swift
@@ -7,8 +7,11 @@
 
 import Foundation
 
-fileprivate enum StatusCodeRange {
-    static let success = 200 ... 299
+fileprivate enum StatusCode {
+    static let okSuccess = 200
+    static let createdSuccess = 201
+    static let acceptedSuccess = 202
+    static let successRange = 200 ... 299
 }
 
 fileprivate enum ContentType {
@@ -18,11 +21,14 @@ fileprivate enum ContentType {
 
 enum TargetURL {
     static let hostURL = "https://market-training.yagom-academy.kr/"
+    case healthChecker
     case productList(pageNumber: Int, itemsPerPage: Int)
     case productDetail(productNumber: Int)
     
     var string: String {
         switch self {
+        case .healthChecker:
+            return "/healthChecker"
         case .productList(let pageNumber, let itemsPerPage):
             return "/api/products?page_no=\(pageNumber)&items_per_page=\(itemsPerPage)"
         case .productDetail(let productNumber):
@@ -40,6 +46,27 @@ struct HTTPManager {
         self.urlSession = urlSession
     }
     
+    func listenHealthChecker() {
+        let requestURL = hostURL + TargetURL.healthChecker.string
+        guard let url = URL(string: requestURL) else {
+            return
+        }
+        let task = urlSession.dataTask(with: url) { data, response, error in
+            if let _ = error {
+                return
+            }
+            guard let httpResponse = response as? HTTPURLResponse,
+                  StatusCode.okSuccess == httpResponse.statusCode else {
+                return
+            }
+            if let mimeType = httpResponse.mimeType,
+               mimeType == ContentType.textPlain {
+                return
+            }
+        }
+        task.resume()
+    }
+    
     func loadData(targetURL: TargetURL, completionHandler: @escaping (Data) -> Void) {
         let requestURL = hostURL + targetURL.string
         guard let url = URL(string: requestURL) else {
@@ -50,7 +77,7 @@ struct HTTPManager {
                 return
             }
             guard let httpResponse = response as? HTTPURLResponse,
-                  (StatusCodeRange.success).contains(httpResponse.statusCode) else {
+                  (StatusCode.successRange).contains(httpResponse.statusCode) else {
                 return
             }
             if let mimeType = httpResponse.mimeType,
