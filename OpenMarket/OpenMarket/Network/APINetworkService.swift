1764661aa7c95f2cb9b7355e4f74d65f056c11ea
Lingo <llingo@kakao.com>
2022-05-11 20:45:48 +0900

    refactor: 중복된 URLSession dataTask 메서드 로직을 request 메서드로 분리 #6

--- OpenMarket/OpenMarket/Network/APINetworkService.swift
+++ OpenMarket/OpenMarket/Network/APINetworkService.swift
@@ -23,69 +23,63 @@ class APINetworkService: NetworkService {
   
   func checkHealth(_ completion: @escaping (Result<String, APINetworkError>) -> Void) {
     let urlString = Constant.baseURL + Constant.healthCheckerPath
-    guard let url = URL(string: urlString) else { return }
     
-    let task = self.urlSession.dataTask(with: url) { data, response, error in
-      guard error == nil else { return }
-      guard let response = response as? HTTPURLResponse else { return }
-      guard let data = data else { return }
-      
-      switch response.statusCode {
-      case 200...299:
-        if let text = String(data: data, encoding: .utf8) { completion(.success(text)) }
-      case 400:
-        completion(.failure(.badRequest))
-      default:
-        break
+    self.request(url: urlString) { result in
+      switch result {
+      case let .success(data):
+        guard let text = String(data: data, encoding: .utf8) else { return }
+        completion(.success(text))
+      case let .failure(error):
+        completion(.failure(error))
       }
     }
-    task.resume()
   }
   
   func fetchProductAll(_ completion: @escaping (Result<[Product], APINetworkError>) -> Void) {
     let urlString = Constant.baseURL + Constant.productsPath
-    guard let url = URL(string: urlString) else { return }
     
-    let task = self.urlSession.dataTask(with: url) { data, response, error in
-      guard error == nil else { return }
-      guard let response = response as? HTTPURLResponse else { return }
-      guard let data = data else { return }
-      
-      guard let apiResponse = try? JSONDecoder().decode(APIResponse.self, from: data) else { return }
-      
-      switch response.statusCode {
-      case 200...299:
-        completion(.success(apiResponse.pages))
-      case 400:
-        completion(.failure(.badRequest))
-      default:
-        break
+    self.request(url: urlString) { result in
+      switch result {
+      case let .success(data):
+        guard let apiResponse = try? JSONDecoder().decode(APIResponse.self, from: data) else { return }
+        let products = apiResponse.pages
+        completion(.success(products))
+      case let .failure(error):
+        completion(.failure(error))
       }
     }
-    task.resume()
   }
   
   func fetchProductOne(productID: Int, _ completion: @escaping (Result<Product, APINetworkError>) -> Void) {
     let urlString = Constant.baseURL + Constant.productPath + String(productID)
+    
+    self.request(url: urlString) { result in
+      switch result {
+      case let .success(data):
+        guard let product = try? JSONDecoder().decode(Product.self, from: data) else { return }
+        completion(.success(product))
+      case let .failure(error):
+        completion(.failure(error))
+      }
+    }
+  }
+  
+  func request(url urlString: String, _ completion: @escaping (Result<Data, APINetworkError>) -> Void) {
     guard let url = URL(string: urlString) else { return }
     
-    let task = self.urlSession.dataTask(with: url) { data, response, error in
+    self.urlSession.dataTask(with: url) { data, response, error in
       guard error == nil else { return }
       guard let response = response as? HTTPURLResponse else { return }
       guard let data = data else { return }
       
-      guard let product = try? JSONDecoder().decode(Product.self, from: data) else { return }
-      
       switch response.statusCode {
       case 200...299:
-        completion(.success(product))
+        completion(.success(data))
       case 400:
         completion(.failure(.badRequest))
       default:
         break
       }
-    }
-    task.resume()
+    }.resume()
   }
 }
-
