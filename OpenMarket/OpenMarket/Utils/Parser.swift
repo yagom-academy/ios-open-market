7825ea354d86a413031465c032b65d9368bf0044
Grumpy <sewoong2890@gmail.com>
2022-05-11 14:27:55 +0900

    🩹Chore : 타입 변경으로 Parser에서 Decode클래스를 사용하도록 수정

--- OpenMarket/OpenMarket/Utils/Parser.swift
+++ OpenMarket/OpenMarket/Utils/Parser.swift
@@ -9,11 +9,10 @@ import UIKit
 
 struct Parser<T: Codable> {
     func decode(name: String) -> T? {
-        let decoder = JSONDecoder()
         guard let asset = NSDataAsset(name: name, bundle: .main) else {
             return nil
         }
-        guard let decodedData = try? decoder.decode(T.self, from: asset.data) else {
+        guard let decodedData = try? Decoder.shared.decode(T.self, from: asset.data) else {
             return nil
         }
         
