0ce57dad3969515fa4c0ef889bc7c7ce891ce87b
doogie97 <chris_97@naver.com>
2022-05-11 14:51:20 +0900

    🔬test : NetworkHandler Unit Test 생성

---
+++ OpenMarket/NetworkHandlerTest/NetworkHandlerTest.swift
@@ -0,0 +1,20 @@
+//
+//  NetworkHandlerTest.swift
+//  NetworkHandlerTest
+//
+//  Created by 두기, minseong on 2022/05/11.
+//
+
+import XCTest
+@testable import OpenMarket
+
+class NetworkHandlerTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+}
