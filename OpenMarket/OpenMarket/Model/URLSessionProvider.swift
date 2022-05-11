e4428a5dcb24da7373c7fbf36ea6d9be066335f5
GoMyungju171 <mooooongdu171@gmail.com>
2022-05-11 17:00:04 +0900

    ♻️[refactor]: query값을 받기 위한 로직 구현
    URLSessionProvider 초기화 시 query값을 받기 위한 parameters 프로퍼티 추가
    query값을 추가하기위해 urlComponents와 URLQueryItem을 이용한 로직 추가

--- OpenMarket/OpenMarket/Model/URLSessionProvider.swift
+++ OpenMarket/OpenMarket/Model/URLSessionProvider.swift
@@ -11,16 +11,26 @@ struct URLSessionProvider<T: Decodable> {
   private let hostApi = "https://market-training.yagom-academy.kr"
   private let session: URLSessionProtocol
   private let path: String
+  private let parameters: [String: String]
   
-  init(session: URLSessionProtocol = URLSession.shared, path: String = "") {
-      self.session = session
-      self.path = path
+  init(session: URLSessionProtocol = URLSession.shared, path: String = "", parameters: [String: String] = [:]) {
+    self.session = session
+    self.path = path
+    self.parameters = parameters
   }
   
   func get(completionHandler: @escaping (Result<T, NetworkError>) -> Void) {
-    guard let url = URL(string: hostApi + path) else {
+    guard var urlComponents = URLComponents(string: hostApi + path) else {
       return
     }
+    let query = parameters.map {
+      URLQueryItem(name: $0.key, value: $0.value)
+    }
+    urlComponents.queryItems = query
+    guard let url = urlComponents.url else {
+      return
+    }
+    
     var request = URLRequest(url: url)
     request.httpMethod = "GET"
     
@@ -30,7 +40,7 @@ struct URLSessionProvider<T: Decodable> {
         return
       }
       guard let response = response as? HTTPURLResponse,
-                (200...299).contains(response.statusCode)
+            (200...299).contains(response.statusCode)
       else {
         completionHandler(.failure(.statusCodeError))
         return
