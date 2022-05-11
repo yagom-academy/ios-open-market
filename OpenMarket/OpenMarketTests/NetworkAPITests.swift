a4b026b5278326bf415a0c0eb5d3f2d16fb78dbd
Lingo <llingo@kakao.com>
2022-05-11 15:13:16 +0900

    chore: NetworkAPITests 파일 생성 #4

---
+++ OpenMarket/OpenMarketTests/NetworkAPITests.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkAPITests.swift
+//  OpenMarketTests
+//
+//  Created by Lingo, Quokka on 2022/05/11.
+//
+
+import XCTest
+
+final class NetworkAPITests: XCTestCase {
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+  }
+  
+  override func tearDownWithError() throws {
+    try super.tearDownWithError()
+  }
+}
