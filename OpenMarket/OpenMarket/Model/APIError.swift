fc5d11698375681ee5f3bcd0424c58a65cfb3e5b
minseong <msb0987@naver.com>
2022-05-11 14:08:59 +0900

    💎feat : APIError 구현

---
+++ OpenMarket/OpenMarket/Model/APIError.swift
@@ -0,0 +1,13 @@
+//
+//  APIError.swift
+//  OpenMarket
+//
+//  Created by 두기, minseong on 2022/05/11.
+//
+
+enum APIError: Error {
+    case convertError
+    case requestError
+    case dataError
+    case responseError
+}
