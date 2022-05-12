4a4f5ee95ef1797e2e837079dfc0ae2e516c9deb
minseong <msb0987@naver.com>
2022-05-12 12:10:39 +0900

    🔬test: getData호출시_responseError를_잘던지는지

--- OpenMarket/NetworkHandlerTest/NetworkHandlerTest.swift
+++ OpenMarket/NetworkHandlerTest/NetworkHandlerTest.swift
@@ -97,4 +97,26 @@ class NetworkHandlerTest: XCTestCase {
         }
         wait(for: [promise], timeout: 5)
     }
+    
+    func test_getData호출시_responseError를_잘던지는지() {
+        //given
+        let promise = expectation(description: "convertError를_잘던지는지")
+        var dummyData = DummyData()
+        dummyData.data = convertJsonToData(fileName: "products")
+        dummyData.response = HTTPURLResponse(url: URL(string: "test")!, statusCode: 404, httpVersion: "2", headerFields: nil)
+        dummyData.error = nil
+        sut.session = StubURLSession(dummyData: dummyData)
+        
+        //when
+        sut.getData(pathString: "test") { data in
+            //then
+            do {
+                let _ = try DataDecoder.decodeItemPage(data: data)
+            } catch {
+                XCTAssertEqual(error as! APIError, APIError.responseError)
+            }
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 5)
+    }
 }
