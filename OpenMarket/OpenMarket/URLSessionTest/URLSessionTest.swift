7996191ae532df6693b0ce8f17cd3a8038cee30f
Taeangel <wngks2050@gmail.com>
2022-05-12 13:06:50 +0900

    ✅ [test]: MockURLSession의 테스트코드작성

--- OpenMarket/OpenMarket/URLSessionTest/URLSessionTest.swift
+++ OpenMarket/OpenMarket/URLSessionTest/URLSessionTest.swift
@@ -6,30 +6,37 @@
 //
 
 import XCTest
+@testable import OpenMarket
 
 class URLSessionTest: XCTestCase {
-
+    var sut: URLSessionProvider<ProductCatalog>!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let session = MockURLSession()
+        sut = URLSessionProvider<ProductCatalog>(session: session)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_네트워크가_연결이_되지않아도_getData의_함수를_호출하면_Mock데이터의_itemspPerPage의_값이_20인지() {
+        //given
+        let promise = expectation(description: "")
+        let url = URL(string: "empty")!
+        let requset = URLRequest(url: url)
+        
+        //when
+        sut.getData(from: requset) { result in
+            //then
+            switch result {
+            case .success(let data):
+                XCTAssertEqual(data.itemspPerPage, 20)
+            case .failure(_):
+                XCTFail()
+            }
+            promise.fulfill()
         }
+        wait(for: [promise], timeout: 10)
     }
-
 }
