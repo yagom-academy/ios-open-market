df4716564ad23e760ae728a9adeaacd6841d4b72
doogie97 <chris_97@naver.com>
2022-05-12 11:38:58 +0900

    🔬test : test_getData호출시_데이터를잘가져오는지

--- OpenMarket/NetworkHandlerTest/NetworkHandlerTest.swift
+++ OpenMarket/NetworkHandlerTest/NetworkHandlerTest.swift
@@ -21,6 +21,7 @@ class NetworkHandlerTest: XCTestCase {
         sut = nil
     }
     
+    //MARK: - 테스트를 위한 메서드
     func convertJsonToData(fileName: String) -> Data {
         let testBundle = Bundle(for: type(of: self))
         let path = testBundle.path(forResource: fileName, ofType: "json")
@@ -28,4 +29,27 @@ class NetworkHandlerTest: XCTestCase {
         return jsonString?.data(using: .utf8) ?? Data()
     }
     
+    //MARK: - 네트워크 없이 테스트
+    func test_getData호출시_데이터를잘가져오는지() {
+        //given
+        let promise = expectation(description: "id가 일치 하는지")
+        var dummyData = DummyData()
+        dummyData.data = convertJsonToData(fileName: "products")
+        dummyData.response = HTTPURLResponse(url: URL(string: "test")!, statusCode: 200, httpVersion: "2", headerFields: nil)
+        dummyData.error = nil
+        sut.session = StubURLSession(dummyData: dummyData)
+        
+        //when
+        sut.getData(pathString: "test") { data in
+            //then
+            do {
+                let itemPage = try DataDecoder.decodeItemPage(data: data)
+                XCTAssertEqual(itemPage.items[0].id, 20)
+            } catch {
+                XCTAssertNil(error)
+            }
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 5)
+    }
 }
