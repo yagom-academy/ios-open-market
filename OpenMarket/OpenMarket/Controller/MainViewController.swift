952edfa34fd4a3d84afefb2160be1db8192269f5
Mino777 <jomino7777@gmail.com>
2022-05-12 23:03:17 +0900

    refactor: viewDidLoad()가 아닌 loadView() 에서 View 변경으로 수정

--- OpenMarket/OpenMarket/Controller/MainViewController.swift
+++ OpenMarket/OpenMarket/Controller/MainViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 final class MainViewController: UIViewController {
     private lazy var mainView = MainView(frame: view.bounds)
     
+    override func loadView() {
+        super.loadView()
+        view = mainView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view = mainView
     }
 }
 
