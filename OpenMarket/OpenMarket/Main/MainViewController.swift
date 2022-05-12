aa2e1134299a6aa83fe6216c4c5d997a67287f9f
saafaaari <111dltldnjs@naver.com>
2022-05-12 19:24:05 +0900

    feat : collectionView delegate, dataSource 메서드 구현

--- OpenMarket/OpenMarket/Main/MainViewController.swift
+++ OpenMarket/OpenMarket/Main/MainViewController.swift
@@ -8,7 +8,18 @@ import UIKit
 
 final class MainViewController: UIViewController {
     private lazy var mainView = MainView(frame: self.view.bounds)
-
+    private let dummyList: [Product]? = {
+        guard let path = Bundle.main.path(forResource: "products", ofType: "json") else { return nil }
+        guard let jsonString = try? String(contentsOfFile: path) else { return nil }
+        guard let data = jsonString.data(using: .utf8) else { return nil }
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'hh:mm:ss.SS"
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.dateDecodingStrategy = .formatted(dateFormatter)
+        return try? jsonDecoder.decode(ProductList.self, from: data).products
+        
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
@@ -17,6 +28,37 @@ final class MainViewController: UIViewController {
     private func configureView() {
         mainView.backgroundColor = .systemBackground
         self.view = mainView
+        configureCollectionView()
+    }
+    
+    private func configureCollectionView() {
+        mainView.collectionView.dataSource = self
+        mainView.collectionView.delegate = self
+        mainView.collectionView.register(ProductCell.self, forCellWithReuseIdentifier: "cell")
     }
 }
 
+extension MainViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dummyList?.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? ProductCell else {
+            return ProductCell()
+        }
+        
+        cell.configure(data: dummyList?[indexPath.item])
+        
+        return cell
+    }
+}
+
+extension MainViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        guard let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout else { return .zero }
+        let width = collectionView.bounds.width / 2 - flowLayout.minimumInteritemSpacing
+        
+        return CGSize(width: width, height: width * 1.5)
+    }
+}
