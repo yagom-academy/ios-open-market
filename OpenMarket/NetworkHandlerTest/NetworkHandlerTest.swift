6840a62e3923258e7696f1d2a3bce4cbd1ad1a58
minseong <msb0987@naver.com>
2022-05-12 11:04:10 +0900

    🔬test : test를 위한 메서드 구현

--- OpenMarket/NetworkHandlerTest/NetworkHandlerTest.swift
+++ OpenMarket/NetworkHandlerTest/NetworkHandlerTest.swift
@@ -9,12 +9,23 @@ import XCTest
 @testable import OpenMarket
 
 class NetworkHandlerTest: XCTestCase {
-
+    var sut: NetworkHandler!
+    
     override func setUpWithError() throws {
         try super.setUpWithError()
+        sut = NetworkHandler()
     }
 
     override func tearDownWithError() throws {
         try super.tearDownWithError()
+        sut = nil
     }
+    
+    func convertJsonToData(fileName: String) -> Data {
+        let testBundle = Bundle(for: type(of: self))
+        let path = testBundle.path(forResource: fileName, ofType: "json")
+        let jsonString = try? String(contentsOfFile: path ?? "")
+        return jsonString?.data(using: .utf8) ?? Data()
+    }
+    
 }
