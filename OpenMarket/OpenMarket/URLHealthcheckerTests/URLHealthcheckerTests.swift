d2f8f24df1a55f094eb7b160ac972a861915a118
Taeangel <wngks2050@gmail.com>
2022-05-12 18:57:16 +0900

    ✅ [test]: HealthChecker 테스트 코드 파일추가 및 구현

---
+++ OpenMarket/OpenMarket/URLHealthcheckerTests/URLHealthcheckerTests.swift
@@ -0,0 +1,39 @@
+//
+//  URLSessionTests.swift
+//  URLSessionTests
+//
+//  Created by song on 2022/05/12.
+//
+
+import XCTest
+@testable import OpenMarket
+
+class URLHealthCheckerSessionTests: XCTestCase {
+
+    var sut: URLSessionProvider<String>!
+    override func setUpWithError() throws {
+        sut = URLSessionProvider()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+    
+    func test_path가_healthChecker일때_getUser_메서드를_호출하면_data의_값이_OK인지() {
+        //given
+        let promise = expectation(description: "")
+        let path = API.healthChecker.path
+        //when
+        sut.fetchData(path: path) { result in
+            //then
+            switch result {
+            case .success(let data):
+                XCTAssertEqual(data, "OK")
+            case .failure(_):
+                XCTFail()
+            }
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 10)
+    }
+}
