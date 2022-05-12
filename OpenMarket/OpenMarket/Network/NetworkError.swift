2eab322c19e4482195c1ab81a509faca0c538164
Taeangel <wngks2050@gmail.com>
2022-05-12 11:23:44 +0900

    ✨[feat]: NetworkError 구현

---
+++ OpenMarket/OpenMarket/Network/NetworkError.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkError.swift
+//  OpenMarket
+//
+//  Created by song on 2022/05/12.
+//
+
+enum NetworkError: Error {
+    case unknownError
+    case statusCodeError
+    case decodeError
+}
