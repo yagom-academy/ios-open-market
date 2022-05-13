e89df64db3e5e4df746390016803f625d4cbdda0
JoSH0318 <jsh9870@naver.com>
2022-05-13 11:17:17 +0900

    ✅ [test]: MockURLProtocol 구현으로 인한 테스트 코드 변경
    * MockURLSession과 MockURLSessionDataTask 삭제와
    새롭게 MockURLProtocol 구현한 것으로 인한 테스트 코드 변경

--- OpenMarket/OpenMarketTests/Mock/MockURLSessionTests.swift
+++ OpenMarket/OpenMarketTests/Mock/MockURLSessionTests.swift
@@ -10,56 +10,96 @@ import XCTest
 
 final class MockURLSessionTests: XCTestCase {
   var sut: URLSessionProvider<ProductsList>!
+  let mockData = MockData().loadData()!
   
   override func setUpWithError() throws {
     try super.setUpWithError()
-    sut = URLSessionProvider<ProductsList>(session: MockURLSession())
+    let configuration = URLSessionConfiguration.ephemeral
+    configuration.protocolClasses = [MockURLProtocol.self]
+    let urlSession = URLSession(configuration: configuration)
+    sut = URLSessionProvider<ProductsList>(session: urlSession)
   }
   
   override func tearDownWithError() throws {
     try super.tearDownWithError()
     sut = nil
   }
-  
+
   func test_mockData_pageNumber은_1이다() {
-    //given, when
-    let data = 1
-    //then
-    sut.get { result in
-      switch result {
-      case .success(let products):
-        XCTAssertEqual(products.pageNumber, data)
-      case .failure(_):
-        XCTFail()
-      }
+    //given
+    MockURLProtocol.requestHandler = {request in
+      let exampleData = self.mockData
+      let response = HTTPURLResponse(url: request.url!,
+                                     statusCode: 200,
+                                     httpVersion: "2.0",
+                                     headerFields: ["Content-Type" : "application/json"])!
+      return (response, exampleData)
     }
+    let expectation = XCTestExpectation(description: "response data")
+    //when
+    sut.get { data in
+      // then
+      switch data {
+      case .success(let products):
+        XCTAssertEqual(products.pageNumber, 1)
+      case .failure(let error):
+        print(error)
+      }
+      
+      expectation.fulfill()
+    }
+    wait(for: [expectation], timeout: 1)
   }
   
-  func test_mockData_items의개수는_10이다() {
-    //given, when
-    let data = 10
-    //then
-    sut.get { result in
-      switch result {
-      case .success(let products):
-        XCTAssertEqual(products.pages.count, data)
-      case .failure(_):
-        XCTFail()
-      }
+  func test_mockData_pages의_count는_10이다() {
+    //given
+    MockURLProtocol.requestHandler = {request in
+      let exampleData = self.mockData
+      let response = HTTPURLResponse(url: request.url!,
+                                     statusCode: 200,
+                                     httpVersion: "2.0",
+                                     headerFields: ["Content-Type" : "application/json"])!
+      return (response, exampleData)
     }
+    let expectation = XCTestExpectation(description: "response data")
+    //when
+    sut.get { data in
+      // then
+      switch data {
+      case .success(let products):
+        XCTAssertEqual(products.pages.count, 10)
+      case .failure(let error):
+        print(error)
+      }
+      
+      expectation.fulfill()
+    }
+    wait(for: [expectation], timeout: 1)
   }
   
-  func test_mockData_items의첫번째id는_20이다() {
-    //given, when
-    let data = 20
-    //then
-    sut.get { result in
-      switch result {
-      case .success(let products):
-        XCTAssertEqual(products.pages.first?.id, data)
-      case .failure(_):
-        XCTFail()
-      }
+  func test_mockData_pages의_첫번째_index_id는_20이다() {
+    //given
+    MockURLProtocol.requestHandler = {request in
+      let exampleData = self.mockData
+      let response = HTTPURLResponse(url: request.url!,
+                                     statusCode: 200,
+                                     httpVersion: "2.0",
+                                     headerFields: ["Content-Type" : "application/json"])!
+      return (response, exampleData)
     }
+    let expectation = XCTestExpectation(description: "response data")
+    //when
+    sut.get { data in
+      // then
+      switch data {
+      case .success(let products):
+        XCTAssertEqual(products.pages.first?.id, 20)
+      case .failure(let error):
+        print(error)
+      }
+      
+      expectation.fulfill()
+    }
+    wait(for: [expectation], timeout: 1)
   }
 }
