bdebc5a8192a3b1038e3b259c5a19b1f46e7575a
Tiana <97kth01@naver.com>
2022-05-13 11:11:55 +0900

    test: MockURLProtocol 구현

---
+++ OpenMarket/HTTPManagerTests/Test Double/MockURLProtocol.swift
@@ -0,0 +1,38 @@
+//
+//  MockURLProtocol.swift
+//  HTTPManagerTests
+//
+//  Created by papri, Tiana on 13/05/2022.
+//
+
+import XCTest
+import Foundation
+
+class MockURLProtocol: URLProtocol {
+    static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data))?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func stopLoading() { }
+    
+    override func startLoading() {
+        guard let handler = MockURLProtocol.requestHandler else {
+            XCTFail("Received unexpected request with no handler set")
+            return
+        }
+        do {
+            let (response, data) = try handler(request)
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            client?.urlProtocol(self, didLoad: data)
+            client?.urlProtocolDidFinishLoading(self)
+        } catch {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
+}
