3daa229d3308597f8d145b59352bfad86ab73b8a
saafaaari <111dltldnjs@naver.com>
2022-05-13 12:48:59 +0900

    improve : diffableDataSource로 변경 #7

--- OpenMarket/OpenMarket/Main/MainViewController.swift
+++ OpenMarket/OpenMarket/Main/MainViewController.swift
@@ -6,10 +6,18 @@
 
 import UIKit
 
+private enum Section {
+    case main
+}
+
 final class MainViewController: UIViewController {
-    private lazy var mainView = MainView(frame: self.view.bounds)
+    private typealias DataSource = UICollectionViewDiffableDataSource<Section, Product>
+    private typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Product>
     
-    private let dummyList: [Product]? = {
+    private lazy var mainView = MainView(frame: self.view.bounds)
+    private lazy var dataSource = makeDataSource()
+    
+    private let dummyList: [Product]! = {
         guard let path = Bundle.main.path(forResource: "products", ofType: "json") else { return nil }
         guard let jsonString = try? String(contentsOfFile: path) else { return nil }
         guard let data = jsonString.data(using: .utf8) else { return nil }
@@ -33,18 +41,7 @@ final class MainViewController: UIViewController {
         super.viewDidLoad()
         configureView()
         configureNavigationBar()
-    }
-    
-    private func configureNavigationBar() {
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonDidTapped))
-        navigationItem.titleView = segmentControl
-    }
-    
-    @objc private func addButtonDidTapped() {}
-    
-    @objc private func segmentValueDidChanged(segmentedControl: UISegmentedControl) {
-        mainView.changeLayout(index: segmentedControl.selectedSegmentIndex)
-        mainView.collectionView.reloadData()
+        applySnapshot()
     }
     
     private func configureView() {
@@ -53,30 +50,54 @@ final class MainViewController: UIViewController {
         configureCollectionView()
     }
     
+    private func configureNavigationBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonDidTapped))
+        navigationItem.titleView = segmentControl
+    }
+    
     private func configureCollectionView() {
-        mainView.collectionView.dataSource = self
         mainView.collectionView.register(ProductGridCell.self, forCellWithReuseIdentifier: ProductGridCell.identifier)
         mainView.collectionView.register(ProductListCell.self, forCellWithReuseIdentifier: ProductListCell.identifier)
     }
+    
+    @objc private func addButtonDidTapped() {}
+    
+    @objc private func segmentValueDidChanged(segmentedControl: UISegmentedControl) {
+        mainView.changeLayout(index: segmentedControl.selectedSegmentIndex)
+        mainView.collectionView.reloadData()
+    }
 }
 
-extension MainViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return dummyList?.count ?? 0
+//MARK: - CollectionView DataSource
+
+extension MainViewController {
+    private func makeDataSource() -> DataSource {
+        let dataSource = DataSource(collectionView: mainView.collectionView) { [weak self] collectionView, indexPath, itemIdentifier in
+            
+            guard let self = self else { return nil }
+            
+            guard let layout = CollectionLayout(rawValue: self.segmentControl.selectedSegmentIndex) else { return nil }
+            
+            switch layout {
+            case .grid:
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProductGridCell.identifier, for: indexPath) as? ProductGridCell else { return ProductGridCell() }
+                cell.configure(data: itemIdentifier)
+                return cell
+            case .list:
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProductListCell.identifier, for: indexPath) as? ProductListCell else { return ProductListCell() }
+                cell.configure(data: itemIdentifier)
+                return cell
+            }
+        }
+        
+        return dataSource
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let layout = CollectionLayout(rawValue: segmentControl.selectedSegmentIndex) else { return UICollectionViewCell() }
+    private func applySnapshot() {
+        var snapshot = Snapshot()
         
-        switch layout {
-        case .grid:
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProductGridCell.identifier, for: indexPath) as? ProductGridCell else { return ProductGridCell() }
-            cell.configure(data: dummyList?[indexPath.item])
-            return cell
-        case .list:
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProductListCell.identifier, for: indexPath) as? ProductListCell else { return ProductListCell() }
-            cell.configure(data: dummyList?[indexPath.item])
-            return cell
-        }
+        snapshot.appendSections([.main])
+        snapshot.appendItems(dummyList)
+        dataSource.apply(snapshot)
     }
 }
