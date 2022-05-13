a473d2772c6e7600f775413fb6ad63a8eee9eff9
onetool <xogns8696@gmail.com>
2022-05-13 16:15:37 +0900

    ✨Feat : segmentedControl 생성 및 레이아웃 설정

--- OpenMarket/OpenMarket/ViewController.swift
+++ OpenMarket/OpenMarket/ViewController.swift
@@ -8,9 +8,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let segmentedControl = UISegmentedControl(items: ["List", "Grid"])
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        segmentLayout()
+        segmentedControl.addTarget(self, action: #selector(arrangementChange(_:)), for: .valueChanged)
+        segmentedControl.selectedSegmentIndex = 0
+        self.arrangementChange(segmentedControl)
+    }
+    
+    
+    func segmentLayout() {
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        segmentedControl.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 10.0).isActive = true
+        segmentedControl.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+    }
+    
     }
 
 
