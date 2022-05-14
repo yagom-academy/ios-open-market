1abf77e2ab44839d751dde899312b1fe13c246db
Taeangel <wngks2050@gmail.com>
2022-05-14 16:00:59 +0900

    ✅[test]: statusCodeError받는부분 테스트 코드 작성

--- OpenMarket/OpenMarket/URLProductCatalogTests/URLProductListTests.swift
+++ OpenMarket/OpenMarket/URLProductCatalogTests/URLProductListTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import OpenMarket
 
-class URLProductListTests: XCTestCase {
+class URLProductCatalogTests: XCTestCase {
 
     var sut: URLSessionProvider<ProductList>!
     override func setUpWithError() throws {
@@ -19,7 +19,7 @@ class URLProductListTests: XCTestCase {
         sut = nil
     }
    
-    func test_URL이_상품리스트_조회_일때_fetchData_메서드를_호출하면_data의offset_값이_10인지() {
+    func test_URL이_상품리스트_조회의_page가_2이고_itemsPerPage이_10일때_fetchData_메서드를_호출하면_data의offset_값이_10인지() {
         //given
         let promise = expectation(description: "The offset value of data is 10")
         
@@ -37,7 +37,7 @@ class URLProductListTests: XCTestCase {
         wait(for: [promise], timeout: 10)
     }
     
-    func test_URL이_상품리스트_조회_일때_fetchData_메서드를_호출하면_data의offset_값이_0인지() {
+    func test_URL이_상품리스트_조회의_page가_1이고_itemsPerPage이_11일때_fetchData_메서드를_호출하면_data의offset_값이_0인지() {
         //given
         let promise = expectation(description: "The offset value of data is 10")
         
@@ -49,6 +49,24 @@ class URLProductListTests: XCTestCase {
                 XCTAssertEqual(data.offset, 0)
             case .failure(_):
                 XCTFail()
+            }
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 10)
+    }
+    
+    func test_잘못된_URL을_요청시_fetchData_메서드를_호출하면_failure를_반환하는지() {
+        //given
+        let promise = expectation(description: "The offset value of data is 10")
+        
+        //when
+        sut.fetchData(from: .detailProduct(id: 123123)) { result in
+            //then
+            switch result {
+            case .success(let data):
+                XCTAssertEqual(data.offset, nil)
+            case .failure(let error):
+                XCTAssertEqual(error, NetworkError.statusCodeError)
             }
             promise.fulfill()
         }
