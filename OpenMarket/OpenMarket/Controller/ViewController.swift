c5b8d5dfc5ed5f8cdc4955092d217a3788a8484d
Lingo <llingo@kakao.com>
2022-05-16 18:00:01 +0900

    feat: 상품 목록 API 서버 요청 #13
    close #13

--- OpenMarket/OpenMarket/Controller/ViewController.swift
+++ OpenMarket/OpenMarket/Controller/ViewController.swift
@@ -6,7 +6,17 @@
 import UIKit
 
 final class ViewController: UIViewController {
+  private let networkService = APINetworkService(urlSession: URLSession.shared)
+  private var productList = [Product]()
+
   override func viewDidLoad() {
     super.viewDidLoad()
+  }
+  
+  private func loadProductListData(page: Int, itemPerPage: Int) {
+    self.networkService.fetchProductAll(pageNumber: page, itemsPerPage: itemPerPage) { result in
+      guard let productList = try? result.get() else { return }
+      self.productList = productList
+    }
   }
 }
