6a6bdfc3ed8c0924552d6e9b865957f5911a4ef1
Lingo <llingo@kakao.com>
2022-05-16 20:27:10 +0900

    chore: ProductListCollectionViewCell 파일 생성 #15

---
+++ OpenMarket/OpenMarket/View/ProductListCollectionViewCell.swift
@@ -0,0 +1,10 @@
+//
+//  ProductListCollectionViewCell.swift
+//  OpenMarket
+//
+//  Created by Lingo, Quokka on 2022/05/16.
+//
+
+import UIKit
+
+class ProductListCollectionViewCell: UICollectionViewCell {}
