9f872c996c4aa4a814ed5c3068456a91f1964a28
JoSH0318 <jsh9870@naver.com>
2022-05-17 02:18:46 +0900

    🚚[move]: extension 코드, 파일 이동

--- OpenMarket/OpenMarket/Model/Util/ImageView+Extension.swift
+++ OpenMarket/OpenMarket/Model/Util/ImageView+Extension.swift
@@ -2,7 +2,22 @@
 //  ImageView+Extension.swift
 //  OpenMarket
 //
-//  Created by 조성훈 on 2022/05/17.
+//  Created by cathy, mmim.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+  func load(urlString: String) {
+    guard let url = URL(string: urlString) else { return }
+    DispatchQueue.global().async { [weak self] in
+      if let data = try? Data(contentsOf: url) {
+        if let image = UIImage(data: data) {
+          DispatchQueue.main.async {
+            self?.image = image
+          }
+        }
+      }
+    }
+  }
+}
--- OpenMarket/OpenMarket/Model/Util/String+Extension.swift
+++ OpenMarket/OpenMarket/Model/Util/String+Extension.swift
@@ -2,7 +2,15 @@
 //  String+Extension.swift
 //  OpenMarket
 //
-//  Created by 조성훈 on 2022/05/17.
+//  Created by cathy, mmim.
 //
 
-import Foundation
+import UIKit
+
+extension String {
+  func strikeThrough() -> NSAttributedString {
+    let attributeString: NSMutableAttributedString = NSMutableAttributedString(string: self)
+    attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: NSMakeRange(0, attributeString.length))
+    return attributeString
+  }
+}
