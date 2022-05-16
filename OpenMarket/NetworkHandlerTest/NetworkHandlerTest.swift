6a87a03bbf447fd1b990a72a911bfd3177853386
doogie97 <chris_97@naver.com>
2022-05-16 12:28:25 +0900

    🔬test : 변경된 로직에 따른 테스트 코드 전면 수정

--- OpenMarket/NetworkHandlerTest/NetworkHandlerTest.swift
+++ OpenMarket/NetworkHandlerTest/NetworkHandlerTest.swift
@@ -9,6 +9,7 @@ import XCTest
 @testable import OpenMarket
 
 class NetworkHandlerTest: XCTestCase {
+    
     //MARK: - 테스트를 위한 메서드
     func convertJsonToData(fileName: String) -> Data {
         let testBundle = Bundle(for: type(of: self))
@@ -17,25 +18,31 @@ class NetworkHandlerTest: XCTestCase {
         return jsonString?.data(using: .utf8) ?? Data()
     }
     
+    func makeDummyData(data: Data?, statusCode: Int, error: Error?) -> ResponseResult {
+        let rsponse = HTTPURLResponse(url: URL(string: "test")!, statusCode: statusCode, httpVersion: "2", headerFields: nil)
+        return ResponseResult(data: data, response: rsponse, error: error)
+    }
+    
     //MARK: - 네트워크 없이 테스트
-    func test_getData호출시_decodeItemPage성공시_0번째items의_id가_20인가() {
+    func test_communicate호출시_ItemPage타입으로_decode성공시_0번째items의_id가_20인가() {
         //given
         let promise = expectation(description: "id가 일치 하는지")
-        var dummyData = DummyData()
-        dummyData.data = convertJsonToData(fileName: "products")
-        dummyData.response = HTTPURLResponse(url: URL(string: "test")!, statusCode: 200, httpVersion: "2", headerFields: nil)
-        dummyData.error = nil
-        
-        let session = StubURLSession(dummyData: dummyData)
-        let netWorkHandler = NetworkHandler(session: session)
+        let data = convertJsonToData(fileName: "products")
+        let dummyData = makeDummyData(data: data, statusCode: 200, error: nil)
+        let netWorkHandler = NetworkHandler(session: StubURLSession(dummyData: dummyData))
         
         //when
-        netWorkHandler.getData(pathString: "test") { data in
-            //then
-            do {
-                let itemPage = try DataDecoder.decode(data: data, dataType: ItemPage.self)
-                XCTAssertEqual(itemPage.items[0].id, 20)
-            } catch {
+        netWorkHandler.communicate(pathString: "test", httpMethod: .get) { data in
+            switch data {
+            case .success(let data):
+                do {
+                    let data = try DataDecoder.decode(data: data, dataType: ItemPage.self)
+                    //then
+                    XCTAssertEqual(data.items[0].id, 20)
+                } catch {
+                    XCTFail()
+                }
+            case .failure(_):
                 XCTFail()
             }
             promise.fulfill()
@@ -43,122 +50,131 @@ class NetworkHandlerTest: XCTestCase {
         wait(for: [promise], timeout: 5)
     }
     
-    func test_getData호출시_path가_올바르지않을경우_convertError를_잘던지는지() {
+    func test_communicate호출시_path가_올바르지않을경우_convertError를_잘던지는지() {
         //given
-        let promise = expectation(description: "convertError를_잘던지는지")
-        let testPath = "te st"
-        var dummyData = DummyData()
-        dummyData.data = convertJsonToData(fileName: "products")
-        dummyData.response = HTTPURLResponse(url: URL(string: "test")!, statusCode: 200, httpVersion: "2", headerFields: nil)
-        dummyData.error = nil
-        let session = StubURLSession(dummyData: dummyData)
-        let netWorkHandler = NetworkHandler(session: session)
-
+        let promise = expectation(description: "id가 일치 하는지")
+        let data = convertJsonToData(fileName: "products")
+        let dummyData = makeDummyData(data: data, statusCode: 200, error: nil)
+        let netWorkHandler = NetworkHandler(session: StubURLSession(dummyData: dummyData))
+        let wrongURLString = "te st"
+        
         //when
-        netWorkHandler.getData(pathString: testPath) { data in
-            //then
-            do {
-                let _ = try DataDecoder.decode(data: data, dataType: ItemPage.self)
-                XCTFail()
-            } catch {
-                XCTAssertEqual(error as! APIError, APIError.convertError)
+        netWorkHandler.communicate(pathString: wrongURLString, httpMethod: .get) { data in
+            switch data {
+            case .success(let data):
+                do {
+                    let _ = try DataDecoder.decode(data: data, dataType: ItemPage.self)
+                    XCTFail()
+                } catch {
+                    XCTFail()
+                }
+            case .failure(let error):
+                //then
+                XCTAssertEqual(error, APIError.convertError)
             }
             promise.fulfill()
         }
         wait(for: [promise], timeout: 5)
     }
-
-    func test_getData호출시_dataTask에서_에러가존재할경우_requestError를_잘던지는지() {
+    
+    func test_communicate호출시_request과정에서에러가발생할경우_transportError를_잘던지는지() {
         //given
-        let promise = expectation(description: "requestError를_잘던지는지")
-        var dummyData = DummyData()
-        dummyData.data = convertJsonToData(fileName: "products")
-        dummyData.response = HTTPURLResponse(url: URL(string: "test")!, statusCode: 200, httpVersion: "2", headerFields: nil)
-        dummyData.error = APIError.convertError
-        let session = StubURLSession(dummyData: dummyData)
-        let netWorkHandler = NetworkHandler(session: session)
-
+        let promise = expectation(description: "id가 일치 하는지")
+        let data = convertJsonToData(fileName: "products")
+        let dummyData = makeDummyData(data: data, statusCode: 200, error: APIError.convertError)
+        let netWorkHandler = NetworkHandler(session: StubURLSession(dummyData: dummyData))
+        
         //when
-        netWorkHandler.getData(pathString: "test") { data in
-            //then
-            do {
-                let _ = try DataDecoder.decode(data: data, dataType: ItemPage.self)
-                XCTFail()
-            } catch {
-                XCTAssertEqual(error as! APIError, APIError.transportError)
+        netWorkHandler.communicate(pathString: "test", httpMethod: .get) { data in
+            switch data {
+            case .success(let data):
+                do {
+                    let _ = try DataDecoder.decode(data: data, dataType: ItemPage.self)
+                    XCTFail()
+                } catch {
+                    XCTFail()
+                }
+            case .failure(let error):
+                //then
+                XCTAssertEqual(error, APIError.transportError)
             }
             promise.fulfill()
         }
         wait(for: [promise], timeout: 5)
     }
-
-    func test_getData호출시_statusCode가_200에서299_아닐때_responseError를_잘던지는지() {
+    
+    func test_communicate호출시_statusCode가_200에서299_아닐때_responseError를_잘던지는지() {
         //given
-        let promise = expectation(description: "responseError를_잘던지는지")
-        var dummyData = DummyData()
-        dummyData.data = convertJsonToData(fileName: "products")
-        dummyData.response = HTTPURLResponse(url: URL(string: "test")!, statusCode: 404, httpVersion: "2", headerFields: nil)
-        dummyData.error = nil
-        let session = StubURLSession(dummyData: dummyData)
-        let netWorkHandler = NetworkHandler(session: session)
-
+        let promise = expectation(description: "id가 일치 하는지")
+        let data = convertJsonToData(fileName: "products")
+        let dummyData = makeDummyData(data: data, statusCode: 404, error: nil)
+        let netWorkHandler = NetworkHandler(session: StubURLSession(dummyData: dummyData))
+        
         //when
-        netWorkHandler.getData(pathString: "test") { data in
-            //then
-            do {
-                let _ = try DataDecoder.decode(data: data, dataType: ItemPage.self)
-                XCTFail()
-            } catch {
-                XCTAssertEqual(error as! APIError, APIError.responseError)
+        netWorkHandler.communicate(pathString: "test", httpMethod: .get) { data in
+            switch data {
+            case .success(let data):
+                do {
+                    let _ = try DataDecoder.decode(data: data, dataType: ItemPage.self)
+                    XCTFail()
+                } catch {
+                    XCTFail()
+                }
+            case .failure(let error):
+                //then
+                XCTAssertEqual(error, APIError.responseError)
             }
             promise.fulfill()
         }
         wait(for: [promise], timeout: 5)
     }
-
-    func test_getData호출시_data가_올바르지않은경우_dataError를_잘던지는지() {
+    
+    func test_communicate호출시_data가_올바르지않은경우_dataError를_잘던지는지() {
         //given
-        let promise = expectation(description: "dataError를_잘던지는지")
-        var dummyData = DummyData()
-        dummyData.data = nil
-        dummyData.response = HTTPURLResponse(url: URL(string: "test")!, statusCode: 200, httpVersion: "2", headerFields: nil)
-        dummyData.error = nil
-        let session = StubURLSession(dummyData: dummyData)
-        let netWorkHandler = NetworkHandler(session: session)
-
+        let promise = expectation(description: "id가 일치 하는지")
+        let dummyData = makeDummyData(data: nil, statusCode: 200, error: nil)
+        let netWorkHandler = NetworkHandler(session: StubURLSession(dummyData: dummyData))
+        
         //when
-        netWorkHandler.getData(pathString: "test") { data in
-            //then
-            do {
-                let _ = try DataDecoder.decode(data: data, dataType: ItemPage.self)
+        netWorkHandler.communicate(pathString: "test", httpMethod: .get) { data in
+            switch data {
+            case .success(let data):
+                do {
+                    let _ = try DataDecoder.decode(data: data, dataType: ItemPage.self)
+                    XCTFail()
+                } catch {
+                    //then
+                    XCTAssertEqual(error as! APIError, APIError.dataError)
+                }
+            case .failure(_):
                 XCTFail()
-            } catch {
-                XCTAssertEqual(error as! APIError, APIError.dataError)
             }
             promise.fulfill()
         }
         wait(for: [promise], timeout: 5)
     }
-
-    func test_getData호출시_decoding실패시_decodeError를_잘던지는지() {
+    
+    func test_communicate호출시_decoding실패시_decodeError를_잘던지는지() {
         //given
-        let promise = expectation(description: "decodeError를_잘던지는지")
-        let testFileName = "wrongFileName"
-        var dummyData = DummyData()
-        dummyData.data = convertJsonToData(fileName: testFileName)
-        dummyData.response = HTTPURLResponse(url: URL(string: "test")!, statusCode: 200, httpVersion: "2", headerFields: nil)
-        dummyData.error = nil
-        let session = StubURLSession(dummyData: dummyData)
-        let netWorkHandler = NetworkHandler(session: session)
-
+        let promise = expectation(description: "id가 일치 하는지")
+        let wrongFileName = "test"
+        let data = convertJsonToData(fileName: wrongFileName)
+        let dummyData = makeDummyData(data: data, statusCode: 200, error: nil)
+        let netWorkHandler = NetworkHandler(session: StubURLSession(dummyData: dummyData))
+        
         //when
-        netWorkHandler.getData(pathString: "test") { data in
-            //then
-            do {
-                let _ = try DataDecoder.decode(data: data, dataType: ItemPage.self)
+        netWorkHandler.communicate(pathString: "test", httpMethod: .get) { data in
+            switch data {
+            case .success(let data):
+                do {
+                    let _ = try DataDecoder.decode(data: data, dataType: ItemPage.self)
+                    XCTFail()
+                } catch {
+                    //then
+                    XCTAssertEqual(error as! APIError, APIError.decodeError)
+                }
+            case .failure(_):
                 XCTFail()
-            } catch {
-                XCTAssertEqual(error as! APIError, APIError.decodeError)
             }
             promise.fulfill()
         }
