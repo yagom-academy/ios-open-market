e31349864be915c4c7b76bf54fdf3f42f9f21417
JoSH0318 <jsh9870@naver.com>
2022-05-17 00:15:22 +0900

    ✨[feat]: strikeThrough(), load() 메서드 구현

--- OpenMarket/OpenMarket/Controller/ListCell.swift
+++ OpenMarket/OpenMarket/Controller/ListCell.swift
@@ -115,4 +115,25 @@ final class ListCell: UICollectionViewCell {
   }
 }
 
+extension String {
+  func strikeThrough() -> NSAttributedString {
+    let attributeString: NSMutableAttributedString = NSMutableAttributedString(string: self)
+    attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: NSMakeRange(0, attributeString.length))
+    return attributeString
+  }
+}
+
+extension UIImageView {
+  func load(urlString: String) {
+    guard let url = URL(string: urlString) else { return }
+    DispatchQueue.global().async { [weak self] in
+      if let data = try? Data(contentsOf: url) {
+        if let image = UIImage(data: data) {
+          DispatchQueue.main.async {
+            self?.image = image
+          }
+        }
+      }
+    }
+  }
 }
