7e776c5afbcd439fb185d17daa7e21921c840be9
Donnie <seo_17@naver.com>
2022-05-16 20:26:54 +0900

    ✨[feat]: SegmentedControl 기능 구현

--- OpenMarket/OpenMarket/MainViewController.swift
+++ OpenMarket/OpenMarket/MainViewController.swift
@@ -7,11 +7,49 @@
 import UIKit
 
 final class MainViewController: UIViewController {
+    
+    private lazy var segmentedControl: UISegmentedControl = {
+        let segment = UISegmentedControl(items: ["LIST", "GRID"])
+        segment.selectedSegmentIndex = 0
+        segment.backgroundColor = .white
+        segment.selectedSegmentTintColor = .systemBlue
+        segment.layer.borderWidth = 2
+        segment.layer.borderColor = UIColor.systemBlue.cgColor
+        segment.addTarget(self, action: #selector(changeSegmentedControl), for: .valueChanged)
+        segment.translatesAutoresizingMaskIntoConstraints = false
+        segment.widthAnchor.constraint(equalToConstant: view.frame.size.width / 2).isActive = true
+        let selectedTextAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20)
+        ]
+        let releasedTextAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.systemBlue
+        ]
+        segment.setTitleTextAttributes(selectedTextAttributes, for: .selected)
+        segment.setTitleTextAttributes(releasedTextAttributes, for: .normal)
+        return segment
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
         
-        view.backgroundColor = .systemPink
+        configureSegmentedControl()
+    }
+    
+    private func configureSegmentedControl() {
+        self.navigationItem.titleView = segmentedControl
+    }
+    
+    @objc func changeSegmentedControl(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            return print("LIST 뷰입니다.")
+        case 1:
+            return print("GIRD 뷰입니다.")
+        default:
+            return
+        }
     }
 }
 
