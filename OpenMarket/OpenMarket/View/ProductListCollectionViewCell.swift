a9efa21328f9c9f05f953dcec9af47eb76e8ac4e
Lingo <llingo@kakao.com>
2022-05-16 23:16:48 +0900

    feat: 아이템을 표시할 셀 프로퍼티 추가 #15, #16

--- OpenMarket/OpenMarket/View/ProductListCollectionViewCell.swift
+++ OpenMarket/OpenMarket/View/ProductListCollectionViewCell.swift
@@ -7,4 +7,54 @@
 
 import UIKit
 
-class ProductListCollectionViewCell: UICollectionViewCell {}
+final class ProductListCollectionViewCell: UICollectionViewCell {
+  private let containerStackView = UIStackView()
+  private let informationStackView = UIStackView()
+  private let priceStackView = UIStackView()
+  private let quantityStackView = UIStackView()
+
+  private let productImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.contentMode = .scaleAspectFit
+    return imageView
+  }()
+  
+  private let titleLabel: UILabel = {
+    let label = UILabel()
+    label.font = .preferredFont(forTextStyle: .headline)
+    return label
+  }()
+  
+  private let priceLabel: UILabel = {
+    let label = UILabel()
+    label.textColor = .systemRed
+    return label
+  }()
+  
+  private let bargainPriceLabel: UILabel = {
+    let label = UILabel()
+    label.textColor = .systemGray
+    return label
+  }()
+  
+  private let stockLabel: UILabel = {
+    let label = UILabel()
+    label.textColor = .systemGray
+    return label
+  }()
+  
+  private let disclosureImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.tintColor = .systemGray
+    imageView.image = UIImage(systemName: "chevron.right")
+    return imageView
+  }()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+  }
+}
