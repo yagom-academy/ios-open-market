133b3fe776cdfd2eb1dc7620f9a43298a8776b8e
Quokkaa <rktrkdml@gmail.com>
2022-05-16 20:24:10 +0900

    feat: Segmented Control 구현 #14
    close #14

--- OpenMarket/OpenMarket/Controller/ProductListViewController.swift
+++ OpenMarket/OpenMarket/Controller/ProductListViewController.swift
@@ -9,8 +9,15 @@ final class ProductListViewController: UIViewController {
   private let networkService = APINetworkService(urlSession: URLSession.shared)
   private var productList = [Product]()
 
+  private let segmentControl: UISegmentedControl = {
+    let segment = UISegmentedControl(items: ["LIST", "GRID"])
+    segment.selectedSegmentIndex = .zero
+    return segment
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.configureUI()
   }
   
   private func loadProductListData(page: Int, itemPerPage: Int) {
@@ -18,5 +25,10 @@ final class ProductListViewController: UIViewController {
       guard let productList = try? result.get() else { return }
       self.productList = productList
     }
+  }
+  
+  private func configureUI() {
+    self.view.backgroundColor = .systemBackground
+    self.navigationItem.titleView = segmentControl
   }
 }
