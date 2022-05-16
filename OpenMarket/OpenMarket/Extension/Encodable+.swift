3bc1aebfb20f788f03d23032e89b8c273f493494
dudu <whqtkf12@naver.com>
2022-05-16 20:24:08 +0900

    feat: Encodable 확장

---
+++ OpenMarket/OpenMarket/Extension/Encodable+.swift
@@ -0,0 +1,14 @@
+//
+//  Encodable.swift
+//  OpenMarket
+//
+//  Created by dudu, safari on 2022/05/16.
+//
+
+import Foundation
+
+extension Encodable {
+    func encodeData() -> Data? {
+        return try? JSONEncoder().encode(self)
+    }
+}
