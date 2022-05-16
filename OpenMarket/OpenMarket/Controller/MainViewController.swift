0b2b062d5f8af5dfe1f694fd25e0169bf72ff829
JoSH0318 <jsh9870@naver.com>
2022-05-17 00:17:34 +0900

    ✨[feat]: MainViewController, DataSource 메서드 구현

--- OpenMarket/OpenMarket/Controller/MainViewController.swift
+++ OpenMarket/OpenMarket/Controller/MainViewController.swift
@@ -10,6 +10,21 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+extension MainViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView,
+                      numberOfItemsInSection section: Int) -> Int {
+    return self.pages.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView,
+                      cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let id = String(describing: ListCell.self)
+    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: id,
+                                                        for: indexPath) as? ListCell else {
+      return UICollectionViewCell()
     }
+    cell.setUpListCell(page: self.pages[indexPath.row])
+    return cell
+  }
 }
 
