23e01e2983b4745491d925cc8d80a1bada26621a
saafaaari <111dltldnjs@naver.com>
2022-05-16 20:46:47 +0900

    refactor : test를 위한 MockURLProtocol 구현 #12

--- OpenMarket/OpenMarketTests/Test Double/MockURLProtocol.swift
+++ OpenMarket/OpenMarketTests/Test Double/MockURLProtocol.swift
@@ -19,4 +19,29 @@ struct DummyData {
     }
 }
 
+final class MockURLProtocol: URLProtocol {
+    static var requsetHandler: ((URLRequest) throws -> (HTTPURLResponse, Data))?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        guard let handler = Self.requsetHandler else { return }
+        do {
+            let (response, data) = try handler(request)
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            client?.urlProtocol(self, didLoad: data)
+            client?.urlProtocolDidFinishLoading(self)
+        } catch {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
+    
+    override func stopLoading() {}
+}
 
