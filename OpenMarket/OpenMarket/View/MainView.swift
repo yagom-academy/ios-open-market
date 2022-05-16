658b5ceade9f53e9633e203bd7a475326e38f01d
Mino777 <jomino7777@gmail.com>
2022-05-16 20:17:25 +0900

    feat: MainView 추가

--- OpenMarket/OpenMarket/View/MainView.swift
+++ OpenMarket/OpenMarket/View/MainView.swift
@@ -8,13 +8,91 @@
 import UIKit
 
 final class MainView: UIView {
+    enum LayoutStatus: Int {
+        case list = 0
+        case grid = 1
+    }
+
+    var layoutStatus: LayoutStatus = .list {
+        didSet {
+            self.collectionView.reloadData()
+        }
+    }
+    
+    lazy var segmentControl: UISegmentedControl = {
+        let segmentControl = UISegmentedControl(items: ["LIST", "GRID"])
+        segmentControl.selectedSegmentIndex = 0
+        return segmentControl
+    }()
+    
+    lazy var addButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(barButtonSystemItem: .add, target: nil, action: nil)
+        return button
+    }()
+    
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: bounds, collectionViewLayout: listLayout)
+        return collectionView
+    }()
+    
+    private lazy var listLayout: UICollectionViewCompositionalLayout = {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.15))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
+    }()
+    
+    private lazy var gridLayout: UICollectionViewCompositionalLayout = {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/2),
+                                              heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .fractionalHeight(0.3))
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+        group.interItemSpacing = .fixed(14)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 8, bottom: 10, trailing: 8)
+        section.interGroupSpacing = 8
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }()
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .white
+        addSubview(collectionView)
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         backgroundColor = .white
+        addSubview(collectionView)
+    }
+    
+    func setUpLayout(segmentIndex: Int) {
+        guard let layoutStatus = LayoutStatus(rawValue: segmentIndex) else {
+            return
+        }
+        
+        switch layoutStatus {
+        case .list:
+            collectionView.collectionViewLayout = listLayout
+            self.layoutStatus = .list
+        case .grid:
+            collectionView.collectionViewLayout = gridLayout
+            self.layoutStatus = .grid
+        }
     }
 }
