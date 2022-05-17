fdf7be70a0903fe77b00aa031752d7b26bb64268
Quokkaa <rktrkdml@gmail.com>
2022-05-17 11:33:57 +0900

    feat: ProductGridCollectionViewCell 프로퍼티 구현 #16

--- OpenMarket/OpenMarket/View/ProductGridCollectionViewCell.swift
+++ OpenMarket/OpenMarket/View/ProductGridCollectionViewCell.swift
@@ -5,3 +5,54 @@
 //  Created by Lingo, Quokka on 2022/05/17.
 //
 
+import UIKit
+
+final class ProductGridCollectionViewCell: UICollectionViewCell {
+  private let containerStackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.axis = .vertical
+    return stackView
+  }()
+  
+  private let informationStackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.axis = .vertical
+    return stackView
+  }()
+  
+  private let priceStackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.axis = .vertical
+    return stackView
+  }()
+  
+  private let productImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.contentMode = .scaleAspectFit
+    return imageView
+  }()
+  
+  private let titleLabel: UILabel = {
+    let label = UILabel()
+    label.font = .preferredFont(forTextStyle: .headline)
+    return label
+  }()
+  
+  private let priceLabel: UILabel = {
+    let label = UILabel()
+    label.textColor = .systemRed
+    return label
+  }()
+  
+  private let bargainPriceLabel: UILabel = {
+    let label = UILabel()
+    label.textColor = .systemGray
+    return label
+  }()
+  
+  private let stockLabel: UILabel = {
+    let label = UILabel()
+    label.textColor = .systemGray
+    return label
+  }()
+}
