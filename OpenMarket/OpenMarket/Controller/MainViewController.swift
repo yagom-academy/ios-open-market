9e1e7914c0279866ae36b5d6a749091c8ec2e314
papriOS <bluecrab30@naver.com>
2022-05-17 11:21:29 +0900

    feat: Navigation Item 세팅

--- OpenMarket/OpenMarket/Controller/MainViewController.swift
+++ OpenMarket/OpenMarket/Controller/MainViewController.swift
@@ -7,12 +7,30 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    lazy var baseView = BaseView(frame: view.bounds)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view = baseView
+        setUpNavigationItem()
     }
-
-
+    
+    func setUpNavigationItem() {
+        setUpSegmentation()
+        navigationItem.titleView = baseView.segmentedControl
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "+", style: .plain, target: self, action: #selector(registerProduct))
+    }
+    
+    func setUpSegmentation() {
+        baseView.segmentedControl.setWidth(view.bounds.width * 0.18 , forSegmentAt: 0)
+        baseView.segmentedControl.setWidth(view.bounds.width * 0.18, forSegmentAt: 1)
+        baseView.segmentedControl.addTarget(self, action: #selector(switchCollectionViewLayout), for: .valueChanged)
+    }
+    
+    @objc func switchCollectionViewLayout() {
+    }
+    
+    @objc func registerProduct() {
+    }
 }
 
