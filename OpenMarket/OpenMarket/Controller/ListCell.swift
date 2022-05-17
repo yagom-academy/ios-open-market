2739e652acb914fb423758338e87d60cfec930a8
Taeangel <wngks2050@gmail.com>
2022-05-17 11:33:04 +0900

    ✨[feat]: OpenMarket에서 CollectionView구현

---
+++ OpenMarket/OpenMarket/Controller/ListCell.swift
@@ -0,0 +1,18 @@
+//
+//  ListCell.swift
+//  OpenMarket
+//
+//  Created by song on 2022/05/17.
+//
+
+import UIKit
+
+class ListCell: UICollectionViewCell {
+   
+}
+
+extension ListCell {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
--- OpenMarket/OpenMarket/Controller/OpenMarketViewController.swift
+++ OpenMarket/OpenMarket/Controller/OpenMarketViewController.swift
@@ -7,11 +7,32 @@
 import UIKit
 
 class OpenMarketViewController: UIViewController {
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: listCellLayout())
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    private func listCellLayout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        return layout
+    }
+}
 
-
+extension OpenMarketViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ListCell.identifier, for: indexPath) as? ListCell else {
+            return UICollectionViewCell()
+        }
+        
+        return cell
+    }
+    
+ 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
 }
