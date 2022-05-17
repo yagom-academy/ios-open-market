b38eb9b8839a50d6cc30ac3c8cb9f82bc5185c10
onetool <xogns8696@gmail.com>
2022-05-17 19:24:58 +0900

    ✨Feat : requestImageDownload에 이미지 캐싱 구현

--- OpenMarket/Extensions/ImageView+.swift
+++ OpenMarket/Extensions/ImageView+.swift
@@ -9,27 +9,34 @@ import UIKit
 
 extension UIImageView {
     func requestImageDownload(url: String) {
-        guard let url = URL(string: url) else {
-            return
-        }
-        var request = URLRequest(url: url)
-        
-        request.httpMethod = "GET"
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            guard error == nil else {
-                return
-            }
-            guard let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200 else {
-                return
-            }
-            guard let data = data, let image = UIImage(data: data) else {
-                return
-            }
-            
+        if let cacheImage = Cache.imageCache.object(forKey: url as NSString) {
             DispatchQueue.main.async() { [weak self] in
-                self?.image = image
+                self?.image = cacheImage
             }
-        }.resume()
+        } else {
+            guard let url = URL(string: url) else {
+                return
+            }
+            var request = URLRequest(url: url)
+            
+            request.httpMethod = "GET"
+            
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                guard error == nil else {
+                    return
+                }
+                guard let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200 else {
+                    return
+                }
+                guard let data = data, let image = UIImage(data: data) else {
+                    return
+                }
+                
+                DispatchQueue.main.async() { [weak self] in
+                    self?.image = image
+                    Cache.imageCache.setObject(image, forKey: url.absoluteString as NSString)
+                }
+            }.resume()
+        }
     }
 }
