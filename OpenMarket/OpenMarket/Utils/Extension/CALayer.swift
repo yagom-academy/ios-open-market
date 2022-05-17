d4cad4d78fbf27fcfcc58a78f3378920faba81ba
malrang <ruehd7924@gmail.com>
2022-05-17 16:12:17 +0900

    ✨[feat]: extension CALayer addBoarder() 메서드 구현

---
+++ OpenMarket/OpenMarket/Utils/Extension/CALayer.swift
@@ -0,0 +1,33 @@
+//
+//  CALayer.swift
+//  OpenMarket
+//
+//  Created by 김동욱 on 2022/05/17.
+//
+
+import UIKit
+
+extension CALayer {
+    func addBorder(edges: [UIRectEdge], color: UIColor, thickness: CGFloat, bottomLeftSpacing: CGFloat = 0) -> CALayer {
+        let border = CALayer()
+        for edge in edges {
+            switch edge {
+            case .top:
+                border.frame = CGRect.init(x: 0, y: 0, width: frame.width, height: thickness)
+            case .bottom:
+                border.frame = CGRect.init(x: bottomLeftSpacing, y: frame.height - thickness, width: frame.width, height: thickness)
+            case .left:
+                border.frame = CGRect.init(x: 0, y: 0, width: thickness, height: frame.height)
+            case .right:
+                border.frame = CGRect.init(x: frame.width, y: 0, width: thickness, height: frame.height)
+            case .all:
+                borderColor = color.cgColor
+                borderWidth = thickness
+            default:
+                return CALayer()
+            }
+            border.backgroundColor = color.cgColor
+        }
+        return border
+    }
+}
