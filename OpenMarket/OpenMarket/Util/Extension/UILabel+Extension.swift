de952bb124886a085426005aae9766095c592f48
Quokkaa <rktrkdml@gmail.com>
2022-05-17 17:42:39 +0900

    feat: 취소선 기능 구현 #21
    close #21

--- OpenMarket/OpenMarket/Util/Extension/UILabel+Extension.swift
+++ OpenMarket/OpenMarket/Util/Extension/UILabel+Extension.swift
@@ -5,5 +5,15 @@
 //  Created by Lingo, Quokka on 2022/05/17.
 //
 
-import Foundation
+import UIKit
 
+extension UILabel {
+  func setStrike(text: String) {
+    let attributedString = NSMutableAttributedString(string: text)
+    attributedString.addAttribute(
+      NSAttributedString.Key.strikethroughStyle,
+      value: 1,
+      range: NSRange(location: .zero, length: attributedString.length))
+    self.attributedText = attributedString
+  }
+}
