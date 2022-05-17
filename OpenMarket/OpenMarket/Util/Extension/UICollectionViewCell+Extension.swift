128ee3e17694553d4c2d3ea01677c168899a0bb9
Quokkaa <rktrkdml@gmail.com>
2022-05-17 11:57:26 +0900

    feat: UICollectionViewCell의 identifier 연산프로퍼티 구현

--- OpenMarket/OpenMarket/Util/Extension/UICollectionViewCell+Extension.swift
+++ OpenMarket/OpenMarket/Util/Extension/UICollectionViewCell+Extension.swift
@@ -5,4 +5,10 @@
 //  Created by Lingo, Quokka on 2022/05/17.
 //
 
-import Foundation
+import UIKit
+
+extension UICollectionViewCell {
+  static var identifier: String {
+    return String(describing: self)
+  }
+}
