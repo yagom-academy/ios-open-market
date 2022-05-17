c513e9939d459df468ce94c26acea10912660824
onetool <xogns8696@gmail.com>
2022-05-17 12:10:13 +0900

    ✨Feat : extension 그룹 생성 및 ImageView 구현

---
+++ OpenMarket/Extensions/ImageView+.swift
@@ -0,0 +1,35 @@
+//
+//  ImageView.swift
+//  OpenMarket
+//
+//  Created by Grumpy, OneTool on 2022/05/17.
+//
+
+import UIKit
+
+extension UIImageView {
+    func requestImageDownload(url: String) {
+        guard let url = URL(string: url) else {
+            return
+        }
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                return
+            }
+            guard let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200 else {
+                return
+            }
+            guard let data = data, let image = UIImage(data: data) else {
+                return
+            }
+            
+            DispatchQueue.main.async() { [weak self] in
+                self?.image = image
+            }
+        }.resume()
+    }
+}
