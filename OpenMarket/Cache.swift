b843c7c4e39ab2719dec5f001122db98f24cee1c
onetool <xogns8696@gmail.com>
2022-05-17 19:23:27 +0900

    ✨Feat : 이미지 캐싱을 위한 싱글톤 생성 및 구현

---
+++ OpenMarket/Cache.swift
@@ -0,0 +1,12 @@
+//
+//  Cache.swift
+//  OpenMarket
+//
+//  Created by Grumpy, OneTool on 2022/05/17.
+//
+
+import UIKit
+
+class Cache {
+    static let imageCache = NSCache<NSString, UIImage>()
+}
