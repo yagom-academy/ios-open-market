38d3f1d0e222a5cdb504dd1e86399515c41f9bf8
onetool <xogns8696@gmail.com>
2022-05-17 18:56:51 +0900

    ✨Feat : extension UIFont를 파일로 분리

---
+++ OpenMarket/Extensions/UIFont+.swift
@@ -0,0 +1,17 @@
+//
+//  UIFont+.swift
+//  OpenMarket
+//
+//  Created by Grumpy, OneTool on 2022/05/17.
+//
+
+import UIKit
+
+extension UIFont {
+    static func preferredFont(for style: TextStyle, weight: Weight) -> UIFont {
+        let metrics = UIFontMetrics(forTextStyle: style)
+        let desc = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style)
+        let font = UIFont.systemFont(ofSize: desc.pointSize, weight: weight)
+        return metrics.scaledFont(for: font)
+    }
+}
