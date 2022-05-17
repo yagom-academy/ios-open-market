a24d7380d5ebdbae56f19275e89c9dd8ac08e8f8
Donnie <seo_17@naver.com>
2022-05-18 01:22:00 +0900

    ➕[add]: view+extension 파일 생성 및 코드 이동

---
+++ OpenMarket/OpenMarket/View/view+extension.swift
@@ -0,0 +1,17 @@
+//
+//  view+extension.swift
+//  OpenMarket
+//
+//  Created by 우롱차, Donnie on 2022/05/18.
+//
+
+import UIKit
+
+extension NSMutableAttributedString {
+    convenience init(allText: String, redText: String) {
+        self.init(string: allText)
+        self.addAttribute(.foregroundColor, value: UIColor.red, range: (allText as NSString).range(of: redText))
+        self.addAttribute(.strikethroughColor, value: UIColor.red, range: (allText as NSString).range(of: redText))
+        self.addAttribute(.strikethroughStyle, value: 1, range: (allText as NSString).range(of: redText))
+    }
+}
