6784fa4874b6534bccf940a7a87e978baf8d9dd6
malrang <ruehd7924@gmail.com>
2022-05-17 11:15:37 +0900

    🪛[chore]: StoryBoard 삭제
    *SceneDelegate rootViewController 를 OpenMarketViewController 로 설정

--- OpenMarket/OpenMarket/SceneDelegate.swift
+++ OpenMarket/OpenMarket/SceneDelegate.swift
@@ -11,6 +11,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = UINavigationController(rootViewController: OpenMarketViewController())
+        window.makeKeyAndVisible()
+        self.window = window
     }
 }
