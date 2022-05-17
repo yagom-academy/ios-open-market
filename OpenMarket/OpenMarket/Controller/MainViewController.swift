be61eb79dc061b894506544a58f3069c434c3196
JoSH0318 <jsh9870@naver.com>
2022-05-17 19:03:38 +0900

    ♻️[refactor]: MainViewController, 메서드 순서 변경 및 로직 수정

--- OpenMarket/OpenMarket/Controller/MainViewController.swift
+++ OpenMarket/OpenMarket/Controller/MainViewController.swift
@@ -29,17 +29,51 @@ final class MainViewController: UIViewController {
   private lazy var dataSource = makeDataSource()
   
   private lazy var collectionView = UICollectionView(frame: .zero,
-                                             collectionViewLayout: configureListLayout())
+                                                     collectionViewLayout: configureListLayout())
   private let urlProvider = URLSessionProvider<ProductsList>(path: "/api/products",
-                                                     parameters: ["page_no":"1",
-                                                                  "items_per_page": "20"])
+                                                             parameters: ["page_no":"1",
+                                                                          "items_per_page": "20"])
+  
+  private var pages: [Page] = [] {
+    didSet {
+      DispatchQueue.main.async {
+        self.applySnapshot()
+      }
+    }
+  }
+  
   private lazy var segmentedControl: UISegmentedControl = {
-    let segment = UISegmentedControl(items: ["LIST", "GRID"])
+    let segment = UISegmentedControl(items: [layoutType.list.string, layoutType.grid.string])
     segment.selectedSegmentIndex = 0
     segment.addTarget(self, action: #selector(changeCollectionViewLayout(_:)), for: .valueChanged)
-    
     return segment
   }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    fetchPages()
+    configureCollectionView()
+    configureNavigationItems()
+    applySnapshot(animatingDifferences: false)
+    collectionView.dataSource = self.dataSource
+  }
+  
+  private func fetchPages() {
+    urlProvider.get { data in
+      guard let products = try? data.get() else {
+        return
+      }
+      self.pages = products.pages
+    }
+  }
+  
+  private func configureCollectionView() {
+    self.view.addSubview(collectionView)
+    collectionView.frame = self.view.safeAreaLayoutGuide.layoutFrame
+    self.collectionView.register(ListCell.self, forCellWithReuseIdentifier: "ListCell")
+    self.collectionView.register(GridCell.self, forCellWithReuseIdentifier: "GridCell")
+    self.collectionView.collectionViewLayout = configureListLayout()
+  }
   
   private func configureNavigationItems() {
     navigationItem.titleView = segmentedControl
@@ -59,36 +93,10 @@ final class MainViewController: UIViewController {
     }
     collectionView.reloadData()
   }
-
-  private var pages: [Page] = [] {
-    didSet {
-      DispatchQueue.main.async {
-        self.collectionView.reloadData()
-        self.applySnapshot()
-      }
-    }
-  }
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    fetchPages()
-    configureCollectionView()
-    collectionView.collectionViewLayout = configureListLayout()
-    configureNavigationItems()
-    applySnapshot(animatingDifferences: false)
-    collectionView.dataSource = self.dataSource
-  }
-  
-  private func configureCollectionView() {
-    self.view.addSubview(collectionView)
-    collectionView.frame = self.view.safeAreaLayoutGuide.layoutFrame
-    self.collectionView.register(ListCell.self, forCellWithReuseIdentifier: "ListCell")
-    self.collectionView.register(GridCell.self, forCellWithReuseIdentifier: "GridCell")
-  }
   
   private func configureListLayout() -> UICollectionViewFlowLayout {
     let layout = UICollectionViewFlowLayout()
-    layout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+    layout.sectionInset = UIEdgeInsets(top: 10, left: 5, bottom: 5, right: 5)
     layout.minimumLineSpacing = 10
     layout.minimumInteritemSpacing = 10
     layout.itemSize = CGSize(width: self.view.bounds.width,
@@ -98,53 +106,39 @@ final class MainViewController: UIViewController {
   
   private func configureGridLayout() -> UICollectionViewFlowLayout {
     let layout = UICollectionViewFlowLayout()
-    layout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+    layout.sectionInset = UIEdgeInsets(top: 10, left: 5, bottom: 5, right: 5)
     layout.minimumLineSpacing = 10
     layout.minimumInteritemSpacing = 10
     layout.itemSize = CGSize(width: self.view.bounds.width/2 - 15,
                              height: self.view.bounds.height/3)
     return layout
   }
-  
-  private func fetchPages() {
-    urlProvider.get { data in
-      guard let products = try? data.get() else {
-        return
-      }
-      self.pages = products.pages
-    }
-  }
-  
-  func makeDataSource() -> DataSource {
+}
+
+extension MainViewController {
+  private func makeDataSource() -> DataSource {
     let dataSource = DataSource(collectionView: collectionView, cellProvider: {
       (collectionView, indexPath, page) ->
       UICollectionViewCell? in
       if self.segmentedControl.selectedSegmentIndex == 0 {
-        let id = String(describing: ListCell.self)
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: id,
-                                                            for: indexPath) as? ListCell else {
-          return UICollectionViewCell()
-        }
-        cell.setUpListCell(page: self.pages[indexPath.row])
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ListCell",
+                                                      for: indexPath) as? ListCell
+        cell?.setUpListCell(page: self.pages[indexPath.row])
         return cell
       } else {
-        let id = String(describing: GridCell.self)
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: id,
-                                                            for: indexPath) as? GridCell else {
-          return UICollectionViewCell()
-        }
-        cell.setUpListCell(page: self.pages[indexPath.row])
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridCell",
+                                                            for: indexPath) as? GridCell
+        cell?.setUpListCell(page: self.pages[indexPath.row])
         return cell
       }
     })
     return dataSource
   }
   
-  func applySnapshot(animatingDifferences: Bool = true) {
+  private func applySnapshot(animatingDifferences: Bool = true) {
     var snapshot = Snapshot()
     snapshot.appendSections([.main])
     snapshot.appendItems(pages)
     dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
   }
 }
-
