f689975cfd6c2780fd2ae99adf8ecd85dfb370b0
dnwhd0112 <dnwhd0112@naver.com>
2022-05-17 19:22:22 +0900

    ♻️[refactor]: ProdcutInformation 이니셜라이저 변경 및 thumbnailImage 프로퍼티 변경, 테스트 변경 및 필요없는 주석 제거

--- OpenMarket/OpenMarket/Model/DataType/ProductInformation.swift
+++ OpenMarket/OpenMarket/Model/DataType/ProductInformation.swift
@@ -20,14 +20,7 @@ struct ProductInformation: Codable, Equatable, Hashable {
     let stock: Int
     let createdAt: String
     let issuedAt: String
-    var thumbnailImage: UIImage? {
-        get {
-        guard let url = URL(string:thumbnail), let data = try? Data(contentsOf: url) else {
-            return nil
-        }
-        return UIImage(data: data) ?? nil
-        }
-    }
+    var thumbnailImage: UIImage?
 
     private enum CodingKeys: String, CodingKey {
         case id
@@ -41,6 +34,30 @@ struct ProductInformation: Codable, Equatable, Hashable {
         case stock
         case createdAt = "created_at"
         case issuedAt = "issued_at"
+    }
+    
+    func convertImage() -> UIImage? {
+        guard let url = URL(string:thumbnail), let data = try? Data(contentsOf: url) else {
+            return nil
+        }
+        return UIImage(data: data) ?? nil
+        
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        vendorId = try container.decode(Int.self, forKey: .vendorId)
+        name = try container.decode(String.self, forKey: .name)
+        thumbnail = try container.decode(String.self, forKey: .thumbnail)
+        currency = try container.decode(String.self, forKey: .currency)
+        price = try container.decode(Double.self, forKey: .price)
+        bargainPrice = try container.decode(Double.self, forKey: .bargainPrice)
+        discountedPrice = try container.decode(Double.self, forKey: .discountedPrice)
+        stock = try container.decode(Int.self, forKey: .stock)
+        createdAt = try container.decode(String.self, forKey: .createdAt)
+        issuedAt = try container.decode(String.self, forKey: .issuedAt)
+        thumbnailImage = convertImage()
     }
 }
 
