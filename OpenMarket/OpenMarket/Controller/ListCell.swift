610cc3c88d6c37e34134d55731637a1e04185227
malrang <ruehd7924@gmail.com>
2022-05-17 13:23:17 +0900

    ✨[feat]: ListCell 레이아웃 추가

--- OpenMarket/OpenMarket/Controller/ListCell.swift
+++ OpenMarket/OpenMarket/Controller/ListCell.swift
@@ -21,8 +21,9 @@ class ListCell: UICollectionViewCell {
         return stackView
     }()
     
-    private lazy var imageView: UIImageView = {
+    private lazy var thumbnailImageView: UIImageView = {
         let image = UIImageView()
+        image.image = UIImage(systemName: "flame")
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
@@ -77,4 +78,57 @@ class ListCell: UICollectionViewCell {
         imageView.image = UIImage(systemName: "chevron.right")
         return imageView
     }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addsubViews()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+// MARK: - layout
+
+extension ListCell {
+    private func addsubViews() {
+        self.contentView.addSubview(cellStackView)
+        self.cellStackView.addArrangedsubViews(thumbnailImageView, informationStackView, stockStackView)
+        self.informationStackView.addArrangedsubViews(nameLabel, priceStackView)
+        self.priceStackView.addArrangedsubViews(priceLabel, bargenLabel)
+        self.stockStackView.addArrangedsubViews(stockLabel, accessoryImage)
+    }
+    
+    private func layout() {
+        
+        
+        NSLayoutConstraint.activate([
+            cellStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            cellStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            cellStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            cellStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            thumbnailImageView.widthAnchor.constraint(equalToConstant: frame.height * 4 / 5),
+            thumbnailImageView.heightAnchor.constraint(equalToConstant: frame.height * 4 / 5),
+            thumbnailImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            thumbnailImageView.trailingAnchor.constraint(equalTo: informationStackView.leadingAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            informationStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            informationStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            informationStackView.trailingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: -100)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stockStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            stockStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -100),
+            stockStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
+    }
 }
