bb18ea3f46bdf4545b6fcda98cf1b912f87e09a1
saafaaari <111dltldnjs@naver.com>
2022-05-19 00:11:11 +0900

    fix : iOS13에서 SegmentControl creash 문제 해결

--- OpenMarket/OpenMarket/Main/CellSegmentControl.swift
+++ OpenMarket/OpenMarket/Main/CellSegmentControl.swift
@@ -14,6 +14,10 @@ class CellSegmentControl: UISegmentedControl {
         configureSegmentControl()
     }
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
