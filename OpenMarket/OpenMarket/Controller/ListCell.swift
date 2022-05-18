9147fd68bfdf6f5c1ca6b8a66e1f827106b9ba97
doogie97 <chris_97@naver.com>
2022-05-18 11:06:05 +0900

    📱storyboard : ListCell 생성 및 autoLayout

---
+++ OpenMarket/OpenMarket/Controller/ListCell.swift
@@ -0,0 +1,17 @@
+//
+//  ListCell.swift
+//  OpenMarket
+//
+//  Created by 두기, minseong on 2022/05/18.
+//
+
+import UIKit
+
+class ListCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
