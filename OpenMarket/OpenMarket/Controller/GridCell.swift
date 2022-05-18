ecb38f4b11ab9395ea2216d6cf84165fefe2650c
minseong <msb0987@naver.com>
2022-05-18 13:22:07 +0900

    📱storyboard : GridCell 생성 및 AutoLayout

---
+++ OpenMarket/OpenMarket/Controller/GridCell.swift
@@ -0,0 +1,17 @@
+//
+//  GridCell.swift
+//  OpenMarket
+//
+//  Created by 두기, minseong on 2022/05/18.
+//
+
+import UIKit
+
+class GridCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
