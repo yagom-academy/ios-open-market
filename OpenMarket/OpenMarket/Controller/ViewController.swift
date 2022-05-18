1d99b7132fd13a8942f5384e2ac568c4fab57d5a
doogie97 <chris_97@naver.com>
2022-05-18 11:27:59 +0900

    💎feat : tableView추가 및 ListCell등록

--- OpenMarket/OpenMarket/Controller/ViewController.swift
+++ OpenMarket/OpenMarket/Controller/ViewController.swift
@@ -7,12 +7,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var openMarketCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        registCell()
+        openMarketCollectionView.dataSource = self
     }
+    
+    private func registCell() {
+        openMarketCollectionView.register(UINib(nibName: "ListCell", bundle: nil), forCellWithReuseIdentifier: "ListCell")
+    }
+}
 
-
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let listCell = collectionView.dequeueReusableCell(withReuseIdentifier: "ListCell", for: indexPath) as? ListCell else { return ListCell() }
+        
+        return listCell
+    }
+    
+    
 }
 
