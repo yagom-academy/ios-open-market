f880735315e450fbedb6ab29336960985b789f38
malrang <ruehd7924@gmail.com>
2022-05-19 18:03:56 +0900

    ✨[feat]: FlowLayout 파일추가 및 구현

---
+++ OpenMarket/OpenMarket/Controller/FlowLayout.swift
@@ -0,0 +1,40 @@
+//
+//  FlowLayout.swift
+//  OpenMarket
+//
+//  Created by 김동욱 on 2022/05/19.
+//
+
+import UIKit
+
+final class FlowLayout: UICollectionViewFlowLayout {
+    override init() {
+        super.init()
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func changeLayout(_ type: LayoutType) {
+        switch type {
+        case .list:
+            listLayout()
+        case .grid:
+            gridLayout()
+        }
+    }
+    
+    private func configure() {
+        scrollDirection = .vertical
+    }
+    
+    private func listLayout() {
+        minimumLineSpacing = 10
+    }
+    
+    private func gridLayout() {
+        sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+    }
+}
