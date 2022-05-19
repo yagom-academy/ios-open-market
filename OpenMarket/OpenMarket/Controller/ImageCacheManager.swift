7146500c59af653a87fa29a4974d33e86fa105a7
minseong <msb0987@naver.com>
2022-05-19 17:41:37 +0900

    💎feat : ImageCacheManager class 생성

---
+++ OpenMarket/OpenMarket/Controller/ImageCacheManager.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCacheManager.swift
+//  OpenMarket
+//
+//  Created by 두기, minseong on 2022/05/19.
+//
+
+import UIKit
+
+class ImageCacheManager {
+    static let shared = NSCache<NSString, UIImage>()
+    
+    private init(){}
+}
