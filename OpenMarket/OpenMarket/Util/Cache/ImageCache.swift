c70e303a99d59db74f877ed2f62cf61fbe7cd677
Quokkaa <rktrkdml@gmail.com>
2022-05-20 16:26:40 +0900

    feat: 이미지 캐싱 기능 구현 #19
    close #19

--- OpenMarket/OpenMarket/Util/Cache/ImageCache.swift
+++ OpenMarket/OpenMarket/Util/Cache/ImageCache.swift
@@ -5,4 +5,9 @@
 //  Created by Lingo, Quokka on 2022/05/20.
 //
 
-import Foundation
+import UIKit
+
+final class ImageCache {
+  static let shared = NSCache<NSString, UIImage>()
+  private init() {}
+}
--- OpenMarket/OpenMarket/Util/Extension/UIImageView+Extension.swift
+++ OpenMarket/OpenMarket/Util/Extension/UIImageView+Extension.swift
@@ -9,12 +9,18 @@ import UIKit
 
 extension UIImageView {
   func setImage(with urlString: String) {
+    if let image = ImageCache.shared.object(forKey: urlString as NSString) {
+      self.image = image
+      return
+    }
+    
     guard let url = URL(string: urlString) else { return }
     
     URLSession.shared.dataTask(with: url) { data, response, error in
       guard error == nil else { return }
       
-      if let data = data {
+      if let data = data, let image = UIImage(data: data) {
+        ImageCache.shared.setObject(image, forKey: urlString as NSString)
         DispatchQueue.main.async {
           self.image = UIImage(data: data)
         }
