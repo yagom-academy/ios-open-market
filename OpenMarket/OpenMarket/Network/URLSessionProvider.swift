beff5a7dd37fa6fecb7c6f0011e91f3d19da8a96
malrang <ruehd7924@gmail.com>
2022-05-20 13:30:57 +0900

    🚚[move]: 메서드IImageView extension 파일로 분리
    fatchImage 메서드 URLSessionProvider -> UIImageView Extension

--- OpenMarket/OpenMarket/Network/URLSessionProvider.swift
+++ OpenMarket/OpenMarket/Network/URLSessionProvider.swift
@@ -97,27 +97,3 @@ struct URLSessionProvider<T: Decodable> {
             task.resume()
         }
 }
-
-
-extension UIImageView {
-    func fetchImage(url: URL, completion: @escaping (UIImage) -> Void) {
-
-        URLSession.shared.dataTask(with: url) { data, response, _ in
-            guard let response = response as? HTTPURLResponse,
-                  (200...299).contains(response.statusCode) else {
-                return
-            }
-            
-            guard let data = data else {
-                return
-            }
-            
-            guard let image = UIImage(data: data) else {
-                return
-            }
-            
-            completion(image)
-            
-        }.resume()
-    }
-}
--- OpenMarket/OpenMarket/Utils/Extension/UIImageView.swift
+++ OpenMarket/OpenMarket/Utils/Extension/UIImageView.swift
@@ -5,4 +5,26 @@
 //  Created by 김동욱 on 2022/05/20.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+    func fetchImage(url: URL, completion: @escaping (UIImage) -> Void) {
+        URLSession.shared.dataTask(with: url) { data, response, _ in
+            guard let response = response as? HTTPURLResponse,
+                  (200...299).contains(response.statusCode) else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            guard let image = UIImage(data: data) else {
+                return
+            }
+            
+            completion(image)
+            
+        }.resume()
+    }
+}
