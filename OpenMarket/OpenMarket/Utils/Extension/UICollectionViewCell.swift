285b8ebadbf9ac6b80b25f11c1b81d9a93add5d3
malrang <ruehd7924@gmail.com>
2022-05-20 20:08:50 +0900

    ✨[feat]: UICollectionViewCell extension 파일추가 및 구현

--- OpenMarket/OpenMarket/Utils/Extension/UICollectionViewCell.swift
+++ OpenMarket/OpenMarket/Utils/Extension/UICollectionViewCell.swift
@@ -5,4 +5,10 @@
 //  Created by 김동욱 on 2022/05/20.
 //
 
-import Foundation
+import UIKit
+
+extension UICollectionViewCell {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
