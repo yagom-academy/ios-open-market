5329dd8a22c590377ce8a94f88e9a533eae5abe9
JoSH0318 <jsh9870@naver.com>
2022-05-20 20:07:25 +0900

    🚚[move]: enum Constants 분리 및 파일 이동

---
+++ OpenMarket/OpenMarket/Util/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  OpenMarket
+//
+//  Created by cathy, mmim.
+//
+
+import Foundation
+
+enum Constants {
+  static let cellCountPerRow = 2.0
+  static let listCellCountPerColumn = 14.0
+  static let gridCellCountPerColumn = 3.0
+}
+
