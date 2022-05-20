38ac5024c795b8cc2c67930cabf1431eefbb86a0
JoSH0318 <jsh9870@naver.com>
2022-05-20 20:08:29 +0900

    🔥[del]: 코드 이동으로 인한 삭제

--- OpenMarket/OpenMarket/Controller/MainViewController.swift
+++ OpenMarket/OpenMarket/Controller/MainViewController.swift
@@ -7,26 +7,6 @@
 import UIKit
 
 final class MainViewController: UIViewController {
-  enum Layout: Int {
-    case list = 0
-    case grid = 1
-    
-    var string: String {
-      switch self {
-      case .list:
-        return "LIST"
-      case .grid:
-        return "GRID"
-      }
-    }
-  }
-  
-  enum Constant {
-    static let cellCountPerRow = 2.0
-    static let listCellCountPerColumn = 14.0
-    static let gridCellCountPerColumn = 3.0
-  }
-  
   enum Section {
     case main
   }
