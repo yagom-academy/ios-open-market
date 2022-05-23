1ed45432d15536ca221270f5b5a1d2d31e73a1c2
Grumpy <sewoong2890@gmail.com>
2022-05-23 18:03:31 +0900

    ✨Feat : 상품등록 화면 레이아웃 구현

--- OpenMarket/OpenMarket/RegisterViewController.swift
+++ OpenMarket/OpenMarket/RegisterViewController.swift
@@ -8,12 +8,83 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+    let mainStackView: UIStackView = {
+       let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    let priceStackView: UIStackView = {
+        let stackView = UIStackView()
+         stackView.axis = .horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+         return stackView
+    }()
+    
+    let nameField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "상품명"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    let priceField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "상품가격"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    let currencyField: UISegmentedControl = UISegmentedControl(items: ["KRW", "USD"])
+    let discountedPriceField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "할인가격"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    let stockField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "재고수량"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    let descriptionField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "상품설명"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "상품등록"
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: nil)
         self.navigationItem.rightBarButtonItem = doneButton
+        self.navigationItem.hidesBackButton = true
+        let backbutton = UIBarButtonItem(title: "Cancel", style: .plain, target: nil, action: nil)
+        backbutton.setTitleTextAttributes([NSAttributedString.Key.font: UIFont.preferredFont(for: .body, weight: .semibold)], for: .normal)
+        self.navigationItem.leftBarButtonItem = backbutton
         
+        // Sub View Structure
+        priceStackView.addArrangedSubview(priceField)
+        priceStackView.addArrangedSubview(currencyField)
+        
+        mainStackView.addArrangedSubview(nameField)
+        mainStackView.addArrangedSubview(priceStackView)
+        mainStackView.addArrangedSubview(discountedPriceField)
+        mainStackView.addArrangedSubview(stockField)
+        mainStackView.addArrangedSubview(descriptionField)
+        
+        self.view.addSubview(mainStackView)
+        
+        // Constraints
+        mainStackView.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        mainStackView.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor).isActive = true
+        mainStackView.heightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.heightAnchor).isActive = true
+        mainStackView.widthAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.widthAnchor).isActive = true
+        priceStackView.widthAnchor.constraint(equalTo: mainStackView.widthAnchor).isActive = true
     }
   
 }
