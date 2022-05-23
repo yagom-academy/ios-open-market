af2e97c1430546163dc2f3ae6eb636ecae1fffc8
marisol <marisol.develop@gmail.com>
2022-05-23 10:56:14 +0900

    [feat]: 데이터 가공해서 cell에 뿌려줄 Presenter 구조체 생성

---
+++ OpenMarket/OpenMarket/Model/Presenter.swift
@@ -0,0 +1,50 @@
+//
+//  Presenter.swift
+//  OpenMarket
+//
+//  Created by Eddy, marisol on 2022/05/21.
+//
+
+import Foundation
+
+struct Presenter {
+    var productImage: String?
+    var productName: String?
+    var price: String?
+    var bargainPrice: String?
+    var totalPrice: String?
+    var stock: String?
+    
+    mutating func setData(of product: Products) -> Presenter {
+        var presenter = Presenter()
+        
+        let urlString = product.thumbnail.absoluteString
+        presenter.productImage = urlString
+        presenter.productName = product.name
+        
+        let formattedPrice = formatNumber(price: product.price)
+        presenter.price = "\(product.currency) \(formattedPrice)"
+        
+        let formattedBargainPrice = formatNumber(price: product.bargainPrice)
+        presenter.bargainPrice = "\(product.currency) \(formattedBargainPrice)"
+        
+        if product.stock == 0 {
+            presenter.stock = "품절"
+        } else {
+            presenter.stock = "잔여수량: \(product.stock)"
+        }
+        
+        return presenter
+    }
+    
+    private func formatNumber(price: Int) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        
+        guard let formattedPrice = numberFormatter.string(from: NSNumber(value: price)) else {
+            return ""
+        }
+        
+        return formattedPrice
+    }
+}
