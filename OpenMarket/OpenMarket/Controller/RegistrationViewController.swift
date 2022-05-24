71b21b2114ce7fe21f472c5613070cbc4f0a4d37
myungju <moooooongdu171@gmail.com>
2022-05-24 17:58:27 +0900

    ✨[feat]: RegistrationViewController 구현

--- OpenMarket/OpenMarket/Controller/RegistrationViewController.swift
+++ OpenMarket/OpenMarket/Controller/RegistrationViewController.swift
@@ -12,6 +12,25 @@ final class RegistrationViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    self.view = registrationView
+    configureRegistration()
+    self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelModal))
+      self.navigationItem.title = "상품등록"
+      navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(cancelModal))
+    navigationController?.navigationBar.backgroundColor = .white
+    navigationController?.navigationBar.scrollEdgeAppearance = UINavigationBarAppearance()
+  }
+  
+  private func configureRegistration() {
+    let safeArea = self.view.safeAreaLayoutGuide
+    self.view.addSubview(registrationView)
+    
+    NSLayoutConstraint.activate([registrationView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+                                     registrationView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+                                     registrationView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+                                     registrationView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)])
+  }
+  
+  @objc private func cancelModal() {
+    self.dismiss(animated: true, completion: nil)
   }
 }
