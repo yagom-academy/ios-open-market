fb8f60bbe48e4c12a20cda3c54cc3c18cafd6b9a
Tiana <97kth01@naver.com>
2022-05-24 17:30:07 +0900

    feat: collectionView cell 구현

--- OpenMarket/OpenMarket/Controller/MainViewControllerUnderiOS14.swift
+++ OpenMarket/OpenMarket/Controller/MainViewControllerUnderiOS14.swift
@@ -8,6 +8,19 @@
 import UIKit
 
 class MainViewControllerUnderiOS14: UIViewController {
+    enum Section {
+        case main
+    }
+    
+    private let dataProvider = DataProvider()
+    private var products: [Product] = [] {
+        didSet {
+            DispatchQueue.main.async {
+                self.collectionView?.reloadData()
+            }
+        }
+    }
+    
     private var collectionView: UICollectionView?
     private var baseView = BaseView()
     private var listLayout: UICollectionViewLayout?
@@ -19,10 +32,23 @@ class MainViewControllerUnderiOS14: UIViewController {
         applyListLayout()
         applyGridLayout()
         configureHierarchy(collectionViewLayout: listLayout ?? UICollectionViewLayout())
+        setUpCollectionView()
+        
+        dataProvider.fetchData() { products in
+            self.products.append(contentsOf: products)
+        }
     }
 }
 
 extension MainViewControllerUnderiOS14 {
+    private func setUpCollectionView() {
+        collectionView?.register(ProductListCell.self, forCellWithReuseIdentifier: "ProductListCell")
+        collectionView?.register(ProductGridCell.self, forCellWithReuseIdentifier: "ProductGridCell")
+        collectionView?.dataSource = self
+        collectionView?.delegate = self
+        collectionView?.backgroundColor = .systemBackground
+    }
+    
     private func setUpNavigationItem() {
         setUpSegmentation()
         navigationItem.titleView = baseView.segmentedControl
@@ -32,10 +58,30 @@ extension MainViewControllerUnderiOS14 {
     private func setUpSegmentation() {
         baseView.segmentedControl.setWidth(view.bounds.width * 0.18 , forSegmentAt: 0)
         baseView.segmentedControl.setWidth(view.bounds.width * 0.18, forSegmentAt: 1)
+        baseView.segmentedControl.addTarget(self, action: #selector(switchCollectionViewLayout), for: .valueChanged)
     }
     
     @objc private func registerProduct() {
         present(RegisterProductViewController(), animated: false)
+    }
+    
+    @objc private func switchCollectionViewLayout() {
+        switch baseView.segmentedControl.selectedSegmentIndex {
+        case 0:
+            guard let listLayout = listLayout else {
+                return
+            }
+            collectionView?.setCollectionViewLayout(listLayout, animated: false)
+            collectionView?.reloadData()
+        case 1:
+            guard let gridLayout = gridLayout else {
+                return
+            }
+            collectionView?.setCollectionViewLayout(gridLayout, animated: false)
+            collectionView?.reloadData()
+        default:
+            break
+        }
     }
 }
 
@@ -83,5 +129,49 @@ extension MainViewControllerUnderiOS14 {
             collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
+    }
+}
+
+extension MainViewControllerUnderiOS14: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if baseView.segmentedControl.selectedSegmentIndex == 0 {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductListCell", for: indexPath) as? ProductListCell else {
+                return UICollectionViewCell()
+            }
+            
+            guard let product = products[safe: indexPath.row] else {
+                return UICollectionViewCell()
+            }
+            
+            cell.update(newItem: product)
+            
+            return cell
+        } else {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductGridCell", for: indexPath) as? ProductGridCell else {
+                return UICollectionViewCell()
+            }
+            
+            guard let product = products[safe: indexPath.row] else {
+                return UICollectionViewCell()
+            }
+            
+            cell.update(newItem: product)
+            return cell
+        }
+    }
+}
+
+extension MainViewControllerUnderiOS14: UICollectionViewDelegate {
+    func collectionView( _ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        guard let _ = products[safe: indexPath.row + 1] else {
+            dataProvider.fetchData { products in
+                self.products.append(contentsOf: products)
+            }
+            return
+        }
     }
 }
