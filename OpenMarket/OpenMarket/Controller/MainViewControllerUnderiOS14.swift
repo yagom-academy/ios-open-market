cafce2ee1578fe75d188306ce68698b83990585b
papriOS <bluecrab30@naver.com>
2022-05-24 17:17:36 +0900

    feat: list, grid layout object 생성하여 프로퍼티에 할당

--- OpenMarket/OpenMarket/Controller/MainViewControllerUnderiOS14.swift
+++ OpenMarket/OpenMarket/Controller/MainViewControllerUnderiOS14.swift
@@ -10,10 +10,15 @@ import UIKit
 class MainViewControllerUnderiOS14: UIViewController {
     private var collectionView: UICollectionView?
     private var baseView = BaseView()
+    private var listLayout: UICollectionViewLayout?
+    private var gridLayout: UICollectionViewLayout?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpNavigationItem()
+        applyListLayout()
+        applyGridLayout()
+        configureHierarchy(collectionViewLayout: listLayout ?? UICollectionViewLayout())
     }
 }
 
@@ -31,5 +36,52 @@ extension MainViewControllerUnderiOS14 {
     
     @objc private func registerProduct() {
         present(RegisterProductViewController(), animated: false)
+    }
+}
+
+extension MainViewControllerUnderiOS14 {
+    private func configureHierarchy(collectionViewLayout: UICollectionViewLayout) {
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: listLayout ?? collectionViewLayout)
+        view.addSubview(collectionView ?? UICollectionView())
+        layoutCollectionView()
+    }
+    
+    private func applyGridLayout() {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.3))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+        group.interItemSpacing = .fixed(10)
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+        section.interGroupSpacing = 10
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        gridLayout = layout
+    }
+    
+    private func applyListLayout() {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .fractionalHeight(0.1))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        listLayout = layout
+    }
+    
+    private func layoutCollectionView() {
+        guard let collectionView = collectionView else {
+            return
+        }
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
     }
 }
