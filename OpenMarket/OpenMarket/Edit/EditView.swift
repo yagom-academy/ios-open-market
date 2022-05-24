8ab4607a6cf57f1db08e6fbdd6643693487051df
saafaaari <111dltldnjs@naver.com>
2022-05-24 14:13:54 +0900

    feat :  EditView 내부 TextField, TextView 구현 #23

--- OpenMarket/OpenMarket/Edit/EditView.swift
+++ OpenMarket/OpenMarket/Edit/EditView.swift
@@ -10,10 +10,18 @@ import UIKit
 class EditView: UIView {
     
     private lazy var baseStackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [collectionView])
+        let stackView = UIStackView(arrangedSubviews: [collectionView,
+                                                       productNameTextField,
+                                                       productCostStackView,
+                                                       productDiscountCostTextField,
+                                                       productStockTextField,
+                                                       productDescriptionTextView])
         
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.directionalLayoutMargins = .init(top: 0, leading: 8, bottom: 0, trailing: 8)
+        stackView.isLayoutMarginsRelativeArrangement = true
         return stackView
     }()
     
@@ -24,6 +32,59 @@ class EditView: UIView {
         collectionView.backgroundColor = .systemBackground
         collectionView.isScrollEnabled = false
         return collectionView
+    }()
+    
+    let productNameTextField: UITextField = {
+        let textField = UITextField()
+        
+        textField.placeholder = "상품명"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    private lazy var productCostStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [productCostTextField, currencySegmentedControl])
+        
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    let productCostTextField: UITextField = {
+        let textField = UITextField()
+        
+        textField.setContentHuggingPriority(.lowest, for: .horizontal)
+        textField.placeholder = "상품가격"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    let currencySegmentedControl: UISegmentedControl = {
+        let segmentedControl = UISegmentedControl(items: ["KRW", "USD"])
+        
+        segmentedControl.selectedSegmentIndex = 0
+        return segmentedControl
+    }()
+    
+    let productDiscountCostTextField: UITextField = {
+        let textField = UITextField()
+        
+        textField.placeholder = "할인금액"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    let productStockTextField: UITextField = {
+        let textField = UITextField()
+        
+        textField.placeholder = "재고수량"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    private let productDescriptionTextView: UITextView = {
+        let textView = UITextView()
+        
+        return textView
     }()
     
     override init(frame: CGRect) {
@@ -40,6 +101,7 @@ class EditView: UIView {
         
         NSLayoutConstraint.activate([
             baseStackView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            baseStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
             baseStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
             baseStackView.trailingAnchor.constraint(equalTo: trailingAnchor)
         ])
