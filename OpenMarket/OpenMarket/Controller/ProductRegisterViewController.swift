209f5cc95d5098daf2cc8c02b722f1d607742602
Lingo <llingo@kakao.com>
2022-05-25 15:55:50 +0900

    feat: [상품 등록] 상품 정보 입력창 UI 구현 #30, #31
    closes #30, #31

--- OpenMarket/OpenMarket/Controller/ProductRegisterViewController.swift
+++ OpenMarket/OpenMarket/Controller/ProductRegisterViewController.swift
@@ -11,6 +11,7 @@ final class ProductRegisterViewController: UIViewController {
   private let containerStackView: UIStackView = {
     let stackView = UIStackView()
     stackView.axis = .vertical
+    stackView.spacing = 20.0
     stackView.translatesAutoresizingMaskIntoConstraints = false
     return stackView
   }()
@@ -25,14 +26,69 @@ final class ProductRegisterViewController: UIViewController {
     let stackView = UIStackView()
     stackView.translatesAutoresizingMaskIntoConstraints = false
     stackView.axis = .horizontal
+    stackView.backgroundColor = .systemRed
     return stackView
   }()
   
-  private let textStackView: UIStackView = {
+  private let productInputStackView: UIStackView = {
     let stackView = UIStackView()
-    stackView.translatesAutoresizingMaskIntoConstraints = false
     stackView.axis = .vertical
+    stackView.distribution = .fillEqually
+    stackView.spacing = 5.0
     return stackView
+  }()
+  
+  private let nameTextField: UITextField = {
+    let textField = UITextField()
+    textField.borderStyle = .roundedRect
+    textField.font = .preferredFont(forTextStyle: .subheadline)
+    textField.placeholder = "상품명"
+    return textField
+  }()
+  
+  private let productPriceStackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.axis = .horizontal
+    stackView.distribution = .fill
+    stackView.spacing = 5.0
+    return stackView
+  }()
+  
+  private let priceTextField: UITextField = {
+    let textField = UITextField()
+    textField.borderStyle = .roundedRect
+    textField.font = .preferredFont(forTextStyle: .subheadline)
+    textField.placeholder = "상품가격"
+    return textField
+  }()
+  
+  private let currencySegment: UISegmentedControl = {
+    let segment = UISegmentedControl(items: ["KRW", "USD"])
+    segment.selectedSegmentIndex = 0
+    return segment
+  }()
+  
+  private let discountedPriceTextField: UITextField = {
+    let textField = UITextField()
+    textField.borderStyle = .roundedRect
+    textField.font = .preferredFont(forTextStyle: .subheadline)
+    textField.placeholder = "할인금액"
+    return textField
+  }()
+  
+  private let stockTextField: UITextField = {
+    let textField = UITextField()
+    textField.borderStyle = .roundedRect
+    textField.font = .preferredFont(forTextStyle: .subheadline)
+    textField.placeholder = "재고수량"
+    return textField
+  }()
+  
+  private let descriptionsTextView: UITextView = {
+    let textView = UITextView()
+    textView.textColor = .black
+    textView.font = .preferredFont(forTextStyle: .subheadline)
+    return textView
   }()
   
   override func viewDidLoad() {
@@ -57,14 +113,24 @@ final class ProductRegisterViewController: UIViewController {
     self.view.backgroundColor = .systemBackground
     self.view.addSubview(containerStackView)
     self.containerStackView.addArrangedSubview(imageScrollView)
-    self.containerStackView.addArrangedSubview(textStackView)
+    self.containerStackView.addArrangedSubview(productInputStackView)
+    self.containerStackView.addArrangedSubview(descriptionsTextView)
+    
     self.imageScrollView.addSubview(imageStackView)
+    
+    self.productPriceStackView.addArrangedSubview(priceTextField)
+    self.productPriceStackView.addArrangedSubview(currencySegment)
+    
+    self.productInputStackView.addArrangedSubview(nameTextField)
+    self.productInputStackView.addArrangedSubview(productPriceStackView)
+    self.productInputStackView.addArrangedSubview(discountedPriceTextField)
+    self.productInputStackView.addArrangedSubview(stockTextField)
     
     NSLayoutConstraint.activate([
       containerStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
       containerStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-      containerStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-      containerStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+      containerStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20.0),
+      containerStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20.0),
       
       imageScrollView.heightAnchor.constraint(equalToConstant: view.frame.height * 0.2),
       
@@ -72,7 +138,10 @@ final class ProductRegisterViewController: UIViewController {
       imageStackView.bottomAnchor.constraint(equalTo: imageScrollView.contentLayoutGuide.bottomAnchor),
       imageStackView.leadingAnchor.constraint(equalTo: imageScrollView.contentLayoutGuide.leadingAnchor),
       imageStackView.trailingAnchor.constraint(equalTo: imageScrollView.contentLayoutGuide.trailingAnchor),
-      imageStackView.heightAnchor.constraint(equalTo: imageScrollView.heightAnchor)
+      imageStackView.heightAnchor.constraint(equalTo: imageScrollView.heightAnchor),
+      imageStackView.widthAnchor.constraint(equalToConstant: view.frame.width + 1),
+      
+      priceTextField.widthAnchor.constraint(equalTo: currencySegment.widthAnchor, multiplier: 2.0)
     ])
   }
   
