81fd8c21aa23321c2764b473844967ec17e57ef9
Taeangel <wngks2050@gmail.com>
2022-05-25 15:08:14 +0900

    ✨[feat]: Image 구조체 생성

---
+++ OpenMarket/OpenMarket/Model/Image.swift
@@ -0,0 +1,14 @@
+//
+//  Image.swift
+//  OpenMarket
+//
+//  Created by song on 2022/05/25.
+//
+
+import Foundation
+
+struct Image {
+    let fileName: String
+    let type: String
+    let data: Data
+}
