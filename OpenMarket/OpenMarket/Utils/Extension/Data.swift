6c53d56be810a153a0f1052808f53a44c5b418cd
Taeangel <wngks2050@gmail.com>
2022-05-25 15:09:39 +0900

    ✨[feat]: Data Extension 생성

---
+++ OpenMarket/OpenMarket/Utils/Extension/Data.swift
@@ -0,0 +1,16 @@
+//
+//  Data.swift
+//  OpenMarket
+//
+//  Created by song on 2022/05/25.
+//
+
+import UIKit
+
+extension Data {
+    mutating func appendString(_ string: String) {
+        if let data = string.data(using: .utf8) {
+            self.append(data)
+        }
+    }
+}
