2163d2c57e55a66071049deb7ac38b78907dfbfc
Taeangel <wngks2050@gmail.com>
2022-05-25 15:09:02 +0900

    ✨[feat]: ProductRegistration 구조체 생성

---
+++ OpenMarket/OpenMarket/Model/ProductRegistration.swift
@@ -0,0 +1,17 @@
+//
+//  ProductRegistration.swift
+//  OpenMarket
+//
+//  Created by song on 2022/05/25.
+//
+
+struct ProductRegistration: Codable {
+    let name: String
+    let price: Int
+    let discountedPrice: Int
+    let bargainPrice: Int
+    let currency: String
+    let secret: String
+    let descriptions: String
+    let stock: Int
+}
