b36eb8e387c5ffea8ee2df44ab17de4c51657562
Taeangel <wngks2050@gmail.com>
2022-05-25 17:40:22 +0900

    ✨[feat]: ProductRegistrationVC의 네비게이션아이템 구현

--- OpenMarket/OpenMarket/Controller/ProductRegistrationViewController.swift
+++ OpenMarket/OpenMarket/Controller/ProductRegistrationViewController.swift
@@ -12,6 +12,22 @@ class ProductRegistrationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setupNavigationItems()
+
     }
+    
+    private func setupNavigationItems() {
+        self.navigationItem.title = "상품등록"
+        
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(didTapCancelButton))
+        navigationItem.leftBarButtonItem = cancelButton
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(didTapDoneButton))
+        navigationItem.rightBarButtonItem = doneButton
+    }
+    
+    @objc private func didTapCancelButton() { }
+    
+    @objc private func didTapDoneButton() { }
 
 }
