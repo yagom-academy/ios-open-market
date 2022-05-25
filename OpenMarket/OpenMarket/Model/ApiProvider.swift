f6d8d3b58e92cbb0ee4214552b75588729a17a33
myungju <moooooongdu171@gmail.com>
2022-05-26 00:12:52 +0900

    ✨[feat]: http post 기능 초기 구현

--- OpenMarket/OpenMarket/Model/ApiProvider.swift
+++ OpenMarket/OpenMarket/Model/ApiProvider.swift
@@ -9,6 +9,7 @@ import Foundation
 
 enum HttpMethod {
   static let get = "GET"
+  static let post = "POST"
 }
 
 struct ApiProvider<T: Decodable> {
@@ -51,5 +52,47 @@ struct ApiProvider<T: Decodable> {
       
       completionHandler(.success(products))
     }.resume()
+  }
+}
+
+extension ApiProvider {
+  func post(_ endpoint: Endpoint, _ params: Params, _ images: [ImageFile], completionHandler: @escaping (Result<Data, NetworkError>) -> Void) {
+    
+    guard let url = endpoint.url else {
+      completionHandler(.failure(.invalid))
+      return
+    }
+    
+    var request = URLRequest(url: url)
+    let boundary = "Boundary-\(UUID().uuidString)"
+    request.httpMethod = HttpMethod.post
+    request.setValue("cd706a3e-66db-11ec-9626-796401f2341a", forHTTPHeaderField: "identifier")
+    request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+    
+    session.dataTask(with: request) { data, response, error in
+      guard error == nil else {
+        completionHandler(.failure(.invalid))
+        return
+      }
+      guard let response = response as? HTTPURLResponse,
+            (200..<300).contains(response.statusCode)
+      else {
+        completionHandler(.failure(.statusCodeError))
+        return
+      }
+      guard let data = data else {
+        completionHandler(.failure(.invalid))
+        return
+      }
+      
+      completionHandler(.success(data))
+    }.resume()
+  }
+  
+  
+  
+  func setupBody(params: Params, images: [ImageFile], boundary: String) -> Data? {
+    var body = Data()
+    return body
   }
 }
