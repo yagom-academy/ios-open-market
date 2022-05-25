ed93f1ac1b101c444350e4ebb5ad5aad44d84316
papriOS <bluecrab30@naver.com>
2022-05-25 10:52:30 +0900

    feat: setUpNavigationItem() 구현

--- OpenMarket/OpenMarket/Controller/RegisterProductViewController.swift
+++ OpenMarket/OpenMarket/Controller/RegisterProductViewController.swift
@@ -8,4 +8,25 @@
 import UIKit
 
 class RegisterProductViewController: UIViewController {
+    
+    override func loadView() {
+        super.loadView()
+        view.backgroundColor = .systemBackground
+        setUpNavigationItem()
+    }
+    
+    private func setUpNavigationItem() {
+        navigationItem.title = "상품등록"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(touchUpDoneButton))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.cancel, target: self, action: #selector(touchUpCancelButton))
+    }
+    
+    @objc private func touchUpDoneButton() {
+        dismiss(animated: true)
+    }
+    
+    @objc private func touchUpCancelButton() {
+        dismiss(animated: true)
+    }
+    
 }
