3a6d43d1c347062375167317b55289556093ecc8
dudu <whqtkf12@naver.com>
2022-05-26 11:11:21 +0900

    feat : textView Delegate를 이용한 입력 제한 #32

--- OpenMarket/OpenMarket/Product/ProductViewController.swift
+++ OpenMarket/OpenMarket/Product/ProductViewController.swift
@@ -41,6 +41,7 @@ class ProductViewController: UIViewController {
         configureCollectionView()
         configureNavigationBar()
         configureTextField()
+        configureTextView()
     }
     
     func configureCollectionView() {
@@ -55,6 +56,10 @@ class ProductViewController: UIViewController {
         mainView?.productCostTextField.delegate = self
         mainView?.productDiscountCostTextField.delegate = self
         mainView?.productStockTextField.delegate = self
+    }
+    
+    func configureTextView() {
+        mainView?.productDescriptionTextView.delegate = self
     }
     
     func configureNavigationBar() {
@@ -154,7 +159,7 @@ extension ProductViewController: UIImagePickerControllerDelegate & UINavigationC
 extension ProductViewController: UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         let currentString = textField.text ?? ""
-        let finalText = text(currentString, shouldChangeCharactersIn: range, replacementString: string)
+        let finalText = makeFinalText(currentString, shouldChangeCharactersIn: range, replacementString: string)
         if finalText.count == 0 { return true }
         
         switch textField {
@@ -176,10 +181,20 @@ extension ProductViewController: UITextFieldDelegate {
         }
     }
     
-    private func text(_ currentString: String, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> String {
+    private func makeFinalText(_ currentString: String, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> String {
         let currentString = currentString as NSString
         let nextString = currentString.replacingCharacters(in: range, with: string)
         
         return nextString
+    }
+}
+
+// MARK: - UITextView Delegate
+
+extension ProductViewController: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let currentString = textView.text ?? ""
+        let finalText = makeFinalText(currentString, shouldChangeCharactersIn: range, replacementString: text)
+        return (0...1000).contains(finalText.count)
     }
 }
