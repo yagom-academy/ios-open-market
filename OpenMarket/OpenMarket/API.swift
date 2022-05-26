9532425f4da519d9542d543c3fe94881c3b6b511
onetool <xogns8696@gmail.com>
2022-05-26 17:27:32 +0900

    🩹Chore : API의 identifier, password 값 변경

--- OpenMarket/OpenMarket/API.swift
+++ OpenMarket/OpenMarket/API.swift
@@ -9,6 +9,6 @@ import Foundation
 
 enum API {
     static let boundary: String = UUID().uuidString
-    static let identifier: String = "cd706a3e-66db-11ec-9626-796401f2341a"
-    static let secret: String = "password"
+    static let identifier: String = "ef12a75e-d1b7-11ec-9676-3deb332404fb"
+    static let secret: String = "jm8s2nwtky"
 }
