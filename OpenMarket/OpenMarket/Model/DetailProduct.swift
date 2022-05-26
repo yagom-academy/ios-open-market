9700859f0397ac815602d18c9f96d23bc52da0f0
JoSH0318 <jsh9870@naver.com>
2022-05-26 23:08:02 +0900

    ♻️[refactor]: DetailProduct 내부 프로퍼티 타입 변경

--- OpenMarket/OpenMarket/Model/DetailProduct.swift
+++ OpenMarket/OpenMarket/Model/DetailProduct.swift
@@ -18,24 +18,20 @@ struct DetailProduct: Decodable {
   let bargainPrice: Int
   let discountedPrice: Int
   let stock: Int
-  let images: Data
-  let vendors: Data
+  let images: [Image]
+  let vendors: Vendor
   let createdAt: String
   let issuedAt: String
   
   private enum CodingKeys: String, CodingKey {
-    case id
+    case id, name, description, thumbnail, currency, price, stock, images, vendors
     case venderId = "vendor_id"
-    case name
-    case description
-    case thumbnail
-    case currency
-    case price
     case bargainPrice = "bargain_price"
     case discountedPrice = "discounted_price"
-    case stock
-    case images
-    case vendors
+    case createdAt = "created_at"
+    case issuedAt = "issued_at"
+  }
+}
 
 struct Image: Decodable {
   let id: Int
