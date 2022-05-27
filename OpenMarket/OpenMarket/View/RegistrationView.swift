f3ac807f6d947523cf6d3bdccd7c3ad7989d5229
JoSH0318 <jsh9870@naver.com>
2022-05-27 17:34:22 +0900

    ♻️[refactor]: ProductUpdaterView 상속에 따른 코드 수정

--- OpenMarket/OpenMarket/View/RegistrationView.swift
+++ OpenMarket/OpenMarket/View/RegistrationView.swift
@@ -7,95 +7,25 @@
 
 import UIKit
 
-class RegistrationView: UIView {
-  
-  init() {
-    super.init(frame: .zero)
+final class RegistrationView: ProductUpdaterView {
+//  enum
+//  
+  override init() {
+    super.init()
     configureLayout()
-    self.backgroundColor = .white
-    self.translatesAutoresizingMaskIntoConstraints = false
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-  //MARK: - image scroll part
-  private let imageScrollView: UIScrollView = {
-    let scrollView = UIScrollView()
-    scrollView.translatesAutoresizingMaskIntoConstraints = false
-    return scrollView
-  }()
   
   let addImageButton: UIButton = {
     let button = UIButton()
     button.backgroundColor = .systemGray4
-    button.setTitle("+", for: .normal)
+    button.setTitle(Constants.buttonTitle, for: .normal)
     button.setTitleColor(UIColor.systemBlue, for: .normal)
     button.translatesAutoresizingMaskIntoConstraints = false
     return button
-  }()
-  //MARK: - text field part
-  private let nameTextField: UITextField = {
-    let textField = UITextField()
-    textField.placeholder = "상품명"
-    textField.borderStyle = .roundedRect
-    textField.backgroundColor = .white
-    textField.keyboardType = .default
-    return textField
-  }()
-  
-  private let priceTextField: UITextField = {
-    let textField = UITextField()
-    textField.placeholder = "상품가격"
-    textField.borderStyle = .roundedRect
-    textField.backgroundColor = .white
-    textField.keyboardType = .numberPad
-    return textField
-  }()
-  
-  private let currencySegmentedControl: UISegmentedControl = {
-    let segmentedControl = UISegmentedControl(items: ["KRW", "USD"])
-    segmentedControl.selectedSegmentIndex = 0
-    segmentedControl.translatesAutoresizingMaskIntoConstraints = false
-    segmentedControl.setContentCompressionResistancePriority(.required, for: .horizontal)
-    return segmentedControl
-  }()
-  
-  private let discountedPriceTextField: UITextField = {
-    let textField = UITextField()
-    textField.placeholder = "할인금액"
-    textField.borderStyle = .roundedRect
-    textField.backgroundColor = .white
-    textField.keyboardType = .numberPad
-    return textField
-  }()
-  
-  private let stockTextField: UITextField = {
-    let textField = UITextField()
-    textField.placeholder = "재고수량"
-    textField.borderStyle = .roundedRect
-    textField.backgroundColor = .white
-    textField.keyboardType = .numberPad
-    return textField
-  }()
-  //MARK: - description part
-  private let descriptionTextView: UITextView = {
-    let textView = UITextView()
-    textView.backgroundColor = .white
-    textView.translatesAutoresizingMaskIntoConstraints = false
-    textView.keyboardType = .default
-    textView.font = .systemFont(ofSize: 17)
-    return textView
-  }()
-  //MARK: - stack view
-  let imageStackView: UIStackView = {
-    let stackView = UIStackView()
-    stackView.axis = .horizontal
-    stackView.alignment = .fill
-    stackView.distribution = .fill
-    stackView.spacing = 5
-    stackView.translatesAutoresizingMaskIntoConstraints = false
-    return stackView
   }()
   
   private let imageWithButtonStackView: UIStackView = {
@@ -103,35 +33,7 @@ class RegistrationView: UIView {
     stackView.axis = .horizontal
     stackView.alignment = .fill
     stackView.distribution = .fill
-    stackView.spacing = 5
-    stackView.translatesAutoresizingMaskIntoConstraints = false
-    return stackView
-  }()
-  
-  private let priceStackView: UIStackView = {
-    let stackView = UIStackView()
-    stackView.axis = .horizontal
-    stackView.alignment = .fill
-    stackView.distribution = .fill
-    stackView.spacing = 5
-    return stackView
-  }()
-  
-  private let textFieldStackView: UIStackView = {
-    let stackView = UIStackView()
-    stackView.axis = .vertical
-    stackView.alignment = .fill
-    stackView.distribution = .fillEqually
-    stackView.spacing = 5
-    return stackView
-  }()
-  
-  private let totalStackView: UIStackView = {
-    let stackView = UIStackView()
-    stackView.axis = .vertical
-    stackView.alignment = .fill
-    stackView.distribution = .fill
-    stackView.spacing = 10
+    stackView.spacing = Constants.stackViewSpacing
     stackView.translatesAutoresizingMaskIntoConstraints = false
     return stackView
   }()
@@ -140,45 +42,48 @@ class RegistrationView: UIView {
     imageScrollView.addSubview(imageWithButtonStackView)
     imageWithButtonStackView.addArrangedSubviews(imageStackView, addImageButton)
     priceStackView.addArrangedSubviews(priceTextField, currencySegmentedControl)
-    textFieldStackView.addArrangedSubviews(nameTextField, priceStackView, discountedPriceTextField, stockTextField)
+    textFieldStackView.addArrangedSubviews(nameTextField,
+                                           priceStackView,
+                                           discountedPriceTextField,
+                                           stockTextField)
     totalStackView.addArrangedSubviews(imageScrollView, textFieldStackView, descriptionTextView)
     self.addSubview(totalStackView)
     
-    NSLayoutConstraint.activate(
-      [totalStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
-       totalStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
-       totalStackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
-       totalStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10),
+    NSLayoutConstraint.activate([
+        totalStackView
+        .leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Constants.anchorSpacing),
+       totalStackView
+        .trailingAnchor
+          .constraint(equalTo: self.trailingAnchor, constant: -Constants.anchorSpacing),
+       totalStackView
+        .topAnchor.constraint(equalTo: self.topAnchor, constant: Constants.anchorSpacing),
+       totalStackView
+        .bottomAnchor
+          .constraint(equalTo: self.bottomAnchor, constant: -Constants.anchorSpacing),
        
-       currencySegmentedControl.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.23),
+       currencySegmentedControl
+        .widthAnchor
+        .constraint(equalTo: self.widthAnchor, multiplier: Constants.currencyWidthScale),
        
-       addImageButton.widthAnchor.constraint(equalTo: addImageButton.heightAnchor, multiplier: 1),
+       addImageButton.widthAnchor.constraint(equalTo: addImageButton.heightAnchor),
        
-       imageScrollView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.17),
+       imageScrollView
+        .heightAnchor
+        .constraint(equalTo: self.heightAnchor, multiplier: Constants.scrollViewHeightScale),
        
-       imageWithButtonStackView.leadingAnchor.constraint(equalTo: imageScrollView.contentLayoutGuide.leadingAnchor),
-       imageWithButtonStackView.trailingAnchor.constraint(equalTo: imageScrollView.contentLayoutGuide.trailingAnchor),
-       imageWithButtonStackView.topAnchor.constraint(equalTo: imageScrollView.contentLayoutGuide.topAnchor),
-       imageWithButtonStackView.bottomAnchor.constraint(equalTo: imageScrollView.contentLayoutGuide.bottomAnchor),
-       imageWithButtonStackView.heightAnchor.constraint(equalTo: imageScrollView.frameLayoutGuide.heightAnchor, multiplier: 1),
-       descriptionTextView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.6)
+       imageWithButtonStackView
+        .leadingAnchor.constraint(equalTo: imageScrollView.contentLayoutGuide.leadingAnchor),
+       imageWithButtonStackView
+        .trailingAnchor.constraint(equalTo: imageScrollView.contentLayoutGuide.trailingAnchor),
+       imageWithButtonStackView
+        .topAnchor.constraint(equalTo: imageScrollView.contentLayoutGuide.topAnchor),
+       imageWithButtonStackView
+        .bottomAnchor.constraint(equalTo: imageScrollView.contentLayoutGuide.bottomAnchor),
+       imageWithButtonStackView
+        .heightAnchor.constraint(equalTo: imageScrollView.frameLayoutGuide.heightAnchor),
+       descriptionTextView
+        .heightAnchor
+        .constraint(equalTo: self.heightAnchor, multiplier: Constants.textViewHeightScale)
       ])
-  }
-  
-  func setupParams() -> Params {
-    var currency = ""
-    if self.currencySegmentedControl.selectedSegmentIndex == 0 {
-      currency = "KRW"
-    } else {
-      currency = "USD"
-    }
-    let params = Params(name: self.nameTextField.text!,
-                        price: Int(self.priceTextField.text!)!,
-                        discountedPrice: Int(self.discountedPriceTextField.text!)!,
-                        stock: Int(self.stockTextField.text!)!,
-                        currency: currency,
-                        descriptions: self.descriptionTextView.text!,
-                        secret: "pqnoec089z")
-    return params
   }
 }
