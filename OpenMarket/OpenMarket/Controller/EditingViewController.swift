7f3254d4c4544ec215f087902cdb74e6217a3b48
myungju <moooooongdu171@gmail.com>
2022-05-27 20:14:01 +0900

    ♻️[refactor]: EditingVC 네임스페이스 추가 및 메서드 위치 변경

--- OpenMarket/OpenMarket/Controller/EditingViewController.swift
+++ OpenMarket/OpenMarket/Controller/EditingViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 class EditingViewController: UIViewController {
   private enum Constants {
     static let navigationBarTitle = "상품수정"
+    static let warningAlertTitle = "입력하지 않은 정보가 있습니다."
+    static let warningAlertMessage = "입력 정보를 확인해주세요."
+    static let warningAlertCancelText = "확인"
   }
   
   private var apiProvider = ApiProvider<DetailProduct>()
@@ -18,21 +21,40 @@ class EditingViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    configureRegistration()
-    configureNavigationBar()
+    configureRegistrationView()
     configureView()
   }
   
-  private func configureRegistration() {
+  private func configureRegistrationView() {
     let safeArea = self.view.safeAreaLayoutGuide
     self.view.addSubview(editingView)
     
-    NSLayoutConstraint.activate(
-      [editingView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
-       editingView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
-       editingView.topAnchor.constraint(equalTo: safeArea.topAnchor),
-       editingView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
-      ])
+    NSLayoutConstraint.activate([
+      editingView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+      editingView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+      editingView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+      editingView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+    ])
+    
+    configureNavigationBar()
+  }
+  
+  private func configureView() {
+    guard let product = delegate else {
+      return
+    }
+    editingView.displayInformation(of: product)
+    configureImageView(with: product.images)
+  }
+  
+  private func configureImageView(with images: [Image]) {
+    for image in images {
+      let imageView = UIImageView()
+      imageView.translatesAutoresizingMaskIntoConstraints = false
+      imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor).isActive = true
+      imageView.loadImage(urlString: image.url)
+      editingView.imageStackView.addArrangedSubviews(imageView)
+    }
   }
   
   private func configureNavigationBar() {
@@ -55,7 +77,7 @@ class EditingViewController: UIViewController {
   @objc private func postEditedData() {
     let params = editingView.setupParams()
     let group = DispatchGroup()
-    guard let delegate = delegate else {
+    guard let product = self.delegate else {
       return
     }
     
@@ -63,9 +85,9 @@ class EditingViewController: UIViewController {
       presentWarningAlert()
       return
     }
-
+    
     DispatchQueue.global().async(group: group) {
-      self.apiProvider.patch(.editing(productId: delegate.id), params) { result in
+      self.apiProvider.patch(.editing(productId: product.id), params) { result in
         switch result {
         case .success(_):
           return
@@ -80,28 +102,11 @@ class EditingViewController: UIViewController {
     }
   }
   
-  private func configureView() {
-    guard let product = delegate else {
-      return
-    }
-    editingView.displayProductInformation(product)
-    loadImage(product.images)
-  }
-  
-  private func loadImage(_ images: [Image]) {
-    for image in images {
-      let imageView = UIImageView()
-      imageView.translatesAutoresizingMaskIntoConstraints = false
-      imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor).isActive = true
-      imageView.loadImage(urlString: image.url)
-      editingView.imageStackView.addArrangedSubviews(imageView)
-    }
-  }
-  
   private func presentWarningAlert() {
-    let alert = UIAlertController(title: "입력하지 않은 정보가 있습니다.",
-                                  message: "입력 정보를 확인해주세요.", preferredStyle: .alert)
-    let cancel = UIAlertAction(title: "확인", style: .cancel, handler: nil)
+    let alert = UIAlertController(title: Constants.warningAlertTitle,
+                                  message: Constants.warningAlertMessage, preferredStyle: .alert)
+    let cancel = UIAlertAction(title: Constants.warningAlertCancelText,
+                               style: .cancel, handler: nil)
     
     alert.addAction(cancel)
     
