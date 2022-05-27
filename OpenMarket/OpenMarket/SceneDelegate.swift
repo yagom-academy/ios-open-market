84b543cfa8fda8afb1c0dd7a2f9d92942e53547d
dudu <whqtkf12@naver.com>
2022-05-27 23:31:28 +0900

    feat : window의 background color 변경

--- OpenMarket/OpenMarket/SceneDelegate.swift
+++ OpenMarket/OpenMarket/SceneDelegate.swift
@@ -13,6 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
+        window?.backgroundColor = .systemBackground
         window?.rootViewController = UINavigationController(rootViewController: MainViewController())
         window?.makeKeyAndVisible()
     }
