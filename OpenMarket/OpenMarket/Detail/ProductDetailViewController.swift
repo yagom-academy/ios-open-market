5c787eef9159061d55af49c21f1889d455962f96
dudu <whqtkf12@naver.com>
2022-05-30 17:40:58 +0900

    feat : ProductDetailViewController 파일 생성

---
+++ OpenMarket/OpenMarket/Detail/ProductDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProductDetailViewController.swift
+//  OpenMarket
+//
+//  Created by dudu, safari on 2022/05/30.
+//
+
+import UIKit
+
+class ProductDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
