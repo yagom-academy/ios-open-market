c0a77f8c6ab6c4d742984a5f84139bd268dfb4af
saafaaari <111dltldnjs@naver.com>
2022-05-30 21:55:45 +0900

    feat : ProductDetail NetWork 통신 로직 구현

--- OpenMarket/OpenMarket/Detail/ProductDetailViewController.swift
+++ OpenMarket/OpenMarket/Detail/ProductDetailViewController.swift
@@ -8,8 +8,10 @@
 import UIKit
 
 final class ProductDetailViewController: UIViewController {
-    private typealias DataSource = UICollectionViewDiffableDataSource<Section, Product>
-    private typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Product>
+    private typealias DataSource = UICollectionViewDiffableDataSource<Section, UIImage>
+    private typealias Snapshot = NSDiffableDataSourceSnapshot<Section, UIImage>
+    
+    private let networkManager = NetworkManager()
     
     private var mainView: ProductDetailView?
     private let product: Product
@@ -34,11 +36,14 @@ final class ProductDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        requestData()
         configureView()
+        
     }
     
+    // MARK: - Configure
+    
     private func configureView() {
-        mainView?.backgroundColor = .systemBackground
         mainView?.configure(data: product)
         configureCollectionView()
     }
@@ -50,13 +55,16 @@ final class ProductDetailViewController: UIViewController {
         snapshot = makeSnapshot()
     }
     
+    // MARK: - CollectionView DataSource
+    
     private func makeDataSource() -> DataSource? {
         guard let mainView = mainView else { return nil }
         
         let datasource = DataSource(collectionView: mainView.productImageCollectionView) { collectionView, indexPath, itemIdentifier in
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProductImageCell.identifier, for: indexPath) as? ProductImageCell ?? ProductImageCell()
             
-            cell.configure(image: UIImage(systemName: "swift")!)
+            cell.configure(image: itemIdentifier)
+            cell.hideRemoveButton()
             
             return cell
         }
@@ -80,11 +88,45 @@ final class ProductDetailViewController: UIViewController {
         return snapshot
     }
     
-    private func applySnapshot(products: [Product]) {
+    private func applySnapshot(images: [UIImage]) {
         DispatchQueue.main.async { [self] in
-            snapshot?.appendItems(products)
+            snapshot?.appendItems(images)
             guard let snapshot = snapshot else { return }
             dataSource?.apply(snapshot, animatingDifferences: false)
+        }
+    }
+    
+    // MARK: - NetWork Method
+
+    private func requestData() {
+        guard let id = product.id else { return }
+        
+        let endPoint = EndPoint.requestProduct(id: id)
+        
+        networkManager.request(endPoint: endPoint) { [weak self] (result: Result<Product, NetworkError>) in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let data):
+                self.mainView?.configure(data: data)
+                let imagesUrl = data.images?.compactMap { $0.url }
+                imagesUrl?.forEach({ url in
+                    self.requestImage(urlString: url)
+                })
+            case .failure(_):
+                AlertDirector(viewController: self).createErrorAlert(message: "데이터를 불러오지 못했습니다.")
+            }
+        }
+    }
+    
+    private func requestImage(urlString: String) {
+        ImageManager.shared.downloadImage(urlString: urlString) { [weak self] result in
+            switch result {
+            case .success(let image):
+                self?.applySnapshot(images: [image])
+            case .failure(_):
+                break
+            }
         }
     }
 }
