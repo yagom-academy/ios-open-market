00b1dbc6eb86efe340a4174801c6ee95e5bffca8
JoSH0318 <jsh9870@naver.com>
2022-05-31 21:19:30 +0900

    ✨[feat]: DetailView 레이아웃 초기 구현

---
+++ OpenMarket/OpenMarket/View/DetailView.swift
@@ -0,0 +1,163 @@
+//
+//  DetailView.swift
+//  OpenMarket
+//
+//  Created by cathy, mmim.
+//
+
+import UIKit
+
+class DetailView: UIView {
+  enum Constants {
+    static let title: Double = 16.0
+    static let body: Double = 15.0
+    static let spacing: Double = 5.0
+  }
+  
+  init() {
+    super.init(frame: .zero)
+    self.backgroundColor = .white
+    self.translatesAutoresizingMaskIntoConstraints = false
+    configureLayout()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  //MARK: - Top part of View
+  private let imageScrollView: UIScrollView = {
+    let scrollView = UIScrollView()
+    scrollView.isScrollEnabled = true
+    scrollView.translatesAutoresizingMaskIntoConstraints = false
+    scrollView.backgroundColor = .white
+    return scrollView
+  }()
+  
+  private let imageStackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.axis = .horizontal
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    return stackView
+  }()
+  
+  //MARK: - Middle part of View
+  private let nameLabel: UILabel = {
+    let label = UILabel()
+    label.textAlignment = .left
+    label.font = .systemFont(ofSize: Constants.title, weight: .bold)
+    label.numberOfLines = 0
+    return label
+  }()
+  
+  private let priceLabel: UILabel = {
+    let label = UILabel()
+    label.textAlignment = .right
+    label.font = .systemFont(ofSize: Constants.body)
+    label.textColor = .systemGray
+    return label
+  }()
+  
+  private let bargainPriceLabel: UILabel = {
+    let label = UILabel()
+    label.textAlignment = .right
+    label.font = .systemFont(ofSize: Constants.body)
+    return label
+  }()
+  
+  private let stockLabel: UILabel = {
+    let label = UILabel()
+    label.textAlignment = .right
+    label.font = .systemFont(ofSize: Constants.body)
+    return label
+  }()
+  
+  private let verticalStackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.axis = .vertical
+    stackView.alignment = .trailing
+    stackView.distribution = .fillEqually
+    stackView.spacing = Constants.spacing
+    return stackView
+  }()
+  
+  private let infoStackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.axis = .horizontal
+    stackView.alignment = .top
+    stackView.distribution = .fill
+    return stackView
+  }()
+  
+  //MARK: - Bottom part of View
+  private let descriptionScrollView: UIScrollView = {
+    let scrollView = UIScrollView()
+    scrollView.isScrollEnabled = true
+    scrollView.translatesAutoresizingMaskIntoConstraints = false
+    return scrollView
+  }()
+  
+  private let descriptionView: UIView = {
+    let view = UIView()
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }()
+  
+  private let descriptionLabel: UILabel = {
+    let label = UILabel()
+    label.numberOfLines = 0
+    label.textAlignment = .left
+    label.font = .systemFont(ofSize: Constants.body)
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+  
+  private let totalStackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.axis = .vertical
+    stackView.alignment = .fill
+    stackView.distribution = .fill
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    return stackView
+  }()
+  
+  private func configureLayout() {
+    self.addSubview(totalStackView)
+    totalStackView.addArrangedSubviews(imageScrollView, infoStackView, descriptionScrollView)
+    imageScrollView.addSubview(imageStackView)
+    infoStackView.addArrangedSubviews(nameLabel, verticalStackView)
+    verticalStackView.addArrangedSubviews(stockLabel, priceLabel, bargainPriceLabel)
+    descriptionScrollView.addSubview(descriptionView)
+    descriptionView.addSubview(descriptionLabel)
+    
+    NSLayoutConstraint.activate([
+      totalStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+      totalStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+      totalStackView.topAnchor.constraint(equalTo: self.topAnchor),
+      totalStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+      
+      imageScrollView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.45),
+      infoStackView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.15),
+      
+      imageStackView.leadingAnchor.constraint(equalTo: imageScrollView.contentLayoutGuide.leadingAnchor),
+      imageStackView.trailingAnchor.constraint(equalTo: imageScrollView.contentLayoutGuide.trailingAnchor),
+      imageStackView.topAnchor.constraint(equalTo: imageScrollView.contentLayoutGuide.topAnchor),
+      imageStackView.bottomAnchor.constraint(equalTo: imageScrollView.contentLayoutGuide.bottomAnchor),
+      imageStackView.heightAnchor.constraint(equalTo: imageStackView.heightAnchor, multiplier: 1),
+      
+      descriptionLabel.leadingAnchor.constraint(equalTo: descriptionView.leadingAnchor),
+      descriptionLabel.trailingAnchor.constraint(equalTo: descriptionView.trailingAnchor),
+      descriptionLabel.topAnchor.constraint(equalTo: descriptionView.topAnchor),
+      descriptionLabel.bottomAnchor.constraint(equalTo: descriptionView.bottomAnchor),
+      
+      descriptionView.leadingAnchor.constraint(equalTo: descriptionScrollView.contentLayoutGuide.leadingAnchor),
+      descriptionView.trailingAnchor.constraint(equalTo: descriptionScrollView.contentLayoutGuide.trailingAnchor),
+      descriptionView.topAnchor.constraint(equalTo: descriptionScrollView.contentLayoutGuide.topAnchor),
+      descriptionView.bottomAnchor.constraint(equalTo: descriptionScrollView.contentLayoutGuide.bottomAnchor),
+      descriptionView.widthAnchor.constraint(equalTo: descriptionScrollView.widthAnchor, multiplier: 1)
+    ])
+  }
+  
+  func displayDetailInformation() {
+    
+  }
+}
