97bdfbd271086a4be07303ca52561666c852d06d
minseong <msb0987@naver.com>
2022-05-31 10:50:46 +0900

    ✏️chore : ItemComponents 파일이동

--- OpenMarket/OpenMarket/Controller/ViewController/AddItemViewController.swift
+++ OpenMarket/OpenMarket/Controller/ViewController/AddItemViewController.swift
@@ -7,16 +7,6 @@
 
 import UIKit
 
-struct ItemComponents {
-    let name: String
-    let price: Int
-    let currency: String
-    let discountedPrice: Int
-    let stock: Int
-    let descriptions: String
-    let imageArray: [UIImage]
-}
-
 protocol AddItemViewControllerDelegate {
     func upDate()
 }
