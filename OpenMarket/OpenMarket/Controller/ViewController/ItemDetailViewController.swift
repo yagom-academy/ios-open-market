95e9859f232ba2390dc63a750ba71762f441ff42
minseong <msb0987@naver.com>
2022-06-01 11:56:36 +0900

    💎feat : makeEditButton 추가 및 보기에 따른 alert구현

--- OpenMarket/OpenMarket/Controller/ViewController/ItemDetailViewController.swift
+++ OpenMarket/OpenMarket/Controller/ViewController/ItemDetailViewController.swift
@@ -15,5 +15,38 @@ final class ItemDetailViewController: UIViewController {
     
     func setInitialView(title: String) {
         self.title = title
+        navigationItem.rightBarButtonItem = makeEditButton()
+    }
+    
+    @objc private func touchEditButton() {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let edit = UIAlertAction(title: "수정", style: .default, handler: nil)
+        let delete = UIAlertAction(title: "삭제", style: .destructive) { _ in
+            let inAlert = UIAlertController(title: "비밀번호를 입력해주세요", message: nil, preferredStyle: .alert)
+            let yesAction = UIAlertAction(title: "확인", style: .default) { _ in
+                print(inAlert.textFields?[0].text ?? "")
+            }
+            
+            inAlert.addTextField()
+            inAlert.addAction(yesAction)
+            
+            self.present(inAlert, animated: true)
+        }
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alert.addAction(edit)
+        alert.addAction(delete)
+        alert.addAction(cancel)
+        
+        present(alert, animated: true)
     }
 }
+// MARK: - about View
+extension ItemDetailViewController {
+    private func makeEditButton() -> UIBarButtonItem {
+        let barButton = UIBarButtonItem(image: UIImage(systemName: "square.and.arrow.up"), style: .plain, target: self, action: #selector(touchEditButton))
+        
+        return barButton
+    }
+}
+
