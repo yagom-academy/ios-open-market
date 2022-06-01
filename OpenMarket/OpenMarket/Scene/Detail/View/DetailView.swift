44b0c1a32c6e95f648d795f0ccda21960c0531f3
Mino777 <jomino7777@gmail.com>
2022-06-02 00:25:50 +0900

    refactor: 접근제어자 수정

--- OpenMarket/OpenMarket/Scene/Detail/View/DetailView.swift
+++ OpenMarket/OpenMarket/Scene/Detail/View/DetailView.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 final class DetailView: UIView {
-    var totalIndex: Int = .zero
+    private var totalIndex: Int = .zero
     
     private let topScrollView: UIScrollView = {
         let scrollView = UIScrollView()
