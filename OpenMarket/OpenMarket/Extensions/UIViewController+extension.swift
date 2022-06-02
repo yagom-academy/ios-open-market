e873b7ea85b43219faa80333268aeb566fa690d6
onetool <xogns8696@gmail.com>
2022-06-02 19:19:46 +0900

    ♻️Refactor : enum 추가 및 처리

--- OpenMarket/OpenMarket/Extensions/UIViewController+extension.swift
+++ OpenMarket/OpenMarket/Extensions/UIViewController+extension.swift
@@ -7,17 +7,21 @@
 
 import UIKit
 
+private extension OpenMarketEnum {
+    static let close = "닫기"
+}
+
 extension UIViewController {
     func showAlert(alertTitle: String) {
         let alert = UIAlertController(title: alertTitle, message: nil, preferredStyle: .alert)
-        let action = UIAlertAction(title: "닫기", style: .default)
+        let action = UIAlertAction(title: OpenMarketEnum.close, style: .default)
         alert.addAction(action)
         present(alert, animated: true)
     }
     
     func showAlert(alertTitle: String, handler: @escaping ((UIAlertAction) -> Void)) {
         let alert = UIAlertController(title: alertTitle, message: nil, preferredStyle: .alert)
-        let action = UIAlertAction(title: "닫기", style: .default, handler: handler)
+        let action = UIAlertAction(title: OpenMarketEnum.close, style: .default, handler: handler)
         alert.addAction(action)
         present(alert, animated: true)
     }
