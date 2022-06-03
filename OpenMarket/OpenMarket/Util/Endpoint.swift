a74c0b3117f253b472644777b47b3e514bb83f40
JoSH0318 <jsh9870@naver.com>
2022-06-03 16:40:34 +0900

    ♻️[refactor]: HttpProvider, 로직 전면 수정
    HttpRequirements 타입 생성으로 인한 로직 변경 및 메서드 통합

--- OpenMarket/OpenMarket/Util/Endpoint.swift
+++ OpenMarket/OpenMarket/Util/Endpoint.swift
@@ -10,32 +10,41 @@ import Foundation
 enum Endpoint {
   case healthChecker
   case page(pageNumber: Int, itemsPerPage: Int)
-  case registration
+  case registration(params: Params, images: [ImageFile])
   case productInformation(productId: Int)
-  case secretKey(productId: Int)
+  case edit(productId: Int, params: Params)
+  case secretKey(productId: Int, secret: String)
   case delete(productId: Int, productSecret: String)
 }
 
 extension Endpoint {
   private enum Url {
     static let base = "https://market-training.yagom-academy.kr/"
-    static let pass = "api/products/"
+    static let path = "api/products/"
   }
   
   var url: URL? {
     switch self {
     case .healthChecker:
       return URL(string: Url.base + "healthChecker")
+      
     case .page(let page, let itemsPerPage):
-      return URL(string: Url.base + Url.pass + "?page_no=\(page)&items_perpage=\(itemsPerPage)")
+      return URL(string: Url.base + Url.path + "?page_no=\(page)&items_perpage=\(itemsPerPage)")
+      
     case .registration:
-      return URL(string: Url.base + Url.pass)
+      return URL(string: Url.base + Url.path)
+      
     case .productInformation(let productId):
-      return URL(string: Url.base + Url.pass + "\(productId)")
-    case .secretKey(let productId):
-      return URL(string: Url.base + Url.pass + "\(productId)/secret")
+      return URL(string: Url.base + Url.path + "\(productId)")
+      
+    case .edit(let productId,_):
+      return URL(string: Url.base + Url.path + "\(productId)")
+      
+    case .secretKey(let productId,_):
+      return URL(string: Url.base + Url.path + "\(productId)/secret")
+      
     case .delete(let productId, let productSecret):
-      return URL(string: Url.base + Url.pass + "\(productId)/\(productSecret)")
+      return URL(string: Url.base + Url.path + "\(productId)/\(productSecret)")
     }
   }
 }
--- OpenMarket/OpenMarket/Util/HttpProvider.swift
+++ OpenMarket/OpenMarket/Util/HttpProvider.swift
@@ -17,72 +17,53 @@ struct HttpProvider {
   init(session: URLSession = URLSession.shared) {
     self.session = session
   }
-  
-  func executeDataTask(
-    with request: URLRequest,
-    _ completionHandler: @escaping (Result<Data, NetworkError>) -> Void
-  ) {
-    session.dataTask(with: request) { data, response, error in
-      guard error == nil else {
-        completionHandler(.failure(.invalid))
-        return
-      }
-      guard let response = response as? HTTPURLResponse,
-            (200..<300).contains(response.statusCode)
-      else {
-        completionHandler(.failure(.statusCodeError))
-        return
-      }
-      guard let data = data else {
-        completionHandler(.failure(.invalid))
-        return
-      }
-      
-      completionHandler(.success(data))
-    }.resume()
-  }
-}
 
-extension HttpProvider {
-  func get(
-    _ endpoint: Endpoint,
+  func execute(
+    _ networkRequirements: HttpRequirements,
     completionHandler: @escaping (Result<Data, NetworkError>) -> Void
   ) {
-    guard let url = endpoint.url else {
+    guard let url = networkRequirements.endpoint.url else {
       completionHandler(.failure(.invalid))
       return
     }
     
-    var request = URLRequest(url: url)
-    request.httpMethod = HttpMethod.get
-    
-    executeDataTask(with: request, completionHandler)
-  }
-}
-
-extension HttpProvider {
-  func post(
-    _ endpoint: Endpoint,
-    _ params: Params,
-    _ images: [ImageFile],
-    completionHandler: @escaping (Result<Data, NetworkError>) -> Void
-  ) {
-    guard let url = endpoint.url else {
-      completionHandler(.failure(.invalid))
-      return
-    }
-    
-    var request = URLRequest(url: url)
     let boundary = "Boundary-\(UUID().uuidString)"
-    request.httpMethod = HttpMethod.post
-    request.setValue("8de44ec8-d1b8-11ec-9676-43acdce229f5", forHTTPHeaderField: "identifier")
-    request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
-    request.httpBody = setupBody(params, images, boundary)
+    var request = URLRequest(url: url)
+    request.httpMethod = networkRequirements.httpMethod.rawValue
+    
+    switch networkRequirements.endpoint {
+    case .healthChecker:
+      break
+      
+    case .page:
+      break
+      
+    case .registration(let params, let images):
+      request.setValue(UserInfo.identifier, forHTTPHeaderField: "identifier")
+      request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+      request.httpBody = setUpPostBody(params, images, boundary)
+      
+    case .productInformation:
+      break
+      
+    case .edit(_, let params):
+      request.setValue(UserInfo.identifier, forHTTPHeaderField: "identifier")
+      request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+      request.httpBody = setUpPatchBody(params)
+      
+    case .secretKey(_, let secret):
+      request.setValue(UserInfo.identifier, forHTTPHeaderField: "identifier")
+      request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+      request.httpBody = "{\"secret\": \"\(secret)\"}".data(using: .utf8)
+      
+    case .delete:
+      request.setValue(UserInfo.identifier, forHTTPHeaderField: "identifier")
+    }
     
     executeDataTask(with: request, completionHandler)
   }
   
-  func setupBody(_ params: Params, _ images: [ImageFile], _ boundary: String) -> Data? {
+  private func setUpPostBody(_ params: Params, _ images: [ImageFile], _ boundary: String) -> Data? {
     guard let jsonData = try? JSONEncoder().encode(params) else {
       return nil
     }
@@ -106,29 +87,8 @@ extension HttpProvider {
     
     return body
   }
-}
-
-extension HttpProvider {
-  func patch(
-    _ endpoint: Endpoint,
-    _ params: Params,
-    completionHandler: @escaping (Result<Data, NetworkError>) -> Void
-  ) {
-    guard let url = endpoint.url else {
-      completionHandler(.failure(.invalid))
-      return
-    }
-    
-    var request = URLRequest(url: url)
-    request.httpMethod = HttpMethod.patch
-    request.setValue("8de44ec8-d1b8-11ec-9676-43acdce229f5", forHTTPHeaderField: "identifier")
-    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.httpBody = setupBody(params)
-    
-    executeDataTask(with: request, completionHandler)
-  }
   
-  func setupBody(_ params: Params) -> Data? {
+  private func setUpPatchBody(_ params: Params) -> Data? {
     guard let jsonData = try? JSONEncoder().encode(params) else {
       return nil
     }
@@ -136,44 +96,29 @@ extension HttpProvider {
     body.append(jsonData)
     return body
   }
-}
-
-extension HttpProvider {
-  func searchSecret(
-    _ endpoint: Endpoint, _ secret: String,
-    completionHandler: @escaping (Result<Data, NetworkError>) -> Void
+  
+  private func executeDataTask(
+    with request: URLRequest,
+    _ completionHandler: @escaping (Result<Data, NetworkError>) -> Void
   ) {
-    guard let url = endpoint.url else {
-      completionHandler(.failure(.invalid))
-      return
-    }
-    
-    var request = URLRequest(url: url)
-    request.httpMethod = HttpMethod.post
-    request.setValue("8de44ec8-d1b8-11ec-9676-43acdce229f5", forHTTPHeaderField: "identifier")
-    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.httpBody = "{\"secret\": \"\(secret)\"}".data(using: .utf8)
-    
-    executeDataTask(with: request, completionHandler)
+    session.dataTask(with: request) { data, response, error in
+      guard error == nil else {
+        completionHandler(.failure(.invalid))
+        return
+      }
+      guard let response = response as? HTTPURLResponse,
+            (200..<300).contains(response.statusCode)
+      else {
+        completionHandler(.failure(.statusCodeError))
+        return
+      }
+      guard let data = data else {
+        completionHandler(.failure(.invalid))
+        return
+      }
+      
+      completionHandler(.success(data))
+    }.resume()
   }
 }
-
-extension HttpProvider {
-  func delete(
-    _ endpoint: Endpoint,
-    completionHandler: @escaping (Result<Data, NetworkError>) -> Void
-  ) {
-    guard let url = endpoint.url else {
-      completionHandler(.failure(.invalid))
-      return
-    }
-    
-    var request = URLRequest(url: url)
-    request.httpMethod = HttpMethod.delete
-    request.setValue("8de44ec8-d1b8-11ec-9676-43acdce229f5", forHTTPHeaderField: "identifier")
-    
-    executeDataTask(with: request, completionHandler)
-  }
-}
-
 
