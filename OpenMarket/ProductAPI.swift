425980b00f3971cddcfe4588db0fc03604994b0c
derrickkim0109 <derrickkim0109@naver.com>
2022-07-11 17:56:08 +0900

    feat: "ProductAPI" 서버 API 기능 구현

--- OpenMarket/ProductAPI.swift
+++ OpenMarket/ProductAPI.swift
@@ -22,4 +22,25 @@ struct ProductAPI {
             return .failure(APIError.failedToDecode)
         }
     }
+
+    func call<T: Decodable>(_ urlString: String, for type: T.Type, completion: @escaping (Result<T, APIError>) -> Void) {
+        guard let url = URL(string: urlString) else {
+            completion(.failure(APIError.failedToDecode))
+            return
+        }
+
+        let requestURL = URLRequest(url: url)
+
+        let task = URLSession.shared.dataTask(with: requestURL) { (data, _, _) in
+            guard let data = data,
+                  let decodedData = try? JSONDecoder().decode(T.self, from: data) else {
+                completion(.failure(APIError.failedToDecode))
+                return
+            }
+
+            completion(.success(decodedData))
+        }
+
+        task.resume()
+    }
 }
