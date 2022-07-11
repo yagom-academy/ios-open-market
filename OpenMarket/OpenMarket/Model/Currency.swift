1228ba33ae55397d3730f88eb9f0d06f943f5f0f
yusw10 <yusw10@naver.com>
2022-07-11 16:24:12 +0900

    feat: "Product" 구조체 및 Currency enum 구현
    - 개별 상품 정보를 표현하기 위해 "Product" 데이터 타입 구현
    - "Product" 데이터 타입에 Codingkey 및 Codable 프로토콜 채택
    - currency 열거형을 구현하여 Product의 currency 프로퍼티에서 사용

---
+++ OpenMarket/OpenMarket/Model/Currency.swift
@@ -0,0 +1,12 @@
+//
+//  Currency.swift
+//  OpenMarket
+//
+//  Created by 웡빙, 보리사랑 on 2022/07/11.
+//
+import Foundation
+
+enum Currency: String, Codable {
+    case krw = "KRW"
+    case usd = "USD"
+}
