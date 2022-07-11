c93652e57daedf3f1334ea8f842177daddbfe0c6
finnn1 <tgss3535@gmail.com>
2022-07-11 21:07:33 +0900

    refactor: 불필요한 메서드 삭제

--- OpenMarket/OpenMarket/Models/ProductsDataManager.swift
+++ OpenMarket/OpenMarket/Models/ProductsDataManager.swift
@@ -30,34 +30,4 @@ struct ProductsDataManager {
         }
         task.resume()
     }
-    
-    func getData(productId: Int, completion: @escaping (Result<Page, Error>) -> ()) {
-        
-        guard let url = URL(string: "https://market-training.yagom-academy.kr/api/products/\(productId)") else { return }
-        
-        let request = URLRequest(url: url)
-        
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-            
-            guard let data = data else {
-                let decodingContext = DecodingError.Context.init(codingPath: Page.CodingKeys.allCases, debugDescription: "")
-                completion(.failure(DecodingError.dataCorrupted(decodingContext)))
-                return
-            }
-            
-            guard let decodedData = try? JSONDecoder().decode(Page.self, from: data) else {
-                let decodingContext = DecodingError.Context.init(codingPath: Page.CodingKeys.allCases, debugDescription: "")
-                completion(.failure(DecodingError.typeMismatch(Page.self, decodingContext)))
-                return
-            }
-            
-            completion(.success(decodedData))
-        }
-        task.resume()
-    }
 }
