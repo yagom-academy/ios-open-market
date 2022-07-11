9cde39dd3d3d91a09cbca51139ed059912b5197d
ZZBAE <jebyun2@gmail.com>
2022-07-11 16:39:24 +0900

    feat: Product 타입 구현
    - Resource, Sources, Models, Data 파일 구분
    - Product json 데이터 추가
    - json 데이터 Model 타입 지정

---
+++ OpenMarket/OpenMarket/Models/Product.swift
@@ -0,0 +1,36 @@
+//
+// /Users/zzbae/Desktop/Yagom Career Starter Camp 6기/ios-open-market/OpenMarket/OpenMarket File.swift
+//  OpenMarket
+//
+//  Created by 주디, 재재 on 2022/07/11.
+//
+
+import Foundation
+
+struct Product: Codable {
+    let id: Int
+    let vendorId: Int
+    let name: String
+    let thumbnail: String
+    let currency: String
+    let price: Int
+    let bargainPrice: Int
+    let discountedPrice: Int
+    let stock: Int
+    let createdAt: String
+    let issuedAt: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case vendorId = "vendor_id"
+        case name
+        case thumbnail
+        case currency
+        case price
+        case bargainPrice = "bargain_price"
+        case discountedPrice = "discounted_price"
+        case stock
+        case createdAt = "created_at"
+        case issuedAt = "issued_at"
+    }
+}
