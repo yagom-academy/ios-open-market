5c0e26c2ea721f6b7d704ac592842c08f093abb6
Judy <joo00ee@naver.com>
2022-07-12 13:01:26 +0900

    feat: URLSessionProvider에 dataTask와 receivePage 메서드 구현
    - dataTask : session의 dataTask를 통해 서버에서 데이터를 받는 메서드
    - receivePage : dataTask 메서드를 통해 특정한 Page 데이터를 받는 메서드
    - DataTaskError 생성

--- OpenMarket/OpenMarket/Models/URLSessionProvider.swift
+++ OpenMarket/OpenMarket/Models/URLSessionProvider.swift
@@ -7,11 +7,43 @@
 
 import Foundation
 
+enum DataTaskError: Error {
+    case incorrectResponseError
+    case invalidDataError
+}
+
 struct URLSessionProvider {
     let session: URLSessionProtocol
     let baseURL = "https://market-training.yagom-academy.kr/"
     
     init(session: URLSessionProtocol) {
         self.session = session
+    }
+    
+    func dataTask(request: URLRequest, completionHandler: @escaping (Result<Data, DataTaskError>) -> Void) {
+        let task = session.dataTask(with: request) { data, urlResponse, error in
+            guard let response = urlResponse as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
+                return completionHandler(.failure(.incorrectResponseError))
+            }
+            
+            guard let data = data else {
+                return completionHandler(.failure(.invalidDataError))
+            }
+            
+            return completionHandler(.success(data))
+        }
+        
+        task.resume()
+    }
+    
+    func receivePage(number: Int, countOfItems: Int, completionHandler: @escaping (Result<Data, DataTaskError>) -> Void) {
+        let pageUrl = baseURL + "api/products?page_no=\(number)&items_per_page=\(countOfItems)"
+        
+        guard let url = URL(string: pageUrl) else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        dataTask(request: request, completionHandler: completionHandler)
     }
 }
