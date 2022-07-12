56bb92c82df8f85ce0c68e4cc42919b1ede79f10
KayAhnDS <dkseotn123@gmail.com>
2022-07-12 20:54:03 +0900

    feat: URLData 클래스 추가

---
+++ OpenMarket/OpenMarket/Model/URLData.swift
@@ -0,0 +1,39 @@
+//
+//  URLData.swift
+//  OpenMarket
+//
+//  Created by 케이, 수꿍 on 2022/07/12.
+//
+
+import Foundation
+
+class URLData {
+    var session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func fetchData<T: Codable>(url: URL, dataType: T.Type, completion: @escaping (Result<T,Error>) -> Void) {
+        let dataTask: URLSessionDataTask = session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse,
+               (200..<300).contains(response.statusCode),
+               let verifiedData = data {
+                do {
+                    let decodedData = try JSONDecoder().decode(T.self, from: verifiedData)
+                    completion(.success(decodedData))
+                } catch {
+                    completion(.failure(NetworkError.unableToParse))
+                }
+            } else {
+                completion(.failure(NetworkError.serverSideProblem))
+            }
+        }
+        dataTask.resume()
+    }
+}
---
+++ OpenMarket/OpenMarket/NetworkError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkError.swift
+//  OpenMarket
+//
+//  Created by 케이, 수꿍 on 2022/07/12.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case unableToParse
+    case serverSideProblem
+}
