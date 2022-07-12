e322b0b0f68308aa3059c4fc8e43723c39111814
seohyeon2 <seo_hyeon418@naver.com>
2022-07-12 20:24:53 +0900

    feat: MockURLSessionDataTask 타입 구현

---
+++ OpenMarket/OpenMarket/Model/MockURLSessionDataTask.swift
@@ -0,0 +1,20 @@
+//
+//  MockURLSessionDataTask.swift
+//  OpenMarket
+//
+//  Created by unchain, hyeon2 on 2022/07/12.
+//
+
+import Foundation
+
+class MockURLSessionDataTask: URLSessionDataTask {
+    private let resumeHandler:() -> Void
+
+    init(resumeHandler: @escaping () -> Void) {
+        self.resumeHandler = resumeHandler
+    }
+
+    override func resume() {
+        resumeHandler()
+    }
+}
