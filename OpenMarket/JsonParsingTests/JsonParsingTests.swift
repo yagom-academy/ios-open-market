b16ff2eda0316c79f8b50fca5560687a51ca394a
apwierk2451 <apwierk2451@gmail.com>
2022-07-12 15:00:36 +0900

    refactor: test할 때 return의 경우 test가 성공되는 문제 수정
    - 강제 언래핑 제거
    - products.json파일에 타겟 추가

--- OpenMarket/JsonParsingTests/JsonParsingTests.swift
+++ OpenMarket/JsonParsingTests/JsonParsingTests.swift
@@ -6,41 +6,41 @@ class JsonParsingTests: XCTestCase {
     override func setUpWithError() throws {
         
     }
-
+    
     override func tearDownWithError() throws {
         
     }
-
+    
     func test_데이터를파싱했을때_pageNo값이잘들어오는지() {
         // given
-        guard let fileLocation = Bundle.main.url(forResource: "products", withExtension: "json"),
-              let data = try? Data(contentsOf: fileLocation) else { return }
+        guard let fileLocation = Bundle.main.url(forResource: "products", withExtension: "json"), let data = try? Data(contentsOf: fileLocation) else { XCTFail(); return }
         
         let expression = 1
         // when
-        let parsedData = try! JSONDecoder().decode(Products.self, from: data)
+        guard let parsedData = try? JSONDecoder().decode(Products.self, from: data) else { XCTFail(); return }
         // then
         XCTAssertEqual(parsedData.pageNo, expression)
     }
     
     func test_데이터를파싱했을때_pages값이잘들어오는지1() {
         // given
-        guard let fileLocation = Bundle.main.url(forResource: "products", withExtension: "json"),
-              let data = try? Data(contentsOf: fileLocation) else { return }
+        guard let fileLocation = Bundle.main.url(forResource: "products", withExtension: "json"), let data = try? Data(contentsOf: fileLocation) else { XCTFail(); return }
+        
         let expression = 20
         // when
-        let parsedData = try! JSONDecoder().decode(Products.self, from: data)
+        guard let parsedData = try? JSONDecoder().decode(Products.self, from: data) else { XCTFail(); return }
         // then
         XCTAssertEqual(parsedData.pages[0].id, expression)
     }
-
+    
     func test_데이터를파싱했을때_pages값이잘들어오는지2() {
         // given
-        guard let fileLocation = Bundle.main.url(forResource: "products", withExtension: "json"),
-              let data = try? Data(contentsOf: fileLocation) else { return }
+        guard let fileLocation = Bundle.main.url(forResource: "products", withExtension: "json"), let data = try? Data(contentsOf: fileLocation) else { XCTFail(); return }
+        
+        
         let expression = "2022-01-04T00:00:00.00"
         // when
-        let parsedData = try! JSONDecoder().decode(Products.self, from: data)
+        guard let parsedData = try? JSONDecoder().decode(Products.self, from: data) else { XCTFail(); return }
         // then
         XCTAssertEqual(parsedData.pages[0].issuedAt, expression)
     }
