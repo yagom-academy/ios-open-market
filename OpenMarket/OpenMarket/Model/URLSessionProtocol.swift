1032f00671871e82b17dd782dfae0de600436049
seohyeon2 <seo_hyeon418@naver.com>
2022-07-12 20:19:57 +0900

    refactor: URLSessionProtocl 타입에 url 매개변수 추가

--- OpenMarket/OpenMarket/Model/URLSessionProtocol.swift
+++ OpenMarket/OpenMarket/Model/URLSessionProtocol.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 protocol URLSessionProtocol {
-    func dataTask(with request: URLRequest,
+    func dataTask(with url: URL,
                       completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
 }
 
