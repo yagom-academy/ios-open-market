e51d8827a822f3b1c8d29d81a6151523280fee50
apwierk2451 <apwierk2451@gmail.com>
2022-07-13 18:07:20 +0900

    feat: CollectionView 생성

--- OpenMarket/OpenMarket/Controllers/ViewController.swift
+++ OpenMarket/OpenMarket/Controllers/ViewController.swift
@@ -1,15 +1,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     var segmentControl: UISegmentedControl?
-    
-    override func loadView() {
-        view = MainView()
-    }
+    var collectionView: UICollectionView?
+    var dataSource: UICollectionViewDiffableDataSource<Section, Int>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureHierarchy()
+        configureDataSoure()
         configureSegmentControl()
     }
 }
@@ -18,6 +18,75 @@ extension ViewController {
     private func configureSegmentControl() {
         segmentControl = UISegmentedControl(items: Titles.toString)
         segmentControl?.selectedSegmentIndex = Titles.LIST.rawValue
+        segmentControl?.addTarget(self, action: #selector(changeLayout), for: .valueChanged)
         navigationItem.titleView = segmentControl
+    }
+}
+
+extension ViewController {
+    private func createGridLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.2), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.2))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        group.interItemSpacing = .fixed(10)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 10
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+    
+    private func createListLayout() -> UICollectionViewLayout {
+        let config = UICollectionLayoutListConfiguration(appearance: .plain)
+        return UICollectionViewCompositionalLayout.list(using: config)
+    }
+}
+
+extension ViewController {
+    private func configureHierarchy() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createListLayout())
+        collectionView?.backgroundColor = .white
+        collectionView?.delegate = self
+        
+        guard let collectionView = collectionView else { return }
+        
+        view.addSubview(collectionView)
+    }
+    
+    private func configureDataSoure() {
+        let cellRegistration = UICollectionView.CellRegistration<ItemCollectionViewCell, Int> { (cell, indexPath, identifier) in
+            cell.backgroundColor = .yellow
+        }
+        
+        guard let collectionView = collectionView else { return }
+        
+        dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: collectionView) { (collectionView, indexPath, identifier) -> UICollectionViewCell? in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: identifier)
+        }
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Int>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(Array(0...94))
+        dataSource?.apply(snapshot, animatingDifferences: false)
+    }
+    
+}
+
+extension ViewController {
+    @objc private func changeLayout() {
+        if segmentControl?.selectedSegmentIndex == Titles.LIST.rawValue {
+            self.collectionView?.setCollectionViewLayout(createListLayout(), animated: true)
+        } else if segmentControl?.selectedSegmentIndex == Titles.GRID.rawValue {
+            self.collectionView?.setCollectionViewLayout(createGridLayout(), animated: true)
+        }
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath)
     }
 }
