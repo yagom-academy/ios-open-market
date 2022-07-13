b723a896161208d9e7f1b031f53f9300e9b18cc1
finnn1 <tgss3535@gmail.com>
2022-07-13 16:28:55 +0900

    feat: 네비게이션바에 Segment Control 추가

--- OpenMarket/OpenMarket/Controllers/ViewController.swift
+++ OpenMarket/OpenMarket/Controllers/ViewController.swift
@@ -1,12 +1,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var segmentControl: UISegmentedControl?
+    
     override func loadView() {
         view = MainView()
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureSegmentControl()
     }
 }
 
+extension ViewController {
+    private func configureSegmentControl() {
+        segmentControl = UISegmentedControl(items: Titles.toString)
+        segmentControl?.selectedSegmentIndex = Titles.LIST.rawValue
+        navigationItem.titleView = segmentControl
+    }
+}
