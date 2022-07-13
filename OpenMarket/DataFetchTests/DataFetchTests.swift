ed135bf6ca972e52b94bc471ac468462b14635e6
yusw10 <yusw10@naver.com>
2022-07-13 12:06:52 +0900

    test: decode 메서드를 검증하기 위한 테스트 추가
    - 타입이 다른 mock data의 추가와 이를 테스트하기위한 테스트코드 작성
    - 잘못된 키를 가진 mock data의 추가와 이를 테스트하기위한 테스트코드 작성
    - value의 타입이 잘못된 mock data의 추가와 이를 테스트하기위한 테스트코드 작성
    - 손상된 mock data의 추가와 이를 테스트하기위한 테스트코드 작성

--- OpenMarket/DataFetchTests/DataFetchTests.swift
+++ OpenMarket/DataFetchTests/DataFetchTests.swift
@@ -30,4 +30,60 @@ class DataFetchTests: XCTestCase {
         //then
         XCTAssertNotNil(result)
     }
+    func test_타입이다른mock데이터에대해서_decode의결과가_리턴이_nil인지테스트() throws {
+        //given
+        guard let filePath = NSDataAsset.init(name: "DiffTypeMockData") else {
+            XCTAssertNotNil(nil)
+            return
+        }
+        //when
+        guard let result = decode(from: filePath.data, to: ProductPage.self) else {
+            XCTAssertNil(nil)
+            return
+        }
+        //then
+        XCTAssertNotNil(result)
+    }
+    func test_잘못된키를가진mock데이터에대해서_decode의결과가_리턴이_nil인지테스트() throws {
+        //given
+        guard let filePath = NSDataAsset.init(name: "WrongKeyMockData") else {
+            XCTAssertNotNil(nil)
+            return
+        }
+        //when
+        guard let result = decode(from: filePath.data, to: ProductPage.self) else {
+            XCTAssertNil(nil)
+            return
+        }
+        //then
+        XCTAssertNotNil(result)
+    }
+    func test_Value의타입이다른mock데이터에대해서_decode의결과가_리턴이_nil인지테스트() throws {
+        //given
+        guard let filePath = NSDataAsset.init(name: "WrongValueTypeMockData") else {
+            XCTAssertNotNil(nil)
+            return
+        }
+        //when
+        guard let result = decode(from: filePath.data, to: ProductPage.self) else {
+            XCTAssertNil(nil)
+            return
+        }
+        //then
+        XCTAssertNotNil(result)
+    }
+    func test_데이터가손상된mock데이터에대해서_decode의결과가_리턴이_nil인지테스트() throws {
+        //given
+        guard let filePath = NSDataAsset.init(name: "CorruptedMockData") else {
+            XCTAssertNotNil(nil)
+            return
+        }
+        //when
+        guard let result = decode(from: filePath.data, to: ProductPage.self) else {
+            XCTAssertNil(nil)
+            return
+        }
+        //then
+        XCTAssertNotNil(result)
+    }
 }
