0de2fd6c6280e4b6e2eae5903f80c50206d51528
KayAhnDS <dkseotn123@gmail.com>
2022-07-13 12:52:40 +0900

    refactor: Webpage 구조체 내 equatable 프로토콜 제거

--- OpenMarket/OpenMarket/Model/WebPage.swift
+++ OpenMarket/OpenMarket/Model/WebPage.swift
@@ -5,11 +5,7 @@
 //  Created by 케이, 수꿍 on 2022/07/11.
 //
 
-struct WebPage: Codable, Equatable {
-    static func == (lhs: WebPage, rhs: WebPage) -> Bool {
-        return type(of: lhs) == type(of: rhs)
-    }
-    
+struct WebPage: Codable {
     let pageNumber: Int
     let itemsPerPage: Int
     let totalCount: Int
