6c8f2a4906b9c2a9dfb3569d6ffbc4ed05473db7
MinsuJeon <chso294@naver.com>
2022-07-14 22:50:43 +0900

    test: 네이밍 변경 및 함수 내부 기능 직접 구현

--- OpenMarket/MockTests/MockTests.swift
+++ OpenMarket/MockTests/MockTests.swift
@@ -24,11 +24,18 @@ class MockTests: XCTestCase {
     
     func test_fetchData_메서드가_status_code가_200일때_mock_data를_예상한_값과_동일하게_가져오는지_테스트() throws {
         // given
-        let expectation: ProductList? = JSONData.decode(fileName: "Mock", fileExtension: "json", dataType: ProductList.self)
+        guard let path = Bundle.main.path(forResource: "Mock", ofType: "json"),
+              let jsonString = try? String(contentsOfFile: path),
+              let data = jsonString.data(using: .utf8) else {
+            return
+        }
+        
+        let decodedData = try? JSONDecoder().decode(ProductList.self, from: data)
+        
+        let expectation: ProductList? = decodedData
         
         let url = "https://market-training.yagom-academy.kr/"
         let mockResponse: MockURLSession.Response = {
-            let data = JSONData.parse(fileName: "Mock", fileExtension: "json")
             let successResponse = HTTPURLResponse(url: URL(string: url)!, statusCode: 200, httpVersion: nil, headerFields: nil)
             return (data: data, urlResponse: successResponse, error: nil)
         }()
@@ -38,7 +45,7 @@ class MockTests: XCTestCase {
         sut = NetworkProvider(session: mockURLSession)
         
         // when
-        sut.fetchData(url: URL(string: url)!, dataType: ProductList.self) { response in
+        sut.requestAndDecode(url: URL(string: url)!, dataType: ProductList.self) { response in
             if case let .success(market) = response {
                 result = market
             }
@@ -48,29 +55,34 @@ class MockTests: XCTestCase {
         XCTAssertEqual(expectation?.pages.count, result?.pages.count)
     }
     
-    func test_fetchData_메서드가_status_code가_500일때_mock_data를_가져오는데_실패하는지_테스트() throws {
+    func test_fetchData_메서드가_status_code가_500일때_networkConnectionIsBad_errorDescription을_반환하는지_테스트() throws {
         // given
-        let expectation: ProductList? = JSONData.decode(fileName: "Mock", fileExtension: "json", dataType: ProductList.self)
+        let expectation = NetworkError.networkConnectionIsBad.errorDescription
         
         let url = "https://market-training.yagom-academy.kr/"
-        let mockResponse: MockURLSession.Response = {
-            let data = JSONData.parse(fileName: "Mock", fileExtension: "json")
+        let mockResponse: MockURLSession.Response? = {
+            guard let path = Bundle.main.path(forResource: "Mock", ofType: "json"),
+                  let jsonString = try? String(contentsOfFile: path),
+                  let data = jsonString.data(using: .utf8) else {
+                return nil
+            }
+            
             let successResponse = HTTPURLResponse(url: URL(string: url)!, statusCode: 500, httpVersion: nil, headerFields: nil)
             return (data: data, urlResponse: successResponse, error: nil)
         }()
         
-        var result: ProductList?
         let mockURLSession = MockURLSession(response: mockResponse!)
         sut = NetworkProvider(session: mockURLSession)
         
         // when
-        sut.fetchData(url: URL(string: url)!, dataType: ProductList.self) { response in
-            if case let .success(market) = response {
-                result = market
+        sut.requestAndDecode(url: URL(string: url)!, dataType: ProductList.self) { response in
+            switch response {
+            case .success(_):
+                break
+            case .failure(let error):
+                // then
+                XCTAssertEqual(expectation, error.errorDescription)
             }
         }
-        
-        // then
-        XCTAssertNotEqual(expectation?.pages.count, result?.pages.count)
     }
 }
