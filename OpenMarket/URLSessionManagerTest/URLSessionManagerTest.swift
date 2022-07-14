4b35424df19f1371e407981f02a4a8f267713dde
Judy <joo00ee@naver.com>
2022-07-14 19:27:38 +0900

    refactor: Mock 객체를 Unit Test 타켓에 위치하도록 변경

--- OpenMarket/URLSessionManagerTest/URLSessionManagerTest.swift
+++ OpenMarket/URLSessionManagerTest/URLSessionManagerTest.swift
@@ -9,47 +9,7 @@ import XCTest
 
 @testable import OpenMarket
 
-final class MockURLSessionDataTask: URLSessionDataTaskProtocol {
-    var newResume: () -> Void = {}
-    
-    func resume() {
-        newResume()
-    }
-}
-
-final class MockURLSession: URLSessionProtocol {
-    var isSuccess: Bool
-    
-    init(isSuccess: Bool) {
-        self.isSuccess = isSuccess
-    }
-    
-    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol {
-        var success: HTTPURLResponse?
-        var failure: HTTPURLResponse?
-        
-        if let url = request.url {
-            success = HTTPURLResponse(url: url, statusCode: 200, httpVersion: "2.0", headerFields: nil)
-            failure = HTTPURLResponse(url: url, statusCode: 404, httpVersion: "2.0", headerFields: nil)
-        }
-        
-        let mockURLSessionDataTask = MockURLSessionDataTask()
-        
-        if isSuccess {
-            let mockData = NSDataAsset.init(name: "MockData")?.data
-            mockURLSessionDataTask.newResume = {
-                completionHandler(mockData, success, nil)
-            }
-        } else {
-            mockURLSessionDataTask.newResume = {
-                completionHandler(nil, failure, nil)
-            }
-        }
-        return mockURLSessionDataTask
-    }
-}
-
-class MockURLSessionTests: XCTestCase {
+class URLSessionManagerTests: XCTestCase {
     let dataDecoder = DataDecoder()
     
     override func setUpWithError() throws {
