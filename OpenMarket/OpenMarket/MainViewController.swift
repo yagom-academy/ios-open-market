c8f2fabcff5c7865c939541c7f8cbc731b354800
yusw10 <yusw10@naver.com>
2022-07-14 16:12:52 +0900

    feat: MainViewController 생성 및 기본 view 선언
    - SceneDelegate를 통해 코드로 프로그램이 시작하게 선언
    - MainStroyBoard 삭제
    - SegmentController 및 교체될 뷰 두개 선언

---
+++ OpenMarket/OpenMarket/MainViewController.swift
@@ -0,0 +1,46 @@
+//
+//  MainViewController.swift
+//  OpenMarket
+//
+//  Created by 웡빙, 보리사랑 on 2022/07/14.
+//
+/*
+ 1. 세그
+ 2. 테이블뷰컨트롤러 설정
+ 3. 콜렉션뷰 설정
+ */
+import UIKit
+
+class MainViewController: UIViewController {
+    // MARK: - Properties
+    private let segmantController: UISegmentedControl = {
+        let segmentController = UISegmentedControl(items: ["Table", "Grid"])
+        segmentController.translatesAutoresizingMaskIntoConstraints = true
+        return segmentController
+    }()
+    
+    private let firstView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .red
+        return view
+    }()
+    
+    private let secondView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .blue
+        return view
+    }()
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addUIComponents()
+    }
+    private func addUIComponents() {
+        self.navigationController?.navigationItem.titleView = segmantController
+        self.view.addSubview(firstView)
+        self.view.addSubview(secondView)
+    }
+}
