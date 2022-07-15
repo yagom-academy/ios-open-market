6cf1e66ebf0a76a880ba5238ab61addee7cbed7e
yusw10 <yusw10@naver.com>
2022-07-15 10:57:55 +0900

    feat: Modern Collection View List Layout 구현
    - Product 타입이 Hashable을 채택하도록 수정
    - Modern Collecion View을 기본 리스트 타입으로 사용하기 위해 Layout과 dataSource 및 셀 생성 구현
    - 데이터 통신을 하기 위해 NetworkManager를 프로퍼티로 생성

--- OpenMarket/OpenMarket/MainViewController.swift
+++ OpenMarket/OpenMarket/MainViewController.swift
@@ -7,10 +7,20 @@
 import UIKit
 
 class MainViewController: UIViewController {
-    // MARK: - Properties
+    // MARK: - Instance Properties
+    private let manager = NetworkManager()
+    
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Product>?
+    
+    enum Section {
+        case main
+    }
+    
+    // MARK: - UI Properties
     private var shouldHideListLayout: Bool? {
         didSet {
             guard let shouldHideListLayout = shouldHideListLayout else { return }
+            print("DID TAPPED SEGMENT CONTROLLER")
         }
     }
     
@@ -21,12 +31,16 @@ class MainViewController: UIViewController {
         return segmentController
     }()
     
+    private var collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+    
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
         addUIComponents()
         setupSegment()
+        configureHierarchy()
+        configureDataSource()
     }
     
     private func addUIComponents() {
@@ -40,5 +54,56 @@ class MainViewController: UIViewController {
     private func setupSegment() {
         didChangeValue(segment: self.segmentController)
         self.segmentController.addTarget(self, action: #selector(didChangeValue(segment:)), for: .valueChanged)
+    }
+}
+// MARK: - Modern Collection View List Style
+extension MainViewController {
+    private func createLayout() -> UICollectionViewLayout {
+        let config = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        return UICollectionViewCompositionalLayout.list(using: config)
+    }
+}
+
+extension MainViewController {
+    private func configureHierarchy(){
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.view.addSubview(collectionView)
+        collectionView.delegate = self
+    }
+    private func configureDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Product> { (cell, indexPath, product) in
+            var content = cell.defaultContentConfiguration()
+            guard let url = URL(string: product.thumbnail) else {
+                return
+            }
+            do {
+                content.image = UIImage(data: try Data(contentsOf: url))
+                content.text = product.name
+                content.secondaryText = String(product.price)
+                cell.contentConfiguration = content
+            } catch {
+                print("ASDASD")
+            }
+        }
+        dataSource = UICollectionViewDiffableDataSource<Section, Product>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, identifier: Product) -> UICollectionViewCell? in
+            
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: identifier)
+        }
+        
+        // initial data
+        manager.dataTask { [weak self] productList in
+            var snapshot = NSDiffableDataSourceSnapshot<Section, Product>()
+            snapshot.appendSections([.main])
+            snapshot.appendItems(productList)
+            self?.dataSource?.apply(snapshot, animatingDifferences: false)
+        }
+    }
+}
+
+extension MainViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
     }
 }
