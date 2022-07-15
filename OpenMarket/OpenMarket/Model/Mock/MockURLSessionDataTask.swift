e09e5961521d2d136090cb2054523dadc49916e8
Hugh-github <tnghk4822@gmail.com>
2022-07-15 15:50:45 +0900

    refactor: 오류 수정

--- OpenMarket/OpenMarket/Model/Mock/MockURLSessionDataTask.swift
+++ OpenMarket/OpenMarket/Model/Mock/MockURLSessionDataTask.swift
@@ -11,6 +11,6 @@ final class MockURLSessionDataTask: URLSessionDataTask {
     var resumeDidCall: () -> Void = { }
     
     override func resume() {
-        resumeDidCall
+        resumeDidCall()
     }
 }
