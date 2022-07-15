a01bb977f66bfadd7f0c50a64c79c8e71aeff368
gjrjs31@naver.com <gjrjs31@naver.com>
2022-07-15 17:58:05 +0900

    feat: ColletionView 생성 및 Json data 연결 테스트

---
+++ OpenMarket/OpenMarket/CustomCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewCell.swift
+//  OpenMarket
+//
+//  Created by 허건 on 2022/07/15.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myLabel: UILabel!
+}
+
--- OpenMarket/OpenMarket/ViewController.swift
+++ OpenMarket/OpenMarket/ViewController.swift
@@ -8,35 +8,62 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var segmentSwitch: UISegmentedControl!
+    @IBOutlet weak var firstView: UIView!
+    @IBOutlet weak var secondView: UIView!
+    
     
     let jsonParser = JSONParser()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getProuctData()
     }
     
     @IBAction func switchView(_ sender: UISegmentedControl) {
         print(sender.selectedSegmentIndex)
         switch sender.selectedSegmentIndex {
         case 0:
-            segmentSwitch.backgroundColor = .yellow
+            // segmentSwitch.backgroundColor = .yellow
+            firstView.alpha = 1
+            secondView.alpha = 0
         case 1:
-            segmentSwitch.backgroundColor = .blue
+            // segmentSwitch.backgroundColor = .blue
+            firstView.alpha = 0
+            secondView.alpha = 1
         default:
             break
         }
+    }
+}
+
+class FirstViewController: UIViewController {
+    let jsonParser = JSONParser()
+    var productData: ProductListResponse?
+    
+    @IBOutlet weak var productCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        productCollectionView.delegate = self
+        productCollectionView.dataSource = self
+        
+        getProuctData()
+        print("--------------")
+        print(productData)
     }
     
     func getProuctData() {
         jsonParser.dataTask(by: URLCollection.productListInquery, completion: { (response) in
             switch response {
             case .success(let data):
-                self.getProuctList(data)
+                // print(data)
+                self.productData = data
+
             case .failure(let data):
                 print(data)
             }
         })
+        print(self.productData)
+
     }
     
     func getProuctList(_ data: ProductListResponse) {
@@ -56,5 +83,30 @@ class ViewController: UIViewController {
         print(data.pages[0].stock)
         print(data.pages[0].createdAt)
         print(data.pages[0].issuedAt)
+    }
+    
+}
+
+extension FirstViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCollectionViewCell
+        jsonParser.dataTask(by: URLCollection.productListInquery, completion: { (response) in
+            switch response {
+            case .success(let data):
+                // print(data)
+                DispatchQueue.main.async {
+                    self.productData = data
+                    cell.myLabel.text = "Hi Collection Label \(indexPath.row), \(self.productData?.pages[indexPath.row].name)"
+                }
+            case .failure(let data):
+                print(data)
+            }
+        })
+
+        return cell
     }
 }
