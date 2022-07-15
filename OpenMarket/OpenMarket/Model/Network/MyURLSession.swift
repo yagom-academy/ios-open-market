693f6c85c62fd752e69fd49a16bc6506f9eb234c
starting1231 <starting1231@naver.com>
2022-07-15 10:50:33 +0900

    style: 폴더순서 정리, 네이밍 수정

--- OpenMarket/OpenMarket/Model/Network/MyURLSession.swift
+++ OpenMarket/OpenMarket/Model/Network/MyURLSession.swift
@@ -10,36 +10,36 @@ import Foundation
 class MyURLSession: SessionProtocol {
     func dataTask<T: Codable>(with request: APIRequest,
                               completionHandler: @escaping (Result<T, Error>) -> Void) {
-        execute(with: request, completion: completionHandler)
+        execute(with: request, completionHandler: completionHandler)
     }
     
     func execute<T: Codable>(with request: APIRequest,
-                             completion: @escaping (Result<T, Error>) -> Void) {
+                             completionHandler: @escaping (Result<T, Error>) -> Void) {
         guard let request = request.urlRequest else {
-            completion(.failure(NetworkError.request))
+            completionHandler(.failure(NetworkError.request))
             return
         }
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             guard error == nil else {
-                completion(.failure(NetworkError.request))
+                completionHandler(.failure(NetworkError.request))
                 return
             }
             guard let response = response as? HTTPURLResponse,
                   200 <= response.statusCode, response.statusCode < 300
             else {
-                completion(.failure(NetworkError.response))
+                completionHandler(.failure(NetworkError.response))
                 return
             }
             guard let safeData = data else {
-                completion(.failure(NetworkError.invalidData))
+                completionHandler(.failure(NetworkError.invalidData))
                 return
             }
             guard let decodedData = try? JSONDecoder().decode(T.self, from: safeData)
             else {
-                completion(.failure(CodableError.decode))
+                completionHandler(.failure(CodableError.decode))
                 return
             }
-            completion(.success(decodedData))
+            completionHandler(.success(decodedData))
         }
         task.resume()
     }
