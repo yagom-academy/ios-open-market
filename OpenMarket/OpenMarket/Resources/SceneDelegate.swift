79bda4c9dcdf02bac8a9b0191a415869153c4015
starting1231 <starting1231@naver.com>
2022-07-17 17:22:40 +0900

    refactor: Main Storyboard 제거 및 OpenMarketViewController 연결

--- OpenMarket/OpenMarket/Resources/SceneDelegate.swift
+++ OpenMarket/OpenMarket/Resources/SceneDelegate.swift
@@ -12,8 +12,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene,
                willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
-       
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let rootViewController = OpenMarketViewController()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) { }
