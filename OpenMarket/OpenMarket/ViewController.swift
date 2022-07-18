33cff5c2258a4dfc2f0c9893d348764455c7ccbf
unchain123 <unchain123@kakao.com>
2022-07-18 21:12:45 +0900

    feat: getProductList메서드 생성

--- OpenMarket/OpenMarket/ViewController.swift
+++ OpenMarket/OpenMarket/ViewController.swift
@@ -10,10 +10,23 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getProductList(about: "3", "1")
+    }
+    
+    private func getProductList(about pageNumber: String, _ itemPerPage: String) {
+        let urlSession = URLSession(configuration: URLSessionConfiguration.default)
+        let networkManger = NetworkManager(session: urlSession)
+        let queryItems = OpenMarketRequest().createQuery(of: pageNumber, with: itemPerPage)
+        let request = OpenMarketRequest().requestProductList(queryItems: queryItems)
         
-        let address = NetworkNamespace.url.name
-        let pageNo = URLQueryItem(name: NetworkNamespace.pageNo.name, value: "1")
-        let itemsPerPage = URLQueryItem(name: NetworkNamespace.itemsPerPage.name, value: "10")
-        let queryItems = [pageNo, itemsPerPage]
+        networkManger.getProductInquiry(request: request) { result in
+            switch result {
+            case .success(let data):
+                guard let productList = try? JSONDecoder().decode(MarketInformation.self, from: data) else { return }
+                print(productList)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
