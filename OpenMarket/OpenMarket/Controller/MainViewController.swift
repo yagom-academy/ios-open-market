ac0734418c030e283278c033d0f9b03e4a0171fb
ic_6_user_28 <toskw@naver.com>
2022-07-19 15:09:34 +0900

    feat: simpleList 구현

--- OpenMarket/OpenMarket/Controller/MainViewController.swift
+++ OpenMarket/OpenMarket/Controller/MainViewController.swift
@@ -6,14 +6,23 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UICollectionViewDelegate {
+    
+    enum Section {
+        case main
+    }
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, Int>! = nil
     let segment = UISegmentedControl(items: ["List", "Grid"])
+    var listCollectionView: UICollectionView! = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         self.navigationItem.titleView = segment
         configureSegment()
+        configureHierarchy()
+        configureDataSource()
     }
 }
 
@@ -31,7 +40,53 @@ extension MainViewController {
         segment.addTarget(self, action: #selector(tapSegment), for: .valueChanged)
     }
     
-    @objc private func tapSegment() {
-        print("hello")
+    @objc private func tapSegment(sender: UISegmentedControl) {
+        let selection = sender.selectedSegmentIndex
+        switch selection {
+        case 0:
+            listCollectionView.isHidden = false
+        case 1:
+            listCollectionView.isHidden = true
+        default:
+            break
+        }
+    }
+    
+    private func createLayout() -> UICollectionViewLayout {
+        let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+        let layout = UICollectionViewCompositionalLayout.list(using: configuration)
+        
+        return layout
+    }
+    
+    private func configureHierarchy() {
+        listCollectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        listCollectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(listCollectionView)
+    }
+    
+    private func configureDataSource() {
+        
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Int> { (cell, indexPath, item) in
+            var content = cell.defaultContentConfiguration()
+            content.text = "\(item)"
+            cell.contentConfiguration = content
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: listCollectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
+            
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: identifier)
+        }
+        
+        // initial data
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Int>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(Array(0..<94))
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
     }
 }
