04f27eb07741f59e4d02b324122504ae37213d92
derrickkim0109 <derrickkim0109@naver.com>
2022-07-19 11:52:58 +0900

    feat: ProductEntity 생성
    - 서버 매핑 모델로부터 필요한 데이터만을 추출하는 Entity 생성

---
+++ OpenMarket/OpenMarket/Application/Domain/Model/ProductEntity.swift
@@ -0,0 +1,17 @@
+//
+//  ProductEntity.swift
+//  OpenMarket
+//
+//  Created by Derrick kim on 2022/07/19.
+//
+
+import UIKit
+
+struct ProductEntity: Hashable {
+    let id = UUID()
+    let thumbnailImage: UIImage
+    let name: String
+    let originalPrice: Int
+    let discountedPrice: Int
+    let stock: Int
+}
