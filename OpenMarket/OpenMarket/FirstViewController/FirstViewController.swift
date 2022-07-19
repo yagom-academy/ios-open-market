c684df97379becc6c6d1c04727ec9b56a1b677c9
gjrjs31@naver.com <gjrjs31@naver.com>
2022-07-19 17:24:17 +0900

    refactor : 저장 타입 변경([ ] -> String), imageView 최대 크기 설정

--- OpenMarket/OpenMarket/FirstViewController/FirstViewController.swift
+++ OpenMarket/OpenMarket/FirstViewController/FirstViewController.swift
@@ -7,18 +7,19 @@
 
 import UIKit
 
+@available(iOS 14.0, *)
 class FirstViewController: UIViewController {
     let jsonParser = JSONParser()
-    var productData: ProductListResponse?
-    
-    var dataStore: [ProductListResponse] = []
-    
     let URLSemaphore = DispatchSemaphore(value: 0)
-    
+    var productData: ProductListResponse?
+
     @IBOutlet weak var productCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let config = UICollectionLayoutListConfiguration(appearance: .plain)
+        productCollectionView.collectionViewLayout = UICollectionViewCompositionalLayout.list(using: config)
+        
         productCollectionView.delegate = self
         productCollectionView.dataSource = self
         self.setData()
@@ -29,7 +30,6 @@ class FirstViewController: UIViewController {
             switch response {
             case .success(let data):
                 self.productData = data
-                self.dataStore = [data]
                 self.URLSemaphore.signal()
             case .failure(let data):
                 print(data)
@@ -39,20 +39,24 @@ class FirstViewController: UIViewController {
     }
 }
 
+@available(iOS 14.0, *)
 extension FirstViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return dataStore[0].pages.count
+        guard let result = productData else { return 0 }
+        return result.itemsPerPage
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCollectionViewCell
-        
-        guard let imageURL: URL = URL(string: dataStore[0].pages[indexPath.row].thumbnail) else { return cell }
-        guard let imageData: Data = try? Data(contentsOf: imageURL) else { return cell }
+        guard let result = productData,
+              let imageURL: URL = URL(string: result.pages[indexPath.row].thumbnail),
+              let imageData: Data = try? Data(contentsOf: imageURL) else {
+            return cell
+        }
 
         cell.productImage.image = UIImage(data: imageData)
-        cell.productName.text = dataStore[0].pages[indexPath.row].name
-        cell.productPrice.text = "\(dataStore[0].pages[indexPath.row].price)"
+        cell.productName.text = result.pages[indexPath.row].name
+        cell.productPrice.text = "\(result.pages[indexPath.row].price)"
         
         return cell
     }
