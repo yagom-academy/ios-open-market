44c385f15976b7495216d45599ca7861f1bb1252
unchain123 <unchain123@kakao.com>
2022-07-20 10:48:17 +0900

    feat: collectionView구현

---
+++ OpenMarket/OpenMarket/MainViewController.swift
@@ -0,0 +1,71 @@
+//
+//  OpenMarket - ViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+class MainViewController: UIViewController {
+    // MARK: Properties
+    
+    lazy var collectionView: UICollectionView = {
+        let config = UICollectionLayoutListConfiguration(appearance: .plain)
+        let layout = UICollectionViewCompositionalLayout.list(using: config)
+        let collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        return collectionView
+    }()
+
+    let stackView: UIStackView = {
+       let stackView = UIStackView()
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(collectionView)
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor)
+        ])
+    }
+    
+    private func getProductList(about pageNumber: String, _ itemPerPage: String) {
+        let urlSession = URLSession(configuration: URLSessionConfiguration.default)
+        let networkManger = NetworkManager(session: urlSession)
+        let queryItems = OpenMarketRequest().createQuery(of: pageNumber, with: itemPerPage)
+        let request = OpenMarketRequest().requestProductList(queryItems: queryItems)
+        
+        networkManger.getProductInquiry(request: request) { result in
+            switch result {
+            case .success(let data):
+                guard let productList = try? JSONDecoder().decode(MarketInformation.self, from: data) else { return }
+                print(productList)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
+
+// MARK: Extension
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+}
