8aa0f7dddf465bcfbe0a631e4f1e382edb9b5579
BaekGom <jihunmm02@gmail.com>
2022-07-20 11:09:03 +0900

    feat : UILbel Padding 기능 구현

---
+++ OpenMarket/OpenMarket/PaddingLabel.swift
@@ -0,0 +1,26 @@
+//
+//  PaddingLabel.swift
+//  OpenMarket
+//
+//  Created by Baek on 2022/07/20.
+//
+
+import UIKit
+
+@IBDesignable class PaddingLabel: UILabel {
+
+    @IBInspectable var topInset: CGFloat = 0.0
+    @IBInspectable var bottomInset: CGFloat = 0.0
+    @IBInspectable var leftInset: CGFloat = 0.0
+    @IBInspectable var rightInset: CGFloat = 0.0
+    
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets.init(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
+        super.drawText(in: rect.inset(by: insets))
+    }
+
+    override var intrinsicContentSize: CGSize {
+    let size = super.intrinsicContentSize
+    return CGSize(width: size.width + leftInset + rightInset, height: size.height + topInset + bottomInset)
+    }
+}
