5b547cb36afc300b06cbbbef0132300eb47be8b6
KayAhnDS <dkseotn123@gmail.com>
2022-07-20 14:54:35 +0900

    refactor: MarketProductsViewController 구조 수정

---
+++ OpenMarket/OpenMarket/Application/Presentation/View/CustomCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CustomCollectionViewCell.swift
+//  OpenMarket
+//
+//  Created by Kay on 2022/07/20.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+}
--- OpenMarket/OpenMarket/Application/Presentation/ViewController/MarketProductsViewController.swift
+++ OpenMarket/OpenMarket/Application/Presentation/ViewController/MarketProductsViewController.swift
@@ -6,16 +6,18 @@
 
 import UIKit
 
-@available(iOS 14.0, *)
-
-class MarketProductsViewController: UIViewController {
-    let segmentedControl: UISegmentedControl = {
+final class MarketProductsViewController: UIViewController {
+    fileprivate enum Section {
+        case main
+    }
+    
+    private let segmentedControl: UISegmentedControl = {
         let control = UISegmentedControl(items: ["LIST", "GRID"])
         control.translatesAutoresizingMaskIntoConstraints = false
         return control
     }()
     
-    var shouldHideListView: Bool? {
+    private var shouldHideListView: Bool? {
         didSet {
             guard let shouldHideListView = self.shouldHideListView else {
                 return
@@ -26,9 +28,26 @@ class MarketProductsViewController: UIViewController {
         }
     }
     
-    var products: [Product] = []
-    var productsModel: [ProductEntity] = []
+    private var products: [Product] = []
+    private var productsModel: [ProductEntity] = []
     
+    private var listCollectionView: UICollectionView!
+    private var listDataSource: UICollectionViewDiffableDataSource<Section, ProductEntity>!
+    
+    private var gridCollectionView: UICollectionView!
+    private var gridDataSource: UICollectionViewDiffableDataSource<Section, ProductEntity>!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        setNavigationItems()
+        getData()
+    }
+}
+
+
+private extension MarketProductsViewController {
     func getData() {
         let url = "https://market-training.yagom-academy.kr/api/products?page_no=1&items_per_page=50"
         
@@ -41,121 +60,47 @@ class MarketProductsViewController: UIViewController {
                     let item = ProductEntity(thumbnailImage: product.thumbnailImage!, name: product.name, currency: product.currency, originalPrice: product.price, discountedPrice: product.bargainPrice, stock: product.stock)
                     self.productsModel.append(item)
                 }
-        
+                
                 DispatchQueue.main.async {
                     self.createGridCollectionView()
-                    self.configDataSource()
+                    self.configureGridDataSource()
                     self.gridCollectionView.isHidden = true
                     self.createListCollectionView()
                     self.configureListDataSource()
                 }
                 
-            default:
-                print("error")
+            case .failure(let error):
+                self.presentConfirmAlert(message: error.errorDescription ?? "")
             }
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        
-        makeSegmentedControl()
-        getData()
-        self.segmentedControl.addTarget(self, action: #selector(didChangeValue(segment:)), for: .valueChanged)
-        self.segmentedControl.selectedSegmentIndex = 0
+    func presentConfirmAlert(message: String) {
+        let alertController = UIAlertController(title: "알림", message: message, preferredStyle: .alert)
+        let confirmAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        alertController.addAction(confirmAction)
+        self.present(alertController, animated: false)
     }
     
-    @objc private func didChangeValue(segment: UISegmentedControl) {
+    @objc func didSegmentedControlTapped(_ segment: UISegmentedControl) {
         self.shouldHideListView = segment.selectedSegmentIndex != 0
     }
     
-    private func makeSegmentedControl() {
+    func setNavigationItems() {
         self.navigationItem.titleView = self.segmentedControl
         self.navigationItem.rightBarButtonItem  = UIBarButtonItem(title: "+", style: .plain, target: self, action: #selector(addTapped))
+        self.segmentedControl.addTarget(self, action: #selector(didSegmentedControlTapped(_:)), for: .valueChanged)
+        self.segmentedControl.selectedSegmentIndex = 0
     }
     
     @objc func addTapped() {
-            
-    }
-    
-    enum Section {
-        case main
-    }
-
-    var gridCollectionView: UICollectionView!
-    var dataSource: UICollectionViewDiffableDataSource<Section, ProductEntity>!
-
-    func createGridLayout() -> UICollectionViewCompositionalLayout{
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(self.view.frame.height * 0.3))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
-        let spacing = CGFloat(10)
-        group.interItemSpacing = .fixed(10)
-
-        let section = NSCollectionLayoutSection(group: group)
-        section.interGroupSpacing = spacing
-        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10)
-
-        let layout = UICollectionViewCompositionalLayout(section: section)
-
-        return layout
-    }
-
-    func createGridCollectionView() {
-        gridCollectionView = UICollectionView(frame: .zero, collectionViewLayout: createGridLayout())
         
-        view.addSubview(gridCollectionView)
-        gridCollectionView.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
-            gridCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            gridCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            gridCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            gridCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-        ])
     }
-    
-    @available(iOS 14.0, *)
-    func configDataSource() {
-        let cellRegistration = UICollectionView.CellRegistration<CollectionGridCell, ProductEntity> { cell, indexPath, item in
-            cell.layer.borderColor = UIColor.systemGray.cgColor
-            cell.layer.borderWidth = 1
-            cell.layer.cornerRadius = 10
+}
 
-            cell.config(item)
-        }
-
-        dataSource = UICollectionViewDiffableDataSource<Section, ProductEntity>(collectionView: gridCollectionView, cellProvider: { (collectionView, indexPath, itemIdentifier) -> UICollectionViewCell? in
-            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
-        })
-
-        var snapShot = NSDiffableDataSourceSnapshot<Section, ProductEntity>()
-        snapShot.appendSections([.main])
-        snapShot.appendItems(productsModel)
-        dataSource.apply(snapShot, animatingDifferences: true)
-    }
-    
-    enum Section2 {
-        case main
-    }
-
-    var listCollectionView: UICollectionView!
-    var listDataSource: UICollectionViewDiffableDataSource<Section2, ProductEntity>!
-
-    func createListLayout() -> UICollectionViewLayout {
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.2))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
-        let section = NSCollectionLayoutSection(group: group)
-        let layout = UICollectionViewCompositionalLayout(section: section)
-        return layout
-    }
-    
-    private func createListCollectionView() {
+// MARK: Collection View
+private extension MarketProductsViewController {
+    func createListCollectionView() {
         listCollectionView = UICollectionView(frame: .zero, collectionViewLayout: createListLayout())
         view.addSubview(listCollectionView)
         listCollectionView.translatesAutoresizingMaskIntoConstraints = false
@@ -168,23 +113,83 @@ class MarketProductsViewController: UIViewController {
         ])
     }
     
-    private func configureListDataSource() {
+    func createListLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.2))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
+        let section = NSCollectionLayoutSection(group: group)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+    
+    func configureListDataSource() {
         let cellRegistration = UICollectionView.CellRegistration<CollectionListViewCell, ProductEntity> { (cell, indexPath, item) in
             cell.config(item)
             cell.accessories = [.disclosureIndicator()]
         }
         
-        listDataSource = UICollectionViewDiffableDataSource<Section2, ProductEntity>(collectionView: listCollectionView) {
+        listDataSource = UICollectionViewDiffableDataSource<Section, ProductEntity>(collectionView: listCollectionView) {
             (collectionView: UICollectionView, indexPath: IndexPath, identifier: ProductEntity) -> UICollectionViewCell? in
             
             return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: identifier)
         }
-
-        var snapshot = NSDiffableDataSourceSnapshot<Section2, ProductEntity>()
-        snapshot.appendSections([.main])
-        snapshot.appendItems(productsModel)
-        listDataSource.apply(snapshot, animatingDifferences: true)
+        
+        applySnapShot(to: listDataSource)
+    }
+    
+    func createGridCollectionView() {
+        gridCollectionView = UICollectionView(frame: .zero, collectionViewLayout: createGridLayout())
+        
+        view.addSubview(gridCollectionView)
+        gridCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            gridCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            gridCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            gridCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            gridCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+    
+    func createGridLayout() -> UICollectionViewCompositionalLayout{
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(self.view.frame.height * 0.3))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+        let spacing = CGFloat(10)
+        group.interItemSpacing = .fixed(10)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = spacing
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
+    }
+    
+    func configureGridDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration<CollectionGridCell, ProductEntity> { cell, indexPath, item in
+            cell.layer.borderColor = UIColor.systemGray.cgColor
+            cell.layer.borderWidth = 1
+            cell.layer.cornerRadius = 10
+            
+            cell.config(item)
+        }
+        
+        gridDataSource = UICollectionViewDiffableDataSource<Section, ProductEntity>(collectionView: gridCollectionView, cellProvider: { (collectionView, indexPath, itemIdentifier) -> UICollectionViewCell? in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        })
+        
+        applySnapShot(to: gridDataSource)
+    }
+    
+    func applySnapShot(to dataSource: UICollectionViewDiffableDataSource<Section, ProductEntity>) {
+        var snapShot = NSDiffableDataSourceSnapshot<Section, ProductEntity>()
+        snapShot.appendSections([.main])
+        snapShot.appendItems(productsModel)
+        dataSource.apply(snapShot, animatingDifferences: true)
     }
 }
-
-
