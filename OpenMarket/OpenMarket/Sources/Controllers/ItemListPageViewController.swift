466c31f8013b15b311ed8211b63d46b9b333588f
minsson <mskaang@gmail.com>
2022-07-22 18:53:51 +0900

    feat: "ItemListPageVC" 상품 표시 방식을 결정할 세그먼트 컨트롤 UI구현
    - 상품 디스플레이 방식(LIST 방식, GRID 방식)을 선택하기 위함

--- OpenMarket/OpenMarket/Sources/Controllers/ItemListPageViewController.swift
+++ OpenMarket/OpenMarket/Sources/Controllers/ItemListPageViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 final class ItemListPageViewController: UIViewController {
     
     // MARK: - Properties
-    
+
     private var itemListPage: ItemListPage?
     
     private lazy var request = Path.products + queryString
     private let queryString = QueryCharacter.questionMark + QueryKey.pageNumber + QueryValue.pageNumber + QueryCharacter.ampersand + QueryKey.itemsPerPage + QueryValue.itemsPerPage
+    
+    // MARK: - UI Components
+    
+    private let segmentedControl: UISegmentedControl = {
+        let selectionItems = ["LIST", "GRID"]
+        let segmentedControl = UISegmentedControl(items: selectionItems)
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        return segmentedControl
+    }()
     
     // MARK: - Life Cycle
     
@@ -23,12 +33,22 @@ final class ItemListPageViewController: UIViewController {
         NetworkManager.performRequestToAPI(from: HostAPI.openMarket.url, with: request) { (result: Result<Data, NetworkingError>) in
             self.fetchParsedData(basedOn: result)
         }
+        
+        setUIComponentsLayout()
     }
 }
 
 // MARK: - Private Actions
 
 private extension ItemListPageViewController {
+    func setUIComponentsLayout() {
+        self.view.addSubview(segmentedControl)
+        
+        segmentedControl.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 8.0).isActive = true
+        segmentedControl.heightAnchor.constraint(equalToConstant: 30.0).isActive = true
+        segmentedControl.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+    }
+    
     func fetchParsedData(basedOn result: Result<Data, NetworkingError>) {
         switch result {
         case .success(let data):
