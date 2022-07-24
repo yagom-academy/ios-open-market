601f79fec4bcc2b1138569bb6ccefe942f7d8132
derrickkim0109 <derrickkim0109@naver.com>
2022-07-24 09:49:47 +0900

    refactor: ProductEntity 상위 모델 생성

--- OpenMarket/OpenMarket/Application/Domain/Model/ProductListEntity.swift
+++ OpenMarket/OpenMarket/Application/Domain/Model/ProductListEntity.swift
@@ -1,11 +1,15 @@
 //
-//  ProductEntity.swift
+//  ProductListEntity.swift
 //  OpenMarket
 //
 //  Created by 데릭, 케이, 수꿍. 
 //
 
 import UIKit
+
+struct ProductListEntity {
+    var productEntity: [ProductEntity]
+}
 
 struct ProductEntity: Hashable {
     let id = UUID()
