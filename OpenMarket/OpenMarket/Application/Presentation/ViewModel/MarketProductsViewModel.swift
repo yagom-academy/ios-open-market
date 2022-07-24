955916332b81c903bb753eae9f32eccd4b4796b5
derrickkim0109 <derrickkim0109@naver.com>
2022-07-24 10:01:10 +0900

    feat: MarketProductsViewModel을 통한 데이터 변형

--- OpenMarket/OpenMarket/Application/Presentation/ViewModel/MarketProductsViewModel.swift
+++ OpenMarket/OpenMarket/Application/Presentation/ViewModel/MarketProductsViewModel.swift
@@ -8,29 +8,98 @@
 import UIKit
 
 final class MarketProductsViewModel {
-    private var products: [Product] = []
-    private var product: ProductEntity?
     // MARK: Properties
     
-    init(products: [Product]) {
-        self.products = products
+    private var productList: ProductList?
+    private var productEntity: ProductEntity?
+    
+    var thumbnailImage: UIImage? {
+        guard let product = productEntity else {
+            return nil
+        }
+        
+        return product.thumbnailImage
     }
     
-    init(product: ProductEntity) {
-        self.product = product
+    var name: String? {
+        guard let product = productEntity else {
+            return nil
+        }
+        
+        return product.name
     }
     
+    var currency: String? {
+        guard let product = productEntity else {
+            return nil
+        }
+        
+        return product.currency
+    }
     
-    func fetchData() -> [ProductEntity] {
-        var entityList: [ProductEntity] = []
+    var originalPriceText: String? {
+        guard let product = productEntity else {
+            return nil
+        }
+        
+        return product.currency + " " + (product.originalPrice.numberFormatter())
+    }
+    
+    var discountedPriceText: String? {
+        guard let product = productEntity else {
+            return nil
+        }
+        
+        return product.currency + " " + (product.discountedPrice.numberFormatter())
+    }
+    
+    var stockText: String? {
+        guard let product = productEntity else {
+            return nil
+        }
+        
+        return isEmptyStock == true ? "품절" : "잔여수량 : \(product.stock)"
+    }
+    
+    var isDiscountedItem: Bool? {
+        guard let product = productEntity else {
+            return nil
+        }
+        
+        return product.originalPrice == product.discountedPrice
+    }
+    
+    var isEmptyStock: Bool? {
+        guard let product = productEntity else {
+            return nil
+        }
+        
+        return product.stock == 0
+    }
+    
+    // MARK: - Initializer
 
-        for product in products {
-            
+    init(_ productList: ProductList) {
+        self.productList = productList
+    }
+    
+    init(_ productEntity: ProductEntity) {
+        self.productEntity = productEntity
+    }
+
+    func formatData() -> ProductListEntity? {
+        var entityList = ProductListEntity(productEntity: [])
+        
+        guard let count = productList?.pages else {
+            return nil
+        }
+        
+        for product in count {
             guard let thumbnailImage = product.thumbnailImage else {
                 break
             }
             
-            entityList.append(
+            entityList.productEntity.append(
                 ProductEntity(
                     thumbnailImage: thumbnailImage,
                     name: product.name,
@@ -42,73 +111,4 @@ final class MarketProductsViewModel {
         
         return entityList
     }
-}
-
-extension MarketProductsViewModel {
-    var thumbnailImage: UIImage? {
-        guard let product = product else {
-            return nil
-        }
-        
-        return product.thumbnailImage
-    }
-    
-    var name: String? {
-        guard let product = product else {
-            return nil
-        }
-        
-        return product.name
-    }
-    
-    var currency: String? {
-        guard let product = product else {
-            return nil
-        }
-        
-        return product.currency
-    }
-    
-    var originalPriceText: String? {
-        guard let product = product else {
-            return nil
-        }
-        
-        return product.currency + " " + (product.originalPrice.numberFormatter())
-    }
-    
-    var discountedPriceText: String? {
-        guard let product = product else {
-            return nil
-        }
-        
-        return product.currency + " " + (product.discountedPrice.numberFormatter())
-    }
-    
-    var stockText: String? {
-        guard let product = product else {
-            return nil
-        }
-        
-        return isEmptyStock == true ? "품절" : "잔여수량 : \(product.stock)"
-    }
-    
-    var isDiscountedItem: Bool? {
-        guard let product = product else {
-            return nil
-        }
-        
-        return product.originalPrice == product.discountedPrice
-    }
-    
-    var isEmptyStock: Bool? {
-        guard let product = product else {
-            return nil
-        }
-        
-        return product.stock == 0
-    }
-    
-    // MARK: - Initializer
-
 }
