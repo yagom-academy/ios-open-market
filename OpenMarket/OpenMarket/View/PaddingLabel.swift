5add33b17fbfd2dbb4d8221f13be9bb6d887aecf
BaekGom <jihunmm02@gmail.com>
2022-07-25 17:27:45 +0900

    refactor : PaddingLabel Ident 수정 및 접근제어자 설정

--- OpenMarket/OpenMarket/View/PaddingLabel.swift
+++ OpenMarket/OpenMarket/View/PaddingLabel.swift
@@ -8,19 +8,19 @@
 import UIKit
 
 @IBDesignable class PaddingLabel: UILabel {
-
-    @IBInspectable var topInset: CGFloat = 0.0
-    @IBInspectable var bottomInset: CGFloat = 0.0
-    @IBInspectable var leftInset: CGFloat = 0.0
-    @IBInspectable var rightInset: CGFloat = 0.0
+    
+    @IBInspectable private var topInset: CGFloat = 0.0
+    @IBInspectable private var bottomInset: CGFloat = 0.0
+    @IBInspectable private var leftInset: CGFloat = 0.0
+    @IBInspectable private var rightInset: CGFloat = 0.0
     
     override func drawText(in rect: CGRect) {
         let insets = UIEdgeInsets.init(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
         super.drawText(in: rect.inset(by: insets))
     }
-
+    
     override var intrinsicContentSize: CGSize {
-    let size = super.intrinsicContentSize
-    return CGSize(width: size.width + leftInset + rightInset, height: size.height + topInset + bottomInset)
+        let size = super.intrinsicContentSize
+        return CGSize(width: size.width + leftInset + rightInset, height: size.height + topInset + bottomInset)
     }
 }
