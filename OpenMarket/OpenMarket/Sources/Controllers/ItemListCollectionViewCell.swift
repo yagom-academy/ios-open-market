1fe59fc5150a7372af8f4cb555ed827f21c2e6ef
yeeton37 <lyeeun37@naver.com>
2022-07-26 21:13:54 +0900

    feat: "ItemListCollectionViewCell" 타입 생성 및 UI 구현
    - UI 요소 정의 및 오토레이아웃 구성
    - receiveData(_:)를 통해 ViewController로부터 데이터를 받아옴
    - configureCell()를 통해 Cell의 UI 요소 업데이트

---
+++ OpenMarket/OpenMarket/Sources/Controllers/ItemListCollectionViewCell.swift
@@ -0,0 +1,148 @@
+//
+//  ItemListCollectionViewCell.swift
+//  OpenMarket
+//
+//  Created by 이예은 on 2022/07/26.
+//
+
+import UIKit
+
+class ItemListCollectionViewCell: UICollectionViewCell {
+    private var item: ItemListPage.Item?
+    
+    let productImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        return imageView
+    }()
+    
+    let accessaryImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.image = UIImage(systemName: "greaterthan")
+        imageView.tintColor = .systemGray
+        return imageView
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .boldSystemFont(ofSize: 18)
+        return label
+    }()
+    
+    let priceLabel: UILabel = {
+        let label = UILabel()
+        
+        label.textColor = .systemGray
+        return label
+    }()
+    
+   let stockLabel: UILabel = {
+        let label = UILabel()
+       
+        label.textAlignment = .right
+        label.textColor = .systemGray
+        return label
+    }()
+    
+    private let firstHorizontalStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.axis = .horizontal
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 8
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private let secondHorizontalStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.axis = .horizontal
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 2
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private let thirdVerticalStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 2
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private let entireStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.alignment = .center
+        stackView.axis = .horizontal
+        stackView.spacing = 2
+        stackView.distribution = .fill
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private func arrangeSubView() {
+        firstHorizontalStackView.addArrangedSubview(stockLabel)
+        firstHorizontalStackView.addArrangedSubview(accessaryImageView)
+        secondHorizontalStackView.addArrangedSubview(nameLabel)
+        secondHorizontalStackView.addArrangedSubview(firstHorizontalStackView)
+        thirdVerticalStackView.addArrangedSubview(secondHorizontalStackView)
+        thirdVerticalStackView.addArrangedSubview(priceLabel)
+        entireStackView.addArrangedSubview(productImageView)
+        entireStackView.addArrangedSubview(thirdVerticalStackView)
+        
+        contentView.addSubview(entireStackView)
+        
+        NSLayoutConstraint.activate([
+            entireStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            entireStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            entireStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            entireStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            
+            productImageView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.8),
+            productImageView.widthAnchor.constraint(equalTo: productImageView.heightAnchor, multiplier: 0.8),
+            
+            accessaryImageView.widthAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.1)
+        ])
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        arrangeSubView()
+        configureCell()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func receiveData(_ data: ItemListPage.Item) {
+        self.item = data
+    }
+
+    func configureCell() {
+        guard let item = item else {
+            return
+        }
+        
+        let imageData = item.thumbnail
+        guard let url = URL(string: imageData) else { return }
+        guard let imageData = try? Data(contentsOf: url) else { return }
+        
+        let image = UIImage(data: imageData)
+        
+        self.productImageView.image = image
+        self.stockLabel.text = "잔여수량 : \(item.stock)"
+        self.priceLabel.text = "\(item.price)"
+        self.nameLabel.text = item.name
+    }
+    
+}
