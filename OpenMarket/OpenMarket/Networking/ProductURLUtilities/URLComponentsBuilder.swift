4edff451700f512281ff5fe041c65523be1f6b2d
MinsuJeon <chso294@naver.com>
2022-07-27 14:24:49 +0900

    feat: 빌더 패턴을 사용하여 URL을 생성하는 class 추가

---
+++ OpenMarket/OpenMarket/Networking/ProductURLUtilities/URLComponentsBuilder.swift
@@ -0,0 +1,59 @@
+//
+//  URLComponentsBuilder.swift
+//  OpenMarket
+//
+//  Created by 전민수 on 2022/07/26.
+//
+
+import Foundation
+
+final class URLComponentsBuilder {
+    private var urlComponents = URLComponents()
+    
+    init() { }
+    
+    func build() -> URLComponents {
+        return urlComponents
+    }
+    
+    func setScheme(_ scheme: String) -> URLComponentsBuilder {
+        urlComponents.scheme = scheme
+        
+        return self
+    }
+    
+    func setHost(_ host: String) -> URLComponentsBuilder {
+        urlComponents.host = host
+        
+        return self
+    }
+    
+    func setPath(_ path: String) -> URLComponentsBuilder {
+        urlComponents.path = path
+        
+        return self
+    }
+    
+    func addQuery(items: [String: String]) -> URLComponentsBuilder {
+        urlComponents.addQuery(items)
+        
+        return self
+    }
+}
+
+extension URLComponents {
+    fileprivate mutating func addQuery(_ items: [String: String]) {
+        var newQueryItems = [URLQueryItem]()
+        
+        for (key, value) in items.sorted(by: { $0.key < $1.key }) {
+            newQueryItems.append(URLQueryItem(name: key,
+                                              value: value))
+        }
+        
+        if self.queryItems == nil {
+            self.queryItems = newQueryItems
+        } else {
+            self.queryItems?.append(contentsOf: newQueryItems)
+        }
+    }
+}
