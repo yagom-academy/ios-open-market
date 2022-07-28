6335e5d723177fba474ed97024fea10a2f923ccf
derrickkim0109 <derrickkim0109@naver.com>
2022-07-28 15:11:27 +0900

    feat: ProductDetailViewController 생성

---
+++ OpenMarket/OpenMarket/Application/Presentation/ViewController/ProductDetail/ProductDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ProductDetailViewController.swift
+//  OpenMarket
+//
+//  Created by Derrick kim on 2022/07/27.
+//
+
+import UIKit
+
+class ProductDetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
