3020178b5858da646e62f5ff03022d9004e9d1a9
minsson <mskaang@gmail.com>
2022-07-28 16:00:39 +0900

    feat: "ItemListPageViewController" 컬렉션뷰 레이아웃 구현
    - setUpItemCollectionViewWithLayout() 메서드: UI 요소 정의 및 오토레이아웃 구성을 위함
    - generateCompositionalLayout(numberOfItemsAtRow:) 메서드: 컴포지셔널 레이아웃 활용하여 컬렉션뷰 내 레이아웃 구성
    - setUIComponentsLayout() 메서드명을 setUpSegmentedControlWithLayout()로 변경
        - setUpItemCollectionViewWithLayout() 메서드와의 통일성 향상을 위함

--- OpenMarket/OpenMarket/Sources/Controllers/ItemListPageViewController.swift
+++ OpenMarket/OpenMarket/Sources/Controllers/ItemListPageViewController.swift
@@ -17,6 +17,8 @@ final class ItemListPageViewController: UIViewController {
     
     // MARK: - UI Components
     
+    private var itemCollectionView: UICollectionView!
+    
     private let segmentedControl: UISegmentedControl = {
         let selectionItems = ["LIST", "GRID"]
         let segmentedControl = UISegmentedControl(items: selectionItems)
@@ -34,14 +36,15 @@ final class ItemListPageViewController: UIViewController {
             self.fetchParsedData(basedOn: result)
         }
         
-        setUIComponentsLayout()
+        setUpSegmentedControlWithLayout()
+        setUpItemCollectionViewWithLayout()
     }
 }
 
 // MARK: - Private Actions
 
 private extension ItemListPageViewController {
-    func setUIComponentsLayout() {
+    func setUpSegmentedControlWithLayout() {
         self.view.addSubview(segmentedControl)
         
         segmentedControl.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 8.0).isActive = true
@@ -56,6 +59,61 @@ private extension ItemListPageViewController {
         case .failure(let error):
             print(error)
         }
+    }
+}
+
+// MARK: - Private Actions for Collection View UI
+
+private extension ItemListPageViewController {
+    func setUpItemCollectionViewWithLayout() {
+        print(#function)
+        itemCollectionView = UICollectionView(frame: .zero, collectionViewLayout: generateCompositionalLayout(numberOfItemsAtRow: 1))
+        
+        view.addSubview(itemCollectionView)
+        
+        itemCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            itemCollectionView.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 16.0),
+            itemCollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16.0),
+            itemCollectionView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 8.0),
+            itemCollectionView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -8.0)
+        ])
+    }
+    
+    func generateCompositionalLayout(numberOfItemsAtRow: Int) -> UICollectionViewLayout {
+        print(#function)
+        let spacing: CGFloat = 10.0
+        
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(100.0)
+        )
+        
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitem: item,
+            count: numberOfItemsAtRow
+        )
+        
+        group.contentInsets = NSDirectionalEdgeInsets(
+            top: spacing,
+            leading: spacing,
+            bottom: 0,
+            trailing: spacing
+        )
+        
+        let section = NSCollectionLayoutSection(group: group)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
     }
 }
 
