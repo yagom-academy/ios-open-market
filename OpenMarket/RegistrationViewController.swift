253c4c8301030c5a331d4bdee35503540c67a9e4
seohyeon2 <seo_hyeon418@naver.com>
2022-08-01 19:51:24 +0900

    feat: RegistrationViewController의 navigationBar 구현

--- OpenMarket/RegistrationViewController.swift
+++ OpenMarket/RegistrationViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class RegistrationViewController: UIViewController {
+    
+    private lazy var doneButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Done", for: .normal)
+        button.setTitleColor(UIColor.systemBlue, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var cancelButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Cancel", for: .normal)
+        button.setTitleColor(UIColor.systemBlue, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
 
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: doneButton)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: cancelButton)
+        self.title = "상품등록"
     }
 }
