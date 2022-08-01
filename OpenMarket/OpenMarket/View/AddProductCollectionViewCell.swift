90ffb0758da69658c25789b2a775f67fcec7f62f
gjrjs31@naver.com <gjrjs31@naver.com>
2022-08-01 17:02:52 +0900

    feat : AddProductViewController CollectionView 구현

---
+++ OpenMarket/OpenMarket/View/AddProductCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  AddProductCollectionViewCell.swift
+//  OpenMarket
+//
+//  Created by 허건 on 2022/08/01.
+//
+
+import UIKit
+
+class AddProductCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    static let reuseIdentifier = "AddProductCollectionViewCell"
+}
--- OpenMarket/OpenMarket/ViewController/AddProductViewController.swift
+++ OpenMarket/OpenMarket/ViewController/AddProductViewController.swift
@@ -9,12 +9,71 @@ import UIKit
 
 class AddProductViewController: UIViewController {
     
+    @IBOutlet weak var addImageCollectionView: UICollectionView!
+
+    let imagePicker = UIImagePickerController()
+    var numberOfCell = 10
+    
     override func viewDidLoad() {
-        print("ok")
+        super.viewDidLoad()
+//        self.imagePicker.sourceType = .photoLibrary
+//        self.imagePicker.allowsEditing = true
+//        self.imagePicker.delegate = self
+        self.addImageCollectionView.delegate = self
+        self.addImageCollectionView.dataSource = self
     }
     
     @IBAction func back(_ sender: UIButton) {
         self.presentingViewController?.dismiss(animated: true)
     }
     
+    @objc func pickImage() {
+        self.present(self.imagePicker, animated: true)
+    }
 }
+
+extension AddProductViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+        return numberOfCell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AddProductCollectionViewCell.reuseIdentifier, for: indexPath) as! AddProductCollectionViewCell
+        cell.backgroundColor = .black
+        cell.profileImageView.image = .add
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        numberOfCell += 1
+        collectionView.reloadData()
+    }
+}
+
+//extension AddProductViewController: UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let flowayout = collectionViewLayout as? UICollectionViewFlowLayout
+//        flowayout?.scrollDirection = .horizontal
+//        flowayout?.minimumLineSpacing = 20
+//        flowayout?.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+//        return CGSize(width: view.frame.width - 100, height: view.frame.height)
+//    }
+//}
+
+//extension AddProductViewController: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+//
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//
+//        var newImage: UIImage? = nil
+//
+//        if let possibleImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+//            newImage = possibleImage
+//        } else if let possibleImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+//            newImage = possibleImage
+//        }
+//
+//        //profileImageView.image = newImage
+//    }
+//}
