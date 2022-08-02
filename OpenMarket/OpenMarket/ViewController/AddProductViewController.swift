07f0e466b85a1ace152b4a80bc4c93985129356e
BaekGom <jihunmm02@gmail.com>
2022-08-02 10:57:35 +0900

    refactor : CollectionView -> ScrollView 변경

--- OpenMarket/OpenMarket/ViewController/AddProductViewController.swift
+++ OpenMarket/OpenMarket/ViewController/AddProductViewController.swift
@@ -19,8 +19,8 @@ class AddProductViewController: UIViewController {
 //        self.imagePicker.sourceType = .photoLibrary
 //        self.imagePicker.allowsEditing = true
 //        self.imagePicker.delegate = self
-        self.addImageCollectionView.delegate = self
-        self.addImageCollectionView.dataSource = self
+//        self.addImageCollectionView.delegate = self
+//        self.addImageCollectionView.dataSource = self
     }
     
     @IBAction func back(_ sender: UIButton) {
@@ -32,35 +32,35 @@ class AddProductViewController: UIViewController {
     }
 }
 
-extension AddProductViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-
-        return numberOfCell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AddProductCollectionViewCell.reuseIdentifier, for: indexPath) as! AddProductCollectionViewCell
-        cell.backgroundColor = .black
-        //cell.profileImageView.image = .add
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        numberOfCell += 1
-        collectionView.reloadData()
-    }
-}
-
-extension AddProductViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let flowayout = collectionViewLayout as? UICollectionViewFlowLayout
-        flowayout?.scrollDirection = .horizontal
-        flowayout?.minimumLineSpacing = 20
-        flowayout?.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
-        return CGSize(width: view.frame.width - 100, height: view.frame.height - 150)
-    }
-}
+//extension AddProductViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+//
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//
+//        return numberOfCell
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AddProductCollectionViewCell.reuseIdentifier, for: indexPath) as! AddProductCollectionViewCell
+//        cell.backgroundColor = .black
+//        //cell.profileImageView.image = .add
+//        return cell
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        numberOfCell += 1
+//        collectionView.reloadData()
+//    }
+//}
+//
+//extension AddProductViewController: UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let flowayout = collectionViewLayout as? UICollectionViewFlowLayout
+//        flowayout?.scrollDirection = .horizontal
+//        flowayout?.minimumLineSpacing = 20
+//        flowayout?.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+//        return CGSize(width: view.frame.width - 100, height: view.frame.height - 150)
+//    }
+//}
 
 //extension AddProductViewController: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 //
