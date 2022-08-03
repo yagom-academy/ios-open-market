7966bd2fb5031e2fc98ae1f2e72e6a5b1f422aba
yeeton37 <lyeeun37@naver.com>
2022-08-03 18:00:50 +0900

    feat: "GridCollectionViewController" childView 구현
    - CollectionViewControllerConfigurable 프로토콜 채택
    - setUpCollectionViewLayout() 구현
        - grid 형태의 layout을 반환
    - configureGridDataSource() 구현
        - viewController의 dataSource 프로퍼티에 dataSource를 담아주기 위함

---
+++ OpenMarket/OpenMarket/Sources/Controllers/GridCollectionViewController.swift
@@ -0,0 +1,109 @@
+//
+//  GridCollectionViewController.swift
+//  OpenMarket
+//
+//  Created by minsson, yeton on 2022/08/03.
+//
+
+import UIKit
+
+final class GridCollectionViewController: UIViewController, CollectionViewControllerConfigurable {
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(
+            frame: .zero,
+            collectionViewLayout: createGridLayout()
+        )
+        return collectionView
+    }()
+    
+    typealias ItemSnapShot = NSDiffableDataSourceSnapshot<Section, ItemListPage.Item>
+    
+    lazy var dataSource = configureGridDataSource()
+    
+    var itemListPage: ItemListPage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpCollectionViewLayout()
+        dataSource = configureGridDataSource()
+        
+        getProductList(from: APIURLComponents.openMarketURLComponents?.url)
+    }
+}
+
+private extension GridCollectionViewController {
+    func setUpCollectionViewLayout() {
+        self.view.addSubview(collectionView)
+        self.collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.topAnchor
+            ),
+            collectionView.bottomAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.bottomAnchor
+            ),
+            collectionView.leadingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.leadingAnchor
+            ),
+            collectionView.trailingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.trailingAnchor
+            )
+        ])
+    }
+    
+    func createGridLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        item.contentInsets = .init(
+            top: 4,
+            leading: 4,
+            bottom: 4,
+            trailing: 4
+        )
+        
+        let groupsize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalWidth(2/3)
+        )
+        
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupsize,
+            subitem: item,
+            count: 2
+        )
+        
+        let section = NSCollectionLayoutSection(group: group)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
+    }
+    
+    func configureGridDataSource()
+    ->  UICollectionViewDiffableDataSource<Section, ItemListPage.Item> {
+        let registration = UICollectionView.CellRegistration<ItemGridCollectionViewCell, ItemListPage.Item>.init { cell, _, item in
+            cell.receiveData(item)
+        }
+        
+        return UICollectionViewDiffableDataSource<Section, ItemListPage.Item> (
+            collectionView: collectionView
+        ) { collectionView, indexPath, item -> UICollectionViewCell? in
+            
+            let cell = collectionView.dequeueConfiguredReusableCell(
+                using: registration,
+                for: indexPath,
+                item: item
+            )
+            
+            return cell
+        }
+    }
+}
+
+
