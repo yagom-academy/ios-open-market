8ff6fe2eb883532bcb5141825041990ca1c3b8a9
starting1231 <starting1231@naver.com>
2022-08-04 14:07:58 +0900

    refactor: MultipartForm data 생성 메서드 구현위치 수정

--- OpenMarket/OpenMarket/Model/Network/APIRequest.swift
+++ OpenMarket/OpenMarket/Model/Network/APIRequest.swift
@@ -95,3 +95,42 @@ extension APIRequest {
         return request
     }
 }
+
+extension APIRequest {
+    func createMultiPartFormBody(boundary: String, paramsData: Data, images: [Data]) -> Data {
+        let lineBreak = "\r\n"
+        var requestBody = Data()
+        
+        requestBody.append(createMultipartformDataParams(boundary: boundary, data: paramsData))
+        images.forEach {
+            requestBody.append(createMultipartformDataImages(boundary: boundary, image: $0))
+        }
+        requestBody.append("\(lineBreak)--\(boundary)--\(lineBreak)")
+        
+        return requestBody
+    }
+    
+    private func createMultipartformDataParams(boundary: String, data: Data) -> Data {
+        let lineBreak = "\r\n"
+        var paramsBody = Data()
+        
+        paramsBody.append("\(lineBreak)--\(boundary + lineBreak)")
+        paramsBody.append("Content-Disposition: form-data; name=\"params\"\(lineBreak)")
+        paramsBody.append("Content-Type: application/json \(lineBreak + lineBreak)")
+        paramsBody.append(data)
+        
+        return paramsBody
+    }
+    
+    private func createMultipartformDataImages(boundary: String, image: Data) -> Data {
+        let lineBreak = "\r\n"
+        var imageBody = Data()
+        
+        imageBody.append("\(lineBreak)--\(boundary + lineBreak)")
+        imageBody.append("Content-Disposition: form-data; name=\"images\"; filename=\"\(boundary).png\"\(lineBreak)")
+        imageBody.append("Content-Type: image/png \(lineBreak + lineBreak)")
+        imageBody.append(image)
+        
+        return imageBody
+    }
+}
