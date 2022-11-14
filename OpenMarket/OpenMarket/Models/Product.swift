62ce78a18cbd4a3a20c91bc9733cc90ef4f0e2e6
Kyo <hyosing92@gmail.com>
2022-11-14 17:29:07 +0900

    feat : Product 타입 생성 및 내부 프로퍼티에 대한 CodingKey 추가

---
+++ OpenMarket/OpenMarket/Models/Product.swift
@@ -0,0 +1,29 @@
+//
+//  Product.swift
+//  OpenMarket
+//
+//  Created by Kyo, LJ on 2022/11/14.
+//
+
+struct Product {
+    let id: Int
+    let vendorId: Int
+    let name: String
+    let thumbnail: String
+    let currency: String
+    let price: Int
+    let bargainPrice: Int
+    let discountedPrice: Int
+    let stock: Int
+    let createdAt: String
+    let issuedAt: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case id, name, thumbnail, currency, price, stock
+        case vendorId = "vendor_id"
+        case bargainPrice = "bargain_price"
+        case discountedPrice = "discounted_price"
+        case createdAt = "created_at"
+        case issuedAt = "issued_at"
+    }
+}
