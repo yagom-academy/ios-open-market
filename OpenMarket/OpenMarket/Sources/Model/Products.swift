c8e426e0833dc86cadd62b5cb52eb1a4fff934ba
DragonYG <kioswe92@gmail.com>
2022-11-14 20:17:09 +0900

    feat: DataAsset 형식에 맞춘 Products, Page 파일 추가

---
+++ OpenMarket/OpenMarket/Sources/Model/Products.swift
@@ -0,0 +1,29 @@
+//
+//  OpenMarket - Products.swift
+//  Created by Zhilly, Dragon. 22/11/14
+//  Copyright © yagom. All rights reserved.
+//
+
+struct Products: Codable {
+    let pageNumber: Int
+    let itemsPerPage: Int
+    let totalCount: Int
+    let offset: Int
+    let limit: Int
+    let pages: [Page]
+    let lastPage: Int
+    let hasNext: Bool
+    let hasPrevious: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case pageNumber = "page_no"
+        case itemsPerPage = "items_per_page"
+        case totalCount = "total_count"
+        case offset
+        case limit
+        case pages
+        case lastPage = "last_page"
+        case hasNext = "has_next"
+        case hasPrevious = "has_prev"
+    }
+}
