c7e14516364315d58dee23faa56fe4e0810e4816
lj-7-77 <swtw72@gmail.com>
2022-11-14 18:27:11 +0900

    feat : ProductPage 타입 구현 및 CodingKeys 구현

---
+++ OpenMarket/OpenMarket/Models/ProductPage.swift
@@ -0,0 +1,28 @@
+//
+//  ProductPage.swift
+//  OpenMarket
+//
+//  Created by Kyo, LJ on 2022/11/14.
+//
+
+struct ProductPage: Decodable {
+    let pageNumber: String
+    let itemsPerPage: Int
+    let totalCount: Int
+    let offset: Int
+    let limit: Int
+    let pages: [Product]
+    let lastPage: Int
+    let hasNext: Bool
+    let hasPrev: Bool
+    
+    private enum CodingKeys: String, CodingKey {
+        case offset, limit, pages
+        case pageNumber = "page_no"
+        case itemsPerPage = "items_per_page"
+        case totalCount = "total_count"
+        case lastPage = "last_page"
+        case hasNext = "has_next"
+        case hasPrev = "has_prev"
+    }
+}
