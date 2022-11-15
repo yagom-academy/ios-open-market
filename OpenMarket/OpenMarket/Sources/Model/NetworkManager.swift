3c91d750f74311059cfe709878600100d6f3a21d
DragonYG <kioswe92@gmail.com>
2022-11-15 17:12:52 +0900

    refactor: NetworkManager 메서드에서 중복되는 코드 메서드로 분리

--- OpenMarket/OpenMarket/Sources/Model/NetworkManager.swift
+++ OpenMarket/OpenMarket/Sources/Model/NetworkManager.swift
@@ -7,40 +7,25 @@
 import Foundation
 
 class NetworkManager {
-    var semaphore = DispatchSemaphore (value: 0)
-    let hostURL: String = "https://openmarket.yagom-academy.kr"
+    private var semaphore = DispatchSemaphore (value: 0)
+    private let hostURL: String = "https://openmarket.yagom-academy.kr"
     
     func getApplicationHealthChecker() {
         guard let targetURL: URL = URL(string: hostURL + "/healthChecker") else {
             return
         }
-        var request: URLRequest = URLRequest(url: targetURL,timeoutInterval: Double.infinity)
+        let targetRequest = makeRequest(url: targetURL)
         
-        request.httpMethod = "GET"
-        
-        let task: URLSessionDataTask = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data else {
-                print(String(describing: error))
-                self.semaphore.signal()
-                return
-            }
-            print(String(data: data, encoding: .utf8)!)
-            self.semaphore.signal()
-        }
-        
-        task.resume()
-        semaphore.wait()
+        dataTask(request: targetRequest)
     }
     
     func getItemListCheck(pageNumber: Int, itemPerPage: Int, searchValue: String? = nil) {
         let pageNumberString: String = "page_no=" + String(pageNumber)
         let itemPerPageString: String = "&items_per_page=" + String(itemPerPage)
         var searchValueString: String = ""
-        
         if let unwrappingSearchValue: String = searchValue {
             searchValueString = "&search_value=" + unwrappingSearchValue
         }
-        
         guard let targetURL: URL = URL(string:
                                         hostURL +
                                        "/api/products?" +
@@ -50,22 +35,9 @@ class NetworkManager {
         ) else {
             return
         }
+        let targetRequest = makeRequest(url: targetURL)
         
-        var request: URLRequest = URLRequest(url: targetURL,timeoutInterval: Double.infinity)
-        request.httpMethod = "GET"
-        
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data else {
-                print(String(describing: error))
-                self.semaphore.signal()
-                return
-            }
-            print(String(data: data, encoding: .utf8)!)
-            self.semaphore.signal()
-        }
-        
-        task.resume()
-        semaphore.wait()
+        dataTask(request: targetRequest)
     }
     
     func getItemDetailListCheck(productID: Int) {
@@ -73,10 +45,20 @@ class NetworkManager {
         guard let targetURL: URL = URL(string: hostURL + "/api/products/" + productIDString) else {
             return
         }
-        var request: URLRequest = URLRequest(url: targetURL,timeoutInterval: Double.infinity)
+        let targetRequest = makeRequest(url: targetURL)
+        
+        dataTask(request: targetRequest)
+    }
+    
+    private func makeRequest(url: URL) -> URLRequest {
+        var request: URLRequest = URLRequest(url: url,timeoutInterval: Double.infinity)
         
         request.httpMethod = "GET"
         
+        return request
+    }
+    
+    private func dataTask(request: URLRequest) {
         let task: URLSessionDataTask = URLSession.shared.dataTask(with: request) { data, response, error in
             guard let data = data else {
                 print(String(describing: error))
