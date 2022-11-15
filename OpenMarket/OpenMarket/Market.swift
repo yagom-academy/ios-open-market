b70a62831b6acc21e7da2e08a7732b18e07cafef
Jae Hyung Kim <smfc.mene@gmail.com>
2022-11-15 14:38:59 +0800

    feat: 유저 요청에 따른 url 생성을 위한 Request enum 구현 및 Market 타입 누락된 프로퍼티 추가

--- OpenMarket/OpenMarket/Market.swift
+++ OpenMarket/OpenMarket/Market.swift
@@ -22,6 +22,7 @@ struct Market: Decodable {
         let id: Int
         let vendorId: Int
         let name: String
+        let description: String
         let thumbnail: String
         let currency: Currency
         let price: Double
@@ -30,10 +31,26 @@ struct Market: Decodable {
         let stock: Int
         let createdAt: String
         let issuedAt: String
+        let images: [Image]?
+        let vendors: Vendors?
+        
+        struct Image: Decodable {
+            let id: Int
+            let url: String
+            let thumbnailUrl: String
+            let issuedAt: String
+        }
+
+        struct Vendors: Decodable {
+            let id: Int
+            let name: String
+        }
     }
     
     enum Currency: String, Decodable {
         case krw = "KRW"
         case usd = "USD"
     }
+    
+    
 }
--- OpenMarket/OpenMarket/URLSessionProvider.swift
+++ OpenMarket/OpenMarket/URLSessionProvider.swift
@@ -7,21 +7,40 @@
 
 import Foundation
 
+enum Request {
+    case healthChecker
+    case productList(number: Int)
+    case productDetail(number: Int)
+    
+    var name: String {
+        switch self {
+        case .healthChecker:
+            return "/healthChecker"
+        case .productList(let number):
+            return "/api/products?page_no=\(number)&items_per_page=100"
+        case .productDetail(let number):
+            return "/api/products/\(number)"
+        }
+    }
+}
+
 class MarketURLSessionProvider {
     let session: URLSession = URLSession(configuration: .default)
-    let baseUrl: String = "https://openmarket.yagom-academy.kr"
+    let baseUrl: URL? = URL(string: "https://openmarket.yagom-academy.kr")
     var market: Market?
     
-    func fetchData() {
-        guard let url = URL(string: baseUrl) else { return }
+    func fetchData<T: Decodable>(request: Request, type: T.Type) {
+        guard let baseUrl = baseUrl,
+              let relativeUrl = URL(string: request.name, relativeTo: baseUrl) else { return }
         
-        let dataTask = session.dataTask(with: url) { (data, response, error) in
+        let dataTask = session.dataTask(with: relativeUrl) { (data, response, error) in
             guard error == nil,
                   let httpResponse = response as? HTTPURLResponse,
                   (200...299).contains(httpResponse.statusCode),
-                  let data = data else { return }
+                  let data = data else {
+                return }
             
-            guard let decodedData = JSONDecoder.decodeFromServer(type: Market.self,
+            guard let decodedData = JSONDecoder.decodeFromServer(type: type,
                                                                  from: data) else { return }
             print(decodedData)
         }
