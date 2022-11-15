6fc4a857a09504c1f05c2538544248999a57387a
jonghancha <jonghancha@gmail.com>
2022-11-15 16:36:03 +0900

    feat: NetworkAPI 추가, URLComponents Extension 추가

---
+++ OpenMarket/OpenMarket/NetworkAPI.swift
@@ -0,0 +1,54 @@
+//  Created by Aejong, Tottale on 2022/11/15.
+
+import Foundation
+
+class NetworkAPI {
+    
+    static let shared = NetworkAPI()
+    private init() {}
+    
+    let scheme = "https"
+    let host = "openmarket.yagom-academy.kr"
+    
+    var urlComponents: URLComponents {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = self.scheme
+        urlComponents.host = self.host
+        
+        return urlComponents
+    }
+    
+    func fetch(path: String, parameters: [String: String]?, completion: @escaping (Data) -> Void) {
+        var urlComponents = self.urlComponents
+        urlComponents.path = path
+        if let parameters = parameters {
+            urlComponents.setQueryItems(with: parameters)
+        }
+        
+        
+        guard let url = urlComponents.url else { return }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                dump(error)
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                self.handleServerError(response)
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            completion(data)
+        }.resume()
+        
+    }
+    
+    private func handleServerError(_ response: URLResponse?) {
+        guard let httpResponse = response as? HTTPURLResponse else { return }
+        print(httpResponse.statusCode)
+    }
+
+}
---
+++ OpenMarket/OpenMarket/URLComponents+Extension.swift
@@ -0,0 +1,9 @@
+//  Created by Aejong, Tottale on 2022/11/15.
+
+import Foundation
+
+extension URLComponents {
+    mutating func setQueryItems(with parameters: [String: String]) {
+        self.queryItems = parameters.map { URLQueryItem(name: $0.key, value: $0.value) }
+    }
+}
