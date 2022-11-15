bd1afb755145a04888c3da1edd1951c611f03b0b
chacha <happyhy0429@gmail.com>
2022-11-15 17:09:08 +0900

    feat: ProductList 타입 구현

---
+++ OpenMarket/Model/ProductList.swift
@@ -0,0 +1,29 @@
+//  ProductList.swift
+//  OpenMarket
+//  Created by SummerCat & Bella on 2022/11/15.
+
+import Foundation
+
+struct ProductList: Decodable {
+    let pageNumber: Int
+    let itemsPerPage: Int
+    let totalCount: Int
+    let firstIndex: Int
+    let lastIndex: Int
+    let lastPage: Int
+    let hasNext: Bool
+    let hasPrev: Bool
+    let pages: [Product]
+    
+    enum CodingKeys: String, CodingKey {
+        case pageNumber = "pageNo"
+        case itemsPerPage
+        case totalCount
+        case firstIndex = "offset"
+        case lastIndex = "limit"
+        case lastPage
+        case hasNext
+        case hasPrev
+        case pages
+    }
+}
