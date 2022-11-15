97d88eee5302969b30dc2335b3b44b1164647665
lws2269 <lws2269@kakao.com>
2022-11-15 14:29:50 +0900

    refactor: API HealthChecker 추가 및 completion Handler result 타입으로 리팩토링

--- OpenMarket/OpenMarket/Models/NetworkManager.swift
+++ OpenMarket/OpenMarket/Models/NetworkManager.swift
@@ -8,31 +8,51 @@
 import Foundation
 
 struct NetworkManager {
-    func fetchItemList(completion: @escaping (ItemList) -> ()) {
-        guard let url: URL = URL(string: "https://openmarket.yagom-academy.kr/api/products?page_no=1&items_per_page=100") else { return }
+    let url: String = "https://openmarket.yagom-academy.kr/"
+    
+    func checkAPIHealth(completion: @escaping (Bool) -> Void) {
+        let urlString = "\(url)healthChecker"
+        
+        guard let url: URL = URL(string: urlString) else { return }
+        
         let session: URLSession = URLSession(configuration: .default)
+        
         let dataTask: URLSessionDataTask = session.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+            if error != nil { return completion(false) }
+            guard let httpResponse = response as? HTTPURLResponse else { return completion(false) }
             
-            if let error = error {
-                print(error.localizedDescription)
-                return
+            return completion(httpResponse.statusCode == 200)
+        }
+        
+        dataTask.resume()
+        
+    }
+    
+    func fetchItemList(pageNo: Int, pageCount: Int, completion: @escaping (Result<ItemList,NetworkError>) -> Void) {
+        let urlString = "\(url)api/products?page_no=\(pageNo)&items_per_page=\(pageCount)"
+        
+        guard let url: URL = URL(string: urlString) else { return }
+        
+        let session: URLSession = URLSession(configuration: .default)
+        
+        let dataTask: URLSessionDataTask = session.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+            if error != nil {
+                return completion(.failure(.invalidError))
             }
             
             guard let httpResponse = response as? HTTPURLResponse,
                   (200..<300).contains(httpResponse.statusCode) else {
-                print("statusCode error")
-                return
+                return completion(.failure(.responseError))
             }
             
-            guard let data = data else { return }
+            guard let data = data else { return completion(.failure(.dataError))}
             
             do {
                 let itemList: ItemList = try JSONDecoder().decode(ItemList.self, from: data)
-                completion(itemList)
+                completion(.success(itemList))
             } catch {
-                print(error)
+                completion(.failure(.parseError))
             }
-            
         }
         
         dataTask.resume()
