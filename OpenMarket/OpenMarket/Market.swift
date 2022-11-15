774926804c95ec5e8013265d68de166f7d3f0fc4
Jae Hyung Kim <smfc.mene@gmail.com>
2022-11-15 09:41:12 +0800

    feat: Market 타입 구현

---
+++ OpenMarket/OpenMarket/Market.swift
@@ -0,0 +1,39 @@
+//
+//  Market.swift
+//  OpenMarket
+//
+//  Created by 써니쿠키, 메네 on 15/11/2022.
+//
+
+import Foundation
+
+struct Market: Decodable {
+    let pageNo: Int
+    let itemsPerPage: Int
+    let totalCount: Int
+    let offset: Int
+    let limit: Int
+    let pages: [Page]
+    let lastPage: Int
+    let hasNext: Bool
+    let hasPrev: Bool
+    
+    struct Page: Decodable {
+        let id: Int
+        let vendorID: Int
+        let name: String
+        let thumbnail: String
+        let currency: Currency
+        let price: Int
+        let bargainPrice: Int
+        let discountedPrice: Int
+        let stock: Int
+        let createdAt: String
+        let issuedAt: String
+    }
+    
+    enum Currency: String, Decodable {
+        case krw = "KRW"
+        case usd = "USD"
+    }
+}
