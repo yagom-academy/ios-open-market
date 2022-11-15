76ee4ea5d65dd05908806fd43c42feab09aadd5d
lws2269 <lws2269@kakao.com>
2022-11-15 14:27:32 +0900

    refactor: API 통신시 JSON 타입에 맞는 모델타입으로 리팩토링

--- OpenMarket/OpenMarket/Models/Item.swift
+++ OpenMarket/OpenMarket/Models/Item.swift
@@ -5,22 +5,20 @@
 //  Created by 스톤, 로빈 on 2022/11/15.
 //
 struct Item: Codable {
-    let id: Int
-    let vendorId: Int
-    let name: String
+    let id, vendorId: Int
+    let vendorName, name, pageDescription: String
     let thumbnail: String
     let currency: Currency
-    let price: Int
-    let bargainPrice: Int
-    let discountedPrice: Int
+    let price, bargainPrice, discountedPrice: Double
     let stock: Int
-    let createdAt: String
-    let issuedAt: String
+    let createdAt, issuedAt: String
     
     enum CodingKeys: String, CodingKey {
         case id
         case vendorId = "vendor_id"
-        case name, thumbnail, currency, price
+        case vendorName, name
+        case pageDescription = "description"
+        case thumbnail, currency, price
         case bargainPrice = "bargain_price"
         case discountedPrice = "discounted_price"
         case stock
--- OpenMarket/OpenMarket/Models/ItemList.swift
+++ OpenMarket/OpenMarket/Models/ItemList.swift
@@ -5,24 +5,8 @@
 //  Created by 스톤, 로빈 on 2022/11/15.
 //
 struct ItemList: Codable {
-    let pageNo: Int
-    let itemsPerPage: Int
-    let totalCount: Int
-    let offset: Int
-    let limit: Int
-    let lastPage: Int
-    let hasNext: Bool
-    let hasPrev: Bool
+    let pageNo, itemsPerPage, totalCount, offset: Int
+    let limit, lastPage: Int
+    let hasNext, hasPrev: Bool
     let pages: [Item]
-    
-    enum CodingKeys: String, CodingKey {
-        case pageNo = "page_no"
-        case itemsPerPage = "items_per_page"
-        case totalCount = "total_count"
-        case offset ,limit
-        case lastPage = "last_page"
-        case hasNext = "has_next"
-        case hasPrev = "has_prev"
-        case pages
-    }
 }
