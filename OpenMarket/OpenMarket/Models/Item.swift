f0e4789efab18d31ad773af82a0ad9550f71acae
yuvinrho <yvrho@naver.com>
2022-11-15 10:52:43 +0900

    feat: Item 타입정의

---
+++ OpenMarket/OpenMarket/Models/Item.swift
@@ -0,0 +1,36 @@
+//
+//  Item.swift
+//  OpenMarket
+//
+//  Created by 스톤, 로빈 on 2022/11/15.
+//
+
+struct Item: Codable {
+    let id: Int
+    let vendorId: Int
+    let name: String
+    let thumbnail: String
+    let currency: Currency
+    let price: Int
+    let bargainPrice: Int
+    let discountedPrice: Int
+    let stock: Int
+    let createdAt: String
+    let issuedAt: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case vendorId = "vendor_id"
+        case name, thumbnail, currency, price
+        case bargainPrice = "bargain_price"
+        case discountedPrice = "discounted_price"
+        case stock
+        case createdAt = "created_at"
+        case issuedAt = "issued_at"
+    }
+}
+
+enum Currency: String {
+    case krw = "KRW"
+    case usd = "USD"
+}
