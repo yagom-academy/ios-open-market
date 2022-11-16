14192ea90807f433df35205749060c326d76bc12
Ayaan <onestar7984@gmail.com>
2022-11-16 11:03:23 +0900

    refactor: DataTask의 Completion에서 Data, Response, Error를 검증하도록 수정

---
+++ OpenMarket/OpenMarket/Error.swift
@@ -0,0 +1,10 @@
+//
+//  Error.swift
+//  OpenMarket
+//
+//  Created by Ayaan, junho on 2022/11/16.
+//
+
+enum OpenMarketError: Error {
+    case badStatus
+}
--- OpenMarket/OpenMarket/URLSession.swift
+++ OpenMarket/OpenMarket/URLSession.swift
@@ -8,60 +8,62 @@
 import Foundation
 
 extension URLSession {
-    private func fetchOpenMarketAPIDataTask(query: String,
-                                            completion: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+    private func fetchOpenMarketDataTask(query: String,
+                                         completion: @escaping (Data?, Error?) -> Void) -> URLSessionDataTask? {
         guard let hostURL = URL(string: "https://openmarket.yagom-academy.kr"),
               let url = URL(string: query, relativeTo: hostURL) else {
-            fatalError()
+            return nil
         }
         
         return self.dataTask(with: url) { (data, response, error) in
-            guard let data = data, error == nil else {
-                completion(nil, response, error)
-                return
+            if let error = error {
+                completion(nil, error)
+            } else if (response as? HTTPURLResponse)?.statusCode != 200 {
+                completion(nil, OpenMarketError.badStatus)
+            } else if let data = data {
+                completion(data, nil)
             }
-            completion(data, response, nil)
         }
     }
     
-    func checkHealthTask(completion: @escaping (Bool) -> Void) {
+    func fetchHealthTask(completion: @escaping (OpenMarketHealth) -> Void) {
         let query: String = "healthChecker"
-        fetchOpenMarketAPIDataTask(query: query) { (_, response, error) in
-            guard error == nil, let httpResponse = response as? HTTPURLResponse,
-                  httpResponse.statusCode == 200 else {
-                completion(false)
-                return
+        
+        fetchOpenMarketDataTask(query: query) { (data, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(.bad)
+            } else if data != nil {
+                completion(.ok)
             }
-            completion(true)
-        }.resume()
+        }?.resume()
     }
     
     func fetchPageTask(pageNumber: Int,
                        productsPerPage: Int,
                        completion: @escaping (Page?) -> Void) {
         let query: String = "api/products?page_no=\(pageNumber)&items_per_page=\(productsPerPage)"
-        fetchOpenMarketAPIDataTask(query: query) { (data, response, error) in
-            guard let httpResponse = response as? HTTPURLResponse,
-                  (200...299).contains(httpResponse.statusCode),
-                  let data = data, error == nil else {
+        
+        fetchOpenMarketDataTask(query: query) { (data, error) in
+            if let error = error {
+                print(error.localizedDescription)
                 completion(nil)
-                return
+            } else if let data = data {
+                completion(try? JSONDecoder().decode(Page.self, from: data))
             }
-            completion(try? JSONDecoder().decode(Page.self, from: data))
-        }.resume()
+        }?.resume()
     }
     
     func fetchProductTask(productNumber: Int,
                           completion: @escaping(Product?) -> Void) {
         let query: String = "api/products/\(productNumber)"
-        fetchOpenMarketAPIDataTask(query: query) { (data, response, error) in
-            guard let httpResponse = response as? HTTPURLResponse,
-                  (200...299).contains(httpResponse.statusCode),
-                  let data = data, error == nil else {
+        fetchOpenMarketDataTask(query: query) { (data, error) in
+            if let error = error {
+                print(error.localizedDescription)
                 completion(nil)
-                return
+            } else if let data = data {
+                completion(try? JSONDecoder().decode(Product.self, from: data))
             }
-            completion(try? JSONDecoder().decode(Product.self, from: data))
-        }.resume()
+        }?.resume()
     }
 }
