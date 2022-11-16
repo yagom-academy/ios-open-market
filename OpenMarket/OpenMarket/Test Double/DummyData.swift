01aebc09a34c7248625fe39b2f7ba3581fd8ed35
yjjem <marksim0606@gmail.com>
2022-11-16 23:22:04 +0900

    feat: DummyData 구조체 구현

---
+++ OpenMarket/OpenMarket/Test Double/DummyData.swift
@@ -0,0 +1,18 @@
+//
+//  DummyData.swift
+//  OpenMarket
+//  Created by inho, Hamo, Jeremy on 2022/11/16.
+//
+
+import Foundation
+
+struct DummyData {
+    let data: Data?
+    let response: URLResponse?
+    let error: Error?
+    let completionHandler: ((Data?, URLResponse?, Error?) -> Void)
+    
+    func completion() {
+        completionHandler(data, response, error)
+    }
+}
