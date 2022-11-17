f00c0c7089ce7966713cabe16d799b39309affa2
ash-youu <ash.you.dev@gmail.com>
2022-11-17 21:37:44 +0900

    refactor: getJSONData 메서드에 data 타입 매개변수 추가

--- OpenMarket/OpenMarket/Model/NetworkManager.swift
+++ OpenMarket/OpenMarket/Model/NetworkManager.swift
@@ -10,7 +10,7 @@ import Foundation
 struct NetworkManager {
     public static let publicNetworkManager = NetworkManager()
     
-    func getJSONData<T: Codable>(url: String, completion: @escaping (T) -> Void) {
+    func getJSONData<T: Codable>(url: String, type: T.Type, completion: @escaping (T) -> Void) {
         HTTPManager.requestGet(url: url) { data in
             guard let data: T = JSONConverter.decodeData(data: data) else {
                 return
