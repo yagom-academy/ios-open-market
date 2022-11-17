253b89100676e0b045ed5c36fad9a4c1cebf3d3b
ash-youu <ash.you.dev@gmail.com>
2022-11-17 21:31:20 +0900

    refactor: Item과 ItemList의 JSON 데이터를 가져오는 메서드 통합

--- OpenMarket/OpenMarket/Model/NetworkManager.swift
+++ OpenMarket/OpenMarket/Model/NetworkManager.swift
@@ -10,19 +10,9 @@ import Foundation
 struct NetworkManager {
     public static let publicNetworkManager = NetworkManager()
     
-    func getItemListData(completion: @escaping (ItemList?) -> Void) {
-        HTTPManager.requestGet(url: OpenMarketURL.base + OpenMarketURLComponent.itemPageComponent(pageNo: 1, itemPerPage: 100).url) { data in
-            guard let data: ItemList = JSONConverter.decodeData(data: data) else {
-                return
-            }
-            
-            completion(data)
-        }
-    }
-    
-    func getItemData(completion: @escaping (Item?) -> Void) {
-        HTTPManager.requestGet(url: OpenMarketURL.base + OpenMarketURLComponent.productComponent(productID: 32).url) { data in
-            guard let data: Item = JSONConverter.decodeData(data: data) else {
+    func getJSONData<T: Codable>(url: String, completion: @escaping (T) -> Void) {
+        HTTPManager.requestGet(url: url) { data in
+            guard let data: T = JSONConverter.decodeData(data: data) else {
                 return
             }
             
