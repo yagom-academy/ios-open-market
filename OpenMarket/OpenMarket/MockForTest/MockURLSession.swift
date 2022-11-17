a416f4564baa3b2cd18f0dd6da0ee3dd9dcd8218
jonghancha <jonghancha@gmail.com>
2022-11-17 12:32:46 +0900

    feat: MockForTest 추가

---
+++ OpenMarket/OpenMarket/MockForTest/MockURLSession.swift
@@ -0,0 +1,45 @@
+//  Created by Aejong, Tottale on 2022/11/17.
+
+import Foundation
+import UIKit
+
+class MockURLSession: URLSessionProtocol {
+    
+    var makeRequestFail = false
+    init(makeRequestFail: Bool = false) {
+        self.makeRequestFail = makeRequestFail
+    }
+    
+    var sessionDataTask: MockURLSessionDataTask?
+    
+    func dataTask(with request: URLRequest,
+                  completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        
+        guard let url: URL = NetworkAPI.productList(query: nil).urlComponents.url,
+              let assetData = NSDataAsset.init(name: "products") else {
+            return MockURLSessionDataTask()
+        }
+        
+        let successResponse = HTTPURLResponse(url: url,
+                                              statusCode: 200,
+                                              httpVersion: "2",
+                                              headerFields: nil)
+        
+        let failureResponse = HTTPURLResponse(url: url,
+                                              statusCode: 410,
+                                              httpVersion: "2",
+                                              headerFields: nil)
+
+        let sessionDataTask = MockURLSessionDataTask()
+        
+        sessionDataTask.resumeDidCall = {
+            if self.makeRequestFail {
+                completionHandler(nil, failureResponse, nil)
+            } else {
+                completionHandler(assetData.data, successResponse, nil)
+            }
+        }
+        self.sessionDataTask = sessionDataTask
+        return sessionDataTask
+    }
+}
