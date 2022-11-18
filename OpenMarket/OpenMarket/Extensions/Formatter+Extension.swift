e9f1c4ec3971c434ccdd0ff5b83ffc9b0c37f442
Kyo <hyosing92@gmail.com>
2022-11-18 22:11:23 +0900

    refactor : DateFormat TimeZone UTC ➡️ KST(한국 표준시)로 변경

--- OpenMarket/OpenMarket/Extensions/Formatter+Extension.swift
+++ OpenMarket/OpenMarket/Extensions/Formatter+Extension.swift
@@ -10,7 +10,7 @@ import Foundation
 extension Formatter {
     static let customDateFormat: DateFormatter = {
         let formatter = DateFormatter()
-        formatter.timeZone = TimeZone(identifier: "UTC")
+        formatter.timeZone = TimeZone(identifier: "KST")
         formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
         
         return formatter
