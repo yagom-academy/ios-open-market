d8702fe2346b5087cf768634e682778665a3e7ce
woong <workplayhard1@naver.com>
2022-11-19 16:37:23 +0900

    feat: getProductsListData(), getProductDetailData() 함수의 파라미터 값 추가

--- OpenMarket/OpenMarket/Controller/ViewController.swift
+++ OpenMarket/OpenMarket/Controller/ViewController.swift
@@ -9,12 +9,15 @@ import UIKit
 class ViewController: UIViewController {
     var networkCommunication = NetworkCommunication()
 
+    var searchListProducts: SearchListProducts?
+    var detailProduct: DetailProduct?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         getResponseAboutHealChecker()
         getProductsListData()
-        getProductDetailData()
+        getProductDetailData(productNumber: "31")
     }
     
     private func getResponseAboutHealChecker() {
@@ -28,12 +31,14 @@ class ViewController: UIViewController {
         }
     }
     
-    private func getProductsListData() {
+    private func getProductsListData(pageNumber: String = "1", itemPerPage: String = "100") {
         networkCommunication.requestProductsInformation(
-            url: "https://openmarket.yagom-academy.kr/api/products?page_no=1&items_per_page=100",
+            url: "https://openmarket.yagom-academy.kr/api/products?page_no=\(pageNumber)&items_per_page=\(itemPerPage)",
             type: SearchListProducts.self) { data in
                 switch data {
                 case .success(let data):
+                    guard let data = data as? SearchListProducts else { return }
+                    self.searchListProducts = data
                     print(data)
                 case .failure(let error):
                     print(error.localizedDescription)
@@ -41,12 +46,14 @@ class ViewController: UIViewController {
             }
     }
     
-    private func getProductDetailData() {
+    private func getProductDetailData(productNumber: String) {
         networkCommunication.requestProductsInformation(
-            url: "https://openmarket.yagom-academy.kr/api/products/32",
+            url: "https://openmarket.yagom-academy.kr/api/products/" + productNumber,
             type: DetailProduct.self) { data in
                 switch data {
                 case .success(let data):
+                    guard let data = data as? DetailProduct else { return }
+                    self.detailProduct = data
                     print(data)
                 case .failure(let error):
                     print(error.localizedDescription)
