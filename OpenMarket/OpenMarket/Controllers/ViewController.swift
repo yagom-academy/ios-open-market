7fc773f3004b8be15d4e29da2fb9b2e715193e83
yuvinrho <yvrho@naver.com>
2022-11-20 16:15:59 +0900

    feat: 네비게이션뷰 설정, segmentedControl 추가

--- OpenMarket/OpenMarket/Controllers/ViewController.swift
+++ OpenMarket/OpenMarket/Controllers/ViewController.swift
@@ -7,9 +7,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let segmentedControl: UISegmentedControl = {
+        let item = ["LIST", "GRID"]
+        let segmentedController = UISegmentedControl(items: item)
+        segmentedController.translatesAutoresizingMaskIntoConstraints = false
+        return segmentedController
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureNavigation()
+    }
+
+    func configureNavigation() {
+        navigationItem.titleView = segmentedControl
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add,
+                                                            target: self, action: nil
+        )
     }
 }
 
