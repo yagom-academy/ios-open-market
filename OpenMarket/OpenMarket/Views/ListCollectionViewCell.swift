c8c11f29378b77c47632745915e31e88a62dbe66
lj-7-77 <swtw72@gmail.com>
2022-11-21 20:02:11 +0900

    feat : ListCollectionViewCell 내부 구현
    - ListCollectionViewCell 에 이미지, 레이블 추가
    - 이미지, 레이블을 stack view로 그룹화

--- OpenMarket/OpenMarket/Views/ListCollectionViewCell.swift
+++ OpenMarket/OpenMarket/Views/ListCollectionViewCell.swift
@@ -9,8 +9,43 @@ import UIKit
 
 class ListCollectionViewCell: UICollectionViewCell {
     
+    let productImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let productNameLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let productDescriptionLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var productLabelStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [productNameLabel, productDescriptionLabel])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 5
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    lazy var productStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [productImageView, productLabelStackView])
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.spacing = 5
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
 }
 
 extension ListCollectionViewCell: ReuseIdentifierProtocol {
-    
 }
