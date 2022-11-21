d499db54487e0dce179a9896f21d79b93bbef967
Kyo <hyosing92@gmail.com>
2022-11-21 10:55:30 +0900

    feat : MainView에 SegmentedControl 구현
    - MainView에 SegmentedControl 구현
    - 추후에 사용할 listView, gridView Conponent 생성

--- OpenMarket/OpenMarket/Views/MainView.swift
+++ OpenMarket/OpenMarket/Views/MainView.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
-class MainView: UIView {
+final class MainView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .white
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    let segmentedControl: UISegmentedControl = {
+        let control = UISegmentedControl(items: ["LIST", "GRID"])
+        control.translatesAutoresizingMaskIntoConstraints = false
+        return control
+    }()
+    
+//    let listView: UICollectionView = {
+//        let listView = UICollectionView()
+//        listView.translatesAutoresizingMaskIntoConstraints = false
+//        return listView
+//    }()
+//    
+//    let gridView: UICollectionView = {
+//        let gridView = UICollectionView()
+//        gridView.translatesAutoresizingMaskIntoConstraints = false
+//        return gridView
+//    }()
+}
+
+
+// MARK: - UI Constraint
+extension MainView {
+    private func setupUI() {
+        self.addSubview(segmentedControl)
+        setupConstraint()
+    }
+    func setupConstraint() {
+        NSLayoutConstraint.activate([
+            segmentedControl.topAnchor.constraint(
+                equalTo: self.safeAreaLayoutGuide.topAnchor),
+            segmentedControl.leadingAnchor.constraint(
+                equalTo: self.safeAreaLayoutGuide.leadingAnchor),
+            segmentedControl.trailingAnchor.constraint(
+                equalTo: self.safeAreaLayoutGuide.trailingAnchor)
+        ])
     }
 }
