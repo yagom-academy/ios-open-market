3f53a395d2491deb7ddcebf0b673d7a64f8f5cbb
Gundy <korea1880@naver.com>
2022-11-21 18:41:48 +0900

    feat: navigationController 추가 및 navigationItem 추가
    - SceneDelegate에서 window?.rootViewController을 navigationController로 변경
    - segmentedControl 추가하여 navigationItem.titleView에 할당
    - addProductButton 추가하여 navigationItem.rightBarButtonItem 할당

--- OpenMarket/OpenMarket/ProductsViewController.swift
+++ OpenMarket/OpenMarket/ProductsViewController.swift
@@ -6,10 +6,30 @@
 
 import UIKit
 
-class ProductsViewController: UIViewController {
+final class ProductsViewController: UIViewController {
+    private let segmentedControl: UISegmentedControl = {
+        let segmentedControl = UISegmentedControl(items: ["LIST", "GRID"])
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.backgroundColor = .systemGray6
+        
+        return segmentedControl
+    }()
+    
+    private let addProductButton: UIBarButtonItem = {
+        let barButton = UIBarButtonItem()
+        barButton.title = "+"
+        let attributes: [NSAttributedString.Key : Any] = [.font: UIFont.boldSystemFont(ofSize: 16)]
+        barButton.setTitleTextAttributes(attributes, for: .normal)
+        
+        return barButton
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        self.navigationItem.titleView = self.segmentedControl
+        self.navigationItem.rightBarButtonItem = self.addProductButton
     }
 }
 
--- OpenMarket/OpenMarket/SceneDelegate.swift
+++ OpenMarket/OpenMarket/SceneDelegate.swift
@@ -12,10 +12,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = scene as? UIWindowScene else { return }
         let rootViewController = ProductsViewController()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
         
         window = UIWindow(frame: scene.coordinateSpace.bounds)
         window?.windowScene = scene
-        window?.rootViewController = rootViewController
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 }
