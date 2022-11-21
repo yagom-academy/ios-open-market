4b4d57022b5f086c2402fc6ea81faf1621b50040
wonbi92 <bin9239@gmail.com>
2022-11-21 11:57:34 +0900

    refactor : JSONDecoder의 decode 로직 수정
    - error처리시 error.localizedDescription를 출력하도록 로직 수정

--- OpenMarket/Extension/JSONDecoder+.swift
+++ OpenMarket/Extension/JSONDecoder+.swift
@@ -14,16 +14,8 @@ extension JSONDecoder {
         var decodedData: T?
         do {
             decodedData = try decoder.decode(type, from: data)
-        } catch DecodingError.keyNotFound(let key, let context) {
-            print("could not find key \(key) in JSON: \(context.debugDescription)")
-        } catch DecodingError.valueNotFound(let type, let context) {
-            print("could not find type \(type) in JSON: \(context.debugDescription)")
-        } catch DecodingError.typeMismatch(let type, let context) {
-            print("type mismatch for type \(type) in JSON: \(context.debugDescription)")
-        } catch DecodingError.dataCorrupted(let context) {
-            print("data found to be corrupted in JSON: \(context.debugDescription)")
-        } catch let error as NSError {
-            NSLog("Error in decode(_:from:) domain: \(error.domain), description: \(error.localizedDescription)")
+        } catch {
+            print(error.localizedDescription)
         }
         return decodedData
     }
