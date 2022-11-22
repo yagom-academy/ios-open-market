a88e9d3e950befde6012a38c871f2832c7fe394e
hashswim <ss991117@naver.com>
2022-11-22 19:13:35 +0900

    feat: navigationController 생성 및 segmentedControl 구현

--- OpenMarket/OpenMarket/ViewController.swift
+++ OpenMarket/OpenMarket/ViewController.swift
@@ -7,9 +7,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var navSegmentedView: UISegmentedControl = {
+        let segmentedControl = UISegmentedControl(items: ["LIST", "GRID"])
+        segmentedControl.addTarget(self, action: #selector(segmentChanged(_:)), for: .valueChanged)
+
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.backgroundColor = .systemBlue
+        return segmentedControl
+    }()
+    
+
+    var navRegisterButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+
+        return button
+    }()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupNavBar()
+        self.view.backgroundColor = .systemBackground
+        
         let networkManager = NetworkManager()
         
         networkManager.getHealthChecker { statusCode in
@@ -19,5 +39,24 @@ class ViewController: UIViewController {
         networkManager.getProductDetail(productNumber: 10) { product in
             print(product)
         }
+        
+    }
+    
+    func setupNavBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .systemGray6
+        self.navigationController?.navigationBar.standardAppearance = appearance
+        self.navigationController?.navigationBar.scrollEdgeAppearance = self.navigationController?.navigationBar.standardAppearance
+        
+        self.navigationController?.navigationBar.backgroundColor = .systemGray
+        
+        self.navigationItem.titleView = navSegmentedView
+        self.navigationItem.rightBarButtonItem = navRegisterButton
+            
+    }
+    
+    @objc func segmentChanged(_ sender: UISegmentedControl) {
+        print(sender.selectedSegmentIndex)
     }
 }
