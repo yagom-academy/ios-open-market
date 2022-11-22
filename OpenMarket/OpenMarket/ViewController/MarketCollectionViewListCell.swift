b65bd873b9dbb700c7e565837cbe6fbe11167a5b
Jae Hyung Kim <smfc.mene@gmail.com>
2022-11-22 14:43:38 +0800

    feat: MarketCollectionViewListCell 셀 contentView 구현 및 레이아웃 지정

--- OpenMarket/OpenMarket/ViewController/MarketCollectionViewListCell.swift
+++ OpenMarket/OpenMarket/ViewController/MarketCollectionViewListCell.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class MarketCollectionViewListCell: UICollectionViewListCell {
     var pageData: Page?
+    lazy var pageListContentView = UIListContentView(configuration: configureListCell())
+    var stockLabel = UILabel()
+    var stockLabelConstraints: (leading: NSLayoutConstraint, trailing: NSLayoutConstraint)?
     
     override var configurationState: UICellConfigurationState {
         var state = super.configurationState
         state.pageData = self.pageData
         return state
+    }
+    
+    func configureListCell() -> UIListContentConfiguration {
+        return .subtitleCell()
     }
 }
 
@@ -25,5 +32,36 @@ extension UIConfigurationState {
     var pageData: Page? {
         get { return self[.page] as? Page }
         set { self[.page] = newValue }
+    }
+}
+
+extension MarketCollectionViewListCell {
+    func setupViewsIfNeeded() {
+        guard stockLabelConstraints == nil else { return }
+        
+        [pageListContentView, stockLabel].forEach {
+            contentView.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        let labelConstraints = (leading:
+                                    stockLabel.leadingAnchor.constraint(
+                                        greaterThanOrEqualTo:pageListContentView.trailingAnchor
+                                    ),
+                                trailing:
+                                    stockLabel.trailingAnchor.constraint(
+                                        equalTo: contentView.trailingAnchor
+                                    ))
+        
+        NSLayoutConstraint.activate([
+            pageListContentView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            pageListContentView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            pageListContentView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            pageListContentView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            labelConstraints.leading,
+            labelConstraints.trailing
+        ])
+        
+        stockLabelConstraints = labelConstraints
     }
 }
