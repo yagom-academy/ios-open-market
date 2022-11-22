8cca1a39620d00d7ec323a8fc76e0ec8c3e57790
zhilly11 <zhzh1x4@gmail.com>
2022-11-22 16:28:39 +0900

    feat: CollectionView에 커스텀 cell 등록, ListCollectionViewCell에 UI요소 할당

--- OpenMarket/OpenMarket/Sources/Controller/ListViewController.swift
+++ OpenMarket/OpenMarket/Sources/Controller/ListViewController.swift
@@ -8,7 +8,34 @@ import UIKit
 
 class ListViewController: UIViewController {
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCollectionView()
+    }
+    
+    private func configureCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        let collectionViewCellNib = UINib(nibName: String(describing: ListCollectionViewCell.self), bundle: nil)
+        
+        self.collectionView.register(collectionViewCellNib, forCellWithReuseIdentifier: String(describing: ListCollectionViewCell.self))
+    }
+}
+
+extension ListViewController: UICollectionViewDelegate {
+    
+}
+
+extension ListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: ListCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "ListCollectionViewCell", for: indexPath) as! ListCollectionViewCell
+        return cell
     }
 }
--- OpenMarket/OpenMarket/Sources/View/ListCollectionViewCell.swift
+++ OpenMarket/OpenMarket/Sources/View/ListCollectionViewCell.swift
@@ -7,7 +7,13 @@
 import UIKit
 
 class ListCollectionViewCell: UICollectionViewCell {
-
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var bargainPriceLabel: UILabel!
+    @IBOutlet weak var stockLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
