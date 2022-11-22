c6589f5d5a1df264bd3601768dcc555f11da5a03
lws2269 <lws2269@kakao.com>
2022-11-22 13:03:38 +0900

    feat: String Extension 추가

---
+++ OpenMarket/OpenMarket/Extension/String+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  String++Extension.swift
+//  OpenMarket
+//
+//  Created by leewonseok on 2022/11/22.
+//
+import UIKit
+
+extension String {
+    func strikeThrough() -> NSAttributedString {
+        let attributeString = NSMutableAttributedString(string: self)
+        attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: NSMakeRange(0, attributeString.length))
+        return attributeString
+    }
+}
