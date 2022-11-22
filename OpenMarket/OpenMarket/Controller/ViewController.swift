c4081475a97ccea71d1852be61c711a3f6128aaa
yjjem <marksim0606@gmail.com>
2022-11-22 16:21:27 +0900

    feat: configureProductCollectionView 메서드를 이용한 CollectionView 구현

--- OpenMarket/OpenMarket/Controller/ViewController.swift
+++ OpenMarket/OpenMarket/Controller/ViewController.swift
@@ -11,13 +11,30 @@ class ViewController: UIViewController {
         let segment = UISegmentedControl(items: ["list", "grid"])
         return segment
     }()
-
+    
+    var productCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.titleView = segmentControl
+        configureProductCollectionView()
     }
-
+    
+    func configureProductCollectionView() {
+        let configure = UICollectionLayoutListConfiguration(appearance: .plain)
+        let layout = UICollectionViewCompositionalLayout.list(using: configure)
+        
+        productCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        productCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(productCollectionView)
+        NSLayoutConstraint.activate([
+            productCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            productCollectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            productCollectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            productCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 
 }
 
