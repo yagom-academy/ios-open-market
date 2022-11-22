1b815c5262a38cee713c8fdc58e37096ed86f7c9
jsa0224 <94514250+jsa0224@users.noreply.github.com>
2022-11-22 13:57:51 +0900

    feat: segmentedControl, plusButton 구현

--- OpenMarket/OpenMarket/Controller/ViewController.swift
+++ OpenMarket/OpenMarket/Controller/ViewController.swift
@@ -7,9 +7,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    let segmentedControl: UISegmentedControl = {
+        let control = UISegmentedControl(items: ["List", "Grid"])
+
+        control.selectedSegmentIndex = 0
+        control.autoresizingMask = .flexibleWidth
+        control.frame = CGRect(x: 0, y: 0, width: 200, height: 30)
+        control.addTarget(self, action: #selector(didChangeValue(segment:)), for: .valueChanged)
+
+        return control
+    }()
+
+    let plusButton = UIBarButtonItem(image: UIImage(systemName: "plus"),
+                                     style: .plain,
+                                     target: self,
+                                     action: #selector(tapped(sender:)))
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.navigationItem.titleView = segmentedControl
+        self.navigationItem.rightBarButtonItem = plusButton
+
+    }
+
+    @objc private func didChangeValue(segment: UISegmentedControl) {
+        print("얍!")
+    }
+
+    @objc private func tapped(sender: UIBarButtonItem) {
+        print("tapped!")
     }
 }
