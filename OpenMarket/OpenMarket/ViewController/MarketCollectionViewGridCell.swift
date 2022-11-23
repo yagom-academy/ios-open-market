d160529a783be0021fbb1a3ea457fb01edda075f
Jae Hyung Kim <smfc.mene@gmail.com>
2022-11-23 10:13:18 +0800

    feat: MarketCollectionViewGridCell 구성요소 오토레이아웃 설정

--- OpenMarket/OpenMarket/ViewController/MarketCollectionViewGridCell.swift
+++ OpenMarket/OpenMarket/ViewController/MarketCollectionViewGridCell.swift
@@ -36,4 +36,32 @@ class MarketCollectionViewGridCell: UICollectionViewCell {
         label.textAlignment = .center
         return label
     }()
+    
+    func creatCellLayout() {
+        [productImage, nameLabel, priceLabel, stockLabel].forEach {
+            contentView.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            productImage.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            productImage.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.8),
+            productImage.heightAnchor.constraint(equalTo: productImage.widthAnchor),
+            productImage.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            
+            nameLabel.topAnchor.constraint(equalTo: productImage.bottomAnchor, constant: 10),
+            nameLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            
+            priceLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 10),
+            priceLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            
+            stockLabel.topAnchor.constraint(equalTo: priceLabel.bottomAnchor, constant: 10),
+            stockLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            stockLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10)
+        ])
+    }
+    
+    
+    
+    
 }
