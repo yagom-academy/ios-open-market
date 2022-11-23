b89695d3e156f0c5ded2632c9f4e38d9d95c7834
summercat <jyerin0812@gmail.com>
2022-11-23 13:11:39 +0900

    chore: VC의 테스트 내역 삭제

--- OpenMarket/OpenMarket/ViewController.swift
+++ OpenMarket/OpenMarket/ViewController.swift
@@ -10,25 +10,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let manager = NetworkManager()
-        manager.request(endpoint: OpenMarketAPI.product(id: 7), dataType: Product.self) { result in
-            switch result {
-            case .success(let data):
-                print(data)
-            case .failure(let error):
-                print(error)
-            }
-        }
-        
-        manager.checkAPIHealth(endpoint: OpenMarketAPI.healthChecker) { result in
-            switch result {
-            case .success(let result):
-                print(result)
-            case .failure(let error):
-                print(error)
-            }
-        }
+
     }
 }
 
