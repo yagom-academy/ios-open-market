106ac22212e75afbd61450011fd417fc5b67dd0d
dylan-yoon <d.dylany21@gmail.com>
2022-11-23 13:56:42 +0900

    feat: SegmentedControl, UIBarButtonItem 생성

--- OpenMarket/OpenMarket/Resource/Controller/ProductsViewController.swift
+++ OpenMarket/OpenMarket/Resource/Controller/ProductsViewController.swift
@@ -7,10 +7,31 @@
 import UIKit
 
 class ProductsViewController: UIViewController {
+    let addButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(barButtonSystemItem: .add, target: nil, action: nil)
+        
+        return button
+    }()
+    
+    lazy var segment: UISegmentedControl = {
+        let segment = UISegmentedControl(items: ["LIST", "GRID"])
+        segment.translatesAutoresizingMaskIntoConstraints = false
+        segment.selectedSegmentIndex = 0
+        segment.addTarget(self, action: #selector(didChangedSegmentIndex(sender:)), for: .valueChanged)
+        
+        return segment
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
         
-        view.backgroundColor = .red
+        self.navigationItem.rightBarButtonItem = addButton
+        self.navigationItem.titleView = segment
+    }
+    
+    @objc func didChangedSegmentIndex(sender: UISegmentedControl) {
+        print(sender.selectedSegmentIndex)
     }
 }
 
