f32dfc652a2717f7917cd7530a6cd3e3d0b2001b
summercat <jyerin0812@gmail.com>
2022-11-25 21:43:57 +0900

    feat: Formatter Extension 생성

---
+++ OpenMarket/OpenMarket/Extension/Formatter+.swift
@@ -0,0 +1,25 @@
+//  Formatter+.swift
+//  OpenMarket
+//  Created by SummerCat & Bella on 2022/11/25.
+
+import Foundation
+
+extension Formatter {
+    static func format(_ number: Double, _ currency: Currency) -> String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        
+        switch currency {
+        case .krw:
+            formatter.maximumFractionDigits = 0
+        case .usd:
+            formatter.maximumFractionDigits = 2
+        }
+
+        guard let result = formatter.string(from: number as NSNumber) else {
+            return "\(number)"
+        }
+        
+        return result
+    }
+}
