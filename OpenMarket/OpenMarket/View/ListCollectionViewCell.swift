099ea3e334761a7ff11a8b756f78e5b1a53d2b6c
ash-youu <ash.you.dev@gmail.com>
2022-11-27 15:04:50 +0900

    feat: ListCollectionViewCell의 오토레이아웃 및 로딩 뷰 설정

--- OpenMarket/OpenMarket/View/ListCollectionViewCell.swift
+++ OpenMarket/OpenMarket/View/ListCollectionViewCell.swift
@@ -8,70 +8,112 @@
 import UIKit
 
 class ListCollectionViewCell: UICollectionViewCell {
-    static let cellIdentifier = "listCell"
+    var product: Item?
     
-    let productImage = UIImageView()
-    let productNameLabel = UILabel()
-    let priceLabel = UILabel()
-    let priceForSaleLabel = UILabel()
-    let stockLabel = UILabel()
+    private let mainStackView = UIStackView()
+    private let labelStackView = UIStackView()
+    private let priceStackView = UIStackView()
+    private var accessoryView = UIImageView()
+    private let productImage = UIImageView()
+    private let productNameLabel = UILabel()
+    private let priceLabel = UILabel()
+    private let priceForSaleLabel = UILabel()
+    private let stockLabel = UILabel()
+    private let spacingView = UIView()
+    private let loadingView = UIActivityIndicatorView(style: .large)
     
-    lazy var mainStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.addArrangedSubview(productImage)
-        stackView.addArrangedSubview(labelStackView)
-        stackView.addArrangedSubview(stockLabel)
-        
-        stackView.axis = .horizontal
-        stackView.alignment = .fill
-        stackView.distribution = .fill
-        stackView.spacing = 10
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        return stackView
-    }()
-    
-    lazy var labelStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.addArrangedSubview(productNameLabel)
-        stackView.addArrangedSubview(priceStackView)
-        
-        stackView.axis = .vertical
-        stackView.alignment = .fill
-        stackView.distribution = .fill
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        return stackView
-    }()
-    
-    lazy var priceStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.addArrangedSubview(priceLabel)
-        stackView.addArrangedSubview(priceForSaleLabel)
-        
-        stackView.axis = .horizontal
-        stackView.alignment = .fill
-        stackView.distribution = .fill
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        return stackView
-    }()
-    
-    init() {
-        super.init(frame: .zero)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureAttribute()
+        configureLayout()
     }
     
+    @available(*, unavailable)
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setConstraints() {
+    private func configureAttribute() {
+        let priceHuggingPriority = priceForSaleLabel.contentHuggingPriority(for: .horizontal) + 1
+        let stockHuggingPriority = productNameLabel.contentHuggingPriority(for: .horizontal) + 1
+        let stockCompressionPriority = productNameLabel.contentCompressionResistancePriority(for: .horizontal) + 1
+        
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        labelStackView.translatesAutoresizingMaskIntoConstraints = false
+        priceStackView.translatesAutoresizingMaskIntoConstraints = false
+        accessoryView.translatesAutoresizingMaskIntoConstraints = false
+        productImage.translatesAutoresizingMaskIntoConstraints = false
+        productNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        stockLabel.translatesAutoresizingMaskIntoConstraints = false
+        spacingView.translatesAutoresizingMaskIntoConstraints = false
+        loadingView.translatesAutoresizingMaskIntoConstraints = false
+        
+        mainStackView.distribution = .equalSpacing
+        mainStackView.axis = .vertical
+        mainStackView.spacing = 10
+        labelStackView.distribution = .equalSpacing
+        labelStackView.axis = .horizontal
+        priceStackView.distribution = .fill
+        priceStackView.axis = .horizontal
+        
+        productNameLabel.textAlignment = .left
+        productNameLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        stockLabel.setContentHuggingPriority(stockHuggingPriority, for: .horizontal)
+        stockLabel.setContentCompressionResistancePriority(stockCompressionPriority, for: .horizontal)
+        stockLabel.textAlignment = .right
+        priceLabel.setContentHuggingPriority(priceHuggingPriority, for: .horizontal)
+        priceLabel.isHidden = true
+        spacingView.setContentHuggingPriority(priceHuggingPriority, for: .horizontal)
+        priceForSaleLabel.textAlignment = .left
+        
+        let image = UIImageView(image: UIImage(systemName: "chevron.right"))
+        accessoryView = image
+        accessoryView.tintColor = .systemGray
+    }
+    
+    private func configureLayout() {
+        contentView.addSubview(loadingView)
+        contentView.addSubview(productImage)
+        contentView.addSubview(mainStackView)
+        labelStackView.addArrangedSubview(productNameLabel)
+        labelStackView.addArrangedSubview(stockLabel)
+        labelStackView.addArrangedSubview(accessoryView)
+        mainStackView.addArrangedSubview(labelStackView)
+        mainStackView.addArrangedSubview(priceStackView)
+        priceStackView.addArrangedSubview(priceLabel)
+        priceStackView.addArrangedSubview(spacingView)
+        priceStackView.addArrangedSubview(priceForSaleLabel)
+        
         NSLayoutConstraint.activate([
-            mainStackView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
-            mainStackView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
-            mainStackView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
-            mainStackView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor)
+            loadingView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 5),
+            loadingView.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor),
+            loadingView.heightAnchor.constraint(equalToConstant: 70),
+            loadingView.widthAnchor.constraint(equalToConstant: 70),
+            
+            productImage.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 5),
+            productImage.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor),
+            productImage.heightAnchor.constraint(equalToConstant: 70),
+            productImage.widthAnchor.constraint(equalToConstant: 70),
+            
+            mainStackView.leadingAnchor.constraint(equalTo: productImage.trailingAnchor, constant: 10),
+            mainStackView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -15),
+            mainStackView.centerYAnchor.constraint(equalTo: productImage.centerYAnchor),
+            
+            productNameLabel.leadingAnchor.constraint(equalTo: priceStackView.leadingAnchor),
+            stockLabel.leadingAnchor.constraint(equalTo: productNameLabel.trailingAnchor, constant: 5),
+            stockLabel.trailingAnchor.constraint(equalTo: accessoryView.leadingAnchor, constant: -5),
+            
+            accessoryView.heightAnchor.constraint(equalToConstant: 17),
+            accessoryView.widthAnchor.constraint(equalTo: accessoryView.heightAnchor),
+            
+            spacingView.widthAnchor.constraint(equalToConstant: 6)
         ])
     }
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        productImage.image = nil
+        loadingView.startAnimating()
+        loadingView.isHidden = false
+    }
 }
