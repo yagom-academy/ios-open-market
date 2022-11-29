5cd92872dabf699138ad89529b0b70d1efcdcb27
lws2269 <lws2269@kakao.com>
2022-11-29 11:25:17 +0900

    feat: NetworkManager addItem, createBody 메서드 추가

--- OpenMarket/OpenMarket/Network/NetworkManager.swift
+++ OpenMarket/OpenMarket/Network/NetworkManager.swift
@@ -6,7 +6,11 @@
 //
 
 import UIKit
-
+struct ImageFile {
+    let filename: String
+    let data: Data
+    let type: String
+}
 struct NetworkManager {
     let baseURL: String
     var session: URLSessionProtocol
@@ -95,11 +99,11 @@ struct NetworkManager {
     
     func fetchImage(url: URL, completion: @escaping (UIImage) -> ()) {
         let cachedKey = NSString(string: "\(url)")
-
+        
         if let cachedImage = ImageCacheManager.shared.object(forKey: cachedKey) {
             return completion(cachedImage)
         }
-
+        
         DispatchQueue.global(qos: .utility).async {
             if let data = try? Data(contentsOf: url),
                let image = UIImage(data: data) {
@@ -107,5 +111,92 @@ struct NetworkManager {
                 completion(image)
             }
         }
+    }
+    // 네트워크 매니저 해야할 일
+    
+    // 상품 수정, 등록, 삭제 2개
+    
+    func createBody(params: [String:Any], boundary: String, images: [ImageFile]?) -> Data {
+        var body = Data()
+        let boundaryPrefix = "--\(boundary)\r\n"
+        
+        for (key, value) in params {
+            print("key: \(key), value: \(value)")
+            body.append(boundaryPrefix.data(using: .utf8)!)
+            body.append("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n".data(using: .utf8)!)
+            body.append("\(value)\r\n".data(using: .utf8)!)
+        }
+        
+        if let images = images {
+            for image in images {
+                body.append(boundaryPrefix.data(using: .utf8)!)
+                body.append("Content-Disposition: form-data; name=\"images\"; filename=\"\(image.filename)\"\r\n".data(using: .utf8)!)
+                body.append("Content-Type: image/\(image.type)\r\n\r\n".data(using: .utf8)!)
+                body.append(image.data)
+                body.append("\r\n".data(using: .utf8)!)
+            }
+        }
+        
+        body.append(boundaryPrefix.data(using: .utf8)!)
+        
+        return body
+    }
+    
+    func addItem(completion: @escaping (String) -> Void){
+        let urlString = "\(baseURL)api/products"
+        let boundary = "Boundary-\(UUID().uuidString)"
+        
+        guard let image = UIImage(named: "Dog")else { return }
+        let fileImageData = ImageFile(filename: "Dog", data: image.pngData()!, type: "png")
+        
+        guard let url = URL(string: urlString) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("c5b13912-43b9-11ed-8b9b-0956155ef06a", forHTTPHeaderField: "identifier")
+        request.setValue("multipart/form-data; boundary\(boundary)", forHTTPHeaderField: "Content-Type")
+        
+        let params: [String:Any] = ["name" : "개껌",
+                                   "description" : "간식",
+                                    "price" : 1000.0,
+                                    "currency" : Currency.krw.rawValue,
+                                   "discounted_price" : 100,
+                                   "stock" : 100,
+                                   "secret" : "snnq45ezg2tn9amy"]
+        
+        request.httpBody = createBody(params: params, boundary: boundary, images: [fileImageData])
+        print("=============================")
+        print(request)
+        print("=============================")
+        URLSession.shared.dataTask(with: request) { data, rsp, err in
+            if err != nil {
+                return completion("dpfj")
+            }
+            
+            guard let httpResponse = rsp as? HTTPURLResponse,
+                  (200..<300).contains(httpResponse.statusCode) else {
+                let abc = rsp as? HTTPURLResponse
+                return completion("\(abc?.statusCode)스테이터스에러")
+            }
+            
+            guard let data = data else { return completion("데이터에러")}
+            
+            do {
+                //                let item: Item = try JSONDecoder().decode(Item.self, from: data)
+                return completion("성공")
+            } catch {
+                return completion("파싱실패")
+                //                completion(.failure(.parseError))
+            }
+        }.resume()
+    }
+    
+    
+    
+    func updateItem() {
+        
+    }
+    
+    func deleteItem(){
+        
     }
 }
