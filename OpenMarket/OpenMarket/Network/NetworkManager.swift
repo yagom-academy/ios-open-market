f14b43d844ec42d876232af6177a4fb37949341a
lws2269 <lws2269@kakao.com>
2022-11-29 19:32:40 +0900

    refactor: Post요청으로 상품등록 구현

--- OpenMarket/OpenMarket/Network/NetworkManager.swift
+++ OpenMarket/OpenMarket/Network/NetworkManager.swift
@@ -15,6 +15,9 @@ struct NetworkManager {
     let baseURL: String
     var session: URLSessionProtocol
     
+    static let identifier = "f5948cd0-6940-11ed-a917-15417865aa81"
+    static let secret = "snnq45ezg2tn9amy"
+    
     init(urlString: String = "https://openmarket.yagom-academy.kr/",
          session: URLSessionProtocol = URLSession(configuration: .default)) {
         self.baseURL = urlString
@@ -112,91 +115,55 @@ struct NetworkManager {
             }
         }
     }
-    // 네트워크 매니저 해야할 일
-    
-    // 상품 수정, 등록, 삭제 2개
-    
-    func createBody(params: [String:Any], boundary: String, images: [ImageFile]?) -> Data {
-        var body = Data()
-        let boundaryPrefix = "--\(boundary)\r\n"
-        
-        for (key, value) in params {
-            print("key: \(key), value: \(value)")
-            body.append(boundaryPrefix.data(using: .utf8)!)
-            body.append("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n".data(using: .utf8)!)
-            body.append("\(value)\r\n".data(using: .utf8)!)
-        }
-        
-        if let images = images {
-            for image in images {
-                body.append(boundaryPrefix.data(using: .utf8)!)
-                body.append("Content-Disposition: form-data; name=\"images\"; filename=\"\(image.filename)\"\r\n".data(using: .utf8)!)
-                body.append("Content-Type: image/\(image.type)\r\n\r\n".data(using: .utf8)!)
-                body.append(image.data)
-                body.append("\r\n".data(using: .utf8)!)
-            }
-        }
-        
-        body.append(boundaryPrefix.data(using: .utf8)!)
-        
-        return body
-    }
-    
-    func addItem(completion: @escaping (String) -> Void){
-        let urlString = "\(baseURL)api/products"
+}
+
+extension NetworkManager {
+    func addItem(completion: @escaping (String) -> ()) {
         let boundary = "Boundary-\(UUID().uuidString)"
         
-        guard let image = UIImage(named: "Dog")else { return }
-        let fileImageData = ImageFile(filename: "Dog", data: image.pngData()!, type: "png")
+        var request = URLRequest(url: URL(string: "https://openmarket.yagom-academy.kr/api/products")!)
         
-        guard let url = URL(string: urlString) else { return }
-        var request = URLRequest(url: url)
+        
         request.httpMethod = "POST"
-        request.addValue("c5b13912-43b9-11ed-8b9b-0956155ef06a", forHTTPHeaderField: "identifier")
-        request.setValue("multipart/form-data; boundary\(boundary)", forHTTPHeaderField: "Content-Type")
-        
-        let params: [String:Any] = ["name" : "개껌",
-                                   "description" : "간식",
-                                    "price" : 1000.0,
-                                    "currency" : Currency.krw.rawValue,
-                                   "discounted_price" : 100,
-                                   "stock" : 100,
-                                   "secret" : "snnq45ezg2tn9amy"]
-        
-        request.httpBody = createBody(params: params, boundary: boundary, images: [fileImageData])
-        print("=============================")
-        print(request)
-        print("=============================")
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        request.setValue(NetworkManager.identifier, forHTTPHeaderField: "identifier")
+        let jsonData = try? JSONSerialization.data(withJSONObject:
+                                                    ["name": "호뻥", "description": "간식", "price": 1000, "currency": "KRW", "stock": 1, "secret": "snnq45ezg2tn9amy"])
+        request.httpBody = createRequestBody(params: ["params" : jsonData!], boundary: boundary)
         URLSession.shared.dataTask(with: request) { data, rsp, err in
-            if err != nil {
-                return completion("dpfj")
-            }
-            
-            guard let httpResponse = rsp as? HTTPURLResponse,
-                  (200..<300).contains(httpResponse.statusCode) else {
-                let abc = rsp as? HTTPURLResponse
-                return completion("\(abc?.statusCode)스테이터스에러")
-            }
-            
-            guard let data = data else { return completion("데이터에러")}
-            
-            do {
-                //                let item: Item = try JSONDecoder().decode(Item.self, from: data)
-                return completion("성공")
-            } catch {
-                return completion("파싱실패")
-                //                completion(.failure(.parseError))
-            }
+            print("======================================")
+            print("=========data==========")
+            print(String(data: data!, encoding: .utf8))
+            print("=========rsp==========")
+            print((rsp as! HTTPURLResponse).statusCode)
+            print("=========err==========")
+            print(err)
+            print("======================================")
         }.resume()
     }
     
-    
-    
-    func updateItem() {
+    func createRequestBody(params: [String: Data], boundary: String) -> Data {
+        let boundaryPrefix = "--\(boundary)\r\n"
         
-    }
-    
-    func deleteItem(){
+        var body = Data()
         
+        for (key, value) in params {
+            body.append(boundaryPrefix.data(using: .utf8)!)
+            body.append("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n".data(using: .utf8)!)
+            body.append(value)
+            body.append("\r\n".data(using: .utf8)!)
+        }
+        let imgDataKey = "images"
+        let filename = "ghvkdvjscl12.jpeg"
+        let mimeType = "image/jpeg"
+        guard let imageData = UIImage(named: "ghvjs12")?.jpegData(compressionQuality: 0.5) else { return Data() }
+        body.append(boundaryPrefix.data(using: .utf8)!)
+        body.append("Content-Disposition: form-data; name=\"\(imgDataKey)\"; filename=\"\(filename)\"\r\n".data(using: .utf8)!)
+        body.append("Content-Type: \(mimeType)\r\n\r\n".data(using: .utf8)!)
+        body.append(imageData)
+        body.append("\r\n".data(using: .utf8)!)
+        print(String(data: body, encoding: .utf8))
+        body.append("--".appending(boundary.appending("--")).data(using: .utf8)!)
+        return body
     }
 }
