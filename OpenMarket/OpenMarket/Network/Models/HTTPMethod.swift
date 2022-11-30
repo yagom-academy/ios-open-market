07e815ff4c4639fc6568b51316153088c01bb39f
lws2269 <lws2269@kakao.com>
2022-11-30 16:48:12 +0900

    feat: HTTPMethod 열거형 추가

---
+++ OpenMarket/OpenMarket/Network/Models/HTTPMethod.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPMethod.swift
+//  OpenMarket
+//
+//  Created by leewonseok on 2022/11/30.
+//
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
