9b20266b59665fbbe52fcf202d7acda8e6e9b0f3
jin <pearlisrich@naver.com>
2022-11-30 15:15:58 +0900

    refactor: NetworkAPIProvider fetch 만 남도록 수정 및 ProductNetworkManager 생성

--- OpenMarket/OpenMarket/Utilities/NetworkAPIProvider.swift
+++ OpenMarket/OpenMarket/Utilities/NetworkAPIProvider.swift
@@ -9,45 +9,33 @@ final class NetworkAPIProvider {
         self.session = session
     }
     
-    func fetchProductList(query: [Query: String]?,
-                          completion: @escaping (Result<ProductList, Error>) -> Void) {
-        fetch(path: .productList(query: query)) { result in
-            switch result {
-            case .failure(let error):
+    
+    func fetch(url: URL?, completion: @escaping (Result<Data, Error>) -> Void) {
+        guard let url else { return }
+        
+        self.session.dataTask(with: url) { data, response, error in
+            if let error = error {
                 completion(.failure(error))
-            case .success(let data):
-                guard let productList: ProductList = JSONDecoder().decode(data: data) else {
-                    completion(.failure(NetworkError.decodeFailed))
-                    return
-                }
-                completion(.success(productList))
+                return
             }
-        }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                completion(.failure(NetworkError.serverFailed))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NetworkError.invalidData))
+                return
+            }
+            
+            completion(.success(data))
+        }.resume()
     }
     
-    func fetchImage(url: String, completion: @escaping (Result<UIImage, Error>) -> Void) {
-        DispatchQueue.global(qos: .background).async {
-            guard let url = URL(string: url) else { return }
-            URLSession.shared.dataTask(with: url) { (data, result, error) in
-                
-                if let error {
-                    completion(.failure(error))
-                    return
-                }
-                
-                if let data = data, let image = UIImage(data: data) {
-                    completion(.success(image))
-                }
-                
-            }.resume()
-        }
-    }
-}
-
-extension NetworkAPIProvider {
-    
-    func fetch(path: NetworkAPI, completion: @escaping (Result<Data, Error>) -> Void) {
-        guard let url = path.urlComponents.url else { return }
+    func fetch(url: String, completion: @escaping (Result<Data, Error>) -> Void) {
+        guard let url = URL(string: url) else { return }
         
         self.session.dataTask(with: url) { data, response, error in
             if let error = error {
--- OpenMarket/OpenMarket/Utilities/ProductNetworkManager.swift
+++ OpenMarket/OpenMarket/Utilities/ProductNetworkManager.swift
@@ -13,8 +13,17 @@ final class ProductNetworkManager {
     private init() {}
     
     func fetchProductList(completion: @escaping (Result<ProductList, Error>) -> Void) {
-        networkProvider.fetchProductList(query: [.itemsPerPage: "200"]) { result in
-            completion(result)
+        networkProvider.fetch(url: NetworkAPI.productList(query: [.itemsPerPage: "200"]).urlComponents.url) { result in
+            switch result {
+            case .success(let data):
+                guard let productList: ProductList = JSONDecoder().decode(data: data) else {
+                    completion(.failure(NetworkError.decodeFailed))
+                    return
+                }
+                completion(.success(productList))
+            case .failure(let error):
+                completion(.failure(error))
+            }
         }
     }
 }
