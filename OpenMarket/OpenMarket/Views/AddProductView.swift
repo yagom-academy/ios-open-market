d2035f28cf6326195b02140df46c94d2bc75d7bf
lj-7-77 <swtw72@gmail.com>
2022-11-30 23:37:13 +0900

    feat : AddProductView UI 생성
    - AddProductView UI Component 구현
    - 오토레이아웃 구현

--- OpenMarket/OpenMarket/Views/AddProductView.swift
+++ OpenMarket/OpenMarket/Views/AddProductView.swift
@@ -11,10 +11,90 @@ final class AddProductView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .white
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
+    let nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "상품명"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    let priceTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "상품가격"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    let salePriceTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "할인가격"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    let stockTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "재고수량"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    let descriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    let currencySegmentedControl: UISegmentedControl = {
+        let segmentedControl = UISegmentedControl(items: ["KRW", "USD"])
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.layer.borderWidth = 1
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        return segmentedControl
+    }()
+    
+    lazy var priceStackView: UIStackView = {
+       let stackView = UIStackView(arrangedSubviews: [priceTextField, currencySegmentedControl])
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    lazy var productStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [nameTextField, priceStackView, salePriceTextField, stockTextField])
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
 }
+
+// MARK: - Constraints
+extension AddProductView {
+    private func setupUI() {
+        setupView()
+        setupConstraints()
+    }
+    
+    private func setupView() {
+        self.addSubview(productStackView)
+        self.addSubview(descriptionTextView)
+    }
+    
+    private func setupConstraints() {
+        
+    }
+}
+
