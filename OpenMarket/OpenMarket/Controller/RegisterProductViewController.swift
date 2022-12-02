71f8383e119052cc324b157e746bec1d20fc7cd9
Mangdi <lkw4670@naver.com>
2022-12-02 10:45:36 +0900

    feat: RegisterProductViewController 생성

---
+++ OpenMarket/OpenMarket/Controller/RegisterProductViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterProductViewController.swift
+//  OpenMarket
+//
+//  Created by Mangdi on 2022/12/02.
+//
+
+import UIKit
+
+class RegisterProductViewController: UIViewController {
+    
+    @IBOutlet weak var cancelBarButtonItem: UIBarButtonItem!
+    @IBOutlet weak var doneBarButtonItem: UIBarButtonItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
