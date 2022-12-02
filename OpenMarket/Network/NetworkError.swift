90ec423bc38cecc4c677f84483e7eb8f1ed10a59
wonbi92 <bin9239@gmail.com>
2022-12-02 12:41:33 +0900

    fix : Info위치로 인한 빌드 실패 오류 수정

---
+++ OpenMarket/Network/NetworkError.swift
@@ -0,0 +1,31 @@
+//
+//  NetworkError.swift
+//  OpenMarket
+//
+//  Created by Gundy, Wonbi on 2022/11/29.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidRequest
+    case requestFailed(description: String)
+    case responseFailed(code: Int)
+    case invalidData
+    case invalidURL
+    
+    var errorDescription: String {
+        switch self {
+        case .invalidRequest:
+            return "invalidRequest"
+        case .requestFailed(let description):
+            return description
+        case .responseFailed(let code):
+            return "statusCode: \(code)"
+        case .invalidData:
+            return "invalidData"
+        case .invalidURL:
+            return "invalidURL"
+        }
+    }
+}
