af163da6a7670ac45754140a5ae57df0e7187995
woong <workplayhard1@naver.com>
2022-12-02 11:40:20 +0900

    feat: 상품등록 UI 구성 및 오토레이아웃, 숫자패드 설정, IBOutlet 설정

--- OpenMarket/OpenMarket/Controller/RegisterProductViewController.swift
+++ OpenMarket/OpenMarket/Controller/RegisterProductViewController.swift
@@ -8,13 +8,30 @@
 import UIKit
 
 class RegisterProductViewController: UIViewController {
+    let networkCommunication = NetworkCommunication()
     
     @IBOutlet weak var cancelBarButtonItem: UIBarButtonItem!
     @IBOutlet weak var doneBarButtonItem: UIBarButtonItem!
 
+    @IBOutlet weak var productNameTextField: UITextField!
+    @IBOutlet weak var productPriceTextField: UITextField!
+    @IBOutlet weak var productDiscountedPriceTextField: UITextField!
+    @IBOutlet weak var productStockTextField: UITextField!
+    @IBOutlet weak var productCurrencySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var productDescriptionTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    @IBAction func touchUpCancelBarButtonItem(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true)
+    }
+    
+    @IBAction func touchUpDoneBarButtonItem(_ sender: UIBarButtonItem) {
+
+        self.dismiss(animated: true)
     }
     
 }
