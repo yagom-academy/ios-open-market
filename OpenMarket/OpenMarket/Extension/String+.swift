c53b9334a79e3de611b4191d9ce66883645a4695
summercat <jyerin0812@gmail.com>
2022-12-03 21:05:48 +0900

    chore: Deployment Target 14.0으로 통일, style: 코드 컨벤션과 가독성을 고려한 개행 수정

--- OpenMarket/OpenMarket/Extension/String+.swift
+++ OpenMarket/OpenMarket/Extension/String+.swift
@@ -7,13 +7,23 @@ import UIKit
 extension String {
     func invalidatePrice() -> NSAttributedString {
         let attributeString = NSMutableAttributedString(string: self)
-        attributeString.addAttributes([NSAttributedString.Key.foregroundColor: UIColor.red, NSAttributedString.Key.strikethroughStyle: NSUnderlineStyle.single.rawValue, NSAttributedString.Key.strikethroughColor: UIColor.red], range: NSMakeRange(0, attributeString.length))
+        
+        attributeString.addAttributes([
+            NSAttributedString.Key.foregroundColor: UIColor.red,
+            NSAttributedString.Key.strikethroughStyle: NSUnderlineStyle.single.rawValue,
+            NSAttributedString.Key.strikethroughColor: UIColor.red
+        ], range: NSMakeRange(0, attributeString.length))
+        
         return attributeString
     }
     
     func markSoldOut() -> NSAttributedString {
         let attributeString = NSMutableAttributedString(string: self)
-        attributeString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.orange, range: NSMakeRange(0, attributeString.length))
+        
+        attributeString.addAttribute(NSAttributedString.Key.foregroundColor,
+                                     value: UIColor.orange,
+                                     range: NSMakeRange(0, attributeString.length))
+        
         return attributeString
     }
 }
