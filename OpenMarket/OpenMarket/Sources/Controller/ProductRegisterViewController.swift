ce278a2b57ddca72a98704d5fafba59cd7576bb7
DragonYG <kioswe92@gmail.com>
2022-12-05 11:42:57 +0900

    feat: 상품등록 페이지에서 ImagePicker 구현

--- OpenMarket/OpenMarket/Sources/Controller/ProductRegisterViewController.swift
+++ OpenMarket/OpenMarket/Sources/Controller/ProductRegisterViewController.swift
@@ -7,6 +7,8 @@
 import UIKit
 
 class ProductRegisterViewController: UIViewController {
+    var imageArray: [UIImage] = []
+    let imagePicker: UIImagePickerController = .init()
     @IBOutlet weak var mainView: ProductRegisterView!
     
     override func loadView() {
@@ -16,6 +18,12 @@ class ProductRegisterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         mainView.delegate = self
+        
+        configureCollectionView()
+        configureImagePicker()
+    }
+    
+    private func configureCollectionView() {
         mainView.collectionView.delegate = self
         mainView.collectionView.dataSource = self
         
@@ -28,6 +36,14 @@ class ProductRegisterViewController: UIViewController {
         mainView.collectionView.register(collectionViewCellNib,
                                          forCellWithReuseIdentifier: ImageCollectionViewCell.stringIdentifier())
     }
+    
+    private func configureImagePicker() {
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
+        
+        imageArray.append(UIImage(named: "PlusImage") ?? UIImage())
+    }
 }
 
 extension ProductRegisterViewController: ProductDelegate {
@@ -37,11 +53,22 @@ extension ProductRegisterViewController: ProductDelegate {
 }
 
 extension ProductRegisterViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if imageArray.count < 6 {
+            present(imagePicker, animated: true)
+        } else {
+            let alert = UIAlertController(title: "안내", message: "사진 추가는 최대 5장입니다.", preferredStyle: .alert)
+            let action = UIAlertAction(title: "확인", style: .default)
+            
+            alert.addAction(action)
+            present(alert, animated: true)
+        }
+    }
 }
 
 extension ProductRegisterViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return imageArray.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -51,6 +78,24 @@ extension ProductRegisterViewController: UICollectionViewDataSource {
                                            for: indexPath) as? ImageCollectionViewCell else {
             return UICollectionViewCell()
         }
+        
+        cell.image.image = imageArray[indexPath.item]
+        
         return cell
     }
+}
+
+extension ProductRegisterViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        imageArray.insert(image, at: 0)
+        mainView.collectionView.reloadData()
+        
+        dismiss(animated: true)
+    }
+}
+
+extension ProductRegisterViewController: UINavigationControllerDelegate {
+    
 }
