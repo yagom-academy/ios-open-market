0ecab2938cda82066e5f6d11258399cc23144a3b
Gundy <korea1880@naver.com>
2022-12-06 17:42:04 +0900

    refactor : 코드 컨벤션 정리 및 접근제어자 수정

--- OpenMarket/Extension/UIImage+.swift
+++ OpenMarket/Extension/UIImage+.swift
@@ -8,16 +8,17 @@
 import UIKit
 
 extension UIImage {
-    public func resize(_ scale: Double) -> UIImage? {
+    func resize(_ scale: Double) -> UIImage? {
         let target = CGRect(x: 0, y: 0, width: (self.size.width * scale), height: (self.size.height * scale))
         guard let cgImage = self.cgImage,
-              let context = CGContext(data: nil, width: Int(target.size.width), height: Int(target.size.height),
-                                      bitsPerComponent: cgImage.bitsPerComponent, bytesPerRow: cgImage.bytesPerRow,
+              let context = CGContext(data: nil,
+                                      width: Int(target.size.width),
+                                      height: Int(target.size.height),
+                                      bitsPerComponent: cgImage.bitsPerComponent,
+                                      bytesPerRow: cgImage.bytesPerRow,
                                       space: cgImage.colorSpace ?? CGColorSpaceCreateDeviceRGB(),
                                       bitmapInfo: cgImage.bitmapInfo.rawValue)
-        else {
-            return self
-        }
+        else { return self }
         context.interpolationQuality = .high
         context.draw(cgImage, in: target)
         
