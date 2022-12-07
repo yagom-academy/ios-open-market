686b75aad4b36742e1e7fbd98d6a4a15cc26c446
hashswim <ss991117@naver.com>
2022-12-07 16:52:47 +0900

    feat: 사진 용량 줄이기 300kb 기준 설정 구현

--- OpenMarket/OpenMarket/ProductViewController/ProductViewController+ImagePickerDelegate.swift
+++ OpenMarket/OpenMarket/ProductViewController/ProductViewController+ImagePickerDelegate.swift
@@ -16,8 +16,12 @@ extension ProductViewController: UIImagePickerControllerDelegate, UINavigationCo
             self.addProductButton.layer.cornerRadius = 4
             
             if let img = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
-                let transSize = CGSize(width: img.size.width / 20, height: img.size.height / 20)
-                let resizeImage = img.resizeImageTo(size: transSize)
+                let transSize = CGSize(width: img.size.width / 10, height: img.size.height / 10)
+                var resizeImage = img.resizeImageTo(size: transSize)
+                
+                while resizeImage.getSizeIn(.kilobyte) > 300 {
+                    resizeImage = resizeImage.resizeImageTo(size: transSize)
+                }
                 
                 let imageView = UIImageView(image: resizeImage)
                 
